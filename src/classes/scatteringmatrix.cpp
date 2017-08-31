ed519324f91bd50130b534417b8879bfe91d80ba
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2017-08-31 14:23:59 +0100

    Added ScatteringMatrix helper class.

---
+++ src/classes/scatteringmatrix.cpp
@@ -0,0 +1,206 @@
+/*
+	*** Scattering Matrix Helper Class
+	*** src/classes/scatteringmatrix.cpp
+	Copyright T. Youngs 2012-2017
+
+	This file is part of dUQ.
+
+	dUQ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	dUQ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with dUQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "classes/scatteringmatrix.h"
+#include "classes/atomtype.h"
+#include "classes/data.h"
+#include "classes/weights.h"
+
+// Constructor
+ScatteringMatrix::ScatteringMatrix()
+{
+}
+
+/*
+ * Data
+ */
+
+// Return number of reference AtomType pairs
+int ScatteringMatrix::nPairs()
+{
+	return typePairs_.nItems();
+}
+
+// Return index of specified AtomType pair
+int ScatteringMatrix::pairIndex(AtomType* typeI, AtomType* typeJ)
+{
+	int index = 0;
+	for (Pair<AtomType*,AtomType*>* pair = typePairs_.first(); pair != NULL; pair = pair->next)
+	{
+		if ((pair->a == typeI) && (pair->b == typeJ)) return index;
+		if ((pair->a == typeJ) && (pair->b == typeI)) return index;
+		++index;
+	}
+
+	return -1;
+}
+
+// Print the matrix
+void ScatteringMatrix::print()
+{
+	// Write header
+	CharString text, line;
+	for (Pair<AtomType*,AtomType*>* pair = typePairs_.first(); pair != NULL; pair = pair->next)
+	{
+		text.sprintf("%s-%s", pair->a->name(), pair->b->name());
+		line.strcatf("%10s ", text.get());
+	}
+	Messenger::print("%s", line.get());
+
+	// Loop over reference data
+	RefListIterator<Data,bool> dataIterator(data_);
+	int row = 0;
+	line.clear();
+	while (Data* data = dataIterator.iterate())
+	{
+		for (int n=0; n<A_.nColumns(); ++n) line.strcatf("%10f ", A_.value(row, n));
+		Messenger::print("%s  %s\n", line.get(), data->name());
+		++row;
+	}
+}
+
+// Generate partials from reference data using the inverse coefficients matrix
+void ScatteringMatrix::generatePartials()
+{
+	// Clear current partials
+	for (int n=0; n<partials_.nItems(); ++n) partials_[n].clear();
+
+		printf("lkjlkj\n");
+	// Loop over rows in A_ / data_
+	int row = 0;
+	RefListIterator<Data,bool> dataIterator(data_);
+	while (Data* data = dataIterator.iterate())
+	{
+		// Loop over columns
+		for (int n=0; n<A_.nColumns(); ++n)
+		{
+			// Add data to relevant partial
+			partials_[n].addInterpolated(data->data(), inverseA_.value(row, n));
+		}
+
+		++row;
+	}
+		printf("lkjlkj2123\n");
+
+	for (int n=0; n<partials_.nItems(); ++n) partials_[n].save(partials_[n].name());
+}
+
+// Return partial for specified AtomType pair
+const XYData& ScatteringMatrix::partial(AtomType* typeI, AtomType* typeJ)
+{
+	int index = 0;
+	for (Pair<AtomType*,AtomType*>* pair = typePairs_.first(); pair != NULL; pair = pair->next)
+	{
+		if ((pair->a == typeI) && (pair->b == typeJ)) return partials_[index];
+		if ((pair->a == typeJ) && (pair->b == typeI)) return partials_[index];
+		++index;
+	}
+
+	// Not found - return dummy value
+	Messenger::warn("Partial between types %s and %s was not found in ScatteringMatrix.\n", typeI->name(), typeJ->name());
+	static XYData dummy;
+	return dummy;
+}
+
+/*
+ * Construction
+ */
+
+// Initialise from supplied list of AtomTypes
+void ScatteringMatrix::initialise(const List<AtomType>& types)
+{
+	// Copy atom types
+	typePairs_.clear();
+	for (AtomType* at1 = types.first(); at1 != NULL; at1 = at1->next)
+	{
+		for (AtomType* at2 = at1; at2 != NULL; at2 = at2->next)
+		{
+			Pair<AtomType*, AtomType*>* pair = typePairs_.add();
+			pair->a = at1;
+			pair->b = at2;
+		}
+	}
+
+	// Create and zero coefficients matrix
+	A_.initialise(typePairs_.nItems(), typePairs_.nItems());
+	A_ = 0.0;
+	inverseA_.clear();
+
+	// Create partials array
+	partials_.initialise(typePairs_.nItems());
+	int index = 0;
+	for (Pair<AtomType*,AtomType*>* pair = typePairs_.first(); pair != NULL; pair = pair->next)
+	{
+		partials_[index].setName(CharString("ScatteringMatrixPartial-%s-%s.txt", pair->a->name(), pair->b->name()));
+		++index;
+	}
+}
+
+// Finalise
+bool ScatteringMatrix::finalise()
+{
+	// Check that we have the correct number of reference data to be able to invert the matrix
+	if (data_.nItems() != A_.nRows())
+	{
+		Messenger::error("Can't finalise this scattering matrix, since there are not enough reference data (%i) compared to rows in the matrix (%i).\n", data_.nItems(), A_.nColumns());
+		return false;
+	}
+
+	inverseA_ = A_;
+	if (!inverseA_.invert()) return false;
+
+	return true;
+}
+
+// Add reference data
+bool ScatteringMatrix::addReferenceData(Data* data, Weights& weights)
+{
+	// Check current list size
+	if (data_.nItems() == typePairs_.nItems())
+	{
+		Messenger::error("Can't add reference data to ScatteringMatrix - rowsize exceeded.\n");
+		return false;
+	}
+
+	// Set coefficients in A_ - size of reference data array data_ gives us our target row index
+	const int row = data_.nItems(), nUsedTypes = weights.nUsedTypes();
+	AtomTypeList& usedTypes = weights.atomTypes();
+	for (int n=0; n<nUsedTypes; ++n)
+	{
+		for (int m=n; m<nUsedTypes; ++m)
+		{
+			int index = pairIndex(usedTypes.atomType(n), usedTypes.atomType(m));
+			if (index == -1)
+			{
+				Messenger::error("Weights associated to reference Data contain one or more unknown AtomTypes ('%s' and/or '%s').\n", usedTypes.atomType(n)->name(), usedTypes.atomType(m)->name());
+				return false;
+			}
+
+			// Now have the local column index of the AtomType pair in our matrix A_...
+			A_.ref(row, index) = weights.fullWeight(n, m);
+		}
+	}
+
+	// Add reference data
+	data_.add(data);
+
+	return true;
+}
---
+++ src/classes/scatteringmatrix.h
@@ -0,0 +1,90 @@
+/*
+	*** Scattering Matrix Helper Class
+	*** src/classes/scatteringmatrix.h
+	Copyright T. Youngs 2012-2017
+
+	This file is part of dUQ.
+
+	dUQ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	dUQ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with dUQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DUQ_SCATTERINGMATRIX_H
+#define DUQ_SCATTERINGMATRIX_H
+
+#include "base/xydata.h"
+#include "templates/list.h"
+#include "templates/reflist.h"
+#include "templates/pair.h"
+#include "templates/array2d.h"
+
+// Forward Declarations
+class AtomType;
+class Weights;
+class Data;
+
+// Scattering Matrix Container
+class ScatteringMatrix
+{
+	public:
+	// Constructor
+	ScatteringMatrix();
+
+
+	/*
+	 * Data
+	 * 
+	 * Representing: AX = B
+	 * 
+	 * 	[  1,1 ... 1,n ] [ x1 ]   [ B2 ]
+	 * 	[  2,1     2,n ] [ x2 ] = [ B2 ]
+	 * 	[  n,1     n,n ] [ xn ]   [ Bn ]
+	 */
+	private:
+	// Reference pairs of AtomTypes
+	List< Pair<AtomType*,AtomType*> > typePairs_;	
+	// Coefficients matrix (A) (ci * cj * bi * bj * (typei == typej ? 1 : 2)) (n * n)
+	Array2D<double> A_;
+	// Inverse of coefficients matrix
+	Array2D<double> inverseA_;
+	// Partials matrix (X) (n * 1)
+	Array<XYData> partials_;
+	// Reference data (B) (n * 1)
+	RefList<Data,bool> data_;
+
+	public:
+	// Return number of reference AtomType pairs
+	int nPairs();
+	// Return index of specified AtomType pair
+	int pairIndex(AtomType* typeI, AtomType* typeJ);
+	// Print the matrix
+	void print();
+	// Generate partials from reference data using inverse matrix
+	void generatePartials();
+	// Return partial for specified AtomType pair
+	const XYData& partial(AtomType* typeI, AtomType* typeJ);
+
+
+	/*
+	 * Construction
+	 */
+	public:
+	// Initialise from supplied list of AtomTypes
+	void initialise(const List<AtomType>& types);
+	// Finalise
+	bool finalise();
+	// Add reference data
+	bool addReferenceData(Data* data, Weights& weights);
+};
+
+#endif
