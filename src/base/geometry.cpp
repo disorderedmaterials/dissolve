0b9e76b84e008586a301812e377c658fdf4532b1
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2017-10-14 21:09:41 +0100

    Added simple Geometry class to store up to four atom indices and a reference value.

---
+++ src/base/geometry.cpp
@@ -0,0 +1,95 @@
+/*
+	*** Geometry
+	*** src/base/geometry.cpp
+	Copyright T. Youngs 2012-2017
+
+	This file is part of dUQ.
+
+	dUQ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	dUQ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with dUQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "base/geometry.h"
+
+// Forward Declarations
+/* none */
+
+// Constructor
+Geometry::Geometry()
+{
+	a_ = -1;
+	b_ = -1;
+	c_ = -1;
+	d_ = -1;
+	value_ = 0.0;
+}
+
+// Destructor
+Geometry::~Geometry()
+{
+}
+
+/*
+ * Data
+ */
+	
+// Set reference value and indices
+void Geometry::set(double value, int a, int b, int c, int d)
+{
+	a_ = a;
+	b_ = b;
+	c_ = c;
+	d_ = d;
+	value_ = value;
+}
+
+// Return index 1
+int Geometry::a()
+{
+	return a_;
+}
+
+// Return index 2
+int Geometry::b()
+{
+	return b_;
+}
+
+// Return index 3
+int Geometry::c()
+{
+	return c_;
+}
+
+// Return index 4
+int Geometry::d()
+{
+	return d_;
+}
+
+// Return reference value
+double Geometry::value()
+{
+	return value_;
+}
+
+
+/*
+ * Parallel Comms
+ */
+
+// Broadcast data from Master to all Slaves
+bool Geometry::broadcast(ProcessPool& procPool, int root)
+{
+}
+
---
+++ src/base/geometry.h
@@ -0,0 +1,78 @@
+/*
+	*** Geometry
+	*** src/base/geometry.h
+	Copyright T. Youngs 2012-2017
+
+	This file is part of dUQ.
+
+	dUQ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	dUQ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with dUQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DUQ_GEOMETRY_H
+#define DUQ_GEOMETRY_H
+
+#include "templates/mpilistitem.h"
+
+// Forward Declarations
+/* none */
+
+// Geometry Definition
+class Geometry : public MPIListItem<Geometry>
+{
+	public:
+	// Constructor
+	Geometry();
+	// Destructor
+	~Geometry();
+
+
+	/*
+	 * Data
+	 */
+	private:
+	// Index 1
+	int a_;
+	// Index 2
+	int b_;
+	// Index 3
+	int c_;
+	// Index 4
+	int d_;
+	// Reference value
+	double value_;
+	
+	public:
+	// Set reference value and indices
+	void set(double value, int a, int b, int c = -1, int d = -1);
+	// Return index 1
+	int a();
+	// Return index 2
+	int b();
+	// Return index 3
+	int c();
+	// Return index 4
+	int d();
+	// Return reference value
+	double value();
+
+
+	/*
+	 * Parallel Comms
+	 */
+	public:
+	// Broadcast data from Master to all Slaves
+	bool broadcast(ProcessPool& procPool, int root = 0);
+};
+
+#endif
