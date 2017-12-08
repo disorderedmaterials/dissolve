71243ef0df6093db81708dbde2ec145327fbf0fb
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2017-12-08 14:34:24 +0000

    PairPotential::setXYDataNames() now also sets the resource names of the objects.

--- src/classes/pairpotential.cpp
+++ src/classes/pairpotential.cpp
@@ -138,14 +138,17 @@ void PairPotential::setXYDataNames()
 		return;
 	}
 
-	CharString s("%s-%s", atomTypeI_->name(), atomTypeJ_->name()), otherName;
-	uFull_.setName(s);
-	otherName.sprintf("%s (Add)", s.get());
-	uAdditional_.setName(otherName);
-	otherName.sprintf("%s (Orig)", s.get());
-	uOriginal_.setName(otherName);
-	otherName.sprintf("%s (dU/dr)", s.get());
-	dUFull_.setName(otherName);
+	uFull_.setName(CharString("%s-%s", atomTypeI_->name(), atomTypeJ_->name()));
+	uFull_.setObjectName(CharString("PairPotential//%s-%s//Full", atomTypeI_->name(), atomTypeJ_->name()));
+
+	uAdditional_.setName(CharString("%s-%s (Add)", atomTypeI_->name(), atomTypeJ_->name()));
+	uAdditional_.setObjectName(CharString("PairPotential//%s-%s//Additional", atomTypeI_->name(), atomTypeJ_->name()));
+
+	uOriginal_.setName(CharString("%s-%s (Orig)", atomTypeI_->name(), atomTypeJ_->name()));
+	uOriginal_.setObjectName(CharString("PairPotential//%s-%s//Original", atomTypeI_->name(), atomTypeJ_->name()));
+
+	dUFull_.setName(CharString("%s-%s (dU/dr)", atomTypeI_->name(), atomTypeJ_->name()));
+	dUFull_.setObjectName(CharString("PairPotential//%s-%s//Force", atomTypeI_->name(), atomTypeJ_->name()));
 }
 
 // Set source parameters from AtomTypes
