bc9403b504effaefd5674e47d9118640a1ed5bf2
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2017-12-14 09:01:31 +0000

    Added GUI widget for EnergyModule.

--- src/modules/energy/energy.h
+++ src/modules/energy/energy.h
@@ -109,6 +109,14 @@ class EnergyModule : public Module
 	public:
 	// Reap data for this Module instance
 	void reapData();
+
+
+	/*
+	 * GUI Widget
+	 */
+	public:
+	// Return a new widget controlling this Module
+	ModuleWidget* createWidget(QWidget* parent, DUQ& dUQ);
 };
 
 #endif
