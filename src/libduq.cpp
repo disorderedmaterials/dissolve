a151c834684f51fbc677cf49cc80d1e50724518d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-01-04 14:02:43 +0000

    Update to CMakeLists throughout.

---
+++ src/libduq.cpp
@@ -0,0 +1,25 @@
+/*
+	*** Lib DUQ Stub
+	*** src/libduq.cpp
+	Copyright T. Youngs 2016-2018
+
+	This file is part of DUQ.
+
+	DUQ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	DUQ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with DUQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * This is an empty for file, which exists only as a dummy source file for
+ * CMake to target when generating shared libs from other library objects.
+ */
