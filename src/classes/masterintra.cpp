0c983573ec9aabdb08928b2c398921fab2783c21
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-01-18 16:56:47 +0000

    MasterIntra did not call constructors for subclasses.

--- src/classes/masterintra.cpp
+++ src/classes/masterintra.cpp
@@ -24,7 +24,7 @@
 #include "base/messenger.h"
 
 // Constructor
-MasterIntra::MasterIntra()
+MasterIntra::MasterIntra() : SpeciesIntra(), ListItem<MasterIntra>()
 {
 	parent_ = NULL;
 	masterParameters_ = NULL;
