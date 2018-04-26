c6d1c7237a9377aa1f5310bd8a936679b8c35f83
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-04-26 15:59:55 +0100

    Removed unused timestamp() function from praxis minimiser.

--- src/templates/praxis.h
+++ src/templates/praxis.h
@@ -26,7 +26,6 @@
 #define DUQ_PRAXIS_H
 
 #include "base/messenger.h"
-
 #include <iomanip>
 
 template <class T> class Praxis
@@ -1943,54 +1942,7 @@ template <class T> class Praxis
 	
 		return;
 	}
-	//****************************************************************************80
-	
-	void timestamp ( )
-	
-	//****************************************************************************80
-	//
-	//  Purpose:
-	//
-	//    TIMESTAMP prints the current YMDHMS date as a time stamp.
-	//
-	//  Example:
-	//
-	//    31 May 2001 09:45:54 AM
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    08 July 2009
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
-	//  Parameters:
-	//
-	//    None
-	//
-	{
-	# define TIME_SIZE 40
-	
-		static char time_buffer[TIME_SIZE];
-		const struct std::tm *tm_ptr;
-		size_t len;
-		std::time_t now;
-	
-		now = std::time ( NULL );
-		tm_ptr = std::localtime ( &now );
-	
-		len = std::strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm_ptr );
-	
-		Messenger::print("%s\n", time_buffer);
-	
-		return;
-	# undef TIME_SIZE
-	}
+
 	//****************************************************************************80
 
 	double praxis ( double t0, double h0, int n, int prin, double x[], PraxisCostFunction f )
