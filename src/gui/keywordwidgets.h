d705de2699638e0fcdb73a1be9443fa9572acd08
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-05-21 22:48:31 +0100

    Added convenience include for all KeywordWidget types.

---
+++ src/gui/keywordwidgets.h
@@ -0,0 +1,31 @@
+/*
+	*** Keyword Widgets
+	*** src/gui/keywordwidgets.h
+	Copyright T. Youngs 2012-2018
+
+	This file is part of DUQ.
+
+	DUQ is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	DUQ is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with DUQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// General include for all KeywordWidget types
+#include "gui/keywordwidgets/base.h"
+#include "gui/keywordwidgets/bool.hui"
+#include "gui/keywordwidgets/broadeningfunction.h"
+#include "gui/keywordwidgets/charstring.hui"
+#include "gui/keywordwidgets/double.hui"
+#include "gui/keywordwidgets/dropwidget.hui"
+#include "gui/keywordwidgets/int.hui"
+#include "gui/keywordwidgets/isotopologuelist.h"
+#include "gui/keywordwidgets/windowfunction.hui"
