2dbeee29930541f8c908600891a88def203717b7
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-08-02 13:51:57 +0100

    Target GenericList was determined in NeutronSQModule::process() but was never used.

--- src/modules/neutronsq/process.cpp
+++ src/modules/neutronsq/process.cpp
@@ -123,8 +123,6 @@ bool NeutronSQModule::process(Dissolve& dissolve, ProcessPool& procPool)
 
 	CharString varName;
 
-	GenericList& moduleData = configurationLocal_ ? targetConfigurations_.firstItem()->moduleData() : dissolve.processingModuleData();
-
 	NeutronSQModule::NormalisationType normalisation = normalisationType(keywords_.asString("Normalisation"));
 	if (normalisation == NeutronSQModule::nNormalisationTypes) return Messenger::error("NeutronSQ: Invalid normalisation type '%s' found.\n", keywords_.asString("Normalisation"));
 	const BroadeningFunction& qBroadening = KeywordListHelper<BroadeningFunction>::retrieve(keywords_, "QBroadening", BroadeningFunction());
