96f817d1964e040552a334670f8c6741e704de16
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-08-08 20:55:26 +0100

    Moved ModuleEditor to main gui/ dir.

--- src/gui/moduleeditor.h
+++ src/gui/moduleeditor.h
@@ -1,6 +1,6 @@
 /*
 	*** Module Editor
-	*** src/gui/widgets/moduleeditor.h
+	*** src/gui/moduleeditor.h
 	Copyright T. Youngs 2012-2018
 
 	This file is part of Dissolve.
--- src/gui/moduleeditor_funcs.cpp
+++ src/gui/moduleeditor_funcs.cpp
@@ -1,6 +1,6 @@
 /*
 	*** Module Editor Functions
-	*** src/gui/widgets/moduleeditor_funcs.cpp
+	*** src/gui/moduleeditor_funcs.cpp
 	Copyright T. Youngs 2012-2018
 
 	This file is part of Dissolve.
@@ -19,7 +19,7 @@
 	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "gui/widgets/moduleeditor.h"
+#include "gui/moduleeditor.h"
 #include "gui/gui.h"
 #include "gui/modulechart.hui"
 #include "gui/modulepalette.hui"
