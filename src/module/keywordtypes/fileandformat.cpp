812366949aee93b8059700323140ea0d93b3db40
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2018-09-04 23:04:21 +0100

    Added FileAndFormatModuleKeyword.

---
+++ src/module/keywordtypes/fileandformat.cpp
@@ -0,0 +1,80 @@
+/*
+	*** Module Keyword - File and Format
+	*** src/modules/keywordtypes/fileandformat.cpp
+	Copyright T. Youngs 2012-2018
+
+	This file is part of Dissolve.
+
+	Dissolve is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dissolve is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "module/keywordtypes/fileandformat.h"
+#include "base/fileandformat.h"
+#include "base/lineparser.h"
+
+// Constructor
+FileAndFormatModuleKeyword::FileAndFormatModuleKeyword(FileAndFormat& fileAndFormat) : ModuleKeywordBase(ModuleKeywordBase::FileAndFormatData), ModuleKeywordData<FileAndFormat&>(fileAndFormat)
+{
+}
+
+// Destructor
+FileAndFormatModuleKeyword::~FileAndFormatModuleKeyword()
+{
+}
+
+/*
+ * Data
+ */
+
+// Return whether the current data value has ever been set
+bool FileAndFormatModuleKeyword::isSet()
+{
+	return set_;
+}
+
+/*
+ * Arguments
+ */
+
+// Return minimum number of arguments accepted
+int FileAndFormatModuleKeyword::minArguments()
+{
+	// Must have data format as bare minimum
+	return 1;
+}
+
+// Return maximum number of arguments accepted
+int FileAndFormatModuleKeyword::maxArguments()
+{
+	// Data format and filename
+	return 2;
+}
+
+// Parse arguments from supplied LineParser, starting at given argument offset, utilising specified ProcessPool if required
+bool FileAndFormatModuleKeyword::read(LineParser& parser, int startArg, ProcessPool& procPool)
+{
+	if (!data_.read(parser, startArg)) return Messenger::error("Failed to read file/format.\n");
+
+	set_ = true;
+
+	return true;
+}
+
+// Write keyword data to specified LineParser
+bool FileAndFormatModuleKeyword::write(LineParser& parser, const char* prefix)
+{
+	if (!parser.writeLineF("%s%s  %s\n", prefix, keyword(), data_.asString())) return false;
+
+	return true;
+}
---
+++ src/module/keywordtypes/fileandformat.h
@@ -0,0 +1,63 @@
+/*
+	*** Module Keyword - File Format
+	*** src/modules/keywordtypes/fileformat.h
+	Copyright T. Youngs 2012-2018
+
+	This file is part of Dissolve.
+
+	Dissolve is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dissolve is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DISSOLVE_MODULEKEYWORD_FILEANDFORMAT_H
+#define DISSOLVE_MODULEKEYWORD_FILEANDFORMAT_H
+
+#include "module/keyworddata.h"
+#include "module/keywordbase.h"
+
+// Forward Declarations
+class FileAndFormat;
+
+// Keyword with a file format 
+class FileAndFormatModuleKeyword : public ModuleKeywordBase, public ModuleKeywordData<FileAndFormat&>
+{
+	public:
+	// Constructor
+	FileAndFormatModuleKeyword(FileAndFormat& fileAndFormat);
+	// Destructor
+	~FileAndFormatModuleKeyword();
+
+
+	/*
+	 * Data
+	 */
+	public:
+	// Return whether the current data value has ever been set
+	bool isSet();
+
+
+	/*
+	 * Arguments
+	 */
+	public:
+	// Return minimum number of arguments accepted
+	int minArguments();
+	// Return maximum number of arguments accepted
+	int maxArguments();
+	// Parse arguments from supplied LineParser, starting at given argument offset, utilising specified ProcessPool if required
+	bool read(LineParser& parser, int startArg, ProcessPool& procPool);
+	// Write keyword data to specified LineParser
+	bool write(LineParser& parser, const char* prefix);
+};
+
+#endif
