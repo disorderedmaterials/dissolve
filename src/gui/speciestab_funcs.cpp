7307ad5a15d228ff371831e2e7e8471adc5445f8
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-01-09 23:02:20 +0000

    Reorganise SpeciesTab, and add placeholder functions for add/remove of objects.

--- src/gui/speciestab_funcs.cpp
+++ src/gui/speciestab_funcs.cpp
@@ -45,11 +45,9 @@ SpeciesTab::SpeciesTab(DissolveWindow* dissolveWindow, Dissolve& dissolve, QTabW
 
 	// Set item delegates in tables
 	// -- SpeciesAtomTable
-	for (int n=1; n<5; ++n) ui.AtomTable->setItemDelegateForColumn(n, new TExponentialSpinDelegate(this));
-	ui.AtomTable->setItemDelegateForColumn(5, new ComboListDelegate(this, new ComboNameListItems<AtomType>(dissolve_.atomTypes())));
+	ui.AtomTable->setItemDelegateForColumn(1, new ComboListDelegate(this, new ComboNameListItems<AtomType>(dissolve_.atomTypes())));
+	for (int n=2; n<6; ++n) ui.AtomTable->setItemDelegateForColumn(n, new TExponentialSpinDelegate(this));
 	ui.AtomTable->horizontalHeader()->setFont(font());
-	// -- Isotope Table
-	ui.IsotopeTable->setItemDelegateForColumn(1, new IsotopeComboDelegate(this));
 	// -- Bond Table
 	for (int n=0; n<2; ++n) ui.BondTable->setItemDelegateForColumn(n, new IntegerSpinDelegate(this, 1, 1e9));
 	ui.BondTable->setItemDelegateForColumn(2, new IntraFormComboDelegate(this, SpeciesBond::nBondFunctions, SpeciesBond::bondFunctions(), dissolve_.masterBonds()));
@@ -57,6 +55,8 @@ SpeciesTab::SpeciesTab(DissolveWindow* dissolveWindow, Dissolve& dissolve, QTabW
 	for (int n=0; n<3; ++n) ui.AngleTable->setItemDelegateForColumn(n, new IntegerSpinDelegate(this, 1, 1e9));
 	// -- Torsion Table
 	for (int n=0; n<4; ++n) ui.TorsionTable->setItemDelegateForColumn(n, new IntegerSpinDelegate(this, 1, 1e9));
+	// -- Isotope Table
+	ui.IsotopeTable->setItemDelegateForColumn(1, new IsotopeComboDelegate(this));
 
 	refreshing_ = false;
 }
@@ -104,6 +104,16 @@ void SpeciesTab::updateAtomTableRow(int row, SpeciesAtom* speciesAtom, bool crea
 	else item = ui.AtomTable->item(row, 0);
 	item->setText(speciesAtom->element()->name());
 
+	// AtomType
+	if (createItems)
+	{
+		item = new QTableWidgetItem;
+		item->setData(Qt::UserRole, VariantPointer<SpeciesAtom>(speciesAtom));
+		ui.AtomTable->setItem(row, 1, item);
+	}
+	else item = ui.AtomTable->item(row, 1);
+	item->setText(speciesAtom->atomType() ? speciesAtom->atomType()->name() : "");
+
 	// Coordinates
 	for (int n=0; n<3; ++n)
 	{
@@ -111,9 +121,9 @@ void SpeciesTab::updateAtomTableRow(int row, SpeciesAtom* speciesAtom, bool crea
 		{
 			item = new QTableWidgetItem;
 			item->setData(Qt::UserRole, VariantPointer<SpeciesAtom>(speciesAtom));
-			ui.AtomTable->setItem(row, n+1, item);
+			ui.AtomTable->setItem(row, n+2, item);
 		}
-		else item = ui.AtomTable->item(row, n+1);
+		else item = ui.AtomTable->item(row, n+2);
 		item->setText(QString::number(speciesAtom->r().get(n)));
 	}
 
@@ -122,47 +132,10 @@ void SpeciesTab::updateAtomTableRow(int row, SpeciesAtom* speciesAtom, bool crea
 	{
 		item = new QTableWidgetItem;
 		item->setData(Qt::UserRole, VariantPointer<SpeciesAtom>(speciesAtom));
-		ui.AtomTable->setItem(row, 4, item);
-	}
-	else item = ui.AtomTable->item(row, 4);
-	item->setText(QString::number(speciesAtom->charge()));
-
-	// AtomTypes
-	if (createItems)
-	{
-		item = new QTableWidgetItem;
-		item->setData(Qt::UserRole, VariantPointer<SpeciesAtom>(speciesAtom));
 		ui.AtomTable->setItem(row, 5, item);
 	}
 	else item = ui.AtomTable->item(row, 5);
-	item->setText(speciesAtom->atomType() ? speciesAtom->atomType()->name() : "");
-}
-
-// IsotopologuesIsotopesTable row update function
-void SpeciesTab::updateIsotopeTableRow(int row, AtomType* atomType, Isotope* isotope, bool createItems)
-{
-	QTableWidgetItem* item;
-
-	// AtomType
-	if (createItems)
-	{
-		item = new QTableWidgetItem;
-		item->setData(Qt::UserRole, VariantPointer<AtomType>(atomType));
-		item->setFlags(Qt::NoItemFlags);
-		ui.IsotopeTable->setItem(row, 0, item);
-	}
-	else item = ui.IsotopeTable->item(row, 0);
-	item->setText(atomType->name());
-
-	// Isotope
-	if (createItems)
-	{
-		item = new QTableWidgetItem;
-		item->setData(Qt::UserRole, VariantPointer<Isotope>(isotope));
-		ui.IsotopeTable->setItem(row, 1, item);
-	}
-	else item = ui.IsotopeTable->item(row, 1);
-	item->setText(IsotopeComboDelegate::textForIsotope(isotope));
+	item->setText(QString::number(speciesAtom->charge()));
 }
 
 // BondTable row update function
@@ -294,6 +267,33 @@ void SpeciesTab::updateTorsionTableRow(int row, SpeciesTorsion* speciesTorsion, 
 	}
 }
 
+// IsotopologuesIsotopesTable row update function
+void SpeciesTab::updateIsotopeTableRow(int row, AtomType* atomType, Isotope* isotope, bool createItems)
+{
+	QTableWidgetItem* item;
+
+	// AtomType
+	if (createItems)
+	{
+		item = new QTableWidgetItem;
+		item->setData(Qt::UserRole, VariantPointer<AtomType>(atomType));
+		item->setFlags(Qt::NoItemFlags);
+		ui.IsotopeTable->setItem(row, 0, item);
+	}
+	else item = ui.IsotopeTable->item(row, 0);
+	item->setText(atomType->name());
+
+	// Isotope
+	if (createItems)
+	{
+		item = new QTableWidgetItem;
+		item->setData(Qt::UserRole, VariantPointer<Isotope>(isotope));
+		ui.IsotopeTable->setItem(row, 1, item);
+	}
+	else item = ui.IsotopeTable->item(row, 1);
+	item->setText(IsotopeComboDelegate::textForIsotope(isotope));
+}
+
 // Update controls in tab
 void SpeciesTab::updateControls()
 {
@@ -356,6 +356,16 @@ Isotopologue* SpeciesTab::currentIsotopologue()
 	return (Isotopologue*) VariantPointer<Isotopologue>(item->data(Qt::UserRole));
 }
 
+void SpeciesTab::on_AtomAddButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
+void SpeciesTab::on_AtomRemoveButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
 void SpeciesTab::on_AtomTable_itemChanged(QTableWidgetItem* w)
 {
 	if (refreshing_) return;
@@ -401,55 +411,14 @@ void SpeciesTab::on_AtomTable_itemChanged(QTableWidgetItem* w)
 	}
 }
 
-void SpeciesTab::on_IsotopologueList_currentRowChanged(int row)
+void SpeciesTab::on_BondAddButton_clicked(bool checked)
 {
-	if (refreshing_) return;
-
-	Isotopologue* isotopologue = currentIsotopologue();
-
-	refreshing_ = true;
-
-	// Isotopologue AtomType/Isotopes Table
-	if (!isotopologue) ui.IsotopeTable->clearContents();
-	else TableWidgetRefListUpdater<SpeciesTab,AtomType,Isotope*> isotopeUpdater(ui.IsotopeTable, isotopologue->isotopes(), this, &SpeciesTab::updateIsotopeTableRow);
-
-	refreshing_ = false;
+	printf("NOT IMPLEMENTED YET!\n");
 }
 
-void SpeciesTab::on_IsotopeTable_itemChanged(QTableWidgetItem* w)
+void SpeciesTab::on_BondRemoveButton_clicked(bool checked)
 {
-	if (refreshing_) return;
-
-	// Get current Isotopologue
-	Isotopologue* iso = currentIsotopologue();
-	if (!iso) return;
-
-	// Get row from the passed widget, and get target AtomType from column 0
-	QTableWidgetItem* item = ui.IsotopeTable->item(w->row(), 0);
-	if (!item) return;
-	
-	AtomType* atomType = VariantPointer<AtomType>(item->data(Qt::UserRole));
-	if (!atomType) return;
-
-	// Column of passed item tells us the type of data we need to change
-	Isotope* isotope;
-	switch (w->column())
-	{
-		// Element
-		case (0):
-			// Should never be called
-			break;
-		// Isotope
-		case (1):
-			// The new Isotope should have been set in the model data for the column
-			isotope = VariantPointer<Isotope>(w->data(Qt::UserRole));
-			if (isotope) iso->setAtomTypeIsotope(atomType, isotope);
-			dissolveWindow_->setModified();
-			break;
-		default:
-			Messenger::error("Don't know what to do with data from column %i of Isotopes table.\n", w->column());
-			break;
-	}
+	printf("NOT IMPLEMENTED YET!\n");
 }
 
 void SpeciesTab::on_BondTable_itemChanged(QTableWidgetItem* w)
@@ -514,6 +483,16 @@ void SpeciesTab::on_BondTable_itemChanged(QTableWidgetItem* w)
 		updateBondTableRow(w->row(), speciesBond, false);
 		refreshing_ = false;
 	}
+}
+
+void SpeciesTab::on_AngleAddButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
+void SpeciesTab::on_AngleRemoveButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
 }
 
 void SpeciesTab::on_AngleTable_itemChanged(QTableWidgetItem* w)
@@ -582,6 +561,16 @@ void SpeciesTab::on_AngleTable_itemChanged(QTableWidgetItem* w)
 	}
 }
 
+void SpeciesTab::on_TorsionAddButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
+void SpeciesTab::on_TorsionRemoveButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
 void SpeciesTab::on_TorsionTable_itemChanged(QTableWidgetItem* w)
 {
 	if (refreshing_ || (!species_)) return;
@@ -647,6 +636,67 @@ void SpeciesTab::on_TorsionTable_itemChanged(QTableWidgetItem* w)
 		refreshing_ = true;
 		updateTorsionTableRow(w->row(), speciesTorsion, false);
 		refreshing_ = false;
+	}
+}
+
+void SpeciesTab::on_IsotopologueAddButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
+void SpeciesTab::on_IsotopologueRemoveButton_clicked(bool checked)
+{
+	printf("NOT IMPLEMENTED YET!\n");
+}
+
+void SpeciesTab::on_IsotopologueList_currentRowChanged(int row)
+{
+	if (refreshing_) return;
+
+	Isotopologue* isotopologue = currentIsotopologue();
+
+	refreshing_ = true;
+
+	// Isotopologue AtomType/Isotopes Table
+	if (!isotopologue) ui.IsotopeTable->clearContents();
+	else TableWidgetRefListUpdater<SpeciesTab,AtomType,Isotope*> isotopeUpdater(ui.IsotopeTable, isotopologue->isotopes(), this, &SpeciesTab::updateIsotopeTableRow);
+
+	refreshing_ = false;
+}
+
+void SpeciesTab::on_IsotopeTable_itemChanged(QTableWidgetItem* w)
+{
+	if (refreshing_) return;
+
+	// Get current Isotopologue
+	Isotopologue* iso = currentIsotopologue();
+	if (!iso) return;
+
+	// Get row from the passed widget, and get target AtomType from column 0
+	QTableWidgetItem* item = ui.IsotopeTable->item(w->row(), 0);
+	if (!item) return;
+	
+	AtomType* atomType = VariantPointer<AtomType>(item->data(Qt::UserRole));
+	if (!atomType) return;
+
+	// Column of passed item tells us the type of data we need to change
+	Isotope* isotope;
+	switch (w->column())
+	{
+		// Element
+		case (0):
+			// Should never be called
+			break;
+		// Isotope
+		case (1):
+			// The new Isotope should have been set in the model data for the column
+			isotope = VariantPointer<Isotope>(w->data(Qt::UserRole));
+			if (isotope) iso->setAtomTypeIsotope(atomType, isotope);
+			dissolveWindow_->setModified();
+			break;
+		default:
+			Messenger::error("Don't know what to do with data from column %i of Isotopes table.\n", w->column());
+			break;
 	}
 }
 
