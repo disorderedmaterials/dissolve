1867ff61fff6c1678e322824086f97dbcb79b1db
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-02-15 15:32:45 +0000

    Add RenderableFactory class.

---
+++ src/gui/viewer/render/renderablefactory.cpp
@@ -0,0 +1,41 @@
+/*
+	*** Renderable Factory
+	*** src/gui/viewer/render/renderablefactory.cpp
+	Copyright T. Youngs 2013-2019
+
+	This file is part of uChroma.
+
+	uChroma is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	uChroma is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with uChroma.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "gui/viewer/render/renderablefactory.h"
+#include "gui/viewer/render/renderabledata1d.h"
+
+// Create Renderable of specified type
+Renderable* RenderableFactory::create(Renderable::RenderableType renderableType, const char* objectTag)
+{
+	Renderable* renderable = NULL;
+	Data1D* data1d;
+
+	if (renderableType == Renderable::Data1DRenderable)
+	{
+		Data1D* data = Data1D::findObject(objectTag);
+		if (!data) Messenger::error("Target Data1D '%s' for Renderable not found.\n", objectTag);
+		else renderable = new RenderableData1D(*Data1D::findObject(objectTag));
+	}
+	else Messenger::error("Don't know how to create a Renderable of type '%s' (object tag = '%s'.\n", Renderable::renderableType(renderableType), objectTag);
+
+	return renderable;
+}
+
---
+++ src/gui/viewer/render/renderablefactory.h
@@ -0,0 +1,37 @@
+/*
+	*** Renderable Factory
+	*** src/gui/viewer/render/renderablefactory.cpp
+	Copyright T. Youngs 2013-2019
+
+	This file is part of uChroma.
+
+	uChroma is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	uChroma is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with uChroma.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DISSOLVE_RENDERABLEFACTORY_H
+#define DISSOLVE_RENDERABLEFACTORY_H
+
+#include "gui/viewer/render/renderable.h"
+
+// Forward Declarations
+/* none */
+
+class RenderableFactory
+{
+	public:
+	// Create Renderable of specified type, from specified data (referenced by objectTag)
+	static Renderable* create(Renderable::RenderableType renderableType, const char* objectTag);
+};
+
+#endif
