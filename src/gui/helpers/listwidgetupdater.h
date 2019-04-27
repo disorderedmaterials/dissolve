fb14d2edb867a15f8fd3c9e495e287b6e673c214
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-04-27 19:57:02 +0100

    Add overloaded ListWidgetUpdater ctor taking RefList<T,bool> as source.

--- src/gui/helpers/listwidgetupdater.h
+++ src/gui/helpers/listwidgetupdater.h
@@ -21,6 +21,7 @@
 
 #include "templates/variantpointer.h"
 #include "templates/list.h"
+#include "templates/reflist.h"
 #include <QListWidget>
 
 #ifndef DISSOLVE_LISTWIDGETUPDATER_H
@@ -117,6 +118,49 @@ template <class T, class I> class ListWidgetUpdater
 				listWidgetItem->setData(Qt::UserRole, VariantPointer<I>(dataItem));
 				listWidgetItem->setText(dataItem->name());
 				if (flags != Qt::NoItemFlags) listWidgetItem->setFlags(listWidgetItem->flags() | flags);
+			}
+
+			++currentRow;
+		}
+	}
+
+	// Update widget from supplied RefList, calling supplied function to create / modify data
+	ListWidgetUpdater(QListWidget* listWidget, const RefList<I,bool>& data, T* functionParent, ListWidgetRowUpdateFunction updateRow)
+	{
+		QListWidgetItem* listWidgetItem;
+
+		int currentRow = 0;
+
+		RefListIterator<I,bool> dataIterator(data);
+		while (I* dataItem = dataIterator.iterate())
+		{
+			// Our table may or may not be populated, and with different items to those in the list.
+
+			// If there is an item already on this row, check it
+			// If it represents the current pointer data, just update it and move on. Otherwise, delete it and check again
+			while (currentRow < listWidget->count())
+			{
+				listWidgetItem = listWidget->item(currentRow);
+				I* rowData = (listWidgetItem ? VariantPointer<I>(listWidgetItem->data(Qt::UserRole)) : NULL);
+				if (rowData == dataItem)
+				{
+					// Update the current row and quit the loop
+					(functionParent->*updateRow)(currentRow, dataItem, false);
+
+					break;
+				}
+				else
+				{
+					QListWidgetItem* oldItem = listWidget->takeItem(currentRow);
+					if (oldItem) delete oldItem;
+				}
+			}
+
+			// If the current row index is (now) out of range, add a new row to the list
+			if (currentRow == listWidget->count())
+			{
+				// Create new items
+				(functionParent->*updateRow)(currentRow, dataItem, true);
 			}
 
 			++currentRow;
