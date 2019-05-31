4f705c12c98aedb29bf91c20bb9093605fc766b4
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-05-31 11:38:14 +0100

    Add parallel comms virtuals to GenericItemBase, and remove ctor/dtor.

--- src/genericitems/base.cpp
+++ src/genericitems/base.cpp
@@ -21,16 +21,6 @@
 
 #include "genericitems/base.h"
 
-// Constructor
-GenericItemBase::GenericItemBase()
-{
-}
-
-// Destructor
-GenericItemBase::~GenericItemBase()
-{
-}
-
 /*
  * I/O
  */
@@ -38,14 +28,27 @@ GenericItemBase::~GenericItemBase()
 // Write data through specified parser
 bool GenericItemBase::write(LineParser& parser)
 {
-	Messenger::error("Tried to write() a class that doesn't support it.\n");
-	return false;
+	return Messenger::error("Tried to write() a class that doesn't support it.\n");
 }
 
 // Read data through specified parser
 bool GenericItemBase::read(LineParser& parser, const CoreData& coreData)
 {
-	Messenger::error("Tried to read() a class that doesn't support it.\n");
-	return false;
+	return Messenger::error("Tried to read() a class that doesn't support it.\n");
 }
 
+/*
+ * Parallel Comms
+ */
+
+// Broadcast item contents
+bool GenericItemBase::broadcast(ProcessPool& procPool, const int root, const CoreData& coreData)
+{
+	return Messenger::error("Tried to broadcast() a class that doesn't support it.\n");
+}
+
+// Return equality between items
+bool GenericItemBase::equality(ProcessPool& procPool)
+{
+	return Messenger::error("Tried to equality() a class that doesn't support it.\n");
+}
--- src/genericitems/base.h
+++ src/genericitems/base.h
@@ -26,17 +26,11 @@
 
 // Forward Declarations
 class CoreData;
+class ProcessPool;
 
 // Base function templates for a Class that is to be used as a GenericItem
 class GenericItemBase
 {
-	public:
-	// Constructor
-	GenericItemBase();
-	// Destructor
-	virtual ~GenericItemBase();
-
-
 	/*
 	 * Item Class
 	 */
@@ -53,6 +47,16 @@ class GenericItemBase
 	virtual bool read(LineParser& parser, const CoreData& coreData);
 	// Write data through specified parser
 	virtual bool write(LineParser& parser);
+
+
+	/*
+	 * Parallel Comms
+	 */
+	public:
+	// Broadcast item contents
+	virtual bool broadcast(ProcessPool& procPool, const int root, const CoreData& coreData);
+	// Return equality between items
+	virtual bool equality(ProcessPool& procPool);
 };
 
 #endif
