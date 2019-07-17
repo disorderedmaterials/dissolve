9a02735f4d8d3e5b557ad17750c76e131827a241
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-07-17 14:35:30 +0100

    Add reading of cartesian 2D data.

--- src/io/import/data2d_cartesian.cpp
+++ src/io/import/data2d_cartesian.cpp
@@ -27,12 +27,47 @@
 // Read cartesian data from specified file
 bool Data2DImportFileFormat::importCartesian(LineParser& parser, Data2D& data)
 {
-	// Expect blocks with three columns (x, y, f(x,y))
-
+	/*
+	 * Cartesian coordinates assume that there are three values per line: x, y, and value.
+	 * The x and y values are assumed to be centre-bin values.
+	 */
 	// Must have a template - otherwise tricky to work out axis limits without reading the whole file in twice
-// 	if (
-	
-// 	return data.load(parser);
-	return false;
-}
+	if ((!axisRangeSet_[0]) || (!axisRangeSet_[1])) return Messenger::error("Must supply x and y ranges (e.g. xrange=min,max,delta) when importing 2D cartesian data.\n");
 
+	// Set up our data
+	data.initialise(axisMinimum_.x, axisMaximum_.x, axisDelta_.x, axisMinimum_.y, axisMaximum_.y, axisDelta_.y);
+
+	// Loop over lines in the file - we expect blocks with three columns (x, y, f(x,y))
+	while (!parser.eofOrBlank())
+	{
+		// Read line
+		if (parser.getArgsDelim(LineParser::SkipBlanks) != LineParser::Success) return false;
+
+		// Check number of arguments
+		if (parser.nArgs() != 3)
+		{
+			Messenger::warn("Skipping bad line: %s...\n", parser.line());
+			continue;
+		}
+
+		double x = parser.argd(0);
+		double y = parser.argd(1);
+		int xBin = (x - axisMinimum_.x) / axisDelta_.x;
+		if ((xBin < 0) || (xBin >= data.constXAxis().nItems()))
+		{
+			Messenger::warn("Coordinates x=%e y=%e are out-of-range (xBin = %i, nBins = %i).\n", x, y, xBin, data.constXAxis().nItems());
+			continue;
+		}
+		int yBin = (y - axisMinimum_.y) / axisDelta_.y;
+		if ((yBin < 0) || (yBin >= data.constYAxis().nItems()))
+		{
+			Messenger::warn("Coordinates x=%e y=%e are out-of-range (yBin = %i, nBins = %i).\n", x, y, yBin, data.constYAxis().nItems());
+			continue;
+		}
+
+		// Set the value
+		data.value(xBin, yBin) = parser.argd(2);
+	}
+
+	return true;
+}
