51f934993119fa6668051410915e9a1b4bc06035
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-07-29 23:22:51 +0100

    Add RefDataList table widget updater.

--- src/gui/helpers/tablewidgetupdater.h
+++ src/gui/helpers/tablewidgetupdater.h
@@ -21,6 +21,7 @@
 
 #include "templates/variantpointer.h"
 #include "templates/list.h"
+#include "templates/refdatalist.h"
 #include <QTableWidget>
 
 #ifndef DISSOLVE_TABLEWIDGETUPDATER_H
@@ -94,6 +95,59 @@ template <class T, class I, class D> class TableWidgetRefListUpdater
 		int rowCount = 0;
 
 		RefListIterator<I,D> itemIterator(data);
+		while (I* item = itemIterator.iterate())
+		{
+			// Our table may or may not be populated, and with different items to those in the list.
+
+			// If there is an item already on this row, check it
+			// If it represents the current pointer data, just update it and move on. Otherwise, delete it and check again
+			while (rowCount < table->rowCount())
+			{
+				tableItem = table->item(rowCount, 0);
+				I* rowData = (tableItem ? VariantPointer<I>(tableItem->data(Qt::UserRole)) : NULL);
+				if (rowData == item)
+				{
+					// Update the current row and quit the loop
+					(functionParent->*updateRow)(rowCount, item, itemIterator.currentData(), false);
+
+					break;
+				}
+				else table->removeRow(rowCount);
+			}
+
+			// If the current row index is (now) out of range, add a new row to the table
+			if (rowCount == table->rowCount())
+			{
+				// Increase row count
+				table->setRowCount(rowCount+1);
+
+				// Create new items
+				(functionParent->*updateRow)(rowCount, item, itemIterator.currentData(), true);
+			}
+
+			++rowCount;
+		}
+
+		// Set the number of table rows again here in order to catch the case where there were zero data items to iterate over
+		table->setRowCount(rowCount);
+	}
+};
+
+// TableWidgetRefDataUpdater - Constructor-only template class to update contents of a QTableWidget from a RefDataList, preserving original items as much as possible
+template <class T, class I, class D> class TableWidgetRefDataListUpdater
+{
+	// Typedefs for passed functions
+	typedef void (T::*TableWidgetRowUpdateFunction)(int row, I* item, D data, bool createItems);
+
+	public:
+	// Constructor
+	TableWidgetRefDataListUpdater(QTableWidget* table, const RefDataList<I,D>& data, T* functionParent, TableWidgetRowUpdateFunction updateRow)
+	{
+		QTableWidgetItem* tableItem;
+
+		int rowCount = 0;
+
+		RefDataListIterator<I,D> itemIterator(data);
 		while (I* item = itemIterator.iterate())
 		{
 			// Our table may or may not be populated, and with different items to those in the list.
