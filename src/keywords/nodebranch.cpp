fd3566a79c4ed0f0b85644b4cc1eb0734bbb9192
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-08-22 21:41:14 +0100

    Add NodeBranchKeyword.

---
+++ src/keywords/nodebranch.cpp
@@ -0,0 +1,76 @@
+/*
+	*** Keyword - NodeBranch
+	*** src/keywords/nodebranch.cpp
+	Copyright T. Youngs 2012-2019
+
+	This file is part of Dissolve.
+
+	Dissolve is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dissolve is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "keywords/nodebranch.h"
+#include "procedure/nodes/node.h"
+#include "procedure/nodes/sequence.h"
+#include "base/lineparser.h"
+
+// Constructor
+NodeBranchKeyword::NodeBranchKeyword(ProcedureNode* parentNode, SequenceProcedureNode** branchPointer, ProcedureNode::NodeContext branchContext) : KeywordData<SequenceProcedureNode**>(KeywordData::NodeBranchData, branchPointer)
+{
+	parentNode_ = parentNode;
+	branchContext_ = branchContext;
+}
+
+// Destructor
+NodeBranchKeyword::~NodeBranchKeyword()
+{
+}
+
+/*
+ * Arguments
+ */
+
+// Return minimum number of arguments accepted
+int NodeBranchKeyword::minArguments()
+{
+	return 0;
+}
+
+// Return maximum number of arguments accepted
+int NodeBranchKeyword::maxArguments()
+{
+	return 0;
+}
+
+// Parse arguments from supplied LineParser, starting at given argument offset
+bool NodeBranchKeyword::read(LineParser& parser, int startArg, const CoreData& coreData)
+{
+	// Check that a branch hasn't already been defined
+	if (*data_) return Messenger::error("Only one %s branch may be defined in a %s node.\n", keyword(), ProcedureNode::nodeTypes().keyword(parentNode_->type()));
+
+	// Create and parse a new branch
+	(*data_) = new SequenceProcedureNode(branchContext_, parentNode_->scope()->procedure(), parentNode_, CharString("End%s", keyword()));
+	if (!(*data_)->read(parser, coreData)) return false;
+
+	return true;
+}
+
+// Write keyword data to specified LineParser
+bool NodeBranchKeyword::write(LineParser& parser, const char* prefix)
+{
+	if (!(*data_)) return true;
+
+	if (!(*data_)->write(parser, CharString("%s  ", prefix))) return false;
+
+	return true;
+}
---
+++ src/keywords/nodebranch.h
@@ -0,0 +1,74 @@
+/*
+	*** Keyword - NodeBranch
+	*** src/keywords/nodebranch.h
+	Copyright T. Youngs 2012-2019
+
+	This file is part of Dissolve.
+
+	Dissolve is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dissolve is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DISSOLVE_KEYWORD_NODEBRANCH_H
+#define DISSOLVE_KEYWORD_NODEBRANCH_H
+
+#include "keywords/data.h"
+#include "keywords/base.h"
+#include "procedure/nodes/node.h"
+
+// Forward Declarations
+class NodeValue;
+class SequenceProcedureNode;
+
+// Keyword with NodeValue
+class NodeBranchKeyword : public KeywordData<SequenceProcedureNode**>
+{
+	public:
+	// Constructor
+	NodeBranchKeyword(ProcedureNode* parentNode, SequenceProcedureNode** branchPointer, ProcedureNode::NodeContext branchContext);
+	// Destructor
+	~NodeBranchKeyword();
+
+
+	/*
+	 * Parent Node
+	 */
+	private:
+	// Parent ProcedureNode
+	ProcedureNode* parentNode_;
+
+
+	/*
+	 * Branch Specification
+	 */
+	private:
+	// Context for the target branch
+	ProcedureNode::NodeContext branchContext_;
+
+
+	/*
+	 * Arguments
+	 */
+	public:
+	// Return minimum number of arguments accepted
+	int minArguments();
+	// Return maximum number of arguments accepted
+	int maxArguments();
+	// Parse arguments from supplied LineParser, starting at given argument offset
+	bool read(LineParser& parser, int startArg, const CoreData& coreData);
+	// Write keyword data to specified LineParser
+	bool write(LineParser& parser, const char* prefix);
+};
+
+#endif
+
