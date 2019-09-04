110bd1e2bf72aa6a991286f378b2b0f346a20913
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-09-04 15:25:19 +0100

    Update dissolve-gui.cpp

--- src/dissolve-gui.cpp
+++ src/dissolve-gui.cpp
@@ -60,7 +60,7 @@ int main(int argc, char **argv)
 					printf("\t-I\t\tIgnore GUI state file\n");
 					printf("\t-q\t\tQuiet mode - print no output\n");
 					printf("\t-v\t\tVerbose mode - be a little more descriptive throughout\n");
-					printf("\t-x\t\tDon't write any restart information and heartbeat file(but still read in the restart file if present)\n");
+					printf("\t-x\t\tDon't write restart or heartbeat files (but still read in the restart file if present)\n");
 					ProcessPool::finalise();
 					return 0;
 					break;
@@ -105,7 +105,7 @@ int main(int argc, char **argv)
 				case ('x'):
 					dontWriteRestart = true;
 					dissolve.setWriteHeartBeat(false);
-					Messenger::print("No restart file or heartbeat files will be written.\n");
+					Messenger::print("No restart or heartbeat files will be written.\n");
 					break;
 				default:
 					printf("Unrecognised command-line switch '%s'.\n", argv[n]);
