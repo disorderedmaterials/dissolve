cfc62d16873e21110454bff8f71afc576414c1a9
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-10-16 18:47:42 +0100

    Don't error when no isotopologues are defined in a species.

--- src/classes/species.cpp
+++ src/classes/species.cpp
@@ -157,12 +157,7 @@ bool Species::checkSetUp()
 	/*
 	 * Check Isotopologues
 	 */
-	if (isotopologues_.nItems() == 0)
-	{
-		Messenger::error("No Isotopologues defined in Species.\n");
-		++nErrors;
-	}
-	else for (Isotopologue* iso = isotopologues_.first(); iso != NULL; iso = iso->next())
+	for (Isotopologue* iso = isotopologues_.first(); iso != NULL; iso = iso->next())
 	{
 		RefDataListIterator<AtomType,Isotope*> isotopeIterator(iso->isotopes());
 		while (AtomType* atomType = isotopeIterator.iterate())
