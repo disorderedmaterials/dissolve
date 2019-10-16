abc7c4c49a2b0b425e45c2866bf1db07f0700b7d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-10-16 22:30:19 +0100

    Don't put quotes around enum option string when writing.

--- src/keywords/enumoptions.h
+++ src/keywords/enumoptions.h
@@ -133,7 +133,7 @@ template <class E> class EnumOptionsKeyword : public EnumOptionsBaseKeyword, pub
 	// Write keyword data to specified LineParser
 	bool write(LineParser& parser, const char* keywordName, const char* prefix)
 	{
-		return parser.writeLineF("%s%s  '%s'\n", prefix, KeywordData< EnumOptions<E> >::name(), KeywordData< EnumOptions<E> >::data_.currentOptionKeyword());
+		return parser.writeLineF("%s%s  %s\n", prefix, KeywordData< EnumOptions<E> >::name(), KeywordData< EnumOptions<E> >::data_.currentOptionKeyword());
 	}
 
 
