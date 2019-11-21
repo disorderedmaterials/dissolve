77b045ef5d27fec2bdeb20372e4a668a094b4554
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-11-21 11:04:19 +0000

    Add barebones CN module.

---
+++ src/modules/calculate_cn/cn.h
@@ -0,0 +1,86 @@
+/*
+	*** CalculateCoordinationNumber Module
+	*** src/modules/calculate_cn/cn.h
+	Copyright T. Youngs 2012-2019
+
+	This file is part of Dissolve.
+
+	Dissolve is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dissolve is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DISSOLVE_MODULE_CALCULATECOORDINATIONNUMBER_H
+#define DISSOLVE_MODULE_CALCULATECOORDINATIONNUMBER_H
+
+#include "module/module.h"
+
+// Forward Declarations
+/* none */
+
+// CalculateCoordinationNumber Module
+class CalculateCoordinationNumberModule : public Module
+{
+	public:
+	// Constructor
+	CalculateCoordinationNumberModule();
+	// Destructor
+	~CalculateCoordinationNumberModule();
+
+
+	/*
+	 * Instances
+	 */
+	public:
+	// Create instance of this module
+	Module* createInstance() const;
+
+
+	/*
+	 * Definition
+	 */
+	public:
+	// Return type of module
+	const char* type() const;
+	// Return category for module
+	const char* category() const;
+	// Return brief description of module
+	const char* brief() const;
+	// Return the maximum number of Configurations the Module can target (or -1 for any number)
+	int nTargetableConfigurations() const;
+
+
+	/*
+	 * Initialisation
+	 */
+	protected:
+	// Perform any necessary initialisation for the Module
+	void initialise();
+	
+	/*
+	 * Processing
+	 */
+	private:
+	// Run main processing
+	bool process(Dissolve& dissolve, ProcessPool& procPool);
+
+
+	/*
+	 * GUI Widget
+	 */
+	public:
+	// Return a new widget controlling this Module
+	ModuleWidget* createWidget(QWidget* parent, Dissolve& dissolve);
+};
+
+#endif
+
