3ac61a068fb04116d85ea44bfb349e11fbc4e2e0
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-11-26 22:17:33 +0000

    Reinstate verbose reporting for Qt installation.

--- ci/windows/build.serial.gui.ps1
+++ ci/windows/build.serial.gui.ps1
@@ -10,8 +10,7 @@ $Wc = New-Object System.Net.WebClient
 $Wc.DownloadFile('http://download.qt.io/archive/qt/5.13/5.13.1/qt-opensource-windows-x86-5.13.1.exe', 'qt.exe')
 echo 'Downloaded qt-opensource-windows-x86-5.13.1.exe'
 $env:QT_INSTALL_DIR = 'C:\\Qt'
-#Start-Process qt.exe -ArgumentList '--verbose --script ci\windows\qtifwsilent.qs' -NoNewWindow -Wait
-Start-Process qt.exe -ArgumentList '--script ci\windows\qtifwsilent.qs' -NoNewWindow -Wait
+Start-Process qt.exe -ArgumentList '--verbose --script ci\windows\qtifwsilent.qs' -NoNewWindow -Wait
 Remove-Item qt.exe
 $env:QTDIR = "C:\\Qt\\5.13.1\\mingw73_64"
 
