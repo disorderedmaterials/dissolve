8cc3e1fd534613244eba2cabbd4317398f5d969d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-12-06 17:12:20 +0000

    Revert to WebClient for Qt download.

--- ci/windows/build.serial.gui.qt5.ps1
+++ ci/windows/build.serial.gui.qt5.ps1
@@ -2,13 +2,14 @@
 # One of a sequence of scripts.
 # - Install Qt 5.13.1
 
-#$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
 # Attempt to free up some disk space before we install
 choco uninstall -y mingw
 
 # Install Qt 5.13.1
-wget 'http://download.qt.io/archive/qt/5.13/5.13.1/qt-opensource-windows-x86-5.13.1.exe' -O 'qt.exe'
+$Wc = New-Object System.Net.WebClient
+$Wc.DownloadFile('http://download.qt.io/archive/qt/5.13/5.13.1/qt-opensource-windows-x86-5.13.1.exe', 'qt.exe')
 echo 'Downloaded qt-opensource-windows-x86-5.13.1.exe'
 $env:QT_INSTALL_DIR = 'C:\\Qt'
 Start-Process qt.exe -ArgumentList '--verbose --script ci\windows\qtifwsilent.qs' -NoNewWindow -Wait
