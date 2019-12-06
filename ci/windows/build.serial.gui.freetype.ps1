5ef15a1c6c00890db85f44179f280e8dfb106dab
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-12-06 13:14:58 +0000

    Dumb test of powershell script.

--- ci/windows/build.serial.gui.freetype.ps1
+++ ci/windows/build.serial.gui.freetype.ps1
@@ -2,21 +2,8 @@
 # One of a sequence of scripts.
 # - Build Freetype
 
-#$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
 # Retrieve / compile Freetype 2.10.1
-cd $HOME
-$Wc = New-Object System.Net.WebClient
-$Wc.DownloadFile('https://download.savannah.gnu.org/releases/freetype/ft2101.zip', 'ft.zip')
-unzip ./ft.zip
-Echo 'Compiling FreeType...'
-mkdir freetype-build
-cd freetype-build
-cmake -G "Ninja" ..\freetype-2.10.1
-ninja
-cd ../
-
-# Set environment
-$env:INCLUDE += "$HOME\\freetype-2.10.1;"
-$env:LIB += "$HOME\\freetype-build;"
+cd $HOME ; $Wc = New-Object System.Net.WebClient; $Wc.DownloadFile('https://download.savannah.gnu.org/releases/freetype/ft2101.zip', 'ft.zip'); unzip ./ft.zip; Echo 'Compiling FreeType...'; mkdir freetype-build; cd freetype-build; cmake -G "Ninja" ..\freetype-2.10.1; ninja; $env:INCLUDE += "$HOME\\freetype-2.10.1;"; $env:LIB += "$HOME\\freetype-build;"
 
