46ad68ea45ffd59e226c73abf74a207e6940de81
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-12-06 21:19:00 +0000

    More debugging for windows scripts.

--- ci/windows/build.serial.gui.freetype.ps1
+++ ci/windows/build.serial.gui.freetype.ps1
@@ -4,6 +4,13 @@
 
 $ErrorActionPreference = 'Stop'
 
+echo $env:PATH
+echo $Env:PATH
+$env:PATH += "C:\\Qt\\Tools\\mingw730_64\\bin;"
+echo $env:PATH
+echo $Env:PATH
+
+
 # Retrieve / compile Freetype 2.10.1
 cd $HOME
 wget 'https://download.savannah.gnu.org/releases/freetype/ft2101.zip' -O 'ft.zip'
