8d2d38c6b989dea36256c4360bfb8b8a31819559
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2019-12-16 22:19:49 +0000

    Tweak header block.

--- src/modules/calculate_cn/cn.h
+++ src/modules/calculate_cn/cn.h
@@ -19,8 +19,8 @@
 	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef DISSOLVE_MODULE_CALCULATECOORDINATIONNUMBER_H
-#define DISSOLVE_MODULE_CALCULATECOORDINATIONNUMBER_H
+#ifndef DISSOLVE_MODULE_CALCULATECN_H
+#define DISSOLVE_MODULE_CALCULATECN_H
 
 #include "module/module.h"
 #include "procedure/procedure.h"
