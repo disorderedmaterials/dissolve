b811da6c44732f01fb8497193bdd5050ab1d99d1
trisyoungs <tristan.youngs@stfc.ac.uk>
2019-12-20 13:30:26 +0000

    Tweak headers for consistency.

--- src/gui/viewer/datawidget.h
+++ src/gui/viewer/datawidget.h
@@ -19,8 +19,8 @@
 	along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef DISSOLVE_DATAWIDGET_H
-#define DISSOLVE_DATAWIDGET_H
+#ifndef DISSOLVE_VIEWER_DATAWIDGET_H
+#define DISSOLVE_VIEWER_DATAWIDGET_H
 
 #include "gui/viewer/ui_datawidget.h"
 #include "base/lock.h"
