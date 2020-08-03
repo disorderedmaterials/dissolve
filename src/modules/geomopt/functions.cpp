ec3fe671aa1095dcdf2e5b7f361abd2deefd8915
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-08-03 15:33:27 +0100

    Template functions to allow flexibility in optimisation target.

--- src/modules/geomopt/functions.cpp
+++ src/modules/geomopt/functions.cpp
@@ -25,7 +25,7 @@
 #include "modules/geomopt/geomopt.h"
 
 // Copy coordinates from supplied Configuration into reference arrays
-void GeometryOptimisationModule::setReferenceCoordinates(Configuration *cfg)
+template <> void GeometryOptimisationModule::setReferenceCoordinates(Configuration *cfg)
 {
     for (int n = 0; n < cfg->nAtoms(); ++n)
     {
@@ -37,7 +37,7 @@ void GeometryOptimisationModule::setReferenceCoordinates(Configuration *cfg)
 }
 
 // Revert Configuration to reference coordinates
-void GeometryOptimisationModule::revertToReferenceCoordinates(Configuration *cfg)
+template <> void GeometryOptimisationModule::revertToReferenceCoordinates(Configuration *cfg)
 {
     for (int n = 0; n < cfg->nAtoms(); ++n)
         cfg->atom(n)->setCoordinates(xRef_[n], yRef_[n], zRef_[n]);
@@ -83,6 +83,7 @@ void GeometryOptimisationModule::sortBoundsAndEnergies(Vec3<double> &bounds, Vec
 }
 
 // Return energy of adjusted coordinates, following the force vectors by the supplied amount
+template <>
 double GeometryOptimisationModule::energyAtGradientPoint(ProcessPool &procPool, Configuration *cfg,
                                                          const PotentialMap &potentialMap, double delta)
 {
@@ -92,150 +93,4 @@ double GeometryOptimisationModule::energyAtGradientPoint(ProcessPool &procPool, 
     cfg->updateCellContents();
 
     return EnergyModule::totalEnergy(procPool, cfg, potentialMap);
-}
-
-// Perform Golden Search within specified bounds
-double GeometryOptimisationModule::goldenSearch(ProcessPool &procPool, Configuration *cfg, const PotentialMap &potentialMap,
-                                                const double tolerance, Vec3<double> &bounds, Vec3<double> &energies,
-                                                int &nPointsAccepted)
-{
-    // Ensure that the energy minimum is the midpoint
-    sortBoundsAndEnergies(bounds, energies);
-
-    // Check convergence, ready for early return
-    if (fabs(bounds.x - bounds.z) < tolerance)
-        return energies.y;
-
-    // Calculate deltas between bound values
-    double dxy = bounds[0] - bounds[1];
-    double dyz = bounds[2] - bounds[1];
-    Messenger::printVerbose("Trying Golden Search -  %f-%f-%f, dxy = %12.5e, dyz = %12.5e", bounds.x, bounds.y, bounds.z, dxy,
-                            dyz);
-
-    // Select largest of two intervals to be the target of the search
-    auto xyLargest = fabs(dxy) > fabs(dyz);
-    double newMinimum = bounds[1] + 0.3819660 * (xyLargest ? dxy : dyz);
-
-    // Test energy at new trial minimum
-    double eNew = energyAtGradientPoint(procPool, cfg, potentialMap, newMinimum);
-    Messenger::printVerbose("--> GOLD point is %12.5e [%12.5e] ", eNew, newMinimum);
-
-    // Set order for checking of energy points
-    Vec3<int> order(1, xyLargest ? 0 : 2, xyLargest ? 2 : 0);
-
-    // Check each energy to see if our new energy is lower. If it is, overwrite it and recurse
-    for (int n = 0; n < 3; ++n)
-    {
-        if (eNew < energies[order[n]])
-        {
-            Messenger::printVerbose("--> GOLD point is lower than point %i...", order[n]);
-
-            // Overwrite the outermost bound with the old minimum
-            bounds[xyLargest ? 0 : 2] = newMinimum;
-            energies[xyLargest ? 0 : 2] = eNew;
-
-            ++nPointsAccepted;
-
-            // Recurse into the new region
-            return goldenSearch(procPool, cfg, potentialMap, tolerance, bounds, energies, nPointsAccepted);
-        }
-    }
-
-    // Nothing better than the current central energy value, so revert to the stored reference coordinates
-    revertToReferenceCoordinates(cfg);
-
-    return energies.y;
-}
-
-// Line minimise supplied Configuration from the reference coordinates along the stored force vectors
-double GeometryOptimisationModule::lineMinimise(ProcessPool &procPool, Configuration *cfg, const PotentialMap &potentialMap,
-                                                const double tolerance, double &stepSize)
-{
-    // Brent-style line minimiser with parabolic interpolation and Golden Search backup
-
-    // Set initial bounding values
-    Vec3<double> bounds, energies;
-    bounds.x = 0.0;
-    energies.x = EnergyModule::totalEnergy(procPool, cfg, potentialMap);
-    bounds.y = stepSize;
-    energies.y = energyAtGradientPoint(procPool, cfg, potentialMap, bounds.y);
-    bounds.z = 2.0 * stepSize;
-    energies.z = energyAtGradientPoint(procPool, cfg, potentialMap, bounds.z);
-
-    Messenger::printVerbose("Initial bounding values/energies = %12.5e (%12.5e) %12.5e (%12.5e) %12.5e (%12.5e)", bounds[0],
-                            energies[0], bounds[1], energies[1], bounds[2], energies[2]);
-
-    // Perform linesearch along the gradient vector
-    do
-    {
-        // Sort w.r.t. energy so that the minimum is in the central point
-        sortBoundsAndEnergies(bounds, energies);
-
-        Messenger::printVerbose("Energies [Bounds] = %12.5e (%12.5e) %12.5e (%12.5e) %12.5e (%12.5e)", energies[0], bounds[0],
-                                energies[1], bounds[1], energies[2], bounds[2]);
-
-        // Perform parabolic interpolation to find new minimium point
-        double b10 = bounds[1] - bounds[0];
-        double b12 = bounds[1] - bounds[2];
-        double a = (b10 * b10 * (energies[1] - energies[2])) - (b12 * b12 * (energies[1] - energies[0]));
-        double b = (b10 * (energies[1] - energies[2])) - (b12 * (energies[1] - energies[0]));
-        double newBound = bounds[1] - 0.5 * (a / b);
-
-        // Compute energy of new point and check that it went down...
-        double eNew = energyAtGradientPoint(procPool, cfg, potentialMap, newBound);
-
-        Messenger::printVerbose("PARABOLIC point gives energy %12.5e @ %12.5e", eNew, newBound);
-        if (eNew < energies[1])
-        {
-            // New point found...
-            Messenger::printVerbose("--> PARABOLIC point is new minimum...");
-
-            // Overwrite the largest of bounds[0] and bounds[2] with the old minimum
-            auto largest = energies[2] > energies[0] ? 2 : 0;
-            bounds.swap(1, largest);
-            energies.swap(1, largest);
-
-            // Set the new central values
-            bounds[1] = newBound;
-            energies[1] = eNew;
-        }
-        else if ((energies[2] - eNew) > tolerance)
-        {
-            Messenger::printVerbose("--> PARABOLIC point is better than bounds[2]...");
-            bounds[2] = newBound;
-            energies[2] = eNew;
-        }
-        else if ((energies[0] - eNew) > tolerance)
-        {
-            Messenger::printVerbose("--> PARABOLIC point is better than bounds[0]...");
-            bounds[0] = newBound;
-            energies[0] = eNew;
-        }
-        else
-        {
-            Messenger::printVerbose("--> PARABOLIC point is worse than all current values...");
-
-            // Revert to the stored reference coordinates
-            revertToReferenceCoordinates(cfg);
-
-            // Try recursive Golden Search instead, into the largest of the two sections
-            auto nPointsAccepted = 0;
-            goldenSearch(procPool, cfg, potentialMap, tolerance, bounds, energies, nPointsAccepted);
-            if (nPointsAccepted == 0)
-                break;
-        }
-        // 		printf("DIFF = %f, 2tol = %f\n", fabs(bounds[0]-bounds[2]), 2.0 * tolerance);
-        // 		++count;
-        // 		if (count > 10) break;
-    } while (fabs(bounds[0] - bounds[2]) > (2.0 * tolerance));
-    // 	printf("Final bounding values are %12.5e %12.5e %12.5e\n",bounds[0],bounds[1],bounds[2]);
-    // 	printf("             energies are %12.5e %12.5e %12.5e\n",energies[0],energies[1],energies[2]);
-
-    // Sort w.r.t. energy so that the minimum is in the central point
-    sortBoundsAndEnergies(bounds, energies);
-
-    // Set an updated step size based on the current bounds
-    stepSize = (bounds.x + bounds.y + bounds.z);
-
-    return energies.y;
 }
