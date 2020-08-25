1b6c845264a81f9cfc16bf24c7484f3b8c8e594d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-08-25 09:51:39 +0100

    Simplify and modernise system functions class (DissolveSys).

--- src/base/sysfunc.cpp
+++ src/base/sysfunc.cpp
@@ -20,13 +20,13 @@
 */
 
 #include "base/sysfunc.h"
-#include "base/charstring.h"
 #include "base/messenger.h"
 #include <ctime>
 #include <ctype.h>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <string.h>
+#include <sstream>
 
 using namespace std;
 
@@ -34,96 +34,30 @@ using namespace std;
  * Conversion Functions
  */
 
-// Convert integer to string representation
-const char *DissolveSys::itoa(int n)
-{
-    static CharString result;
-    result.sprintf("%i", n);
-    return result;
-}
-
-// Convert double to string representation (as %f)
-const char *DissolveSys::ftoa(double f)
-{
-    static CharString result;
-    result.sprintf("%f", f);
-    return result;
-}
-
-// Convert double to string representation with supplied format
-const char *DissolveSys::ftoa(double f, const char *fmt)
-{
-    static CharString result;
-    result.sprintf(fmt, f);
-    return result;
-}
-
 // Convert string to boolean
-bool DissolveSys::atob(const char *s)
+bool DissolveSys::stob(std::string_view s)
 {
-    if (sameString(s, "true") || sameString(s, "on"))
+    if (sameString(s, "true") || sameString(s, "on") || sameString(s, "yes") || sameString(s, "1"))
         return true;
     return false;
 }
 
 // Convert boolean to string representation ("True" or "False")
-const char *DissolveSys::btoa(bool b) { return (b ? "True" : "False"); }
+std::string_view DissolveSys::btoa(bool b) { return (b ? "True" : "False"); }
 
 // Convert boolean to string representation ("On" or "Off")
-const char *DissolveSys::onOff(bool b) { return (b ? "On" : "Off"); }
+std::string_view DissolveSys::onOff(bool b) { return (b ? "On" : "Off"); }
 
 /*
  * String Functions
  */
 
-// Convert string to uppercase
-const char *DissolveSys::upperCase(const char *s)
-{
-    static CharString result(1024);
-    result.clear();
-    for (int i = 0; s[i] != '\0'; i++)
-        result += toupper(s[i]);
-    return result;
-}
-
-// Convert string to lowercase
-const char *DissolveSys::lowerCase(const char *s)
-{
-    static CharString result(1024);
-    result.clear();
-    for (int i = 0; s[i] != '\0'; i++)
-        result += tolower(s[i]);
-    return result;
-}
-
-// Perform case-(in)sensitive string comparison
-bool DissolveSys::sameString(const char *s1, const char *s2, bool caseSensitive)
-{
-    int len1 = strlen(s1), len2 = strlen(s2);
-    if (len1 != len2)
-        return false;
-
-    if (caseSensitive)
-    {
-        for (int n = 0; n < len1; ++n)
-            if (s1[n] != s2[n])
-                return false;
-    }
-    else
-    {
-        for (int n = 0; n < len1; ++n)
-            if (tolower(s1[n]) != tolower(s2[n]))
-                return false;
-    }
-
-    return true;
-}
-
-// Perform case-(in)sensitive, wildcard-enabled string comparison
-bool DissolveSys::sameWildString(const char *wild, const char *s2, bool caseSensitive)
+// Wildcard match the supplied strings
+bool DissolveSys::wildCardMatch(std::string_view::const_iterator wild, std::string_view::const_iterator wildEnd,
+                                std::string_view::const_iterator s2, std::string_view::const_iterator s2End, bool caseSensitive)
 {
     // If we reach at the end of both strings, the match was a success
-    if ((*wild == '\0') && (*s2 == '\0'))
+    if ((wild == wildEnd) && (s2 == s2End))
         return true;
 
     // Handle wildcards '*' and '?'
@@ -135,293 +69,163 @@ bool DissolveSys::sameWildString(const char *wild, const char *s2, bool caseSens
          * character in wild with the next character in s2. This allows the '*' to match as many characters as it can,
          * but always checks for absolute character matching between the current characters.
          */
-        if ((*(wild + 1) != '\0') && (*s2 == '\0'))
+        if (((wild + 1) != wildEnd) && (s2 == s2End))
             return false;
         else
-            return sameWildString(wild + 1, s2, caseSensitive) || sameWildString(wild, s2 + 1, caseSensitive);
+            return wildCardMatch(wild + 1, wildEnd, s2, s2End, caseSensitive) ||
+                   wildCardMatch(wild, wildEnd, s2 + 1, s2End, caseSensitive);
     }
     else
     {
         // Wildcard '?' matches any single character, so increase both character positions and continue
         if (*wild == '?')
-            return sameWildString(wild + 1, s2 + 1, caseSensitive);
+            return wildCardMatch(wild + 1, wildEnd, s2 + 1, s2End, caseSensitive);
     }
 
     // Not a wildcard, so do absolute character matching
     if (caseSensitive)
     {
-        if (tolower(*wild) == tolower(*s2))
-            return sameWildString(wild + 1, s2 + 1, caseSensitive);
+        if (std::tolower(*wild) == std::tolower(*s2))
+            return wildCardMatch(wild + 1, wildEnd, s2 + 1, s2End, caseSensitive);
     }
     else if (*wild == *s2)
-        return sameWildString(wild + 1, s2 + 1, caseSensitive);
+        return wildCardMatch(wild + 1, wildEnd, s2 + 1, s2End, caseSensitive);
 
     // Characters do not match, so return false
     return false;
 }
 
-// Get characters before first occurrence of designated character, or an empty string if the character does not exist
-const char *DissolveSys::beforeChar(const char *s, char delim)
+// Perform case-(in)sensitive string comparison
+bool DissolveSys::sameString(const std::string_view s1, const std::string_view s2, bool caseSensitive)
 {
-    static CharString result(1024);
-    result.clear();
-    for (int i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == delim)
-            return result;
-        result += s[i];
-    }
+    // Check string lengths
+    if (s1.length() != s2.length())
+        return false;
+
+    if (caseSensitive)
+        return s1 == s2;
+
+    return std::equal(s1.cbegin(), s1.cend(), s2.cbegin(), [](auto &l, auto &r) { return std::toupper(l) == std::toupper(r); });
+}
+
+// Perform case-(in)sensitive, wildcard-enabled string comparison
+bool DissolveSys::sameWildString(const std::string_view wild, const std::string_view s2, bool caseSensitive)
+{
+    return wildCardMatch(wild.cbegin(), wild.cend(), s2.cbegin(), s2.cend(), caseSensitive);
+}
+
+// Get characters before first occurrence of designated character, or an empty string if the character does not exist
+std::string_view DissolveSys::beforeChar(const std::string_view s, char c)
+{
+    std::size_t pos = s.find(c);
+    if (pos == std::string::npos)
+        return "";
+
+    return s.substr(0, pos);
+}
+
+// Get characters before first occurrence of any of the supplied characters, or an empty string if none are present
+std::string_view DissolveSys::beforeChar(const std::string_view s, std::string_view chars)
+{
+    for (auto n = 0; n < s.length(); ++n)
+        if (chars.find(s[n]) != std::string::npos)
+            return s.substr(0, n);
+
     return "";
 }
 
 // Get characters after first occurrence of designated character, or an empty string if the character does not exist
-const char *DissolveSys::afterChar(const char *s, char delim)
+std::string_view DissolveSys::afterChar(const std::string_view s, char c)
 {
-    static CharString result(1024);
-    result.clear();
-    auto found = false;
-    for (int i = 0; s[i] != '\0'; i++)
-    {
-        if (found)
-            result += s[i];
-        if (s[i] == delim)
-            found = true;
-    }
-    return (found ? result.get() : "");
-}
-
-// Get characters after last occurrence of designated character
-const char *DissolveSys::afterLastChar(const char *s, char delim)
-{
-    static CharString result;
-    result.clear();
-    const char *c, *d = NULL;
-    for (c = &s[0]; *c != '\0'; ++c)
-        if (*c == delim)
-            d = c;
-    if (d != NULL)
-        result = ++d;
-    return result;
-}
-
-// Get characters before last occurrence of designated character
-const char *DissolveSys::beforeLastChar(const char *s, char delim)
-{
-    static CharString result;
-    result.clear();
-
-    // First, find last occurrence of specified character
-    auto lastPos = 0, count = 0;
-    const char *c;
-    for (c = &s[0]; *c != '\0'; ++c, ++count)
-        if (*c == delim)
-            lastPos = count;
-
-    // Now, grab 'lastPos' characters from the string
-    for (count = 0; count < lastPos; ++count)
-        result += s[count];
-    return result;
-}
-
-// Get characters before first occurrence of designated string
-const char *DissolveSys::beforeStr(const char *s, const char *search)
-{
-    // Search for first occurrence of string
-    static char result[8096];
-    strcpy(result, s);
-    char *c = strstr(result, search);
-    if (c == NULL)
+    std::size_t pos = s.find(c);
+    if ((pos == std::string::npos) || (pos == s.length() - 1))
         return "";
-    *c = '\0';
-    return result;
+
+    return s.substr(pos + 1);
 }
 
-// Get characters after first occurrence of designated character
-const char *DissolveSys::afterStr(const char *s, const char *search)
+// Get characters after first occurrence of any of the supplied characters, or an empty string if none are present
+std::string_view DissolveSys::afterChar(const std::string_view s, std::string_view chars)
 {
-    const char *c = strstr(s, search);
-    if (c == NULL)
+    for (auto n = 0; n < s.length(); ++n)
+        if (chars.find(s[n]) != std::string::npos)
+            return s.substr(n + 1);
+
+    return "";
+}
+
+// Get characters before last occurrence of designated character, or an empty string if the character does not exist
+std::string_view DissolveSys::beforeLastChar(const std::string_view s, char c)
+{
+    std::size_t pos = s.rfind(c);
+    if (pos == std::string::npos)
         return "";
-    for (const char *d = &search[0]; *d != '\0'; ++d)
-        c++;
-    return c;
+
+    return s.substr(0, pos);
+}
+
+// Get characters after last occurrence of designated character, or an empty string if the character does not exist
+std::string_view DissolveSys::afterLastChar(const std::string_view s, char c)
+{
+    std::size_t pos = s.rfind(c);
+    if ((pos == std::string::npos) || (pos == s.length() - 1))
+        return "";
+
+    return s.substr(pos + 1);
 }
 
 // Return if the target string ends with the specified characters
-bool DissolveSys::endsWith(const char *target, const char *ending)
+bool DissolveSys::endsWith(std::string_view target, std::string_view ending)
 {
-    // Check for enough characters to compare
-    const auto targetLength = strlen(target);
-    const auto endingLength = strlen(ending);
-    if (targetLength < endingLength)
+    if (ending.size() > target.size())
         return false;
 
-    const char *c1 = &target[targetLength - 1];
-    const char *c2 = &ending[endingLength - 1];
-    for (int n = 0; n < endingLength; ++n)
-    {
-        if ((*c1) != (*c2))
-            return false;
-        --c1;
-        --c2;
-    }
-
-    return true;
+    return std::equal(ending.rbegin(), ending.rend(), target.rbegin());
 }
 
-// Remove comments from line
-void DissolveSys::removeComments(char *s)
+// Remove comments from supplied string
+void DissolveSys::removeComments(std::string &s)
 {
-    char *c, quotechar = '\0';
+    auto quoteChar = '\0';
     auto escaped = false;
-    for (c = s; *c != '\0'; ++c)
+    for (auto n = 0; n < s.length(); ++n)
     {
+        const auto c = s[n];
+
         // Remember current quoting info...
-        if (*c == '"')
+        if (c == '"')
         {
-            if (quotechar == '\0')
-                quotechar = '"';
-            else if (quotechar == '"')
-                quotechar = '\0';
+            if (quoteChar == '\0')
+                quoteChar = '"';
+            else if (quoteChar == '"')
+                quoteChar = '\0';
         }
-        if (*c == '\'')
+        if (c == '\'')
         {
-            if (quotechar == '\0')
-                quotechar = '\'';
-            else if (quotechar == '\'')
-                quotechar = '\0';
+            if (quoteChar == '\0')
+                quoteChar = '\'';
+            else if (quoteChar == '\'')
+                quoteChar = '\0';
         }
-        if ((*c == '#') && (!escaped) && (quotechar == '\0'))
+        if ((c == '#') && (!escaped) && (quoteChar == '\0'))
         {
-            *c = '\0';
-            break;
+            s = s.substr(0, n);
+            return;
         }
-        // Removed July 2019 - C-style comments not permitted in input files as this breaks object tags (which contain
-        // '//' as delimiters) 		else if ((*c == '/') && (!escaped) && (quotechar == '\0'))
-        // 		{
-        // 			char* c2 = c;
-        // 			c2++;
-        // 			if (*c2 == '/')
-        // 			{
-        // 				*c = '\0';
-        // 				break;
-        // 			}
-        // 		}
-        escaped = *c == '\\';
-    }
-}
 
-// Strip trailing whitespace from string
-const char *DissolveSys::stripTrailing(const char *s)
-{
-    int n;
-    static char result[1024];
-    // Go backwards through string and find first non-whitespace character
-    for (n = strlen(s) - 1; n >= 0; n--)
-        if (s[n] != ' ')
-            break;
-    strncpy(result, s, n + 1);
-    result[n + 1] = '\0';
-    return result;
-}
-
-// Replace all of the supplied characters in the source string
-const char *DissolveSys::replaceChars(const char *s, const char *charstoreplace, char r)
-{
-    static CharString result;
-    bool found;
-    char const *c1, *c2;
-    result.clear();
-    for (c1 = &s[0]; *c1 != '\0'; c1++)
-    {
-        found = false;
-        for (c2 = charstoreplace; *c2 != '\0'; c2++)
-        {
-            if (*c1 == *c2)
-            {
-                found = true;
-                break;
-            }
-        }
-        if (found)
-            result += r;
-        else
-            result += *c1;
+        // Set "escaped" status
+        escaped = c == '\\';
     }
-    return result.get();
-}
-
-// Strip all of the supplied characters from the source string
-const char *DissolveSys::stripChars(const char *s, const char *charstostrip)
-{
-    static CharString result;
-    char const *c1, *c2;
-    bool found;
-    result.clear();
-    for (c1 = &s[0]; *c1 != '\0'; c1++)
-    {
-        found = false;
-        for (c2 = &charstostrip[0]; *c2 != '\0'; c2++)
-        {
-            if (*c1 == *c2)
-            {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-            result += *c1;
-    }
-    return result.get();
-}
-
-// Count number of times that supplied characters occur in supplied string
-int DissolveSys::countChars(const char *s, const char *chars, int offset)
-{
-    auto total = 0, count = 0;
-    while (*s != '\0')
-    {
-        if (count >= offset)
-        {
-            for (int n = 0; chars[n] != '\0'; n++)
-                if (chars[n] == *s)
-                    total++;
-        }
-        s++;
-        count++;
-    }
-    return total;
-}
-
-// Return whether string consists of empty whitespace characters only
-bool DissolveSys::isEmpty(const char *s)
-{
-    if (s == NULL)
-        return true;
-    for (const char *c = s; *c != '\0'; ++c)
-    {
-        switch (*c)
-        {
-            case (' '):
-            case ('\t'):
-            case ('\n'):
-            case ('\r'):
-                continue;
-            default:
-                return false;
-        }
-    }
-    return true;
 }
 
 // Return nice version of string (no spaces, slashes etc.)
-const char *DissolveSys::niceName(const char *original)
+std::string DissolveSys::niceName(std::string_view original)
 {
-    static CharString result;
-    result = original;
-    result.replace(" /\\#*$", '_');
+    std::string s{original};
 
-    return result.get();
+    std::replace_if(s.begin(), s.end(), [](auto &c) { return " /\\#*$"sv.find(c) != std::string::npos; }, '_');
+
+    return s;
 }
 
 /*
@@ -429,21 +233,21 @@ const char *DissolveSys::niceName(const char *original)
  */
 
 // Return whether the supplied string is a number
-bool DissolveSys::isNumber(const char *text)
+bool DissolveSys::isNumber(std::string_view text)
 {
     bool isFloatingPoint;
     return isNumber(text, isFloatingPoint);
 }
 
 // Return whether the supplied string is a number, and also whether it is floating-point
-bool DissolveSys::isNumber(const char *text, bool &isFloatingPoint)
+bool DissolveSys::isNumber(std::string_view text, bool &isFloatingPoint)
 {
     // Assume integer to start with
     isFloatingPoint = false;
 
     auto exponentIndex = -1;
 
-    const auto length = strlen(text);
+    const auto length = text.size();
     for (int n = 0; n < length; ++n)
     {
         char c = text[n];
@@ -490,9 +294,9 @@ bool DissolveSys::isNumber(const char *text, bool &isFloatingPoint)
  */
 
 // Return whether file exists
-bool DissolveSys::fileExists(const char *filename)
+bool DissolveSys::fileExists(std::string_view filename)
 {
-    fstream f(filename, ios::in);
+    fstream f(std::string(filename), ios::in);
     if (f.is_open())
     {
         f.close();
@@ -507,11 +311,12 @@ bool DissolveSys::fileExists(const char *filename)
  */
 
 // Return string of current time / date
-const char *DissolveSys::currentTimeAndDate()
+std::string DissolveSys::currentTimeAndDate()
 {
-    static char result[128];
-    time_t currentTime = time(NULL);
-    tm *converted = localtime(&currentTime);
-    strftime(result, sizeof(result), "%H:%M:%S on %d-%m-%Y", converted);
-    return result;
+    auto currentTime = time(NULL);
+    auto *local = localtime(&currentTime);
+
+    std::ostringstream timeStream;
+    timeStream << std::put_time(local, "%H:%M:%S on %d-%m-%Y");
+    return timeStream.str();
 }
