3e6624d3113c6352077365f247b0666e148506e9
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-09-02 16:02:17 +0100

    Add general integer/double tuple vector keyword.

--- src/keywords/types.h
+++ src/keywords/types.h
@@ -68,5 +68,6 @@
 #include "keywords/vec3double.h"
 #include "keywords/vec3integer.h"
 #include "keywords/vec3nodevalue.h"
+#include "keywords/vector_id.h"
 #include "keywords/vector_is.h"
 #include "keywords/windowfunction.h"
---
+++ src/keywords/vector_id.h
@@ -0,0 +1,62 @@
+/*
+    *** Keyword - Vector of Integer/Double Tuples
+    *** src/keywords/vector_id.h
+    Copyright T. Youngs 2012-2020
+
+    This file is part of Dissolve.
+
+    Dissolve is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Dissolve is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Dissolve.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "keywords/data.h"
+#include <tuple>
+#include <vector>
+
+// Forward Declarations
+/* none */
+
+// TypeDef
+typedef std::vector<std::tuple<std::vector<int>, std::vector<double>>> IntegerDoubleVectorKeywordData;
+
+// Keyword with list of Tuples of Vectors
+class IntegerDoubleVectorKeyword : public KeywordData<IntegerDoubleVectorKeywordData &>
+{
+    public:
+    IntegerDoubleVectorKeyword(IntegerDoubleVectorKeywordData &data, int nRequiredIntegers, int nRequiredValues = -1);
+    ~IntegerDoubleVectorKeyword();
+
+    /*
+     * Data
+     */
+    private:
+    // Number of required integers
+    int nRequiredIntegers_;
+    // Number of required values (or -1 for any number, but at least one)
+    int nRequiredValues_;
+
+    /*
+     * Arguments
+     */
+    public:
+    // Return minimum number of arguments accepted
+    int minArguments() const;
+    // Return maximum number of arguments accepted
+    int maxArguments() const;
+    // Parse arguments from supplied LineParser, starting at given argument offset
+    bool read(LineParser &parser, int startArg, CoreData &coreData);
+    // Write keyword data to specified LineParser
+    bool write(LineParser &parser, std::string_view keywordName, std::string_view prefix);
+};
