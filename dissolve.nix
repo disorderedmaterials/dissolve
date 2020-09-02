ab9b0b34f8499648de68e9b871896d4e1adec82c
Adam Washington <adam.washington@stfc.ac.uk>
2020-09-02 16:04:37 +0100

    Start writing nix builder

---
+++ dissolve.nix
@@ -0,0 +1,35 @@
+{pkgs ? import <nixos> {}}:
+
+pkgs.stdenv.mkDerivation {
+  name = "dissolve";
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DGUI=OFF"
+    "-DPARALLEL=OFF"
+  ];
+  buildInputs = [
+    pkgs.antlr
+    pkgs.bison
+    pkgs.ccls
+    pkgs.clang-tools
+    pkgs.cmake
+    pkgs.distcc
+    pkgs.freetype
+    pkgs.ftgl
+    pkgs.gdb
+    pkgs.libGL
+    pkgs.libglvnd
+    pkgs.libglvnd.dev
+    pkgs.openmpi
+    pkgs.qt5.full
+    pkgs.tbb
+    pkgs.valgrind
+  ];
+  src = pkgs.fetchFromGitHub {
+    owner = "projectdissolve";
+    repo = "dissolve";
+    rev = "e8da2be5c0ca2026464431f7c9cc9d58980bd15f";
+    sha256 = "0fxbh4d4wbapv1yhjm7vb9xnnv9h4v22k8x9jpmd3xvc0i687g42";
+  };
+  installPhase = "cp -r .$prefix $out";
+}
