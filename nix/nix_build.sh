af845db501aa5e5290ba8855403dbcae18436a6d
Adam Washington <adam.washington@stfc.ac.uk>
2020-09-11 17:12:08 +0100

    Don't try and build the GUI right now
    Things are slow enough as it is

--- nix/nix_build.sh
+++ nix/nix_build.sh
@@ -2,4 +2,4 @@
 nix-build dissolve.nix
 nix-build -o dissolve.docker docker.nix
 nix-build -o dissolve-oci.tar.bz2 oci.nix
-nix-build -o dissolve.snap snap.nix
+# nix-build -o dissolve.snap snap.nix
