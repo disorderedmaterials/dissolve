40d88d5ebb87f60a606cc474eb02447200756a89
Adam Washington <adam.washington@stfc.ac.uk>
2020-09-17 15:18:42 +0100

    Actually use gitignoreSource
    Instead of just mentioning it in the code

--- nix/dissolve.nix
+++ nix/dissolve.nix
@@ -5,13 +5,13 @@
 
 let
   cmakeBool = x: if x then "ON" else "OFF";
-    gitignoreSrc = pkgs.fetchFromGitHub {
+  gitignoreSrc = pkgs.fetchFromGitHub {
     owner = "hercules-ci";
     repo = "gitignore";
     # put the latest commit sha of gitignore Nix library here:
     rev = "c4662e662462e7bf3c2a968483478a665d00e717";
     # use what nix suggests in the mismatch message here:
-    sha256 = "sha256:0000000000000000000000000000000000000000000000000000";
+    sha256 = "sha256:1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
     };
     inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
 in
@@ -44,7 +44,7 @@ pkgs.stdenv.mkDerivation {
   ];
   nativeBuildInputs = pkgs.lib.optional gui pkgs.qt5.wrapQtAppsHook;
 
-  src = builtins.path { path = ./..; name = "dissolve-source";};
+  src = gitignoreSource ./..;
   installPhase = "cp -r .$prefix $out";
   meta = {
     name = "Dissolve";
