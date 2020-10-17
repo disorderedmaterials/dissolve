3db378edae407acc8b90e689e95968b7b897c55f
trisyoungs <tristan.youngs@stfc.ac.uk>
2020-10-17 20:28:54 +0100

    Refactor option handling in main.

---
+++ src/main/cli.h
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Team Dissolve and contributors
+
+#pragma once
+
+#include <optional>
+#include <string>
+
+// CLI Options Parser
+class CLIOptions
+{
+    public:
+    CLIOptions() = default;
+    ~CLIOptions() = default;
+
+    /*
+     * Options
+     */
+    private:
+    // Input file to load
+    std::optional<std::string> inputFile_;
+    // Number of iterations to perform
+    int nIterations_;
+    // Frequency at which to write restart file
+    int restartFileFrequency_;
+    // Redirection basename (for per-process output)
+    std::optional<std::string> redirectionBasename_;
+    // Restart file to load, overriding default
+    std::optional<std::string> restartFilename_;
+    // New input file to write (after reading supplied file)
+    std::optional<std::string> writeInputFilename_;
+    // Whether to just check the input file, and then quit
+    bool checkInputOnly_;
+    // Whether to ignore restart file (if it exists)
+    bool ignoreRestartFile_;
+    // Whether to ignore GUI state file (if it exists)
+    bool ignoreStateFile_;
+    // Whether to prevent writing of all output files
+    bool writeNoFiles_;
+
+    public:
+    // Parse supplied options
+    int parse(int nArgs, char **args, bool isGUI = false, bool isParallel = false);
+    // Return input file to load
+    std::optional<std::string> inputFile() const;
+    // Return number of iterations to perform
+    int nIterations() const;
+    // Return frequency at which to write restart file
+    int restartFileFrequency() const;
+    // Return redirection basename (for per-process output)
+    std::optional<std::string> redirectionBasename() const;
+    // Return restart file to load, overriding default
+    std::optional<std::string> restartFilename() const;
+    // Return new input file to write (after reading supplied file)
+    std::optional<std::string> writeInputFilename() const;
+    // Return whether to just check the input file, and then quit
+    bool checkInputOnly() const;
+    // Return whether to ignore restart file if it exists
+    bool ignoreRestartFile() const;
+    // Return whether to ignore GUI state file (if it exists)
+    bool ignoreStateFile() const;
+    // Return whether to prevent writing of all output files
+    bool writeNoFiles() const;
+};
