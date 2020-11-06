e986bf28c458099db59cd6d4ab59ea0075b9ec70
Adam Washington <adam.washington@stfc.ac.uk>
2020-11-06 11:37:36 +0000

    Fix formatting
    I don't know why clang-format does it differently on my machine, but
    I can look at that later

--- src/main/cli.cpp
+++ src/main/cli.cpp
@@ -27,13 +27,11 @@ int CLIOptions::parse(const int args, char **argv, bool isGUI, bool isParallel)
     // Basic Control
     app.add_option("-n,--iterations", nIterations_, "Number of iterations to run (default = 5)")->group("Basic Control");
     app.add_flag_callback("-m,--mpi-output-all", []() { Messenger::setMasterOnly(false) });
-    app.add_flag(
-           "-q,--quiet", [](int count) { Messenger::setQuiet(true); },
-           "Be quiet - don't output any messages whatsoever (output files are still written)")
+    app.add_flag( "-q,--quiet", [](int count) { Messenger::setQuiet(true); },
+                  "Be quiet - don't output any messages whatsoever (output files are still written)")
         ->group("Basic Control");
-    app.add_flag(
-           "-v,--verbose", [](int count) { Messenger::setVerbose(true); },
-           "Print lots of additional output, useful for debugging")
+    app.add_flag( "-v,--verbose", [](int count) { Messenger::setVerbose(true); },
+                  "Print lots of additional output, useful for debugging")
         ->group("Basic Control");
 
     // Input Files
