bc035ee871fef4268dc60b45d1ce1e5be2652af8
Adam Washington <adam.washington@stfc.ac.uk>
2020-12-03 14:39:13 +0000

    Fix copy and pasted type messages in processpool.cpp

--- src/base/processpool.cpp
+++ src/base/processpool.cpp
@@ -1287,13 +1287,13 @@ bool ProcessPool::broadcast(Data1D &array, int rootRank, ProcessPool::Communicat
 
     if (!broadcast(array.xAxis(), rootRank, commType))
     {
-        Messenger::print("Failed to broadcast std::vector<long int> data from root rank {} (world rank {}).\n", rootRank,
+        Messenger::print("Failed to broadcast Data1D from root rank {} (world rank {}).\n", rootRank,
                          worldRanks_[rootRank]);
         return false;
     }
     if (!broadcast(array.values(), rootRank, commType))
     {
-        Messenger::print("Failed to broadcast std::vector<long int> data from root rank {} (world rank {}).\n", rootRank,
+        Messenger::print("Failed to broadcast Data1D from root rank {} (world rank {}).\n", rootRank,
                          worldRanks_[rootRank]);
         return false;
     }
@@ -1301,7 +1301,7 @@ bool ProcessPool::broadcast(Data1D &array, int rootRank, ProcessPool::Communicat
     {
         if (!broadcast(array.errors(), rootRank, commType))
         {
-            Messenger::print("Failed to broadcast std::vector<long int> data from root rank {} (world rank {}).\n", rootRank,
+            Messenger::print("Failed to broadcast Data1D from root rank {} (world rank {}).\n", rootRank,
                              worldRanks_[rootRank]);
             return false;
         }
@@ -1399,7 +1399,7 @@ bool ProcessPool::broadcast(std::vector<char> &array, int rootRank, ProcessPool:
 
     if (!broadcast(array.data(), array.size(), rootRank, commType))
     {
-        Messenger::print("Failed to broadcast std::vector<long int> data from root rank {} (world rank {}).\n", rootRank,
+        Messenger::print("Failed to broadcast std::vector<char> data from root rank {} (world rank {}).\n", rootRank,
                          worldRanks_[rootRank]);
         return false;
     }
@@ -1795,9 +1795,8 @@ bool ProcessPool::broadcast(Array2D<char> &array, int rootRank, ProcessPool::Com
         {
             if (!broadcast(array.linearArray(), rootRank, commType))
             {
-                Messenger::print("Slave {} (world rank {}) failed to receive ArrayD<bool> data from root rank{} .\n ",
+                return Messenger::error("Slave {} (world rank {}) failed to receive ArrayD<bool> data from root rank{} .\n ",
                                  poolRank_, worldRank_, rootRank);
-                return false;
             }
         }
         else
