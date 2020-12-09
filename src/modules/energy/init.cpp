2244e8c7af6b115ee60b252ba72ead8513d489c1
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-12-09 15:04:17 +0000

    Update energy/force module keyword descriptions.

--- src/modules/energy/init.cpp
+++ src/modules/energy/init.cpp
@@ -14,9 +14,10 @@ void EnergyModule::initialise()
                   "Number of points over which to assess the stability of the energy (per Configuration)");
 
     // Test
-    keywords_.add("Test", new BoolKeyword(false), "Test", "Test parallel energy routines against simplified, serial ones");
+    keywords_.add("Test", new BoolKeyword(false), "Test",
+                  "Test parallel energy routines against basic serial versions and supplied reference values");
     keywords_.add("Test", new BoolKeyword(false), "TestAnalytic",
-                  "Compare parallel energy routines against exact (analytic) energy rather than tabulated values");
+                  "Use analytic interatomic energies rather than (production) tabulated potentials for tests");
     keywords_.add("Test", new DoubleKeyword(0.0), "TestReferenceInter",
                   "Reference value for interatomic energy against which to test calculated value");
     keywords_.add("Test", new DoubleKeyword(0.0), "TestReferenceIntra",
@@ -24,5 +25,6 @@ void EnergyModule::initialise()
     keywords_.add("Test", new DoubleKeyword(0.1), "TestThreshold", "Threshold of energy at which test comparison will fail");
 
     // Export
-    keywords_.add("Export", new BoolKeyword(false), "Save", "Save calculate energy points to the file '<name>.energy.txt'");
+    keywords_.add("Export", new BoolKeyword(false), "Save",
+                  "Save calculated energies to disk, one file per targetted configuration");
 }
