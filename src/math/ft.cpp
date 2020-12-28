64c7442d4792c68f1800f6aa775de5c8ce7bb29e
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-12-28 20:07:52 +0000

    Remove Fourier class and put in namespace.

--- src/math/ft.cpp
+++ src/math/ft.cpp
@@ -2,13 +2,14 @@
 // Copyright (c) 2020 Team Dissolve and contributors
 
 #include "math/ft.h"
-#include "base/sysfunc.h"
 #include "math/data1d.h"
 
+namespace Fourier
+{
 // Perform Fourier sine transform of current distribution function, over range specified, and with specified broadening
 // function, modification function, and window applied (if requested)
-bool Fourier::sineFT(Data1D &data, double normFactor, double wMin, double wStep, double wMax, WindowFunction windowFunction,
-                     BroadeningFunction broadening)
+bool sineFT(Data1D &data, double normFactor, double wMin, double wStep, double wMax, WindowFunction windowFunction,
+            BroadeningFunction broadening)
 {
     /*
      * Perform sine Fourier transform of current data. Function has no notion of forward or backwards transforms -
@@ -99,72 +100,4 @@ bool Fourier::sineFT(Data1D &data, double normFactor, double wMin, double wStep,
 
     return true;
 }
-
-// // Fourier transform current data, applying line-width broadening in real-space using the modified Lorch function
-// bool Data1D::transformLorch(double atomicDensity, double step, double rMax, double beta, double delta0, bool qToR)
-// {
-// 	/*
-// 	 * Fourier transforms from Q to r-space, or r to Q-space, employing the modified Lorch function as described by Soper in
-// 	 * XXX TODO.
-// 	 * XXX TODO Only valid when input x_[] values are bin boundaries, not centre-bin values.
-// 	 */
-//
-// 	// Okay to continue with transform?
-// 	if (!checkBeforeTransform()) return false;
-//
-// 	double deltaIn = x_[1] - x_[0];
-// 	double deltar, width, r, factor, norm, ftx, qr, j1qr, j2qr;
-// 	int n, m, nPts = x_.size();
-//
-// 	// Create working arrays
-// 	Array<double> result;
-// 	r = step * 0.5;
-//
-// 	// Set up correct factor, depending on whether we are going from Q -> r or r -> Q
-// 	if (qToR) factor = 1.0 / (2.0 * PI * PI * atomicDensity);
-// 	else factor = 4.0 * PI * atomicDensity;
-//
-// 	// Perform Fourier sine transform
-// 	while (r <= rMax)
-// 	{
-// 		// Reset sum, and calculate r-dependent values
-// 		ftx = 0.0;
-// 		norm = factor / (r*r*r);
-// 		deltar = delta0 * (1.0 + pow(r, beta));
-//
-// 		// Calculate first argument at left-hand bin boundary
-// 		qr = r * x_[0];
-// 		j1qr = sin(qr) - qr * cos(qr);
-//
-// 		for (m=0; m<nPts-1; ++m)
-// 		{
-// 			// Calculate second argument at right-hand bin boundary
-// 			qr = r * x_[m+1];
-// 			j2qr = sin(qr) - qr * cos(qr);
-//
-// 			// Calculate 'width' at centre of bin
-// 			width = 0.5 * (x_[m]+x_[m+1]) * deltar;
-//
-// 			// Form integral of (Q sin Qr)/r from r1 to r2 (as detailed in equation 3.86 in reference above)
-// 			// Modified Lorch function is constructed and applied if the width is non-zero and positive
-// 			if (width < 0.0) ftx += y_[m] * (j2qr - j1qr);
-// 			else ftx += y_[m] * (j2qr - j1qr) * (3.0/(width*width*width)) * (sin(width) - width*cos(width));
-//
-// 			// Overwrite first Lorch argument
-// 			j1qr = j2qr;
-// 		}
-//
-// 		// Sum
-// 		result.add(ftx * norm);
-//
-// 		r += step;
-// 	}
-//
-// 	// Copy transform data over initial data
-// 	y_ = result;
-// 	x_.forgetData();
-// 	for (n=0; n<y_.size(); ++n) x_.add((n+0.5)*step);
-// 	interpolationInterval_ = -1;
-//
-// 	return true;
-// }
+} // namespace Fourier
--- src/math/ft.h
+++ src/math/ft.h
@@ -10,11 +10,10 @@
 class Data1D;
 
 // Fourier Transforms
-class Fourier
+namespace Fourier
 {
-    public:
-    // Perform Fourier sine transform of supplied data, over range specified, and with specified window and broadening
-    // functions applied
-    static bool sineFT(Data1D &data, double normFactor, double wMin, double wStep, double wMax,
-                       WindowFunction windowFunction = WindowFunction(), BroadeningFunction broadening = BroadeningFunction());
-};
+// Perform Fourier sine transform of supplied data, over range specified, and with specified window and broadening
+// functions applied
+bool sineFT(Data1D &data, double normFactor, double wMin, double wStep, double wMax,
+            WindowFunction windowFunction = WindowFunction(), BroadeningFunction broadening = BroadeningFunction());
+}; // namespace Fourier
