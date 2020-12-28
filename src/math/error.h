658ad01e3aaf2914bc9470d979984c3f359220d3
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-12-28 19:52:27 +0000

    Remove Error class and put in namespace.

--- src/math/error.h
+++ src/math/error.h
@@ -10,39 +10,36 @@ class Data1D;
 class Data2D;
 
 // Error Calculation
-class Error
+namespace Error
 {
-    /*
-     * Error Type
-     */
-    public:
-    // Error Types
-    enum ErrorType
-    {
-        RMSEError,
-        MAAPEError,
-        MAPEError,
-        PercentError,
-        RFactorError,
-        nErrorCalculationTypes
-    };
-    // Return enum options for ErrorType
-    static EnumOptions<Error::ErrorType> errorTypes();
 
-    /*
-     * Data1D
-     */
-    public:
-    // Return erorr of specified type between supplied data
-    static double error(ErrorType errorType, const Data1D &A, const Data1D &B, bool quiet = false);
-    // Return RMSE between supplied data
-    static double rmse(const Data1D &A, const Data1D &B, bool quiet = false);
-    // Return MAAPE between supplied data
-    static double maape(const Data1D &A, const Data1D &B, bool quiet = false);
-    // Return MAPE between supplied data
-    static double mape(const Data1D &A, const Data1D &B, bool quiet = false);
-    // Return percentage error between supplied data
-    static double percent(const Data1D &A, const Data1D &B, bool quiet = false);
-    // Return R-Factor (average squared error per point) between supplied data
-    static double rFactor(const Data1D &A, const Data1D &B, bool quiet = false);
+// Error Types
+enum ErrorType
+{
+    RMSEError,
+    MAAPEError,
+    MAPEError,
+    PercentError,
+    RFactorError,
+    nErrorCalculationTypes
 };
+// Return enum options for ErrorType
+EnumOptions<Error::ErrorType> errorTypes();
+
+/*
+ * Data1D
+ */
+
+// Return error of specified type between supplied data
+double error(ErrorType errorType, const Data1D &A, const Data1D &B, bool quiet = false);
+// Return RMSE between supplied data
+double rmse(const Data1D &A, const Data1D &B, bool quiet = false);
+// Return MAAPE between supplied data
+double maape(const Data1D &A, const Data1D &B, bool quiet = false);
+// Return MAPE between supplied data
+double mape(const Data1D &A, const Data1D &B, bool quiet = false);
+// Return percentage error between supplied data
+double percent(const Data1D &A, const Data1D &B, bool quiet = false);
+// Return R-Factor (average squared error per point) between supplied data
+double rFactor(const Data1D &A, const Data1D &B, bool quiet = false);
+}; // namespace Error
