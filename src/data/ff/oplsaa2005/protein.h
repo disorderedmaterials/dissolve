9a2bd6d209eb6112d1edc23fa9d71c44ac2baeaa
Adam Washington <adam.washington@stfc.ac.uk>
2021-01-11 16:10:35 +0000

    Start working on converting protein data.

---
+++ src/data/ff/oplsaa2005/protein.h
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#pragma once
+
+#include "data/ff/oplsaa2005/base.h"
+
+// Forward Declarations
+/* none */
+
+// OPLS-AA/2005 Alkanes Forcefield
+class Forcefield_OPLSAA2005_Protein : public OPLSAA2005BaseForcefield
+{
+    public:
+    Forcefield_OPLSAA2005_Protein() = default;
+    ~Forcefield_OPLSAA2005_Protein() = default;
+
+    /*
+     * Set Up
+     */
+    protected:
+    // Set up / create all forcefield terms
+    bool setUp();
+
+    /*
+     * Definition
+     */
+    public:
+    // Return name of Forcefield
+    std::string_view name() const;
+    // Return description for Forcefield
+    std::string_view description() const;
+};
