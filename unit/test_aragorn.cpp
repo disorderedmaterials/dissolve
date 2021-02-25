281550285b311e2a442401f32f085642c514c8f5
Adam Washington <adam.washington@stfc.ac.uk>
2021-02-25 11:52:17 +0000

    Fix unit test formatting

--- unit/test_aragorn.cpp
+++ unit/test_aragorn.cpp
@@ -3,8 +3,8 @@
 
 #include "templates/algorithms.h"
 #include <algorithm>
-#include <numeric>
 #include <gtest/gtest.h>
+#include <numeric>
 #include <vector>
 
 namespace UnitTest
@@ -17,18 +17,20 @@ TEST(AragornTest, ArithmaticSeries)
     std::iota(ints.begin(), ints.end(), 1);
     int total = std::accumulate(ints.begin(), ints.end(), 0);
 
-    EXPECT_EQ(total, size*(size+1)>>1);
+    EXPECT_EQ(total, size * (size + 1) >> 1);
 
     // Try many different cut lengs
-    for (int cuts = 1; cuts < 40; ++cuts) {
-      int count = 0;
-      // Sum each cut, then take the sum of the cuts
-      for (int n = 0; n < cuts; ++n) {
-	auto [begin, end] = cut_range(ints.begin(), ints.end(), cuts, n);
-	count += std::accumulate(begin, end, 0);
-      }
-      // Make sure that the sum of the cuts in equal to the total
-      EXPECT_EQ(total, count);
+    for (int cuts = 1; cuts < 40; ++cuts)
+    {
+        int count = 0;
+        // Sum each cut, then take the sum of the cuts
+        for (int n = 0; n < cuts; ++n)
+        {
+            auto [begin, end] = cut_range(ints.begin(), ints.end(), cuts, n);
+            count += std::accumulate(begin, end, 0);
+        }
+        // Make sure that the sum of the cuts in equal to the total
+        EXPECT_EQ(total, count);
     }
 }
 
