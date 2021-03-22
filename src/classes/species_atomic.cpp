744e21db6b86dbe8bf9d2b9b7f52b10df0346ca6
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-03-22 13:58:04 +0000

    Tidy up bond removal, fix atom removal.

--- src/classes/species_atomic.cpp
+++ src/classes/species_atomic.cpp
@@ -24,13 +24,30 @@ void Species::removeAtom(SpeciesAtom *i)
      * species definitions upon which the simulation has no dependencies.
      */
 
-    // Remove any bond terms that involve 'i'
-    while (i->nBonds())
-        removeBond(i, i->bond(0).partner(i));
+    // Clear higher-order terms
+    angles_.clear();
+    torsions_.clear();
+    impropers_.clear();
+
+    // Detach & remove any bond terms that involve 'i'
+    auto it = std::remove_if(bonds_.begin(), bonds_.end(), [i](auto &bond) {
+        if (bond.i() == i || bond.j() == i)
+        {
+            bond.detach();
+            return true;
+        }
+        else
+            return false;
+    });
+    if (it != bonds_.end())
+        bonds_.erase(it, bonds_.end());
 
     // Now remove the atom
-    atoms_.erase(std::remove_if(atoms_.begin(), atoms_.end(), [&](const auto &p) { return i == &p; }), atoms_.end());
+    auto atomIt = std::find_if(atoms_.begin(), atoms_.end(), [&](const auto &p) { return i == &p; });
+    atoms_.erase(atomIt);
     selectedAtoms_.remove(i);
+
+    renumberAtoms();
 
     ++version_;
 }
