81f2052923aab33bb174fc1b2dbf9e7ac32e4a69
Calum Green <calum.green@stfc.ac.uk>
2021-04-27 15:45:11 +0100

    updated oct/sqbp atom types

--- src/data/ff/uff/uff4mof.cpp
+++ src/data/ff/uff/uff4mof.cpp
@@ -39,34 +39,34 @@ bool Forcefield_UFF4MOF::setUp()
                    "Aluminium (octahedral, oxidation state = +3)", 1.22, 90.0, 4.499, 0.505, 11.278, 1.792, 11.278, 0.0, 1.25);
     addUFFAtomType(Elements::Sc, 205, "Sc6+3", "geometry=oct, nbonds=6, -&203(n=4), -&223",
                    "Scandium (octahedral, oxidation state = +3)", 1.44, 90.0, 3.295, 0.019, 12.0, 2.595, 3.395, 0.0, 0.7);
-    addUFFAtomType(Elements::Ti, 206, "Ti4+2", "nbonds>=5, -&223(n=2), -Ti(root)", "Titanium (oct/sqbp, oxidation state = +2)",
+    addUFFAtomType(Elements::Ti, 206, "Ti4+2", "nbonds>=5, -&223(n=4), -Ti(root)", "Titanium (oct/sqbp, oxidation state = +2)",
                    1.38, 90.0, 3.175, 0.017, 12.0, 2.659, 3.47, 0.0, 0.7);
-    addUFFAtomType(Elements::V, 207, "V_4+2", "nbonds>=5, -&223(n=2), -V(root)", "Vanadium (oct/sqbp, oxidation state = +2)",
+    addUFFAtomType(Elements::V, 207, "V_4+2", "nbonds>=5, -&223(n=4), -V(root)", "Vanadium (oct/sqbp, oxidation state = +2)",
                    1.18, 90.0, 3.144, 0.016, 12.0, 2.679, 3.65, 0.0, 0.7);
     addUFFAtomType(Elements::V, 208, "V_6+3", "geometry=oct, nbonds=6,  -&203(n=4), -&223",
                    "Vanadium (octahedral, oxidation state = +3)", 1.30, 90.0, 3.144, 0.016, 12.0, 2.679, 3.65, 0.0, 0.7);
-    addUFFAtomType(Elements::Cr, 209, "Cr4+2", "nbonds>=5, -&223(n=2), -Cr(root)", "Chromium (oct/sqbp, oxidation state = +2)",
+    addUFFAtomType(Elements::Cr, 209, "Cr4+2", "nbonds>=5, -&223(n=4), -Cr(root)", "Chromium (oct/sqbp, oxidation state = +2)",
                    1.10, 90.0, 3.023, 0.015, 12.0, 2.463, 3.415, 0.0, 0.7);
     addUFFAtomType(Elements::Cr, 210, "Cr6f3", "geometry=oct, nbonds=6, -&203(n=4), -&223",
                    "Chromium (octahedral, oxidation state = +3)", 1.28, 90.0, 3.023, 0.015, 12.0, 2.463, 3.415, 0.0, 0.7);
     addUFFAtomType(Elements::Mn, 211, "Mn6+3", "geometry=oct, nbonds=6, -&203(n=4), -&223",
                    "Manganese (octahedral, oxidation state = +3)", 1.34, 90.0, 2.961, 0.013, 12.0, 2.43, 3.325, 0.0, 0.7);
-    addUFFAtomType(Elements::Mn, 212, "Mn4+2", "nbonds>=5, -&223(n=2), -Mn(root)", "Manganese (oct/sqbp, oxidation state = +2)",
+    addUFFAtomType(Elements::Mn, 212, "Mn4+2", "nbonds>=5, -&223(n=4), -Mn(root)", "Manganese (oct/sqbp, oxidation state = +2)",
                    1.26, 90.0, 2.961, 0.013, 12.0, 2.43, 3.325, 0.0, 0.7);
     addUFFAtomType(Elements::Fe, 213, "Fe6+3", "geometry=oct, nbonds=6, -&203(n=4), -&223",
                    "Iron (octahedral, oxidation state = +3)", 1.32, 90.0, 2.912, 0.013, 12.0, 2.43, 3.76, 0.0, 0.7);
-    addUFFAtomType(Elements::Fe, 214, "Fe4+2", "nbonds>=5, nbonds=6, -&223(n=2), -Fe(root)",
+    addUFFAtomType(Elements::Fe, 214, "Fe4+2", "nbonds>=5, nbonds=6, -&223(n=4), -Fe(root)",
                    "Iron (oct/sqbp, oxidation state = +2)", 1.10, 90.0, 2.912, 0.013, 12.0, 2.43, 3.76, 0.0, 0.7);
     addUFFAtomType(Elements::Co, 215, "Co3+2", "geometry=tet, nbonds=4, -&13(n=3), -&35(n=1)",
                    "Cobalt (tetrahedral, oxidation state = +2)", 1.24, 109.47, 2.872, 0.014, 12.0, 1.308, 4.105, 0.0, 0.7);
-    addUFFAtomType(Elements::Co, 216, "Co4+2", "nbonds>=5, -&223(n=2), -Ti(root)", "Cobalt (oct/sqbp, oxidation state = +3)",
+    addUFFAtomType(Elements::Co, 216, "Co4+2", "nbonds>=5, -&223(n=4), -Ti(root)", "Cobalt (oct/sqbp, oxidation state = +3)",
                    1.16, 90.0, 2.872, 0.014, 12.0, 1.308, 4.105, 0.0, 0.7);
     addUFFAtomType(Elements::Co, 217, "Co4+2", "geometry=oct, nbonds=6, -&223(n=6)",
                    "Cobalt (octahedral, oxidation state = +3) in MFU-4 SBU", 1.16, 90.0, 2.872, 0.014, 12.0, 1.308, 4.105, 0.0,
                    0.7);
-    addUFFAtomType(Elements::Cu, 218, "Cu4+2", "nbonds>=5, -&223(n=2), -Cu(root)", "Copper (oct/sqbp, oxidation state = +2)",
+    addUFFAtomType(Elements::Cu, 218, "Cu4+2", "nbonds>=5, -&223(n=4), -Cu(root)", "Copper (oct/sqbp, oxidation state = +2)",
                    1.28, 90.0, 3.495, 0.005, 12.0, 2.43, 4.2, 0.0, 0.7);
-    addUFFAtomType(Elements::Zn, 219, "Zn4+2", "nbonds>=5, -&223(n=2), -Zn(root)", "Zinc (oct/sqbp, oxidation state = +2)",
+    addUFFAtomType(Elements::Zn, 219, "Zn4+2", "nbonds>=5, -&223(n=4), -Zn(root)", "Zinc (oct/sqbp, oxidation state = +2)",
                    1.34, 90.0, 2.763, 0.124, 12.0, 1.308, 5.106, 0.0, 0.7);
     addUFFAtomType(Elements::Zn, 220, "Zn4+2", "geometry=oct, nbonds=6, -&223(n=6)",
                    "Zinc (octahedral, oxidation state = +2) in MFU-4 SBU", 1.34, 90.0, 2.763, 0.124, 12.0, 1.308, 5.106, 0.0,
