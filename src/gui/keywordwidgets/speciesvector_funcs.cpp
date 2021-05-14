bc3d4ed31ef4e1a5c808d9b30a95b6a186a32a70
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-05-14 15:40:13 +0100

    Move Species reflist keyword to use (local) std::vector.

---
+++ src/gui/keywordwidgets/speciesvector_funcs.cpp
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#include "classes/coredata.h"
+#include "classes/species.h"
+#include "gui/helpers/listwidgetupdater.h"
+#include "gui/keywordwidgets/speciesvector.h"
+#include <QComboBox>
+#include <QHBoxLayout>
+#include <QString>
+
+SpeciesVectorKeywordWidget::SpeciesVectorKeywordWidget(QWidget *parent, KeywordBase *keyword, const CoreData &coreData)
+    : KeywordDropDown(this), KeywordWidgetBase(coreData)
+{
+    // Create and set up the UI for our widget in the drop-down's widget container
+    ui_.setupUi(dropWidget());
+    ui_.SpeciesList->setModel(&speciesModel_);
+
+    // Connect signals / slots
+    connect(&speciesModel_, SIGNAL(dataChanged(const QModelIndex &, const QModelIndex &)), this,
+            SLOT(modelDataChanged(const QModelIndex &, const QModelIndex &)));
+
+    // Cast the pointer up into the parent class type
+    keyword_ = dynamic_cast<SpeciesVectorKeyword *>(keyword);
+    if (!keyword_)
+        throw(
+            std::runtime_error(fmt::format("Couldn't cast base keyword '{}' into SpeciesRefListKeyword.\n", keyword->name())));
+    speciesModel_.setCheckStateData(keyword_->data());
+}
+
+/*
+ * Widgets
+ */
+
+void SpeciesVectorKeywordWidget::modelDataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight)
+{
+    if (refreshing_)
+        return;
+
+    keyword_->hasBeenSet();
+
+    updateSummaryText();
+
+    emit(keywordValueChanged(keyword_->optionMask()));
+}
+
+/*
+ * Update
+ */
+
+// Update value displayed in widget
+void SpeciesVectorKeywordWidget::updateValue() { updateWidgetValues(coreData_); }
+
+// Update widget values data based on keyword data
+void SpeciesVectorKeywordWidget::updateWidgetValues(const CoreData &coreData)
+{
+    refreshing_ = true;
+
+    speciesModel_.setData(coreData.species());
+    speciesModel_.setCheckStateData(keyword_->data());
+
+    updateSummaryText();
+
+    refreshing_ = false;
+}
+
+// Update keyword data based on widget values
+void SpeciesVectorKeywordWidget::updateKeywordData()
+{
+    // Handled by model
+}
+
+// Update summary text
+void SpeciesVectorKeywordWidget::updateSummaryText()
+{
+    // Create summary text for the KeywordDropDown button
+    auto &selection = keyword_->data();
+    if (selection.empty())
+        setSummaryText("<None>");
+    else
+    {
+        std::string summaryText;
+        for (const auto *sp : selection)
+            summaryText += fmt::format("{}{}", summaryText.empty() ? "" : ", ", sp->name());
+
+        setSummaryText(QString::fromStdString(summaryText));
+    }
+}
---
+++ src/keywords/speciesvector.cpp
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#include "keywords/speciesvector.h"
+#include "base/lineparser.h"
+#include "classes/coredata.h"
+#include "classes/species.h"
+
+SpeciesVectorKeyword::SpeciesVectorKeyword(std::vector<const Species *> references)
+    : KeywordData<std::vector<const Species *>>(KeywordBase::SpeciesVectorData, references)
+{
+}
+
+SpeciesVectorKeyword::~SpeciesVectorKeyword() = default;
+
+/*
+ * Data
+ */
+
+// Determine whether current data is 'empty', and should be considered as 'not set'
+bool SpeciesVectorKeyword::isDataEmpty() const { return data_.empty(); }
+
+/*
+ * Arguments
+ */
+
+// Return minimum number of arguments accepted
+int SpeciesVectorKeyword::minArguments() const { return 1; }
+
+// Return maximum number of arguments accepted
+int SpeciesVectorKeyword::maxArguments() const { return 99; }
+
+// Parse arguments from supplied LineParser, starting at given argument offset
+bool SpeciesVectorKeyword::read(LineParser &parser, int startArg, const CoreData &coreData)
+{
+    // Each argument is the name of a Species that we will add to our list
+    for (auto n = startArg; n < parser.nArgs(); ++n)
+    {
+        const auto *sp = coreData.findSpecies(parser.argsv(n));
+        if (!sp)
+            return Messenger::error("Error reading keyword '{}' - no Species named '{}' exists.\n", name(), parser.argsv(n));
+
+        data_.push_back(sp);
+    }
+
+    set_ = true;
+
+    return true;
+}
+
+// Write keyword data to specified LineParser
+bool SpeciesVectorKeyword::write(LineParser &parser, std::string_view keywordName, std::string_view prefix) const
+{
+    // Loop over list of Species
+    std::string speciesString;
+    for (const auto *sp : data_)
+        speciesString += fmt::format("  '{}'", sp->name());
+
+    if (!parser.writeLineF("{}{}  {}\n", prefix, keywordName, speciesString))
+        return false;
+
+    return true;
+}
+
+/*
+ * Object Management
+ */
+
+// Prune any references to the supplied Species in the contained data
+void SpeciesVectorKeyword::removeReferencesTo(Species *sp)
+{
+    data_.erase(std::remove(data_.begin(), data_.end(), sp), data_.end());
+}
