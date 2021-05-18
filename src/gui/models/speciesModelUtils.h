9a39ed1673cae941fb7404ac78a859b700e9de49
Adam Washington <adam.washington@stfc.ac.uk>
2021-05-18 14:45:38 +0100

    Document utility functions

--- src/gui/models/speciesModelUtils.h
+++ src/gui/models/speciesModelUtils.h
@@ -1,7 +1,9 @@
 #include "classes/speciesintra.h"
 #include <QString>
 
-// Join the parameters values of a
+// Join the parameters into a comma separated string
 QString joinParameters(const SpeciesIntra &source);
 
+// Populate the parameters of a SpeciesIntra with a comma separated
+// string of values
 bool splitParameters(const QString &params, SpeciesIntra &destination);
