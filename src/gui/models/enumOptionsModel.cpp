d80391238797e0dea1528760221c02c9def0783c
trisyoungs <tristan.youngs@stfc.ac.uk>
2021-06-01 07:40:22 +0100

    Add EnumOptions model.

---
+++ src/gui/models/enumOptionsModel.cpp
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#include "gui/models/enumOptionsModel.h"
+#include "base/enumoptionsbase.h"
+
+// Set source AtomType data
+void EnumOptionsModel::setData(EnumOptionsBase &options)
+{
+    beginResetModel();
+    enumOptions_ = options;
+    endResetModel();
+}
+
+/*
+ * QAbstractItemModel overrides
+ */
+
+int EnumOptionsModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return enumOptions_ ? enumOptions_->get().nOptions() : 0;
+}
+
+QVariant EnumOptionsModel::data(const QModelIndex &index, int role) const
+{
+    if (role != Qt::DisplayRole || index.column() != 0)
+        return QVariant();
+
+    return QString::fromStdString(enumOptions_->get().keywordByIndex(index.row()));
+}
+
+Qt::ItemFlags EnumOptionsModel::flags(const QModelIndex &index) const
+{
+    return index.column() == 1 ? Qt::ItemIsSelectable | Qt::ItemIsEnabled
+                               : Qt::ItemIsSelectable | Qt::ItemIsEditable | Qt::ItemIsEnabled;
+}
+
+QVariant EnumOptionsModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole || orientation != Qt::Horizontal)
+        return QVariant();
+
+    if (section == 0)
+        return "Option";
+
+    return QVariant();
+}
---
+++ src/gui/models/enumOptionsModel.h
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#pragma once
+
+#include "templates/optionalref.h"
+#include <QAbstractListModel>
+#include <QModelIndex>
+#include <vector>
+
+// Forward Declarations
+class EnumOptionsBase;
+
+class EnumOptionsModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+    private:
+    // Source EnumOptions data
+    OptionalReferenceWrapper<EnumOptionsBase> enumOptions_;
+
+    public:
+    // Set source EnumOptions data
+    void setData(EnumOptionsBase &options);
+
+    /*
+     * QAbstractItemModel overrides
+     */
+    public:
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+};
