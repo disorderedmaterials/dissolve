22daf7df45c1f0eaa50a3484073dfd7fdf8ee062
Adam Washington <adam.washington@stfc.ac.uk>
2021-10-05 15:55:12 +0100

    Move to just for_each configuration

--- src/modules/rdf/functions.cpp
+++ src/modules/rdf/functions.cpp
@@ -228,23 +228,16 @@ bool RDFModule::calculateGRCells(ProcessPool &procPool, Configuration *cfg, Part
 
         // Add contributions between atoms in cellI
         PairIterator pairs(atomsI.size());
-        partialSet =
-            dissolve::transform_reduce(ParallelPolicies::seq, pairs.begin(), pairs.end(), partialSet,
-                                       [](PartialSet set, std::tuple<int, int, double> tup) -> PartialSet {
-                                           auto [i, j, dist] = tup;
-                                           if (dist > 0)
-                                               set.fullHistogram(i, j).bin(dist);
-                                           return set;
-                                       },
-                                       [&atomsI](auto it) -> std::tuple<int, int, double> {
-                                           auto [idx, jdx] = it;
-                                           if (idx == jdx)
-                                               return {0, 0, -1};
-                                           auto i = atomsI[idx];
-                                           auto j = atomsI[jdx];
-                                           // No need to perform MIM since we're in the same cell
-                                           return {i->localTypeIndex(), j->localTypeIndex(), (i->r() - j->r()).magnitude()};
-                                       });
+        std::for_each(pairs.begin(), pairs.end(), [&atomsI, &partialSet](auto it) {
+            auto [idx, jdx] = it;
+            auto &i = atomsI[idx];
+            auto &j = atomsI[jdx];
+            if (idx == jdx)
+                return;
+            // No need to perform MIM since we're in the same cell
+            double distance = (i->r() - j->r()).magnitude();
+            partialSet.fullHistogram(i->localTypeIndex(), j->localTypeIndex()).bin(distance);
+        });
     }
     return true;
 }
