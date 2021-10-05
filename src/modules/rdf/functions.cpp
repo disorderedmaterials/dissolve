5f6085966d1515783a7fa5a97ed50f5c536ec5f5
Adam Washington <adam.washington@stfc.ac.uk>
2021-10-05 14:48:47 +0100

    Fix formatting

--- src/modules/rdf/functions.cpp
+++ src/modules/rdf/functions.cpp
@@ -228,23 +228,23 @@ bool RDFModule::calculateGRCells(ProcessPool &procPool, Configuration *cfg, Part
 
         // Add contributions between atoms in cellI
         PairIterator pairs(atomsI.size());
-        partialSet = dissolve::transform_reduce(
-            ParallelPolicies::seq, pairs.begin(), pairs.end(), partialSet,
-            [](PartialSet set, auto tup) -> PartialSet {
-                auto [i, j, dist] = tup;
-                if (dist > 0)
-                    set.fullHistogram(i, j).bin(dist);
-                return set;
-            },
-            [&atomsI](auto it) -> std::tuple<int, int, double> {
-                auto [idx, jdx] = it;
-                if (idx == jdx)
-                    return {0, 0, -1};
-                auto i = atomsI[idx];
-                auto j = atomsI[jdx];
-                // No need to perform MIM since we're in the same cell
-                return {i->localTypeIndex(), j->localTypeIndex(), (i->r() - j->r()).magnitude()};
-            });
+        partialSet =
+            dissolve::transform_reduce(ParallelPolicies::seq, pairs.begin(), pairs.end(), partialSet,
+                                       [](PartialSet set, auto tup) -> PartialSet {
+                                           auto [i, j, dist] = tup;
+                                           if (dist > 0)
+                                               set.fullHistogram(i, j).bin(dist);
+                                           return set;
+                                       },
+                                       [&atomsI](auto it) -> std::tuple<int, int, double> {
+                                           auto [idx, jdx] = it;
+                                           if (idx == jdx)
+                                               return {0, 0, -1};
+                                           auto i = atomsI[idx];
+                                           auto j = atomsI[jdx];
+                                           // No need to perform MIM since we're in the same cell
+                                           return {i->localTypeIndex(), j->localTypeIndex(), (i->r() - j->r()).magnitude()};
+                                       });
     }
     return true;
 }
@@ -732,8 +732,7 @@ bool RDFModule::testReferencePartials(const Data1DStore &testData, double testTh
     {
         // Grab the name, replace hyphens with '-', and parse the string into arguments
         std::string dataName{data.tag()};
-        std::replace_if(
-            dataName.begin(), dataName.end(), [](auto &c) { return c == '-'; }, ' ');
+        std::replace_if(dataName.begin(), dataName.end(), [](auto &c) { return c == '-'; }, ' ');
         parser.getArgsDelim(LineParser::Defaults, dataName);
 
         // Sanity check on number of arguments
@@ -763,8 +762,7 @@ bool RDFModule::testReferencePartials(const Data1DStore &testData, double testTh
     {
         // Grab the name, replace hyphens with '-', and parse the string into arguments
         std::string dataName{data.tag()};
-        std::replace_if(
-            dataName.begin(), dataName.end(), [](auto &c) { return c == '-'; }, ' ');
+        std::replace_if(dataName.begin(), dataName.end(), [](auto &c) { return c == '-'; }, ' ');
         parser.getArgsDelim(LineParser::Defaults, dataName);
 
         // Sanity check on number of arguments
