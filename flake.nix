6307c4911338f6535a004948d30d2983d23941f4
Adam Washington <adam.washington@stfc.ac.uk>
2021-10-06 14:57:07 +0100

    Nix formatting

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
           pname = "dissolve";
           version = "0.9.0";
           src = ./.;
-          patches = [./nix/patches/no-conan.patch];
+          patches = [ ./nix/patches/no-conan.patch ];
           buildInputs = with pkgs; [
             antlr4
             antlr4.runtime.cpp
