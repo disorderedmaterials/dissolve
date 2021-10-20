f3d3b720db652ecf1602b8ce9b49b4e8e0a80db9
trisyoungs <tristan.youngs@stfc.ac.uk>
2021-10-20 23:34:44 +0100

    Calculate and show total charge on species page.

--- src/gui/speciestab.h
+++ src/gui/speciestab.h
@@ -87,14 +87,18 @@ class SpeciesTab : public QWidget, public MainTab
     std::vector<std::string> validAtomTypeNames(const QModelIndex &index);
 
     private slots:
+    // Atom table item changed
+    void atomTableDataChanged(const QModelIndex &, const QModelIndex &);
     // Respond to changes in the Atom Table
     void updateAtomTableSelection();
     // Respond to selection in the underlying model
     void updateUnderlyingAtomSelection();
 
     public slots:
-    // Update Geometry tab
-    void updateGeometryTab();
+    // Update total charges
+    void updateTotalCharges();
+    // Update Geometry tablss
+    void updateGeometryTables();
 
     /*
      * Widget Functions - Isotopologues
