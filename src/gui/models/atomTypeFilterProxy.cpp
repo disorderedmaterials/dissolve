acbb46730ce031230f0d65cf9f77b33a194b3123
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-10-20 12:52:21 +0100

    Add atom type filter proxy for model.

---
+++ src/gui/models/atomTypeFilterProxy.cpp
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#include "gui/models/atomTypeFilterProxy.h"
+#include "classes/atomtype.h"
+#include "data/elements.h"
+
+// Set filter element
+void AtomTypeFilterProxy::setFilterElement(Elements::Element Z)
+{
+    filterElement_ = Z;
+    invalidateFilter();
+}
+
+// Clear filter element
+void AtomTypeFilterProxy::clearFilterElement()
+{
+    filterElement_ = std::nullopt;
+    invalidateFilter();
+}
+
+/*
+ * QSortFilterProxyModel overrides
+ */
+
+bool AtomTypeFilterProxy::filterAcceptsRow(int row, const QModelIndex &parent) const
+{
+    if (!filterElement_)
+        return true;
+
+    const auto at = sourceModel()->data(sourceModel()->index(row, 0, parent), Qt::UserRole).value<std::shared_ptr<AtomType>>();
+    return at->Z() == filterElement_.value();
+}
