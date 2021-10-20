53b885b33f016e60a81d7dabc3ced2a44663a936
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-10-20 13:32:20 +0100

    Add widget/dialog for selecting atom type.

---
+++ src/gui/selectatomtypewidget.h
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#pragma once
+
+#include "gui/models/atomTypeFilterProxy.h"
+#include "gui/models/atomTypeModel.h"
+#include "gui/ui_selectatomtypewidget.h"
+#include <QWidget>
+
+// Forward Declarations
+class AtomType;
+
+// Select AtomType Widget
+class SelectAtomTypeWidget : public QWidget
+{
+    Q_OBJECT
+
+    public:
+    SelectAtomTypeWidget(QWidget *parent);
+    ~SelectAtomTypeWidget() override = default;
+
+    /*
+     * UI
+     */
+    private:
+    // Main form declaration
+    Ui::SelectAtomTypeWidget ui_;
+    // Model for the AtomType list
+    AtomTypeModel atomTypeModel_;
+    // Filter proxy for the AtomType model
+    AtomTypeFilterProxy atomTypeFilterProxy_;
+
+    /*
+     * Data
+     */
+    public:
+    // Set filter element
+    void setFilterElement(Elements::Element Z);
+    // Clear filter element
+    void clearFilterElement();
+    // Set target AtomType data
+    void setAtomTypes(const std::vector<std::shared_ptr<AtomType>> &atomTypes);
+    // Reset widget
+    void reset();
+
+    /*
+     * Signals / Slots
+     */
+    private slots:
+    void selectionChanged(const QItemSelection &, const QItemSelection &);
+    void on_AtomTypeList_doubleClicked(const QModelIndex &index);
+
+    signals:
+    void atomTypeSelectionChanged(bool isValid);
+    void atomTypeDoubleClicked();
+
+    public:
+    // Return the currently-selected AtomType
+    const std::shared_ptr<AtomType> currentAtomType() const;
+};
