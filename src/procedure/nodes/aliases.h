cdf2e784e79ad7aa75436ade57289900be5b3bdb
Adam Washington <adam.washington@stfc.ac.uk>
2021-11-04 17:36:22 +0000

    Actually include node type aliases in source control

---
+++ src/procedure/nodes/aliases.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Team Dissolve and contributors
+
+#pragma once
+
+class ProcedureNode;
+using NodeRef = std::shared_ptr<ProcedureNode>;
+using ConstNodeRef = std::shared_ptr<const ProcedureNode>;
