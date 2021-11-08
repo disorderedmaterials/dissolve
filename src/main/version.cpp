0ca91444a43789f62071020edaa0beb94eb6869f
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-11-08 10:43:56 +0000

    Bump version number.

--- src/main/version.cpp
+++ src/main/version.cpp
@@ -4,7 +4,7 @@
 #include "main/version.h"
 #include <fmt/core.h>
 
-#define DISSOLVEVERSION "0.8.6"
+#define DISSOLVEVERSION "0.8.7"
 #define DISSOLVESHORTHASH ""
 #define DISSOLVEREPO "https://github.com/disorderedmaterials/dissolve.git"
 
