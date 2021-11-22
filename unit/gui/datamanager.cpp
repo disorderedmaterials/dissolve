803a87d3b23a9fe1a5f63890426f73f66490ec17
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2021-11-22 12:22:36 +0000

    Update / fix DataManager test.

--- unit/gui/datamanager.cpp
+++ unit/gui/datamanager.cpp
@@ -46,7 +46,9 @@ TEST_F(DataManagerTest, DataManager)
     EXPECT_EQ(model.data(model.index(0, 1)).toString().toStdString(), "restart/benzene.txt.restart.test");
 
     EXPECT_EQ(sim.columnCount(), 3);
-    EXPECT_EQ(sim.rowCount(), 29);
+    // There are 38 processing data items in the restart file, but an additional "IsEnergyModuleTarget" is created when
+    // the EnergyModule setup stage is automatically run
+    EXPECT_EQ(sim.rowCount(), 39);
     EXPECT_EQ(sim.data(sim.index(0, 0)).toString().toStdString(), "5050//FullWeights@benzene");
     EXPECT_EQ(sim.data(sim.index(0, 1)).toString().toStdString(), "NeutronWeights");
     EXPECT_EQ(sim.data(sim.index(0, 2)).toInt(), 1);
