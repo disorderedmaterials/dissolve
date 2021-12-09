fdff1a6d56f1c8d0eccb9958a5b735037c37ec6b
Adam Washington <adam.washington@stfc.ac.uk>
2021-12-09 15:39:53 +0000

    Start re-implementing KeywordBase as a variant
    This will eventually allow for a custom delegate

--- src/gui/models/procedureModel.cpp
+++ src/gui/models/procedureModel.cpp
@@ -1,40 +1,67 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Team Dissolve and contributors
 
-#include "procedure/procedure.h"
 #include "gui/models/procedureModel.h"
+#include "keywords/bool.h"
+#include "keywords/integer.h"
+#include "keywords/stdstring.h"
+#include "procedure/procedure.h"
 
+Q_DECLARE_METATYPE(KeywordBase *);
 
 /*
  * QAbstractItemModel overrides
  */
 const quintptr OFFSET = 0x10000;
 
-ProcedureModel::ProcedureModel(Procedure &procedure) : procedure_(procedure) {}
+QString KeywordBaseToString(KeywordBase *k)
+{
+    if (BoolKeyword *b = dynamic_cast<BoolKeyword *>(k))
+    {
+        return b->data() ? "True" : "False";
+    }
+    if (IntegerKeyword *i = dynamic_cast<IntegerKeyword *>(k))
+    {
+        return QString::fromStdString(fmt::format("{}", i->data()));
+    }
+    if (StringKeyword *s = dynamic_cast<StringKeyword *>(k))
+    {
+        return QString::fromStdString(s->data());
+    }
+    return "Unknown Keyword Type:";
+}
 
-QModelIndex ProcedureModel::index(int row, int column, const QModelIndex& parent) const {
+ProcedureModel::ProcedureModel(Procedure &procedure) : procedure_(procedure)
+{
+    QMetaType::registerConverter<KeywordBase *, QString>(KeywordBaseToString);
+}
+
+QModelIndex ProcedureModel::index(int row, int column, const QModelIndex &parent) const
+{
     quintptr child;
     if (!parent.isValid())
-      child = 0;
+        child = 0;
     else if (!parent.parent().isValid())
         child = parent.row() + 1;
     else
-      child = OFFSET * (parent.row() + 1) + parent.parent().row() + 1;
+        child = OFFSET * (parent.row() + 1) + parent.parent().row() + 1;
 
     return createIndex(row, column, child);
 }
 
-QModelIndex ProcedureModel::parent(const QModelIndex& index) const {
-  quintptr root = 0;
-  if (index.internalId() == 0)
+QModelIndex ProcedureModel::parent(const QModelIndex &index) const
+{
+    quintptr root = 0;
+    if (index.internalId() == 0)
         return {};
-  if (index.internalId() < OFFSET)
-    return createIndex(index.internalId() - 1, 0, root);
-  return createIndex(index.internalId() / OFFSET - 1, 0, index.internalId() % OFFSET);
+    if (index.internalId() < OFFSET)
+        return createIndex(index.internalId() - 1, 0, root);
+    return createIndex(index.internalId() / OFFSET - 1, 0, index.internalId() % OFFSET);
 }
 
-bool ProcedureModel::hasChildren(const QModelIndex &parent) const {
-  return !parent.internalId() || !parent.parent().internalId();
+bool ProcedureModel::hasChildren(const QModelIndex &parent) const
+{
+    return !parent.internalId() || !parent.parent().internalId();
 }
 
 int ProcedureModel::columnCount(const QModelIndex &parent) const
@@ -48,10 +75,11 @@ int ProcedureModel::rowCount(const QModelIndex &parent) const
     Q_UNUSED(parent);
     auto nodes = procedure_.nodes();
     if (!parent.isValid())
-      return nodes.size();
-    if (!parent.parent().isValid()) {
-      auto groups = nodes[parent.row()]->keywords().displayGroups();
-      return groups.size();
+        return nodes.size();
+    if (!parent.parent().isValid())
+    {
+        auto groups = nodes[parent.row()]->keywords().displayGroups();
+        return groups.size();
     }
     auto groups = nodes[parent.parent().row()]->keywords().displayGroups();
     return groups[parent.row()].second.size();
@@ -59,49 +87,53 @@ int ProcedureModel::rowCount(const QModelIndex &parent) const
 
 QVariant ProcedureModel::data(const QModelIndex &index, int role) const
 {
-  if (role != Qt::DisplayRole) return {};
-  auto nodes = procedure_.nodes();
-  if (!index.parent().isValid()) {
-    switch (index.column()) {
-    case 0:
-      return QString::fromStdString(std::string(nodes[index.row()]->name()));
-    default: return {};
+    if (role != Qt::DisplayRole)
+        return {};
+    auto nodes = procedure_.nodes();
+    if (!index.parent().isValid())
+    {
+        switch (index.column())
+        {
+            case 0:
+                return QString::fromStdString(std::string(nodes[index.row()]->name()));
+            default:
+                return {};
+        }
     }
-  }
-  else if (!index.parent().parent().isValid()) {
-    auto groups = nodes[index.parent().row()]->keywords().displayGroups();
+    else if (!index.parent().parent().isValid())
+    {
+        auto groups = nodes[index.parent().row()]->keywords().displayGroups();
 
-    switch (index.column()) {
-    case 0:
-    return QString::fromStdString(std::string(groups[index.row()].first));
-    default: return {};
+        switch (index.column())
+        {
+            case 0:
+                return QString::fromStdString(std::string(groups[index.row()].first));
+            default:
+                return {};
+        }
     }
-  }
-  else {
-    auto groups = nodes[index.parent().parent().row()]->keywords().displayGroups();
-    auto keyword = groups[index.parent().row()].second[index.row()];
+    else
+    {
+        auto groups = nodes[index.parent().parent().row()]->keywords().displayGroups();
+        auto keyword = groups[index.parent().row()].second[index.row()];
 
-    switch (index.column()) {
-    case 0:
-    return QString::fromStdString(std::string(keyword->name()));
-    case 1: return QString::fromStdString(keyword->toString());
-    default: return {};
+        switch (index.column())
+        {
+            case 0:
+                return QString::fromStdString(std::string(keyword->name()));
+            case 1:
+                return QVariant::fromValue(keyword);
+            default:
+                return {};
+        }
     }
-  }
-
 
     return {};
 }
 
-bool ProcedureModel::setData(const QModelIndex &index, const QVariant &value, int role)
-{
-    return false;
-}
+bool ProcedureModel::setData(const QModelIndex &index, const QVariant &value, int role) { return false; }
 
-Qt::ItemFlags ProcedureModel::flags(const QModelIndex &index) const
-{
-    return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
-}
+Qt::ItemFlags ProcedureModel::flags(const QModelIndex &index) const { return Qt::ItemIsSelectable | Qt::ItemIsEnabled; }
 
 QVariant ProcedureModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
@@ -113,8 +145,8 @@ QVariant ProcedureModel::headerData(int section, Qt::Orientation orientation, in
         {
             case 0:
                 return "Name";
-	    case 1:
-	        return "Type";
+            case 1:
+                return "Type";
             default:
                 return {};
         }
