336f612731dd227e340d2aa11cae64d41731eb34
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2022-01-14 14:16:43 +0000

    Namespace SVD, add unit test.

--- src/math/svd.h
+++ src/math/svd.h
@@ -7,14 +7,11 @@
 template <class A> class Array2D;
 
 // Single Value Decomposition
-class SVD
+namespace SVD
 {
-    public:
-    // Perform single value decomposition of the supplied matrix A, putting left-orthogonal (U), diagonal single-value (S),
-    // and right-orthogonal (V) matrices into the supplied Arrays
-    static bool decompose(const Array2D<double> &A, Array2D<double> &U, Array2D<double> &S, Array2D<double> &Vt);
-    // Test SVD
-    static void test();
-    // Compute in-place pseudoinverse of supplied matrix
-    static bool pseudoinverse(Array2D<double> &A);
-};
+// Perform single value decomposition of the supplied matrix A, putting left-orthogonal (U), diagonal single-value (S),
+// and right-orthogonal (V) matrices into the supplied Arrays
+bool decompose(const Array2D<double> &A, Array2D<double> &U, Array2D<double> &S, Array2D<double> &Vt);
+// Compute in-place pseudoinverse of supplied matrix
+bool pseudoinverse(Array2D<double> &A);
+}; // namespace SVD
