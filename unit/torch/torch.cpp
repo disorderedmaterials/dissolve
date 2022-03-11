f1a45669c72dea69e6bf4095da352e183fbe6815
Adam Washington <adam.washington@stfc.ac.uk>
2022-03-11 12:49:54 +0000

    Start adding pairwise test

--- unit/torch/torch.cpp
+++ unit/torch/torch.cpp
@@ -23,4 +23,25 @@ TEST(TorchTest, Torch)
     auto it = result.data_ptr<double>();
     EXPECT_TRUE(std::all_of(it, it + result.size(0), [](auto x) { return std::abs(x - 1.0) < 1e-8; }));
 }
+
+TEST(TorchTest, Pairs)
+{
+    std::vector<double> rs = {1, 0, 0, 1, 1, 0, 0, 1, 0, 2, 1, 0};
+    // std::vector<double> idx(cfg->nAtoms());
+    // auto it = rs.begin();
+    // auto jt = idx.begin();
+    // for (auto &at : cfg->atoms())
+    // {
+    //	*it++ = at.r().x;
+    //	*it++ = at.r().y;
+    //	*it++ = at.r().z;
+    // *jt++ = at.localTypeIndex();
+    // }
+
+    auto opts = torch::TensorOptions().dtype(torch::kFloat64);
+    auto r = torch::from_blob(rs.data(), {4, 3}, opts);
+    auto comb = torch::nn::functional::pdist(r).reshape({3, 2});
+    EXPECT_EQ(comb.size(0), 3);
+    EXPECT_EQ(comb.size(1), 2);
+}
 } // namespace UnitTest
