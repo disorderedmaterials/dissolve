ca962a2d55ae68cb7ce1cbc69b1ceaea724b8a27
Adam Washington <adam.washington@stfc.ac.uk>
2022-03-11 14:25:59 +0000

    Full histogram calculation in Torch

--- unit/torch/torch.cpp
+++ unit/torch/torch.cpp
@@ -27,6 +27,7 @@ TEST(TorchTest, Torch)
 TEST(TorchTest, Pairs)
 {
     std::vector<double> rs = {1, 0, 0, 1, 1, 0, 0, 1, 0, 3, 1, 0};
+    std::vector<char> types = {1, 1, 1, 2};
     int N = rs.size() / 3;
     // std::vector<double> idx(cfg->nAtoms());
     // auto it = rs.begin();
@@ -42,9 +43,24 @@ TEST(TorchTest, Pairs)
     auto opts = torch::TensorOptions().dtype(torch::kFloat64);
     auto r = torch::from_blob(rs.data(), {N, 3}, opts);
     // std::cout << r << std::endl;
-    auto comb = at::cdist(r, r);
+    auto dist_matrix = at::cdist(r, r);
     // std::cout << comb << std::endl;
-    EXPECT_EQ(comb.size(0), N);
-    EXPECT_EQ(comb.size(0), N);
+
+    auto typeIdx = torch::from_blob(types.data(), {N}, opts.dtype(torch::kInt8)).repeat({N, 1});
+    auto typeJdx = typeIdx.transpose(0, 1);
+    std::cout << typeIdx << std::endl;
+    std::cout << typeJdx << std::endl;
+
+    EXPECT_EQ(dist_matrix.size(0), N);
+    EXPECT_EQ(dist_matrix.size(0), N - 1);
+    for (char j = 1; j < 3; j++)
+	for (char i = j; i < 3; i++)
+	{
+	    auto tagged = dist_matrix.where((typeIdx == i) & (typeJdx == j), torch::zeros({N, N}, opts));
+	    auto [counts, edges] = torch::masked_select(tagged, tagged != 0).histogram(3);
+	    std::cout << tagged << std::endl;
+	    std::cout << counts << std::endl;
+	    std::cout << edges << std::endl;
+	}
 }
 } // namespace UnitTest
