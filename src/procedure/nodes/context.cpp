ceae53f7c3dee46c706daae4538a1d991b4783d7
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2022-03-18 15:49:28 +0000

    Add optional PotentialMap to context.

--- src/procedure/nodes/context.cpp
+++ src/procedure/nodes/context.cpp
@@ -4,8 +4,15 @@
 #include "procedure/nodes/context.h"
 #include <stdexcept>
 
+ProcedureContext::ProcedureContext(const ProcessPool &procPool) : processPool_(procPool) {}
+
 ProcedureContext::ProcedureContext(const ProcessPool &procPool, Configuration *cfg)
     : processPool_(procPool), configuration_(cfg)
+{
+}
+
+ProcedureContext::ProcedureContext(const ProcessPool &procPool, const PotentialMap &potentialMap)
+    : processPool_(procPool), potentialMap_(potentialMap)
 {
 }
 
@@ -39,4 +46,15 @@ GenericList &ProcedureContext::dataList() const
     if (!dataList_)
         throw(std::runtime_error("No data list set in this procedure's context.\n"));
     return dataList_->get();
+}
+
+// Set potential map
+void ProcedureContext::setPotentialMap(const PotentialMap &potentialMap) { potentialMap_ = potentialMap; }
+
+// Return potential map
+const PotentialMap &ProcedureContext::potentialMap() const
+{
+    if (!potentialMap_)
+        throw(std::runtime_error("No potential map was set in this procedure's context.\n"));
+    return potentialMap_->get();
 }
