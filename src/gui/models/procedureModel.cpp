a7d8500fec79c8dd2c474393208a3740c5098708
Adam Washington <adam.washington@stfc.ac.uk>
2022-04-27 17:27:49 +0100

    clang-format procedureModel.cpp

--- src/gui/models/procedureModel.cpp
+++ src/gui/models/procedureModel.cpp
@@ -36,13 +36,15 @@ QVariant ProcedureModel::data(const QModelIndex &index, int role) const
             if (node->name().empty())
                 return QString::fromStdString(std::string(node->nodeTypes().keyword(node->type())));
             else
-                return QString("%1 (%2)").arg(QString::fromStdString(std::string(ProcedureNode::nodeTypes().keyword(node->type()))),
-                                              QString::fromStdString(std::string(node->name())));
+                return QString("%1 (%2)").arg(
+                    QString::fromStdString(std::string(ProcedureNode::nodeTypes().keyword(node->type()))),
+                    QString::fromStdString(std::string(node->name())));
         case Qt::UserRole:
             return QVariant::fromValue(node->shared_from_this());
-         case Qt::DecorationRole:
-             return QIcon((QPixmap(
-                 QString(":/nodes/icons/nodes_%1.svg").arg(QString::fromStdString(std::string(ProcedureNode::nodeTypes().keyword(node->type()))).toLower()))));
+        case Qt::DecorationRole:
+            return QIcon((QPixmap(
+                QString(":/nodes/icons/nodes_%1.svg")
+                    .arg(QString::fromStdString(std::string(ProcedureNode::nodeTypes().keyword(node->type()))).toLower()))));
         default:
             return {};
     }
