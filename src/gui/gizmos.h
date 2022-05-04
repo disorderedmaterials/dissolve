8e9cd9862cbdbebd4327a1f4c7a083b98274e020
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2022-05-04 20:49:32 +0100

    Remove Gizmos.

--- src/gui/gizmos.h
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2022 Team Dissolve and contributors
-
-#pragma once
-
-/* Include file for all Gizmo types */
-#include "gui/graphgizmo.h"
-#include "gui/integrator1dgizmo.h"
