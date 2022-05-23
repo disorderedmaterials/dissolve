df0f46ba36d404b86fc385afeda826e6ac6dabea
Adam Washington <adam.washington@stfc.ac.uk>
2022-05-23 11:11:19 +0000

    Update src/classes/species.cpp
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>

--- src/classes/species.cpp
+++ src/classes/species.cpp
@@ -6,7 +6,7 @@
 #include "data/ff/ff.h"
 #include "data/isotopes.h"
 
-Species::Species() : Species(std::string("Natural")) {}
+Species::Species() : Species(std::string("Unnamed")) {}
 Species::Species(std::string name) : attachedAtomListsGenerated_(false), forcefield_(nullptr), name_(name)
 {
     box_ = std::make_unique<SingleImageBox>();
