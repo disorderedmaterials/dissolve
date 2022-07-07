847d24a5920500aee086da0bab599e564c5ffec1
trisyoungs <tristan.youngs@stfc.ac.uk>
2022-07-07 09:16:28 +0100

    Groundwork.

---
+++ src/gui/addconfigurationdialog.h
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2022 Team Dissolve and contributors
+
+#pragma once
+
+#include "gui/ui_addconfigurationdialog.h"
+#include "gui/wizard.hui"
+#include "main/dissolve.h"
+
+// Add Configuration Dialog
+class AddConfigurationDialog : public WizardDialog
+{
+    Q_OBJECT
+
+    public:
+    AddConfigurationDialog(QWidget *parent, Dissolve &dissolve);
+    ~AddConfigurationDialog() = default;
+
+    private:
+    // Main form declaration
+    Ui::AddConfigurationDialog ui_;
+
+    /*
+     * Data
+     */
+    private:
+    // Main instance of Dissolve that we're using as a reference
+    Dissolve &dissolve_;
+
+    /*
+     * Wizard
+     */
+    private:
+    // Pages Enum
+    enum WizardPage
+    {
+        SelectForcefieldPage, /* Select Forcefield to apply to Species */
+        AnotherPage
+    };
+
+    protected:
+    // Return whether progression to the next page from the current page is allowed
+    bool progressionAllowed(int index) const override;
+    // Perform any necessary actions before moving to the next page
+    bool prepareForNextPage(int currentIndex) override;
+    // Determine next page for the current page, based on current data
+    std::optional<int> determineNextPage(int currentIndex) override;
+    // Perform any necessary actions before moving to the previous page
+    bool prepareForPreviousPage(int currentIndex) override;
+    // Perform any final actions before the wizard is closed
+    void finalise() override;
+
+    /*
+     * Select Forcefield Page
+     */
+    private slots:
+    void on_ForcefieldWidget_forcefieldSelectionChanged(bool isValid);
+    void on_ForcefieldWidget_forcefieldDoubleClicked();
+};
---
+++ src/gui/addconfigurationdialog_funcs.cpp
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2022 Team Dissolve and contributors
+
+#include "classes/configuration.h"
+#include "gui/addconfigurationdialog.h"
+#include "templates/algorithms.h"
+#include <QInputDialog>
+#include <QMessageBox>
+
+AddConfigurationDialog::AddConfigurationDialog(QWidget *parent, Dissolve &dissolve) : WizardDialog(parent), dissolve_(dissolve)
+{
+    ui_.setupUi(this);
+
+    // Register pages with the wizard
+    registerPage(AddConfigurationDialog::SelectForcefieldPage, "Select Forcefield", AddConfigurationDialog::AnotherPage);
+    initialise(this, ui_.MainStack, AddConfigurationDialog::SelectForcefieldPage);
+}
+
+/*
+ * Controls
+ */
+
+// Return whether progression to the next page from the current page is allowed
+bool AddConfigurationDialog::progressionAllowed(int index) const
+{
+    if (index == AddConfigurationDialog::SelectForcefieldPage)
+    {
+        if (ui_.SpeciesWidget->nSelected() == 0 ||
+            std::count_if(ui_.SpeciesWidget->currentSpecies().begin(), ui_.SpeciesWidget->currentSpecies().end(),
+                          [](const auto *sp) { return sp->box()->type() != Box::BoxType::NonPeriodic; }) > 1)
+            return false;
+    }
+
+    return true;
+}
+
+// Perform any necessary actions before moving to the next page
+bool AddConfigurationDialog::prepareForNextPage(int currentIndex) { return true; }
+
+// Determine next page for the current page, based on current data
+std::optional<int> AddConfigurationDialog::determineNextPage(int currentIndex) { return std::nullopt; }
+
+// Perform any necessary actions before moving to the previous page
+bool AddConfigurationDialog::prepareForPreviousPage(int currentIndex) { return true; }
+
+// Perform any final actions before the wizard is closed
+void AddConfigurationDialog::finalise() {}
+
+/*
+ * Select Forcefield Page
+ */
+
+void AddConfigurationDialog::on_ForcefieldWidget_forcefieldSelectionChanged(bool isValid) {}
+
+void AddConfigurationDialog::on_ForcefieldWidget_forcefieldDoubleClicked() {}
