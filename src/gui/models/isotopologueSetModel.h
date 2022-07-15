7a2fd01ca7d32fe9d929307c3855e46a9bd459c8
trisyoungs <tristan.youngs@stfc.ac.uk>
2022-07-15 10:27:27 +0100

    Tidy isotopologue model.

--- src/gui/models/isotopologueSetModel.h
+++ src/gui/models/isotopologueSetModel.h
@@ -18,26 +18,21 @@ class IsotopologueSetModel : public QAbstractItemModel
     public:
     IsotopologueSetModel(OptionalReferenceWrapper<IsotopologueSet> set = std::nullopt);
     void setSourceData(IsotopologueSet &set);
-
-    QModelIndex parent(const QModelIndex &index) const override;
-
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
-
-    bool hasChildren(const QModelIndex &parent) const override;
-
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
-
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
-
     std::vector<QModelIndex> addMissingSpecies(const std::vector<std::unique_ptr<Species>> &species);
     void addIsotopologueWeight(const QModelIndex index);
     void removeIndex(const QModelIndex index);
+
+    /*
+     * QAbstractItemModel overrides
+     */
+    public:
+    QModelIndex parent(const QModelIndex &index) const override;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
+    bool hasChildren(const QModelIndex &parent) const override;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
 };
