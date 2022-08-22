65701dc51503781576817600f8f7acf3067672da
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2022-08-22 21:56:26 +0100

    Add unit tests for OptionalIntegerKeyword. (#1193)

---
+++ unit/gui/optionalIntegerKeyword.cpp
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2022 Team Dissolve and contributors
+
+#include "classes/coredata.h"
+#include "gui/keywordwidgets/optionalint.hui"
+#include <QApplication>
+#include <gtest/gtest.h>
+
+namespace UnitTest
+{
+
+TEST(OptionalIntegerTest, Widget)
+{
+    char arg0[] = "OptionalIntegerTest";
+    char arg1[] = "-platform";
+    char arg2[] = "offscreen";
+    char *argv[] = {arg0, arg1, arg2};
+    int argc{3};
+    QApplication app(argc, argv);
+
+    // Minimum at -50, maximum at 50
+    std::optional<int> value_;
+    OptionalIntegerKeyword keyword(value_, -50, 50, 1, "Null Text");
+    CoreData coreData_;
+    OptionalIntegerKeywordWidget widget(nullptr, &keyword, coreData_);
+
+    // Null value_ to start with
+    EXPECT_TRUE(!value_);
+    // Step the spinbox up
+    widget.stepBy(10);
+    EXPECT_EQ(value_.value(), -40);
+    // Direct set
+    widget.setValue(12);
+    EXPECT_EQ(value_.value(), 12);
+    // Step the spinbox down
+    widget.stepBy(-10);
+    EXPECT_EQ(value_.value(), 2);
+    // Up to above the maximum
+    widget.stepBy(1e3);
+    EXPECT_EQ(value_.value(), 50);
+    // And back down, far enough to hit the minimum and nullify the value
+    widget.stepBy(-1e6);
+    EXPECT_TRUE(!value_);
+    // Check the text in the spinbox
+    EXPECT_STREQ("Null Text", qPrintable(widget.text()));
+}
+
+} // namespace UnitTest
---
+++ unit/keywords/optionalInteger.cpp
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2022 Team Dissolve and contributors
+
+#include "keywords/optionalint.h"
+#include <gtest/gtest.h>
+
+namespace UnitTest
+{
+TEST(OptionalIntegerTest, Standard)
+{
+    // Minimum at zero, no maximum
+    std::optional<int> value_;
+    OptionalIntegerKeyword keyword(value_, 0, std::nullopt, 1, "Null Text");
+
+    // Null value_ to start with
+    EXPECT_TRUE(!value_);
+    // Set an actual value
+    EXPECT_TRUE(keyword.setData(1));
+    EXPECT_TRUE(value_);
+    EXPECT_EQ(value_.value(), 1);
+    // Set back to the value indicating null
+    EXPECT_TRUE(keyword.setData(0));
+    EXPECT_TRUE(!value_);
+    // Set below the minimum value indicating null
+    EXPECT_TRUE(keyword.setData(-100));
+    EXPECT_TRUE(!value_);
+}
+
+TEST(OptionalIntegerTest, Clamped)
+{
+    // Minimum at 10, maximum at 100
+    std::optional<int> value_;
+    OptionalIntegerKeyword keyword(value_, 10, 100, 1, "Null Text");
+
+    // Null value_ to start with
+    EXPECT_TRUE(!value_);
+    // Set the value representing null
+    EXPECT_TRUE(keyword.setData(10));
+    EXPECT_TRUE(!value_);
+    // Set an actual value
+    EXPECT_TRUE(keyword.setData(50));
+    EXPECT_TRUE(value_);
+    EXPECT_EQ(value_.value(), 50);
+    // Set the maximum value
+    EXPECT_TRUE(keyword.setData(100));
+    EXPECT_TRUE(value_);
+    EXPECT_EQ(value_.value(), 100);
+    // Set above the maximum value (fail)
+    EXPECT_FALSE(keyword.setData(105));
+    EXPECT_TRUE(value_);
+    EXPECT_EQ(value_.value(), 100);
+    // Set below the minimum value indicating null
+    EXPECT_TRUE(keyword.setData(-100));
+    EXPECT_TRUE(!value_);
+}
+
+} // namespace UnitTest
