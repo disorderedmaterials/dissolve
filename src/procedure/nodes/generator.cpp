437032fda01d85d88cbd83859e30a09e5da429e6
Adam Washington <adam.washington@stfc.ac.uk>
2022-10-05 17:25:18 +0100

    Handle generating parameters node

--- src/procedure/nodes/generator.cpp
+++ src/procedure/nodes/generator.cpp
@@ -1,6 +1,7 @@
 #include "procedure/nodes/generator.h"
 #include "procedure/nodes/add.h"
 #include "procedure/nodes/box.h"
+#include "procedure/nodes/parameters.h"
 
 NodeRef nodeGenerator(ProcedureNode::NodeType type, const SerialisedValue &node, const CoreData &data)
 {
@@ -12,6 +13,9 @@ NodeRef nodeGenerator(ProcedureNode::NodeType type, const SerialisedValue &node,
             break;
         case ProcedureNode::NodeType::Box:
             result = std::make_shared<BoxProcedureNode>();
+            break;
+        case ProcedureNode::NodeType::Parameters:
+            result = std::make_shared<ParametersProcedureNode>();
             break;
     }
     result->deserialise(node, data);
