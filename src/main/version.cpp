3764285db39b642d33e16449b04a86fb580e6aad
trisyoungs <tristan.youngs@stfc.ac.uk>
2022-11-29 20:49:50 +0000

    Bump version.

--- src/main/version.cpp
+++ src/main/version.cpp
@@ -4,7 +4,7 @@
 #include "main/version.h"
 #include <fmt/format.h>
 
-#define DISSOLVEVERSION "0.9.10"
+#define DISSOLVEVERSION "0.9.11"
 #define DISSOLVESHORTHASH ""
 #define DISSOLVEREPO "https://github.com/disorderedmaterials/dissolve.git"
 
