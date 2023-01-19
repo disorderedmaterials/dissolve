e1cf77910a6098cc29aa922134dbe58520c6c11e
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-01-19 14:26:23 +0000

    Use box definition for vector calculation.

--- src/modules/forces/functions.cpp
+++ src/modules/forces/functions.cpp
@@ -288,6 +288,7 @@ void ForcesModule::totalForces(const ProcessPool &procPool, const Species *sp, c
     std::fill(f.begin(), f.end(), Vec3<double>());
 
     double rij, magjisq;
+    auto *box = sp->box();
     const auto cutoffSq = potentialMap.range() * potentialMap.range();
     Vec3<double> vecij;
     // NOTE PR #334 : use for_each_pair
@@ -305,7 +306,7 @@ void ForcesModule::totalForces(const ProcessPool &procPool, const Species *sp, c
                 continue;
 
             // Determine final forces
-            vecij = r[indexJ] - r[indexI];
+            vecij = box->minimumVector(r[indexI], r[indexJ]);
             magjisq = vecij.magnitudeSq();
             if (magjisq > cutoffSq)
                 continue;
