999a8f3ed91cd4246a57212b30003e2359d235f0
Adam Washington <adam.washington@stfc.ac.uk>
2023-01-23 16:49:55 +0000

    Move robuest benchmark script

--- scripts/linux-benchmark.sh
+++ scripts/linux-benchmark.sh
@@ -3,10 +3,15 @@
 # Run this script before benchmarking on a linux CPU to prevent CPU
 # frequency scaling that could invalidate the numbers.
 
+if [ "$#" -ne 2 ] || ! [ -x "$1" ]; then
+  echo "Usage: $0 BENCHMARK_EXECUTABLE OUTPUT_FILE" >&2
+  exit 1
+fi
+
 echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
 
-out/build/Release/benchmarks --benchmark_filter=Interpolator --benchmark_out_format=csv --benchmark_out=original.csv
+$1 --benchmark_filter=Interpolator --benchmark_out_format=csv --benchmark_out=$2
 
 echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
