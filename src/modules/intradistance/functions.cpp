35770833e4891f5bb4a327757e93980cfa4e536e
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-03-20 13:09:11 +0000

    Remove unnecessary file.

--- src/modules/intradistance/functions.cpp
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2023 Team Dissolve and contributors
-
-#include "modules/intradistance/intradistance.h"
-#include "procedure/nodes/process1d.h"
-#include "procedure/nodes/sum1d.h"
