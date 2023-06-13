8079e596fc2dd9a060b151afb8cff5d00fcf9f52
Jared Swift <100244619+jws-1@users.noreply.github.com>
2023-06-13 14:30:55 +0100

    Remove duplicated code.
    Co-authored-by: Adam Washington <adam.washington@stfc.ac.uk>

--- src/templates/array3d.h
+++ src/templates/array3d.h
@@ -98,12 +98,7 @@ template <class A> class Array3D
     }
     A &operator[](Array3DIterator it)
     {
-        auto [x, y, z] = *it;
-        assert(x >= 0 && x < nX_);
-        assert(y >= 0 && y < nY_);
-        assert(z >= 0 && z < nZ_);
-
-        return array_[sliceOffsets_[x] + y * nZ_ + z];
+        return (*this)[*it];
     }
     // Return specified element as const-reference
     const A &operator[](std::tuple<int, int, int> index) const
