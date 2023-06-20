e4c76dc5d463f469c4ddf82ab1e896a12cb035ed
Jared Swift <j.w.swift@outlook.com>
2023-06-20 17:09:31 +0100

    Clean and fix comment.

--- src/classes/speciessite.h
+++ src/classes/speciessite.h
@@ -150,16 +150,12 @@ class SpeciesSite : public Serialisable
      */
     private:
     NETADefinition fragment_;
-    // Vector of {origin indices, x indices, y indices} tuples
-    std::vector<std::tuple<std::vector<int>, std::vector<int>, std::vector<int>>> uniqueMatches_; 
-        //std::vector<std::pair<std::vector<const SpeciesAtom*>, const std::set<std::string>>> uniqueMatches_; 
 
     public:
     const NETADefinition &fragment() const;
-    const std::vector<std::tuple<std::vector<int>, std::vector<int>, std::vector<int>>> &uniqueMatches() const;
 
     /*
-     * Advances Sites
+     * Advanced Sites
      */
     private:
     // Species atoms whose average position is the oriin of the site
