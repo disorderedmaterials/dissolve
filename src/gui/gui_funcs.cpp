1ece6c33a80b2569dc57fd89730cddf2fae1debf
Jared Swift <100244619+jws-1@users.noreply.github.com>
2023-06-21 10:15:55 +0100

    Species Charge Handling Part 2 (#1464)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>

--- src/gui/gui_funcs.cpp
+++ src/gui/gui_funcs.cpp
@@ -351,6 +351,7 @@ void DissolveWindow::updateMenus()
     ui_.SpeciesCopyChargesFromAtomTypesAction->setEnabled(activeTab->type() == MainTab::TabType::Species);
     ui_.SpeciesSetAtomTypeChargesFromSpeciesAction->setEnabled(activeTab->type() == MainTab::TabType::Species);
     ui_.SpeciesScaleChargesAction->setEnabled(activeTab->type() == MainTab::TabType::Species);
+    ui_.SpeciesSmoothChargesAction->setEnabled(activeTab->type() == MainTab::TabType::Species);
     ui_.SpeciesReduceChargesSigFigsAction->setEnabled(activeTab->type() == MainTab::TabType::Species);
     ui_.SpeciesRegenerateIntraFromConnectivityAction->setEnabled(activeTab->type() == MainTab::TabType::Species);
 
