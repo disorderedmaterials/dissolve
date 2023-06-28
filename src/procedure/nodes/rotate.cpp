83f1a84615f6e35bb9660510961a3a88f572a163
Jared Swift <j.w.swift@outlook.com>
2023-06-28 16:55:08 +0100

    Allow any site type to be rotated.

--- src/procedure/nodes/rotate.cpp
+++ src/procedure/nodes/rotate.cpp
@@ -22,28 +22,16 @@ RotateProcedureNode::RotateProcedureNode(std::shared_ptr<SelectProcedureNode> si
                                                     true);
     keywords_.add<NodeValueKeyword>("Rotation", "Rotation to perform", rotation_, this);
     keywords_.add<EnumOptionsKeyword<OrientedSite::SiteAxis>>("Axis", "Axis for rotation", axis_, OrientedSite::siteAxis());
-    // keywords_.add<Vec3NodeValueKeyword>("Origin", "Origin to rotate to", origin_, this);
 }
 
 bool RotateProcedureNode::execute(const ProcedureContext &procedureContext)
 {
-    /*
-        Should find all atoms in the fragment originating at this analysis site, and then rotate each atom as specified.
-    */
     auto site = site_->currentSite();
     auto parent = site->parent();
     auto parentIndex = site->uniqueSiteIndex();
     auto molecule = site->molecule();
-    assert(parent);
-    if (parent->type() != SpeciesSite::SiteType::Fragment)
-    {
-        Messenger::warn("Rotation can only be applied to fragment sites, site {} originates from {} which is not a fragment "
-                        "site, so it will be skipped.",
-                        parent->name(), parentIndex);
-        return false;
-    }
+    
     Matrix3 rotationMatrix;
-
     switch (axis_)
     {
         case (OrientedSite::SiteAxis::XAxis):
