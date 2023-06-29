8644fcbab82ccb25652eb77b76e23e4de19c2186
Jared Swift <j.w.swift@outlook.com>
2023-06-29 22:26:09 +0100

    Better test for rotating fragments.

--- unit/procedure/rotatefragment.cpp
+++ unit/procedure/rotatefragment.cpp
@@ -29,33 +29,41 @@ TEST(RotateTest, Benzene)
     site.setFragmentDefinitionString("?C, #origin, ring(C(-H),C(-H),C(-H),C(-H),C(-H),C(-H))");
     std::vector<const SpeciesSite *> sites = {&site};
 
+    // Setup configuration
     auto *cfg = dissolve.addConfiguration();
     auto procedure = cfg->generator();
     cfg->createBoxAndCells({20, 20, 20}, {90, 90, 90}, false, dissolve.pairPotentialRange());
     cfg->cells().generate(cfg->box(), 7.0, dissolve.pairPotentialRange());
 
+    // Add a single Benzene molecule
     auto add = procedure.createRootNode<AddProcedureNode>("Benzene", benzene, 1);
     add->keywords().setEnumeration("Positioning", AddProcedureNode::PositioningType::Central);
 
+    // Select the site and set up a RotateFragment node
     auto select = procedure.createRootNode<SelectProcedureNode>("BenzeneSite", sites);
     auto rotate = procedure.createRootNode<RotateFragmentProcedureNode>("RotateBenzene", select);
 
-    for (auto x = 0.0; x <= 360.0; ++x)
+    // Rotate four times around the x-axis of the site
+    for (auto x = 90.0; x <= 360.0; x += 90.0)
     {
+        auto molecules_before = cfg->molecules();
+
         rotate->keywords().set("Rotation", NodeValue{x});
         cfg->generate(ProcedureContext(ProcessPool(), dissolve.potentialMap()));
-        for (auto &mol : cfg->molecules())
-            for (auto atom : mol->atoms())
-                ASSERT_FLOAT_EQ(atom->r().z, 0.0);
-    }
 
-    for (auto y = 0.0; y <= 360.0; ++y)
-    {
-        rotate->keywords().set("Rotation", NodeValue{y});
-        cfg->generate(ProcedureContext(ProcessPool(), dissolve.potentialMap()));
-        for (auto &mol : cfg->molecules())
-            for (auto atom : mol->atoms())
-                ASSERT_FLOAT_EQ(atom->r().z, 0.0);
+        for (const auto &[mol1, mol2] : zip(molecules_before, cfg->molecules()))
+        {
+            for (const auto &[atom1, atom2] : zip(mol1->atoms(), mol2->atoms()))
+            {
+                // Check that
+                // x' = x
+                // y' = z
+                // z' = -y
+                ASSERT_EQ(atom2->r().x, atom1->r().x);
+                ASSERT_EQ(atom2->r().y, atom2->r().z);
+                ASSERT_EQ(atom1->r().z, -atom2->r().y);
+            }
+        }
     }
 }
 } // namespace UnitTest
