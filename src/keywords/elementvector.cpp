b04575e0fdccb1ce90e71b7d6b44543a1fa75808
Adam Washington <adam.washington@stfc.ac.uk>
2023-06-30 11:55:52 +0100

    Fix whitespace

--- src/keywords/elementvector.cpp
+++ src/keywords/elementvector.cpp
@@ -55,6 +55,5 @@ bool ElementVectorKeyword::serialise(LineParser &parser, std::string_view keywor
                              joinStrings(data_, "  ", [](const auto &el) { return Elements::symbol(el); }));
 }
 
-
 // Express as a serialisable value
 SerialisedValue ElementVectorKeyword::serialise() const { throw std::runtime_error("Cannot serialise ElementVectorKeyword"); }
