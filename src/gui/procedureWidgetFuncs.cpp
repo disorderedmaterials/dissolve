e81b07c150c5f4a136a2457c4aa25899d370be14
Jared Swift <j.w.swift@outlook.com>
2023-07-05 15:41:58 +0100

    Remove unnecessary include

--- src/gui/procedureWidgetFuncs.cpp
+++ src/gui/procedureWidgetFuncs.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2023 Team Dissolve and contributors
 
-#include "base/messenger.h"
 #include "gui/gui.h"
 #include "gui/keywordWidgets/widget.hui"
 #include "gui/nodeControlWidget.h"
