deb20c61fd0a2d13d135393fb855293df6e10069
Jared Swift <j.w.swift@outlook.com>
2023-07-09 13:14:26 +0100

    Fix static site crashes.

--- src/classes/speciesSite.cpp
+++ src/classes/speciesSite.cpp
@@ -70,7 +70,7 @@ SpeciesSite::SiteType SpeciesSite::type() const { return type_; }
 // Return whether the site has defined axes sites
 bool SpeciesSite::hasAxes() const
 {
-    return (type_ == SiteType::Static && !(staticXAxisAtoms_.empty() && staticYAxisAtoms_.empty())) ||
+    return (type_ == SiteType::Static && !(staticXAxisAtoms_.empty() || staticYAxisAtoms_.empty())) ||
            (type_ == SiteType::Fragment && fragment_.hasAxes());
 }
 
