7bc4da260f0dbe3f4677db25ee472f9306a1c2ba
Jared Swift <j.w.swift@outlook.com>
2023-07-10 11:04:14 +0100

    Format

--- src/base/sysFunc.cpp
+++ src/base/sysFunc.cpp
@@ -295,7 +295,7 @@ bool DissolveSys::isNumber(std::string_view text, bool &isFloatingPoint)
                 break;
         }
     }
-    
+
     isFloatingPoint &= nDigits > 0;
     return nDigits > 0;
 }
