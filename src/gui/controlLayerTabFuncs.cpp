fabd7ae887ff0c106bc934e91ac94602e583337c
Jared Swift <j.w.swift@outlook.com>
2023-07-24 16:02:13 +0100

    Revert "Get rid of control layer."
    This reverts commit 72c48d2a534d0332e6b5820b10963b70009770eb.

---
+++ src/gui/controlLayerTabFuncs.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#include "gui/controlLayerTab.h"
+#include "gui/mainTab.h"
+
+ControlLayerTab::ControlLayerTab(DissolveWindow *dissolveWindow, Dissolve& dissolve, MainTabsWidget *parent, const QString title) : MainTab(dissolveWindow, dissolve, parent, QString("Control"), this){}
+
+MainTab::TabType ControlLayerTab::type() const { return MainTab::TabType::ControlLayer; }
+bool ControlLayerTab::canClose() const {return false;}
+void ControlLayerTab::updateControls() {}
+void ControlLayerTab::preventEditing() {}
+void ControlLayerTab::allowEditing() {}
+
+void ControlLayerTab::on_RunButton_clicked(bool checked) {}
