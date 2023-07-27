1840601bb8437d6c65adee6abbc3684637264925
Jared Swift <100244619+jws-1@users.noreply.github.com>
2023-07-27 15:19:33 +0100

    Filter Node Tree (#1574)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>

---
+++ src/gui/models/nodePaletteFilterProxy.cpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#include "gui/models/nodePaletteFilterProxy.h"
+#include "procedure/nodes/registry.h"
+
+// Set current context
+void NodePaletteFilterProxy::setContext(ProcedureNode::NodeContext context)
+{
+    context_ = context;
+    invalidateFilter();
+}
+
+/*
+ * QSortFilterProxyModel overrides
+ */
+
+bool NodePaletteFilterProxy::filterAcceptsRow(int row, const QModelIndex &parent) const
+{
+    auto category = std::next(ProcedureNodeRegistry::categoryMap().begin(), parent.isValid() ? parent.row() : row)->first;
+
+    switch (context_)
+    {
+        case (ProcedureNode::NodeContext::AnyContext):
+            return true;
+        case (ProcedureNode::NodeContext::NoContext):
+            return false;
+        case (ProcedureNode::NodeContext::OperateContext):
+            return category == "Operate";
+        case (ProcedureNode::NodeContext::GenerationContext):
+            return category != "Operate";
+        case (ProcedureNode::NodeContext::AnalysisContext):
+            return category != "Build" && category != "Potentials" && category != "Operate" && category != "Sites";
+        default:
+            throw("Context '{}' is not handled in the Node Palette.", ProcedureNode::nodeContexts().keyword(context_));
+    }
+}
---
+++ src/gui/models/nodePaletteFilterProxy.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#pragma once
+
+#include "procedure/nodes/node.h"
+#include <QSortFilterProxyModel>
+
+class NodePaletteFilterProxy : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+    private:
+    // Current context
+    ProcedureNode::NodeContext context_{ProcedureNode::NodeContext::NoContext};
+
+    public:
+    // Set current context
+    void setContext(const ProcedureNode::NodeContext context);
+
+    /*
+     * QSortFilterProxyModel overrides
+     */
+    private:
+    bool filterAcceptsRow(int row, const QModelIndex &parent) const;
+};
