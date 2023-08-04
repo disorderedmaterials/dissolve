f53543e1eecea89a648e9fff017b4af934cea5f8
trisyoungs <tristan.youngs@stfc.ac.uk>
2023-08-04 12:34:14 +0100

    Format.

--- src/procedure/nodes/customRegion.h
+++ src/procedure/nodes/customRegion.h
@@ -10,7 +10,8 @@
 class CustomRegionVoxelKernel : public VoxelKernel
 {
     public:
-    explicit CustomRegionVoxelKernel(std::string_view expressionString = "", double minimumValue = 0.0, double maximumValue = 1.0);
+    explicit CustomRegionVoxelKernel(std::string_view expressionString = "", double minimumValue = 0.0,
+                                     double maximumValue = 1.0);
 
     protected:
     // Local variables, set when checking voxels
