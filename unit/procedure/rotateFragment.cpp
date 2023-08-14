a4fd5eefb24e422376e73fc3da5b8aaf654d0868
Jared Swift <j.w.swift@outlook.com>
2023-08-14 15:07:08 +0100

    Use for each branch.

--- unit/procedure/rotateFragment.cpp
+++ unit/procedure/rotateFragment.cpp
@@ -34,7 +34,7 @@ TEST(RotateTest, Benzene)
     // Setup configuration
     auto *cfg = coreData.addConfiguration();
     auto &procedure = cfg->generator();
-    auto box = procedure.createRootNode<BoxProcedureNode>("Box", Vec3<NodeValue>(20, 20 ,20), Vec3<NodeValue>(90, 90, 90));
+    auto box = procedure.createRootNode<BoxProcedureNode>("Box", Vec3<NodeValue>(20, 20, 20), Vec3<NodeValue>(90, 90, 90));
 
     // Add a single Benzene molecule
     auto add = procedure.createRootNode<AddProcedureNode>("Benzene", benzene, 1);
@@ -42,25 +42,25 @@ TEST(RotateTest, Benzene)
     add->keywords().setEnumeration("BoxAction", AddProcedureNode::BoxActionStyle::None);
     add->keywords().set("Rotate", false);
 
-    // Select the site
-    auto select = procedure.createRootNode<SelectProcedureNode>("BenzeneSite", sites);
-
     // Set up the prior configuration
     cfg->generate({ProcessPool(), dissolve});
-
     // Move the molecule to the origin
     auto mol = cfg->molecule(0);
     mol->setCentreOfGeometry(cfg->box(), {0., 0., 0.});
 
     // Atom coordinates prior and posterior to rotations
     std::vector<Vec3<double>> coordinatesBefore(mol->nAtoms()), coordinatesAfter(mol->nAtoms());
-    std::transform(mol->atoms().begin(), mol->atoms().end(), coordinatesBefore.begin(), [&](const auto& at) { return at->r(); });
+    std::transform(mol->atoms().begin(), mol->atoms().end(), coordinatesBefore.begin(),
+                   [&](const auto &at) { return at->r(); });
 
+    // Select the site
+    auto select = procedure.createRootNode<SelectProcedureNode>("BenzeneSite", sites);
     // Rotate Benzene around the Z-Axis of the site (which is defined at the COG, which is at the origin)
-    auto rotate = procedure.createRootNode<RotateFragmentProcedureNode>("RotateBenzene", select);
+    auto& forEachB = select->branch()->get();
+    auto rotate = forEachB.create<RotateFragmentProcedureNode>("RotateBenzene", select);
     rotate->keywords().setEnumeration("Axis", OrientedSite::SiteAxis::ZAxis);
 
-    for (auto x = 180.0; x <= 360.0; x+= 180.0)
+    for (auto x = 180.0; x <= 360.0; x += 180.0)
     {
         // Rotate by 'x' degrees
         rotate->keywords().set("Rotation", NodeValueProxy(x));
@@ -73,7 +73,8 @@ TEST(RotateTest, Benzene)
         mol->setCentreOfGeometry(cfg->box(), {0., 0., 0.});
 
         // Posterior atom coordinates
-        std::transform(mol->atoms().begin(), mol->atoms().end(), coordinatesAfter.begin(), [&](const auto& at) { return at->r(); });
+        std::transform(mol->atoms().begin(), mol->atoms().end(), coordinatesAfter.begin(),
+                       [&](const auto &at) { return at->r(); });
         for (auto &&[r1, r2] : zip(coordinatesBefore, coordinatesAfter))
         {
             // Check that
