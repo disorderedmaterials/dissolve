0a8277bfc394633577ecbfc26057c522dd26e377
Jared Swift <j.w.swift@outlook.com>
2023-08-14 13:56:51 +0100

    Test rotation around the Z-axis.

--- unit/procedure/rotateFragment.cpp
+++ unit/procedure/rotateFragment.cpp
@@ -28,48 +28,64 @@ TEST(RotateTest, Benzene)
     // Set up site
     auto site = SpeciesSite(benzene, "COG", SpeciesSite::SiteType::Fragment);
     site.setFragmentDefinitionString(
-        "?C,#origin, ring(C(#origin, -H),C(#origin, -H),C(#origin, -H),C(#origin,#x, -H),C(#origin,#y, -H),C(#origin,#y, -H))");
+        "?C,#origin, ring(C(#origin, -H),C(#origin,#y, -H),C(#origin,#y, -H),C(#origin,#x, -H),C(#origin, -H),C(#origin, -H))");
     std::vector<const SpeciesSite *> sites = {&site};
 
     // Setup configuration
     auto *cfg = coreData.addConfiguration();
     auto &procedure = cfg->generator();
-    auto box = procedure.createRootNode<BoxProcedureNode>("Box", Vec3<NodeValue>(20, 20, 20), Vec3<NodeValue>(90, 90, 90));
+    auto box = procedure.createRootNode<BoxProcedureNode>("Box", Vec3<NodeValue>(20, 20 ,20), Vec3<NodeValue>(90, 90, 90));
 
     // Add a single Benzene molecule
     auto add = procedure.createRootNode<AddProcedureNode>("Benzene", benzene, 1);
-    add->keywords().setEnumeration("Positioning", AddProcedureNode::PositioningType::Central);
+    add->keywords().setEnumeration("Positioning", AddProcedureNode::PositioningType::Current);
     add->keywords().setEnumeration("BoxAction", AddProcedureNode::BoxActionStyle::None);
-    add->keywords().setEnumeration("Rotate", false);
+    add->keywords().set("Rotate", false);
 
-    // Select the site and set up a RotateFragment node
+    // Select the site
     auto select = procedure.createRootNode<SelectProcedureNode>("BenzeneSite", sites);
-    auto rotate = procedure.createRootNode<RotateFragmentProcedureNode>("RotateBenzene", select);
 
-    // Set up the configuration before the main loop starts
+    // Set up the prior configuration
     cfg->generate(ProcedureContext(ProcessPool(), dissolve.potentialMap()));
 
-    // Rotate four times around the x-axis of the site
-    for (auto x = 90.0; x <= 360.0; x += 90.0)
-    {
-        auto molecules_before = cfg->molecules();
+    // Move the molecule to the origin
+    auto mol = cfg->molecule(0);
+    mol->setCentreOfGeometry(cfg->box(), {0., 0., 0.});
 
+    // Atom coordinates prior and posterior to rotations
+    std::vector<Vec3<double>> coordinatesBefore(mol->nAtoms()), coordinatesAfter(mol->nAtoms());
+    std::transform(mol->atoms().begin(), mol->atoms().end(), coordinatesBefore.begin(), [&](const auto& at) { return at->r(); });
+
+    // Rotate Benzene around the Z-Axis of the site (which is defined at the COG)
+    auto rotate = procedure.createRootNode<RotateFragmentProcedureNode>("RotateBenzene", select);
+    rotate->keywords().setEnumeration("Axis", OrientedSite::SiteAxis::ZAxis);
+
+    for (auto x = 180.0; x <= 360.0; x+= 180.0)
+    {
+        // Rotate by 'x' degrees
         rotate->keywords().set("Rotation", NodeValue{x});
+
+        // Re-generate the configuration
         cfg->generate(ProcedureContext(ProcessPool(), dissolve.potentialMap()));
 
-        for (const auto &[mol1, mol2] : zip(molecules_before, cfg->molecules()))
+        // Move the molecule to the origin
+        mol = cfg->molecule(0);
+        mol->setCentreOfGeometry(cfg->box(), {0., 0., 0.});
+
+        // Posterior atom coordinates
+        std::transform(mol->atoms().begin(), mol->atoms().end(), coordinatesAfter.begin(), [&](const auto& at) { return at->r(); });
+        for (auto &&[r1, r2] : zip(coordinatesBefore, coordinatesAfter))
         {
-            for (const auto &[atom1, atom2] : zip(mol1->atoms(), mol2->atoms()))
-            {
-                // Check that
-                // x' = x
-                // y' = z
-                // z' = -y
-                ASSERT_EQ(atom2->r().x, atom1->r().x);
-                ASSERT_EQ(atom2->r().y, atom2->r().z);
-                ASSERT_EQ(atom1->r().z, -atom2->r().y);
-            }
+            // Check that
+            // x' = -x
+            // y' = -y
+            // z' = z
+            ASSERT_NEAR(r2.x, -r1.x, 1e-5);
+            ASSERT_NEAR(r2.y, -r1.y, 1e-5);
+            ASSERT_EQ(r2.z, r1.z);
+            ASSERT_EQ(r2.z, 0.0);
         }
+        coordinatesBefore = coordinatesAfter;
     }
 }
 } // namespace UnitTest
