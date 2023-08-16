ee0b063a099c8153457c2fd1503ce3ad6b04f89c
trisyoungs <tristan.youngs@stfc.ac.uk>
2023-08-16 20:50:32 +0100

    AvgMol test is now a single file.

--- tests/modules/avgMol.cpp
+++ tests/modules/avgMol.cpp
@@ -7,6 +7,27 @@
 
 namespace UnitTest
 {
+// Water Molecule
+class AvgMolModuleWaterTest : public ::testing::Test
+{
+    protected:
+    DissolveSystemTest systemTest;
+
+    void SetUp() override
+    {
+        ASSERT_NO_THROW(systemTest.setUp("dissolve/avgMol-water.txt"));
+        ASSERT_TRUE(systemTest.iterate(95));
+    }
+};
+
+TEST_F(AvgMolModuleWaterTest, AverageCoordinates)
+{
+    EXPECT_TRUE(systemTest.checkSampledVector("AvgMol01//X", {-0.83305, 0.00016, 0.83305}));
+    EXPECT_TRUE(systemTest.checkSampledVector("AvgMol01//Y", {0.00000, 0.60443, 0.00000}));
+    EXPECT_TRUE(systemTest.checkSampledVector("AvgMol01//Z", {0.00000, 0.00000, 0.00000}));
+}
+
+// BeNDy Molecule
 class AvgMolModuleBendyTest : public ::testing::Test
 {
     protected:
