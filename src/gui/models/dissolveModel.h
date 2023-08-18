e499aa545675af3fc5c73ab4bb0f37748b144e9f
Jared Swift <100244619+jws-1@users.noreply.github.com>
2023-08-18 10:03:18 +0100

    Missing license and pragma.

--- src/gui/models/dissolveModel.h
+++ src/gui/models/dissolveModel.h
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#pragma once
+
 #include "main/dissolve.h"
 #include "templates/optionalRef.h"
 #include <QAbstractTableModel>
