9dc1ed67a9f9207dc89d1c4c3355f9826689a5a0
trisyoungs <tristan.youngs@stfc.ac.uk>
2023-08-22 10:00:50 +0100

    Energyforce 3-5.

--- tests/modules/energy.cpp
+++ tests/modules/energy.cpp
@@ -13,7 +13,11 @@ class EnergyModuleTest : public ::testing::Test
     DissolveSystemTest systemTest;
 };
 
-TEST_F(EnergyModuleTest, Water3000Full)
+/*
+ * Tests against energies calculated with DL_POLY Classic 2.19
+ */
+
+TEST_F(EnergyModuleTest, DLPOLYWater3000Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt"));
     systemTest.setModuleEnabled("Forces01", false);
@@ -28,7 +32,7 @@ TEST_F(EnergyModuleTest, Water3000Full)
     EXPECT_TRUE(systemTest.checkDouble("intramolecular energy", intraEnergy.values().back(), 5584.45180873, 3.5e-3));
 }
 
-TEST_F(EnergyModuleTest, Water3000VanDerWaals)
+TEST_F(EnergyModuleTest, DLPOLYWater3000VanDerWaals)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -46,7 +50,7 @@ TEST_F(EnergyModuleTest, Water3000VanDerWaals)
         systemTest.checkDouble("interatomic van der Waals energy", interEnergy.values().back(), 1770.1666370083758, 6.0e-2));
 }
 
-TEST_F(EnergyModuleTest, Water3000Electrostatics)
+TEST_F(EnergyModuleTest, DLPOLYWater3000Electrostatics)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -69,7 +73,7 @@ TEST_F(EnergyModuleTest, Water3000Electrostatics)
     EXPECT_TRUE(systemTest.checkDouble("straight coulomb energy", interEnergy.values().back(), -34173.942995962962, 1.0e-2));
 }
 
-TEST_F(EnergyModuleTest, Water3000Bound)
+TEST_F(EnergyModuleTest, DLPOLYWater3000Bound)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -85,7 +89,7 @@ TEST_F(EnergyModuleTest, Water3000Bound)
     EXPECT_TRUE(systemTest.checkDouble("bound term", intraEnergy.values().back(), 5584.45180873, 3.5e-3));
 }
 
-TEST_F(EnergyModuleTest, Hexane1Full)
+TEST_F(EnergyModuleTest, DLPOLYHexane1Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane1.txt"));
     systemTest.setModuleEnabled("Forces01", false);
@@ -100,7 +104,7 @@ TEST_F(EnergyModuleTest, Hexane1Full)
     EXPECT_TRUE(systemTest.checkDouble("intramolecular energy", intraEnergy.values().back(), 71.59299, 6.0e-4));
 }
 
-TEST_F(EnergyModuleTest, Hexane2Full)
+TEST_F(EnergyModuleTest, DLPOLYHexane2Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane2.txt"));
     systemTest.setModuleEnabled("Forces01", false);
@@ -115,7 +119,7 @@ TEST_F(EnergyModuleTest, Hexane2Full)
     EXPECT_TRUE(systemTest.checkDouble("intramolecular energy", intraEnergy.values().back(), 130.636420, 6.0e-4));
 }
 
-TEST_F(EnergyModuleTest, Hexane200Full)
+TEST_F(EnergyModuleTest, DLPOLYHexane200Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane200.txt"));
     systemTest.setModuleEnabled("Forces01", false);
@@ -130,7 +134,7 @@ TEST_F(EnergyModuleTest, Hexane200Full)
     EXPECT_TRUE(systemTest.checkDouble("intramolecular energy", intraEnergy.values().back(), 11488.122, 6.0e-4));
 }
 
-TEST_F(EnergyModuleTest, Hexane200Bound)
+TEST_F(EnergyModuleTest, DLPOLYHexane200Bound)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane200.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -148,7 +152,7 @@ TEST_F(EnergyModuleTest, Hexane200Bound)
     EXPECT_TRUE(systemTest.checkDouble("intramolecular energy", intraEnergy.values().back(), 11488.122, 6.0e-4));
 }
 
-TEST_F(EnergyModuleTest, Hexane200Torsions)
+TEST_F(EnergyModuleTest, DLPOLYHexane200Torsions)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane200.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -167,6 +171,139 @@ TEST_F(EnergyModuleTest, Hexane200Torsions)
 
     auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Liquid//Intra");
     EXPECT_TRUE(systemTest.checkDouble("torsion energy", intraEnergy.values().back(), 2173.978, 5.0e-4));
+}
+
+TEST_F(EnergyModuleTest, DLPOLYBenzene181Full)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt"));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Full interatomic energy: -5.911071e+02   # 5.612389E+02 (elec) - 1.334653E+03 (VDW) + 0.182307E+03 (VDW LR correction)
+    auto &interEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Bulk//Inter");
+    EXPECT_TRUE(systemTest.checkDouble("interatomic energy", interEnergy.values().back(), -5.911071e2, 3.5e-2));
+
+    // Full intramolecular energy
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Bulk//Intra");
+    EXPECT_TRUE(systemTest.checkDouble("intramolecular energy", intraEnergy.values().back(), 5.901646e+03, 3.5e-3));
+}
+
+TEST_F(EnergyModuleTest, DLPOLYBenzene181VanDerWaals)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt",
+                                     [](Dissolve &D, CoreData &C)
+                                     {
+                                         D.setAtomTypeChargeSource(true);
+                                         D.setAutomaticChargeSource(false);
+                                         for (auto &b : C.masterBonds())
+                                            b->setInteractionForm(BondFunctions::Form::None);
+                                         for (auto &a : C.masterAngles())
+                                            a->setInteractionForm(AngleFunctions::Form::None);
+                                         for (auto &t : C.masterTorsions())
+                                            t->setInteractionForm(TorsionFunctions::Form::None);
+                                     }));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Full van der Waals Energy: -1.152346e+03   # -1.334653E+03 (full) + 0.182307E+03 (LR correction)
+    auto &interEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Bulk//Inter");
+    EXPECT_TRUE(
+        systemTest.checkDouble("interatomic van der Waals energy", interEnergy.values().back(), -1.152346e+03, 3.5e-2));
+}
+
+TEST_F(EnergyModuleTest, DLPOLYBenzene181Electrostatics)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt",
+                                     [](Dissolve &D, CoreData &C)
+                                     {
+                                         C.atomType(0)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                         C.atomType(1)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                         for (auto &b : C.masterBonds())
+                                             b->setInteractionForm(BondFunctions::Form::None);
+                                         for (auto &a : C.masterAngles())
+                                             a->setInteractionForm(AngleFunctions::Form::None);
+                                         for (auto &t : C.masterTorsions())
+                                             t->setInteractionForm(TorsionFunctions::Form::None);
+                                     }));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+    auto &interEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Bulk//Inter");
+
+    // Shifted coulomb sum
+    EXPECT_TRUE(systemTest.checkDouble("shifted coulomb energy", interEnergy.values().back(), 5.612389E+02, 1.8e-4));
+}
+
+TEST_F(EnergyModuleTest, DLPOLYBenzene181Bound)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt",
+                                     [](Dissolve &D, CoreData &C)
+                                     {
+                                         D.setAtomTypeChargeSource(true);
+                                         C.atomType(0)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                         C.atomType(1)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                     }));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Bulk//Intra");
+
+    EXPECT_TRUE(systemTest.checkDouble("bound term", intraEnergy.values().back(), 5.901646e+03, 3.5e-3));
+}
+
+// Tests against energies calculated with MOSCITO 4.180.
+
+TEST_F(EnergyModuleTest, MoscitoPOETorsions)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-POE.txt"));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Intramolecular energy: 183.4801   # (2.866876 per molecule) * 64 molecules
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//POE//Intra");
+    EXPECT_TRUE(systemTest.checkDouble("torsion energy", intraEnergy.values().back(), 183.4801, 1.0e-2));
+}
+
+TEST_F(EnergyModuleTest, MoscitoPy4OHNTf2Torsions)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py4OH-NTf2.txt"));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Intramolecular energy: 51.050222   # (25.525111 per molecule) * 2 molecules
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Py4OH-NTf2//Intra");
+    EXPECT_TRUE(systemTest.checkDouble("torsion energy", intraEnergy.values().back(), 51.050222, 2.0e-5));
+}
+
+TEST_F(EnergyModuleTest, MoscitoPy4OHNTf2Impropers)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py4OH-NTf2-impropers.txt"));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Intramolecular energy: 0.055228   # (0.027614 per molecule) * 2 molecules
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Py4OH-NTf2//Intra");
+    EXPECT_TRUE(systemTest.checkDouble("improper energy", intraEnergy.values().back(), 0.055228 , 2.0e-5));
+}
+
+TEST_F(EnergyModuleTest, MoscitoPy5NTf2Torsions)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py5-NTf2.txt"));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Intramolecular energy: 39.29711  # (19.648555 per molecule) * 2 molecules
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Py5-NTf2//Intra");
+    EXPECT_TRUE(systemTest.checkDouble("torsion energy", intraEnergy.values().back(), 39.29711, 5.0e-5));
+}
+
+TEST_F(EnergyModuleTest, MoscitoPy5NTf2Impropers)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py5-NTf2-impropers.txt"));
+    systemTest.setModuleEnabled("Forces01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Intramolecular energy: 0.34961  # (0.174805 per molecule) * 2 molecules
+    auto &intraEnergy = systemTest.dissolve().processingModuleData().value<Data1D>("Energy01//Py5-NTf2//Intra");
+    EXPECT_TRUE(systemTest.checkDouble("improper energy", intraEnergy.values().back(), 0.34961, 5.0e-5));
 }
 
 } // namespace UnitTest
--- tests/modules/forces.cpp
+++ tests/modules/forces.cpp
@@ -17,16 +17,22 @@ class ForcesModuleTest : public ::testing::Test
     DissolveSystemTest systemTest;
 };
 
-TEST_F(ForcesModuleTest, Water3000Full)
+/*
+ * Tests against forces calculated with DL_POLY Classic 2.19
+ */
+
+TEST_F(ForcesModuleTest, DLPOLYWater3000Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt"));
     systemTest.setModuleEnabled("Energy01", false);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Bulk//Forces", {"dlpoly/water3000_energyForce/full.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.09);
+    systemTest.checkVec3Vector("Forces01//Bulk//Forces",
+                               {"dlpoly/water3000_energyForce/full.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY},
+                               0.09);
 }
 
-TEST_F(ForcesModuleTest, Water3000VanDerWaals)
+TEST_F(ForcesModuleTest, DLPOLYWater3000VanDerWaals)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -39,10 +45,12 @@ TEST_F(ForcesModuleTest, Water3000VanDerWaals)
     systemTest.setModuleEnabled("Energy01", false);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Bulk//Forces", {"dlpoly/water3000_energyForce/vdw.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 8.0e-2);
+    systemTest.checkVec3Vector("Forces01//Bulk//Forces",
+                               {"dlpoly/water3000_energyForce/vdw.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY},
+                               8.0e-2);
 }
 
-TEST_F(ForcesModuleTest, Water3000Electrostatics)
+TEST_F(ForcesModuleTest, DLPOLYWater3000Electrostatics)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -56,15 +64,19 @@ TEST_F(ForcesModuleTest, Water3000Electrostatics)
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
     // Shifted coulomb sum
-    systemTest.checkVec3Vector("Forces01//Bulk//Forces", {"dlpoly/water3000_energyForce/shifted.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 1.7e-2);
+    systemTest.checkVec3Vector(
+        "Forces01//Bulk//Forces",
+        {"dlpoly/water3000_energyForce/shifted.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 1.7e-2);
 
     // Straight Coulomb sum (no truncation)
     PairPotential::setCoulombTruncationScheme(PairPotential::CoulombTruncationScheme::NoCoulombTruncation);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
-    systemTest.checkVec3Vector("Forces01//Bulk//Forces", {"dlpoly/water3000_energyForce/coulomb.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 5.0e-3);
+    systemTest.checkVec3Vector(
+        "Forces01//Bulk//Forces",
+        {"dlpoly/water3000_energyForce/coulomb.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 5.0e-3);
 }
 
-TEST_F(ForcesModuleTest, Water3000Bound)
+TEST_F(ForcesModuleTest, DLPOLYWater3000Bound)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-water3000.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -76,28 +88,32 @@ TEST_F(ForcesModuleTest, Water3000Bound)
     systemTest.setModuleEnabled("Energy01", false);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Bulk//Forces", {"dlpoly/water3000_energyForce/intra.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 2.5e-2);
+    systemTest.checkVec3Vector("Forces01//Bulk//Forces",
+                               {"dlpoly/water3000_energyForce/intra.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY},
+                               2.5e-2);
 }
 
-TEST_F(ForcesModuleTest, Hexane1Full)
+TEST_F(ForcesModuleTest, DLPOLYHexane1Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane1.txt"));
     systemTest.setModuleEnabled("Energy01", false);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Liquid//Forces", {"dlpoly/hexane1/REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.03);
+    systemTest.checkVec3Vector("Forces01//Liquid//Forces",
+                               {"dlpoly/hexane1/REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.03);
 }
 
-TEST_F(ForcesModuleTest, Hexane2Full)
+TEST_F(ForcesModuleTest, DLPOLYHexane2Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane2.txt"));
     systemTest.setModuleEnabled("Energy01", false);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Liquid//Forces", {"dlpoly/hexane2/REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 3.0e-2);
+    systemTest.checkVec3Vector("Forces01//Liquid//Forces",
+                               {"dlpoly/hexane2/REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 3.0e-2);
 }
 
-TEST_F(ForcesModuleTest, Hexane200Full)
+TEST_F(ForcesModuleTest, DLPOLYHexane200Full)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane200.txt"));
     systemTest.setModuleEnabled("Energy01", false);
@@ -105,10 +121,11 @@ TEST_F(ForcesModuleTest, Hexane200Full)
     forcesModule->keywords().set("TestThreshold", 2.0e-5);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Liquid//Forces", {"dlpoly/hexane200/full.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.2);
+    systemTest.checkVec3Vector("Forces01//Liquid//Forces",
+                               {"dlpoly/hexane200/full.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.2);
 }
 
-TEST_F(ForcesModuleTest, Hexane200Unbound)
+TEST_F(ForcesModuleTest, DLPOLYHexane200Unbound)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane200.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -125,10 +142,11 @@ TEST_F(ForcesModuleTest, Hexane200Unbound)
     forcesModule->keywords().set("TestThreshold", 5.0e-2);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Liquid//Forces", {"dlpoly/hexane200/unbound.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.3);
+    systemTest.checkVec3Vector("Forces01//Liquid//Forces",
+                               {"dlpoly/hexane200/unbound.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 0.3);
 }
 
-TEST_F(ForcesModuleTest, Hexane200Bound)
+TEST_F(ForcesModuleTest, DLPOLYHexane200Bound)
 {
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-hexane200.txt",
                                      [](Dissolve &D, CoreData &C)
@@ -141,7 +159,138 @@ TEST_F(ForcesModuleTest, Hexane200Bound)
     systemTest.setModuleEnabled("Energy01", false);
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    systemTest.checkVec3Vector("Forces01//Liquid//Forces", {"dlpoly/hexane200/bound.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 2.5e-4);
+    systemTest.checkVec3Vector("Forces01//Liquid//Forces",
+                               {"dlpoly/hexane200/bound.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 2.5e-4);
+}
+
+TEST_F(ForcesModuleTest, DLPOLYBenzene181Full)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt"));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector("Forces01//Bulk//Forces",
+                               {"dlpoly/benzene181/benzene181-full.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY},
+                               0.15);
+}
+
+TEST_F(ForcesModuleTest, DLPOLYBenzene181VanDerWaals)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt",
+                                     [](Dissolve &D, CoreData &C)
+                                     {
+                                         D.setAtomTypeChargeSource(true);
+                                         D.setAutomaticChargeSource(false);
+                                         for (auto &b : C.masterBonds())
+                                             b->setInteractionForm(BondFunctions::Form::None);
+                                         for (auto &a : C.masterAngles())
+                                             a->setInteractionForm(AngleFunctions::Form::None);
+                                         for (auto &t : C.masterTorsions())
+                                             t->setInteractionForm(TorsionFunctions::Form::None);
+                                     }));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector("Forces01//Bulk//Forces",
+                               {"dlpoly/benzene181/benzene181-vdw.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY},
+                               0.12);
+}
+
+TEST_F(ForcesModuleTest, DLPOLYBenzene181Electrostatics)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt",
+                                     [](Dissolve &D, CoreData &C)
+                                     {
+                                         C.atomType(0)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                         C.atomType(1)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                         for (auto &b : C.masterBonds())
+                                             b->setInteractionForm(BondFunctions::Form::None);
+                                         for (auto &a : C.masterAngles())
+                                             a->setInteractionForm(AngleFunctions::Form::None);
+                                         for (auto &t : C.masterTorsions())
+                                             t->setInteractionForm(TorsionFunctions::Form::None);
+                                     }));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    // Shifted coulomb sum
+    systemTest.checkVec3Vector(
+        "Forces01//Bulk//Forces",
+        {"dlpoly/benzene181/benzene181-elec.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY}, 3.0e-4);
+}
+
+TEST_F(ForcesModuleTest, DLPOLYBenzene181Bound)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-benzene181.txt",
+                                     [](Dissolve &D, CoreData &C)
+                                     {
+                                         D.setAtomTypeChargeSource(true);
+                                         C.atomType(0)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                         C.atomType(1)->interactionPotential().parseParameters("epsilon=0.0 sigma=0.0");
+                                     }));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector("Forces01//Bulk//Forces",
+                               {"dlpoly/benzene181/benzene181-intra.REVCON", ForceImportFileFormat::ForceImportFormat::DLPOLY},
+                               1.0e-7);
+}
+
+// Tests against energies calculated with MOSCITO 4.180.
+
+TEST_F(ForcesModuleTest, MoscitoPOETorsions)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-POE.txt"));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector("Forces01//POE//Forces",
+                               {"moscito/poe64_torsions/torsions-final.str", ForceImportFileFormat::ForceImportFormat::Moscito},
+                               8.0e-2);
+}
+
+TEST_F(ForcesModuleTest, MoscitoPy4OHNTf2Torsions)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py4OH-NTf2.txt"));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector(
+        "Forces01//Py4OH-NTf2//Forces",
+        {"moscito/py4oh_torsions/py4oh-ntf2-final.str", ForceImportFileFormat::ForceImportFormat::Moscito}, 5.0e-2);
+}
+
+TEST_F(ForcesModuleTest, MoscitoPy4OHNTf2Impropers)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py4OH-NTf2-impropers.txt"));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector(
+        "Forces01//Py4OH-NTf2//Forces",
+        {"moscito/py4oh_impropers/py4oh-ntf2-final.str", ForceImportFileFormat::ForceImportFormat::Moscito}, 5.0e-4);
+}
+
+TEST_F(ForcesModuleTest, MoscitoPy5NTf2Torsions)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py5-NTf2.txt"));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector(
+        "Forces01//Py5-NTf2//Forces",
+        {"moscito/py5_torsions/py5-ntf2-final.str", ForceImportFileFormat::ForceImportFormat::Moscito}, 0.1);
+}
+
+TEST_F(ForcesModuleTest, MoscitoPy5NTf2Impropers)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/energyForce-Py5-NTf2-impropers.txt"));
+    systemTest.setModuleEnabled("Energy01", false);
+    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+
+    systemTest.checkVec3Vector(
+        "Forces01//Py5-NTf2//Forces",
+        {"moscito/py5_impropers/py5-ntf2-final.str", ForceImportFileFormat::ForceImportFormat::Moscito}, 5.0e-3);
 }
 
 } // namespace UnitTest
