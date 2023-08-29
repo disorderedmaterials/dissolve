21b6407f652b77399edcf3da5aaadba852d5814c
rhinoella <noella.spitz@gmail.com>
2023-08-29 22:38:10 +0100

    WIP compare module widget

---
+++ src/modules/compare/gui/compareWidgetFuncs.cpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#include "gui/render/renderableData1D.h"
+#include "main/dissolve.h"
+#include "modules/compare/compare.h"
+#include "modules/compare/gui/compareWidget.h"
+
+CompareModuleWidget::CompareModuleWidget(QWidget *parent, CompareModule *module, Dissolve &dissolve)
+    : ModuleWidget(parent, dissolve), module_(module)
+{
+    // Set up user interface
+    ui_.setupUi(this);
+
+    // Set up the main graph
+    graph_ = ui_.PlotWidget->dataViewer();
+    rangeErrorTable_ = ui_.RangeErrorTable;
+
+    for (auto &[dataA, dataB] : module->compareData1D())
+    {
+        auto dataButton = new QPushButton(this);
+        dataButton->setText(QString::fromStdString(fmt::format("{} vs {}", dataA.get()->tag(), dataB.get()->tag())));
+        ui_.ButtonLayout->addWidget(dataButton);
+    }
+}
+
+/*
+ * UI
+ */
+
+// Update controls within widget
+void CompareModuleWidget::updateControls(const Flags<ModuleWidget::UpdateFlags> &updateFlags)
+{
+    if (updateFlags.isSet(ModuleWidget::RecreateRenderablesFlag) || ui_.PlotWidget->dataViewer()->renderables().empty())
+    {
+    }
+}
