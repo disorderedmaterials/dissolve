01eb8cf0825caddf73d464a99fcb487db03092e9
rhinoella <noella.spitz@gmail.com>
2023-09-06 14:46:12 +0100

    Improved comments for dataSource.h class

--- src/classes/dataSource.h
+++ src/classes/dataSource.h
@@ -32,18 +32,28 @@ class DataSource : public Serialisable<const CoreData &>
     // Return data source type enum
     DataSourceType dataSourceType() const;
 
+    /*
+     * Data
+     */
+
     private:
+    // Type of data being stored
     DataSourceType dataSourceType_;
+    // String to hold internal data tag (if internal)
     std::string internalDataSource_;
+    // File and format of external data (if external)
     std::unique_ptr<FileAndFormat> externalDataSource_;
+    // DataBases pointer to hold data
     std::unique_ptr<DataBase> data_;
 
     public:
+    // Function to add internal data
     void addData(std::string_view internalDataSource)
     {
         dataSourceType_ = Internal;
         internalDataSource_ = internalDataSource;
     }
+    // Overloaded function to add external data
     template <class DataType, class Format> void addData(DataType data, Format &fileAndFormat)
     {
         dataSourceType_ = External;
@@ -51,6 +61,7 @@ class DataSource : public Serialisable<const CoreData &>
         auto tempData = std::make_unique<DataType>(data);
         data_ = std::move(tempData);
     }
+    // Function to source data (only required for internal data sources)
     template <class DataType, class Format> bool sourceData(GenericList &processingModuleData)
     {
         if (!dataExists())
@@ -69,14 +80,14 @@ class DataSource : public Serialisable<const CoreData &>
                     return Messenger::error("No data with tag '{}' exists.\n", internalDataSource_);
                 }
                 Messenger::print("Sourced data '{}'", internalDataSource_);
-                // Fill object with located data
+                // Create unique pointer for data
                 data_ = std::make_unique<DataType>(optData->get());
             }
 
             // Data2D, Data3D
             else
             {
-                // Locate target data from tag and cast to base
+                // Locate target data from tag
                 auto optData = processingModuleData.search<const DataType>(internalDataSource_);
                 if (!optData)
                 {
@@ -84,13 +95,14 @@ class DataSource : public Serialisable<const CoreData &>
                 }
                 Messenger::print("Sourced data '{}'", internalDataSource_);
 
-                // Fill object with located data
+                // Create unique pointer for data
                 data_ = std::make_unique<DataType>(optData->get());
             }
         }
 
         return data_.get() != nullptr ? true : false;
     }
+    // Returns the data casted to the requested type
     template <class DataType> DataType data() const
     {
         assert(dataExists());
