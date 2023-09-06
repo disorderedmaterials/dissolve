0618d51c457befde6586eac5f07e9cf591820845
rhinoella <noella.spitz@gmail.com>
2023-09-06 15:18:10 +0100

    Updated comments

--- src/modules/compare/compare.cpp
+++ src/modules/compare/compare.cpp
@@ -10,12 +10,6 @@ CompareModule::CompareModule() : Module(ModuleTypes::Compare)
     keywords_.setOrganisation("Options");
     keywords_.add<DataSourceKeyword<Data1D, Data1DImportFileFormat>>("Data1D", "Specify one-dimensional test reference data",
                                                                      dataSources_, "EndData1D");
-    /*keywords_.add<DataSourceKeyword< Data2D, std::function<bool(std::vector<Data2D>)>>>(
-        "Data2D", "Specify two-dimensional test reference data", [&](std::vector<Data2D> d) { return addData<Data2D>(d); },
-        "EndData2D");
-    keywords_.add<DataSourceKeyword<Data3D, std::function<bool(std::vector<Data3D>)>>>(
-        "Data3D", "Specify three-dimensional test reference data", [&](std::vector<Data3D> d) { return addData<Data3D>(d); },
-        "EndData3D");*/
     keywords_.add<EnumOptionsKeyword<Error::ErrorType>>("ErrorType", "Type of error calculation to use", errorType_,
                                                         Error::errorTypes());
     keywords_.add<RangeVectorKeyword>("RFactorRanges", "Ranges over which to calculate RFactors", ranges_);
