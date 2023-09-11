e3af4645839c23af4d78d85a27924847c916b6b0
Jared Swift <j.w.swift@outlook.com>
2023-09-11 09:29:18 +0100

    Handle invalid index / id.

--- src/gui/models/dissolveModelImageProvider.cpp
+++ src/gui/models/dissolveModelImageProvider.cpp
@@ -12,11 +12,17 @@ QImage DissolveModelImageProvider::requestImage(const QString &id, QSize * size,
     if (id.startsWith(QString("moduleLayer/")))
     {
         auto parts = id.split(QString("/"));
+        if (parts.size() != 3)
+            return {};
         auto moduleLayerRow = parts[1].toInt();
         auto moduleRow = parts[2].toInt();
         auto moduleLayerIndex = dissolveModel_->moduleLayersModel()->index(moduleLayerRow, 0);
+        if (!moduleLayerIndex.isValid())
+            return {};
         auto moduleLayerModel = dissolveModel_->moduleLayersModel()->data(moduleLayerIndex, Qt::UserRole + 1).value<ModuleLayerModel*>();
         auto moduleIndex = moduleLayerModel->index(moduleRow, 0);
+        if (!moduleIndex.isValid())
+            return {};
         *size = requestedSize;
         return moduleLayerModel->data(moduleIndex, Qt::DecorationRole).value<QIcon>().pixmap(QSize(16, 16)).toImage();;
     }
