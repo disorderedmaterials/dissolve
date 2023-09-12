364df1174ca2b700f85e5f68f4925bbeaa45cee7
Danbr4d <laydb3@nottingham.ac.uk>
2023-09-12 14:07:33 +0100

    its not CN its QSpecies

--- src/modules/qSpecies/qSpecies.cpp
+++ src/modules/qSpecies/qSpecies.cpp
@@ -78,7 +78,7 @@ QSpeciesModule::QSpeciesModule() : Module(ModuleTypes::QSpecies), analyser_(Proc
 
     keywords_.setOrganisation("Options", "Ranges");
     keywords_.add<RangeKeyword>("DistanceRange",
-                                "Distance range (min, max) over which to calculate coordination number from central site",
+                                "Distance range (min, max) over which to calculate Q-Species from central site",
                                 distanceRange_, 0.0, std::nullopt, Vec3Labels::MinMaxDeltaLabels);
     keywords_.setOrganisation("Export");
     keywords_.add<FileAndFormatKeyword>("Export", "File format and file name under which to save calculated QSpecies data",
