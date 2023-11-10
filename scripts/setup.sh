1f85c66e1d1e38df259bbbbeaac7721c3128f103
RobBuchananCompPhys <robertbb269@hotmail.com>
2023-11-10 09:47:14 +0000

    use setup script and create .envrc

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-dos2unix flake.nix
-"/root/.nix-profile/bin/nix" "develop" "--extra-experimental-features" "nix-command" "--extra-experimental-features" "flakes" "/dissolve#"
+echo "use flake" > .envrc
+direnv allow
+"/root/.ni x-profile/bin/nix" "develop" "--extra-experimental-features" "nix-command" "--extra-experimental-features" "flakes" "/dissolve#"
