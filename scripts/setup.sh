958cbefc3b375409e87da18e9ba2ffdc036a16ac
RobBuchananCompPhys <robertbb269@hotmail.com>
2023-11-10 10:00:46 +0000

    removed setup script, ot be replaced with unix formatted script

--- scripts/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "use flake" > .envrc
-direnv allow
-"/root/.ni x-profile/bin/nix" "develop" "--extra-experimental-features" "nix-command" "--extra-experimental-features" "flakes" "/dissolve#"
