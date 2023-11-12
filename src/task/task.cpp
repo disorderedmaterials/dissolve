95e2a9eb800f16507f5cad67626a748620b55d33
Jared Swift <j.w.swift@outlook.com>
2023-11-12 11:49:03 +0000

    Setup Task class.

---
+++ src/task/task.cpp
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#include "task/task.h"
+
+
+// Set name of task
+void Task::setName(std::string_view name)
+{
+    name_ = name;
+}
+
+// Return name of task
+std::string_view Task::name() const
+{
+    return name_;
+}
+
+// Set procedure that the task will execute
+void Task::setProcedure(Procedure procedure)
+{
+    procedure_ = procedure;
+}
+
+
+// Return procedure that the task will execute
+Procedure Task::procedure() const
+{
+    return procedure_;
+}
---
+++ src/task/task.h
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#pragma once
+
+#include "base/serialiser.h"
+#include "classes/coreData.h"
+#include "procedure/procedure.h"
+#include <string>
+
+
+class Task : public Serialisable<const CoreData &>
+{
+    public:
+    Task() = default;
+    ~Task() = default;
+
+    /*
+     * Task Definition
+     */
+    private:
+    // Name of task
+    std::string name_{"Untitled Task"};
+    // Procedure that the task will execute
+    Procedure  procedure_;
+
+
+    public:
+    // Set name of task
+    void setName(std::string_view name);
+    // Return name of task
+    std::string_view name() const;
+    // Set procedure that the task will execute
+    void setProcedure(Procedure procedure);
+    // Return procedure that the task will execute
+    Procedure procedure() const;
+
+};
