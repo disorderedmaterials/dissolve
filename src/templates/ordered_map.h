698f82a1645d88125ba49cc6a72ee2e8c2643a0e
Adam Washington <adam.washington@stfc.ac.uk>
2023-11-24 15:38:37 +0000

    Use a vector pair backend

--- src/templates/ordered_map.h
+++ src/templates/ordered_map.h
@@ -10,38 +10,20 @@
 namespace dissolve
 {
 
-template <typename Key, typename Value> class OrderedMapIterator
-{
-    private:
-    std::map<Key, Value> &values_;
-    std::vector<Key> &keys_;
-    int index_;
-
-    public:
-    OrderedMapIterator(std::map<Key, Value> &values, std::vector<Key> &keys, int index)
-        : values_(values), keys_(keys), index_(index){};
-
-    bool operator==(const OrderedMapIterator &other) const { return index_ == other.index_; }
-    bool operator!=(const OrderedMapIterator &other) const { return index_ != other.index_; }
-    OrderedMapIterator &operator++()
-    {
-        index_++;
-        return *this;
-    }
-    std::tuple<Key, Value> operator*() { return {keys_[index_], values_[keys_[index_]]}; }
-};
-
 template <typename Key, typename Value> class ordered_map
 {
 
     private:
     int counter_ = 0;
-    std::vector<Key> keys_;
-    std::map<Key, Value> values_;
+    std::map<Key, int> index_;
+    std::vector<std::pair<Key, Value>> values_;
     void insistKey_(const Key &key)
     {
-        if (std::find(keys_.begin(), keys_.end(), key) == keys_.end())
-            keys_.push_back(key);
+        if (index_.find(key) == index_.end())
+        {
+            index_[key] = values_.size();
+            values_.emplace_back(key, Value{});
+        }
     }
 
     public:
@@ -53,19 +35,21 @@ template <typename Key, typename Value> class ordered_map
     //         insert(i.first, i.second);
     //     }
     // };
-    OrderedMapIterator<Key, Value> begin() { return OrderedMapIterator<Key, Value>(values_, keys_, 0); }
-    OrderedMapIterator<Key, Value> end() { return OrderedMapIterator<Key, Value>(values_, keys_, keys_.size()); }
+    typename std::vector<std::pair<Key, Value>>::iterator begin() { return values_.begin(); }
+    typename std::vector<std::pair<Key, Value>>::iterator end() { return values_.end(); }
+    typename std::vector<std::pair<Key, Value>>::const_iterator begin() const { return values_.begin(); }
+    typename std::vector<std::pair<Key, Value>>::const_iterator end() const { return values_.end(); }
     auto count(const Key &key) const { return values_.count(key); }
     Value &at(const Key &key) const { return values_.at(key); }
     Value &operator[](const Key &key)
     {
         insistKey_(key);
-        return values_[key];
+        return values_[index_[key]].second;
     }
     Value &operator[](Key &&key)
     {
         insistKey_(key);
-        return values_[key];
+        return values_[index_[key]].second;
     }
 };
 
