6fac6b6c57d7e5be76a5486c0ed2380137d43f76
Adam Washington <adam.washington@stfc.ac.uk>
2023-11-27 15:37:59 +0000

    More documentation on ordered_map

--- src/templates/ordered_map.h
+++ src/templates/ordered_map.h
@@ -10,12 +10,24 @@
 namespace dissolve
 {
 
+// This class is similar to std::map, but the values are returned in
+// order of insertion, rather than in alphabetical order (like
+// std::map) or random order (like std:unordered_map)
+//
+// Access is still O(1) and insertion is O(log(N)).  Erasure hasn't
+// been implemented yet and will likely be O(N), unlike the O(log(N))
+// of std::map.  However, seeing as removal hasn't even been required
+// at this point, it seems unlikely that it will ever serve as a true
+// bottleneck.
 template <typename Key, typename Value> class ordered_map
 {
 
     private:
+    // A maping between keys and indices
     std::map<Key, int> index_;
+    // An association array of the keys and values
     std::vector<std::pair<Key, Value>> values_;
+    // If a key does not exist, add it to the array
     void insistKey_(const Key &key)
     {
         if (index_.find(key) == index_.end())
@@ -26,16 +38,28 @@ template <typename Key, typename Value> class ordered_map
     }
 
     public:
+    // Constructors
     ordered_map(){};
+    // Constructor from an iterator off a std::map or std::unorderedmap
     template <typename Iter> ordered_map(Iter begin, Iter end)
     {
         for (auto i = begin; i != end; i++)
             (*this)[i->first] = i->second;
     };
+
+    // Iterator access
     typename std::vector<std::pair<Key, Value>>::iterator begin() { return values_.begin(); }
     typename std::vector<std::pair<Key, Value>>::iterator end() { return values_.end(); }
     typename std::vector<std::pair<Key, Value>>::const_iterator begin() const { return values_.begin(); }
     typename std::vector<std::pair<Key, Value>>::const_iterator end() const { return values_.end(); }
+
+    // Required std::map interface functions
+    const Value &at(const Key &key) const { return values_[index_.at(key)].second; }
+    Value &at(const Key &key) { return values_[index_.at(key)].second; }
+    // The number of times a key is used.  This can only be zero or one.
+    auto count(const Key &key) const { return index_.count(key); }
+
+    // Operator overloads from std::map
     bool operator==(const ordered_map<Key, Value> other) const
     {
         if (values_.size() != other.values_.size())
@@ -48,9 +72,6 @@ template <typename Key, typename Value> class ordered_map
                 return false;
         return true;
     }
-    auto count(const Key &key) const { return index_.count(key); }
-    const Value &at(const Key &key) const { return values_[index_.at(key)].second; }
-    Value &at(const Key &key) { return values_[index_.at(key)].second; }
     Value &operator[](const Key &key)
     {
         insistKey_(key);
