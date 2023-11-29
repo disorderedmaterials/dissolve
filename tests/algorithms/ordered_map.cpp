38bd8a4807e60c011857724e249e912ae8d2c1a7
Adam Washington <adam.washington@stfc.ac.uk>
2023-11-29 14:00:43 +0000

    proper auto variables

--- tests/algorithms/ordered_map.cpp
+++ tests/algorithms/ordered_map.cpp
@@ -14,10 +14,10 @@ TEST(OrderedMapTest, BasicOrderedMap)
     SerialisedValue example;
     std::vector<std::string> keys = {"foo", "bar", "quux", "xyzzy"};
     std::vector<int> values = {2, 7, 5, 3};
-    for (int i = 0; i < keys.size(); i++)
+    for (auto i = 0; i < keys.size(); i++)
         example[keys[i]] = values[i];
 
-    int index = 0;
+    auto index = 0;
     for (auto [k, v] : example.as_table())
     {
         EXPECT_EQ(keys[index], k);
