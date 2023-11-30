27b9653e79d6da8d713f8718f1467620ce31abe8
Adam Washington <adam.washington@stfc.ac.uk>
2023-11-30 11:12:07 +0000

    Add reified version class

--- tests/io/version.cpp
+++ tests/io/version.cpp
@@ -8,6 +8,27 @@
 namespace UnitTest
 {
 
+TEST(VersionTest, VersionClass)
+{
+    using Version::DissolveVersion;
+    DissolveVersion first{"1.10.3"}, second{"2.1.12"}, minor{"2.2.1"}, patch{"2.2.2"}, third{"3.0.0"};
+
+    // Main sequence
+    EXPECT_LT(first, second);
+    EXPECT_LT(second, minor);
+    EXPECT_LT(minor, patch);
+    EXPECT_LT(patch, third);
+
+    // Full combinatorial
+    EXPECT_LT(first, minor);
+    EXPECT_LT(first, patch);
+    EXPECT_LT(first, third);
+    EXPECT_LT(second, minor);
+    EXPECT_LT(second, patch);
+    EXPECT_LT(second, third);
+    EXPECT_LT(minor, third);
+}
+
 TEST(VersionTest, VersionInfo)
 {
     CoreData coreData;
@@ -15,7 +36,9 @@ TEST(VersionTest, VersionInfo)
     dissolve.loadInput("dissolve/input/rdfMethod.txt");
     auto serialised = dissolve.serialise();
 
-    EXPECT_EQ(serialised["version"].as_string().str, Version::semantic());
+    Version::DissolveVersion fileVersion(serialised["version"].as_string().str), actual(Version::semantic());
+
+    EXPECT_EQ(fileVersion, actual);
 }
 
 } // namespace UnitTest
