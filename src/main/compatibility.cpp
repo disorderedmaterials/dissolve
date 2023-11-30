b2c8a9ba585da4b08aa24e3cae67c3c789ba4f0c
Adam Washington <adam.washington@stfc.ac.uk>
2023-11-30 14:12:17 +0000

    More descriptive comments

--- src/main/compatibility.cpp
+++ src/main/compatibility.cpp
@@ -4,6 +4,9 @@
 #include "main/compatibility.h"
 #include <iostream>
 
+// Upgrade from version zero, which never supported TOML in the first
+// place.  This exists purely to allow testing the backward
+// compatibility code before there is any pass worth checking.
 SerialisedValue unreal_upgrade(SerialisedValue contents)
 {
     if (contents["pairPotentials"].contains("coulombTruncation"))
@@ -13,9 +16,13 @@ SerialisedValue unreal_upgrade(SerialisedValue contents)
     return contents;
 }
 
+// Upgrade a file to the current version.
 SerialisedValue backwards_upgrade(SerialisedValue contents)
 {
     using Version::DissolveVersion;
+
+    // A map of version numbers and the corresponding function to
+    // upgrade the file.
     std::map<std::string, std::function<SerialisedValue(SerialisedValue)>> breaking_changes = {{"1.2.0", unreal_upgrade}};
 
     DissolveVersion current(toml::find<std::string>(contents, "version"));
