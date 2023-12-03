024af5793672843f7d1eb33bd9f55d8b37b25b71
Jared Swift <j.w.swift@outlook.com>
2023-12-03 16:28:32 +0000

    Specify type.

--- src/procedure/nodes/runModule.cpp
+++ src/procedure/nodes/runModule.cpp
@@ -3,11 +3,12 @@
 
 #include "procedure/nodes/runModule.h"
 #include "keywords/module.h"
+#include "procedure/nodes/node.h"
 
 RunModuleNode::RunModuleNode(const Module *module) : ProcedureNode(ProcedureNode::NodeType::RunModule, {ProcedureNode::ControlContext}), module_(module)
 {
     keywords_.setOrganisation("Options", "Target");
-    keywords_.add<ModuleKeyword>("Module", "Target module to run", module_);
+    keywords_.add<ModuleKeyword<Module>>("Module", "Target module to run", module_, ModuleTypes::ModuleType::Any);
 }
 
 /*
