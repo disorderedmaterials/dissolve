68ed95ead6d05f6e6b197c9c9a55abb01a0780e8
rhinoella <noella.spitz@ndw2544mac.isis.cclrc.ac.uk>
2023-12-04 15:54:00 +0000

    FTGLFix

--- cmake/Modules/FindFTGL.cmake
+++ cmake/Modules/FindFTGL.cmake
@@ -18,41 +18,39 @@
 #=============================================================================
 
 # Created by Richard Ulrich.
-if(NOT FTGL_INCLUDE_DIRS AND NOT FTGL_LIBRARIES)
-  FIND_PATH(FTGL_INCLUDE_DIR FTGL/ftgl.h 
-    HINTS
-    $ENV{FTGL_DIR}
-    PATH_SUFFIXES include src
-    PATHS
-    /usr/include
-    /usr/local/include
-    /sw/include
-    /opt/local/include
-    /usr/freeware/include
-  )
+FIND_PATH(FTGL_INCLUDE_DIR FTGL/ftgl.h 
+  HINTS
+  $ENV{FTGL_DIR}
+  PATH_SUFFIXES include src
+  PATHS
+  /usr/include
+  /usr/local/include
+  /sw/include
+  /opt/local/include
+  /usr/freeware/include
+)
 
-  FIND_LIBRARY(FTGL_LIBRARY
-    NAMES ftgl libftgl ftgl_static
-    HINTS
-    $ENV{FTGL_DIR}
-    PATH_SUFFIXES lib64 lib
-    PATHS
-    /usr/lib
-    /usr/local/lib
-    /sw
-    /usr/freeware
-  )
+FIND_LIBRARY(FTGL_LIBRARY
+  NAMES ftgl libftgl ftgl_static
+  HINTS
+  $ENV{FTGL_DIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+  /usr/lib
+  /usr/local/lib
+  /sw
+  /usr/freeware
+)
 
   # set the user variables
-  IF(FTGL_INCLUDE_DIR)
-    SET(FTGL_INCLUDE_DIRS "${FTGL_INCLUDE_DIR}")
-  ENDIF()
-  SET(FTGL_LIBRARIES "${FTGL_LIBRARY}")
+IF(FTGL_INCLUDE_DIR)
+  SET(FTGL_INCLUDE_DIRS "${FTGL_INCLUDE_DIR}")
+ENDIF()
+SET(FTGL_LIBRARIES "${FTGL_LIBRARY}")
 
-  # handle the QUIETLY and REQUIRED arguments and set FTGL_FOUND to TRUE if 
-  # all listed variables are TRUE
-  INCLUDE("FindPackageHandleStandardArgs")
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(FTGL  DEFAULT_MSG  FTGL_LIBRARY  FTGL_INCLUDE_DIR)
+ # handle the QUIETLY and REQUIRED arguments and set FTGL_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE("FindPackageHandleStandardArgs")
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(FTGL  DEFAULT_MSG  FTGL_LIBRARY  FTGL_INCLUDE_DIR)
 
-  MARK_AS_ADVANCED(FTGL_LIBRARY FTGL_INCLUDE_DIR)
-endif()
+ MARK_AS_ADVANCED(FTGL_LIBRARY FTGL_INCLUDE_DIR)
