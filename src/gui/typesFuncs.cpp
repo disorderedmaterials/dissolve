25af6ed3ea33376bd6a7e3d76f0df756985f779c
RobBuchananCompPhys <robertbb269@hotmail.com>
2023-12-06 15:00:44 +0000

    added new qml types

--- src/gui/typesFuncs.cpp
+++ src/gui/typesFuncs.cpp
@@ -14,4 +14,9 @@ void registerDissolveQmlTypes() const
     qmlRegisterType<SpeciesModel>(PROJECT, 1, 0, "SpeciesModel");
     qmlRegisterType<ConfigurationModel>(PROJECT, 1, 0, "ConfigurationModel");
     qmlRegisterType<ModuleLayersModel>(PROJECT, 1, 0, "ModuleLayersModel");
+    qmlRegisterType<AtomTypeModel>(PROJECT, 1, 0, "AtomTypeModel");
+    qmlRegisterType<MasterAngleModel>(PROJECT, 1, 0, "MasterAngleModel");
+    qmlRegisterType<MasterBondModel>(PROJECT, 1, 0, "MasterBondModel");
+    qmlRegisterType<MasterImproperModel>(PROJECT, 1, 0, "MasterImproperModel");
+    qmlRegisterType<MasterTorsionModel>(PROJECT, 1, 0, "MasterTorsionModel");
 }
