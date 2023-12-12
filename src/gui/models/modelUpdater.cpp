cf292d44b9ca23f4fbce525f1bec55a2862fe270
RobBuchananCompPhys <robertbb269@hotmail.com>
2023-12-12 09:10:10 +0000

    added type specifier and member name for model updater methods

--- src/gui/models/modelUpdater.cpp
+++ src/gui/models/modelUpdater.cpp
@@ -4,28 +4,28 @@
 #include "gui/models/modelUpdater.h"
 #include <QAbstractItemModel>
 
-ModelUpdater::setModel(const QAbstractItemModel *model) { model = model; }
+void ModelUpdater::setModel(const QAbstractItemModel *model) { model = model; }
 
 void ModelUpdater::connectModelSignals()
 {
-    connect(model, &columnsAboutToBeInserted, this, &update);
-    connect(model, &columnsAboutToBeMoved, this, &update);
-    connect(model, &columnsAboutToBeRemoved, this, &update);
-    connect(model, &columnsInserted, this, &update);
-    connect(model, &columnsMoved, this, &update);
-    connect(model, &columnsRemoved, this, &update);
-    connect(model, &dataChanged, this, &update);
-    connect(model, &headerDataChanged, this, &update);
-    connect(model, &layoutAboutToBeChanged, this, &update);
-    connect(model, &layoutChanged, this, &update);
-    connect(model, &modelAboutToBeReset, this, &update);
-    connect(model, &modelReset, this, &update);
-    connect(model, &rowsABoutToBeInserted, this, &update);
-    connect(model, &rowsABoutToBeMoved, this, &update);
-    connect(model, &rowsABoutToBeRemoved, this, &update);
-    connect(model, &rowsInserted, this, &update);
-    connect(model, &rowsMoved, this, &update);
-    connect(model, &rowsRemoved, this, &update);
+    connect(model, &QAbstractItemModel::columnsAboutToBeInserted, this, &update);
+    connect(model, &QAbstractItemModel::columnsAboutToBeMoved, this, &update);
+    connect(model, &QAbstractItemModel::columnsAboutToBeRemoved, this, &update);
+    connect(model, &QAbstractItemModel::columnsInserted, this, &update);
+    connect(model, &QAbstractItemModel::columnsMoved, this, &update);
+    connect(model, &QAbstractItemModel::columnsRemoved, this, &update);
+    connect(model, &QAbstractItemModel::dataChanged, this, &update);
+    connect(model, &QAbstractItemModel::headerDataChanged, this, &update);
+    connect(model, &QAbstractItemModel::layoutAboutToBeChanged, this, &update);
+    connect(model, &QAbstractItemModel::layoutChanged, this, &update);
+    connect(model, &QAbstractItemModel::modelAboutToBeReset, this, &update);
+    connect(model, &QAbstractItemModel::modelReset, this, &update);
+    connect(model, &QAbstractItemModel::rowsABoutToBeInserted, this, &update);
+    connect(model, &QAbstractItemModel::rowsABoutToBeMoved, this, &update);
+    connect(model, &QAbstractItemModel::rowsABoutToBeRemoved, this, &update);
+    connect(model, &QAbstractItemModel::rowsInserted, this, &update);
+    connect(model, &QAbstractItemModel::rowsMoved, this, &update);
+    connect(model, &QAbstractItemModel::rowsRemoved, this, &update);
 }
 
 void ModelUpdater::update() { emit model->modelsUpdated(); }
