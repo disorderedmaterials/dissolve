1c129c1fb924fa1e1af04292c5bc2a101335f750
Danbr4d <laydb3@nottingham.ac.uk>
2024-01-11 16:32:22 +0000

    update to 2020 and Q_SLOTS

--- src/modules/compare/gui/compareWidget.h
+++ src/modules/compare/gui/compareWidget.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2023 Team Dissolve and contributors
+// Copyright (c) 2024 Team Dissolve and contributors
 
 #pragma once
 
@@ -45,6 +45,6 @@ class CompareModuleWidget : public ModuleWidget
      * Widgets / Functions
      */
 
-    private slots:
+    private Q_SLOTS:
     void on_Data1DButton_clicked(bool checked);
 };
