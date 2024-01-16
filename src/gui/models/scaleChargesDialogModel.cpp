3514fa13b5c01dec8ab7d1a09d0ef8f35080416b
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-01-16 11:54:49 +0000

    display qml scale charges dialog

---
+++ src/gui/models/scaleChargesDialogModel.cpp
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Team Dissolve and contributors
+
+#include "gui/models/scaleChargesDialogModel.h"
+
+ScaleChargesDialogModel::ScaleChargesDialogModel() {}
+
+ScaleChargesDialogModel::~ScaleChargesDialogModel() {}
+
+double ScaleChargesDialogModel::value() const { return currentValue_; }
+
+void ScaleChargesDialogModel::updateValue(double newVal) { currentValue_ = newVal; }
+
+void ScaleChargesDialogModel::setOption(int option) {
+    switch(option){
+        case 1:
+            scale_=true;
+            scaleTo_=false;
+            break;
+        case 2:
+            scale_=false;
+            scaleTo_=true;
+            break;
+        default:
+            break;
+    }
+}
