514a5860c58bb5d9327e6cf50c8ae8ebca422f74
Adam Washington <adam.washington@stfc.ac.uk>
2024-01-18 12:12:10 +0000

    Fix formatting

--- src/gui/models/moduleLayerModel.cpp
+++ src/gui/models/moduleLayerModel.cpp
@@ -142,9 +142,9 @@ bool ModuleLayerModel::setData(const QModelIndex &index, const QVariant &value, 
 
             // Ensure uniqueness of new name
             auto oldName = QString::fromStdString(std::string(module->name()));
-            auto newName = DissolveSys::uniqueName(
-                DissolveSys::niceName(value.toString().toStdString()), coreData_->moduleInstances(),
-                [&](const auto &inst) { return inst == module ? std::string() : inst->name(); });
+            auto newName =
+                DissolveSys::uniqueName(DissolveSys::niceName(value.toString().toStdString()), coreData_->moduleInstances(),
+                                        [&](const auto &inst) { return inst == module ? std::string() : inst->name(); });
             module->setName(newName);
 
             Q_EMIT(dataChanged(index, index));
