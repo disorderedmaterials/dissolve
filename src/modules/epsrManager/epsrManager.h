fef4a6e7f40d54b667fedd275e6f62c34d1c95b2
Danbr4d <laydb3@nottingham.ac.uk>
2024-01-18 16:34:36 +0000

    added in vector of empirical potentials and function to return vector

--- src/modules/epsrManager/epsrManager.h
+++ src/modules/epsrManager/epsrManager.h
@@ -17,40 +17,12 @@ class EPSRManagerModule : public Module
     /*
      * Definition
      */
-    public:
-    enum ExpansionFunctionType
-    {
-        PoissonExpansionFunction,  /* Fit difference functions using Poisson (power exponential) functions */
-        GaussianExpansionFunction, /* Fit difference functions using Gaussian functions */
-        nExpansionFunctionTypes
-    };
-    // Number of coefficients used to define the empirical potential
-    std::optional<int> nCoeffP_;
-    // Radius at which potential truncation goes to zero (-1.0 to use pair potential maximum range)
-    std::optional<double> rMaxPT_;
-    // Radius at which potential truncation begins (-1.0 to set to 2.0 Angstroms under rmaxpt)
-    std::optional<double> rMinPT_;
-    // Width for Gaussian function in reciprocal space
-    double gSigma1_{0.1};
-    // Width for Gaussian function in real space
-    double gSigma2_{0.2};
 
     private:
     // Target Module containing data to refine against
     std::vector<Module *> target_;
     // Scattering matrix
     ScatteringMatrix scatteringMatrix_;
-    // Expansion function type to use for potential fits
-    EPSRManagerModule::ExpansionFunctionType expansionFunction_{EPSRManagerModule::PoissonExpansionFunction};
-
-    public:
-    /*
-     * Functions
-     */
-    bool generateEmpiricalPotentials(std::string_view modulename, GenericList &moduleData, Dissolve &dissolve,
-                                     double averagedRho, std::optional<int> ncoeffp, double rminpt, double rmaxpt,
-                                     double sigma1, double sigma2);
-    void truncate(Data1D &data, double rMin, double rMax);
 
     public:
     /*
