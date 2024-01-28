3c5621a8b500fc8e52838c364b7e5f8c9df78d62
Jared Swift <j.w.swift@outlook.com>
2024-01-28 13:42:37 +0000

    Slight refactor.

--- src/analyser/dataNormaliser1D.cpp
+++ src/analyser/dataNormaliser1D.cpp
@@ -19,24 +19,29 @@ void DataNormaliser1D::normaliseBySphericalShell()
     // We expect x values to be centre-bin values, and regularly spaced
     const auto &xAxis = targetData_->xAxis();
     auto &values = targetData_->values();
-    if (xAxis.size() >= 2)
-    {
-        // Derive first left-bin boundary from the delta between points 0 and 1
-        double leftBin = xAxis[0] - (xAxis[1] - xAxis[0]) * 0.5, rightBin, divisor;
-        double r1Cubed = pow(leftBin, 3), r2Cubed;
-        for (auto n = 0; n < xAxis.size(); ++n)
-        {
-            // Get new right-bin from existing left bin boundary and current bin centre
-            rightBin = leftBin + 2 * (xAxis[n] - leftBin);
-            r2Cubed = pow(rightBin, 3);
-            divisor = (4.0 / 3.0) * PI * (r2Cubed - r1Cubed);
-            values[n] /= divisor;
-            if (targetData_->valuesHaveErrors())
-                targetData_->error(n) /= divisor;
 
-            // Overwrite old values
-            r1Cubed = r2Cubed;
-            leftBin = rightBin;
-        }
+    if (xAxis.size() < 2)
+        return;
+
+    // Derive first left-bin boundary from the delta between points 0 and 1
+    double leftBin = xAxis[0] - (xAxis[1] - xAxis[0]) * 0.5, rightBin, divisor;
+    double r1Cubed = pow(leftBin, 3), r2Cubed;
+    for (auto n = 0; n < xAxis.size(); ++n)
+    {
+        // Get new right-bin from existing left bin boundary and current bin centre
+        rightBin = leftBin + 2 * (xAxis[n] - leftBin);
+        r2Cubed = pow(rightBin, 3);
+
+        // Calculate divisor for normalisation
+        divisor = (4.0 / 3.0) * PI * (r2Cubed - r1Cubed);
+
+        // Peform normalisation step
+        values[n] /= divisor;
+        if (targetData_->valuesHaveErrors())
+            targetData_->error(n) /= divisor;
+
+        // Overwrite old values for next iteration
+        r1Cubed = r2Cubed;
+        leftBin = rightBin;
     }
 }
