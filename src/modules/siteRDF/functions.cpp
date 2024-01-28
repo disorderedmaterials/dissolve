c3dd378dd495b495aa097de0b6911426c5d5b759
Jared Swift <j.w.swift@outlook.com>
2024-01-28 12:32:10 +0000

    Drop commented blocks.

--- src/modules/siteRDF/functions.cpp
+++ src/modules/siteRDF/functions.cpp
@@ -5,20 +5,5 @@
 #include "procedure/nodes/process1D.h"
 #include "procedure/nodes/sum1D.h"
 
-// // Return Collect1DNode for A-B RDF
-// std::shared_ptr<Collect1DProcedureNode> SiteRDFModule::collectDistanceNode() const { return collectDistance_; }
-
-// // Return SelectNode for site A
-// std::shared_ptr<SelectProcedureNode> SiteRDFModule::selectANode() const { return selectA_; }
-
 // Return whether specified coordination number range is enabled
 bool SiteRDFModule::isRangeEnabled(int id) const { return rangeEnabled_[id]; }
-
-// // Return Process1DNode result (i.e. RDF)
-// std::shared_ptr<Process1DProcedureNode> SiteRDFModule::rdfResult() const
-// {
-//     if ((!processDistance_) || (!processDistance_->hasProcessedData()))
-//         return nullptr;
-
-//     return processDistance_;
-// }
