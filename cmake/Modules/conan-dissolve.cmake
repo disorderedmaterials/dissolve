9dda68981ad0131caabef18f7cb7a30680909b81
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2024-01-30 14:23:20 +0000

    Cleanse.

--- cmake/Modules/conan-dissolve.cmake
+++ cmake/Modules/conan-dissolve.cmake
@@ -7,18 +7,18 @@ include(${CMAKE_BINARY_DIR}/conan.cmake)
 
 # Set basic requirements and options
 set(_conan_requires
-        cli11/1.9.1
-        fmt/8.1.1
-        pugixml/1.11
-        onetbb/2021.10.0
-        onedpl/2022.3.0
-        toml11/3.7.0
-        antlr4-cppruntime/4.13.1
+    cli11/1.9.1
+    fmt/8.1.1
+    pugixml/1.11
+    onetbb/2021.10.0
+    onedpl/2022.3.0
+    toml11/3.7.0
+    antlr4-cppruntime/4.13.1
 )
 set(_conan_options
-        fmt:header_only=True
-        pugixml:header_only=False
-        antlr4-cppruntime:shared=True
+    fmt:header_only=True
+    pugixml:header_only=False
+    antlr4-cppruntime:shared=True
 )
 
 # Handle platform-specific requirements
@@ -34,7 +34,7 @@ endif(BUILD_TESTS)
 conan_cmake_configure(
   REQUIRES
     ${_conan_requires}
-  GENERATORS cmake_find_package cmake
+  GENERATORS cmake_find_package
   OPTIONS
     ${_conan_options}
 )
