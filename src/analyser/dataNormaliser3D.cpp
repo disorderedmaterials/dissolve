7aab9841cca9924ea94af10b5ef408ad4b7cd711
Jared Swift <j.w.swift@outlook.com>
2024-02-04 14:00:37 +0000

    3D grid normalisation.

--- src/analyser/dataNormaliser3D.cpp
+++ src/analyser/dataNormaliser3D.cpp
@@ -9,4 +9,13 @@ DataNormaliser3D::DataNormaliser3D(Data3D &targetData) : targetData_(targetData)
 
 void DataNormaliser3D::normaliseBySitePopulation(double population) { targetData_ /= population; }
 
-void DataNormaliser3D::normaliseByGrid() {}
+void DataNormaliser3D::normaliseByGrid()
+{
+    // Determine bin area from first points of data
+    auto xBinWidth = targetData_->xAxis().at(1) - targetData_->xAxis().at(0);
+    auto yBinWidth = targetData_->yAxis().at(1) - targetData_->yAxis().at(0);
+    auto zBinWidth = targetData_->zAxis().at(1) - targetData_->zAxis().at(0);
+    auto binVolume = xBinWidth * yBinWidth * zBinWidth;
+
+    targetData_ /= binVolume;
+}
