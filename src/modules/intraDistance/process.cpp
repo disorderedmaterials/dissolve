19979ba625cca56b6bde4df08062b4ce7050a946
Jared Swift <j.w.swift@outlook.com>
2024-02-04 11:24:33 +0000

    Normalise by value.

--- src/modules/intraDistance/process.cpp
+++ src/modules/intraDistance/process.cpp
@@ -51,7 +51,7 @@ Module::ExecutionResult IntraDistanceModule::process(ModuleContext &moduleContex
     auto dataNormalisedHisto = processingData.realise<Data1D>("NormalisedHistogram", name(), GenericItem::InRestartFileFlag);
     dataNormalisedHisto = histAB.accumulatedData();
     DataNormaliser1D histogramNormaliser(dataNormalisedHisto);
-    histogramNormaliser.normalise();
+    histogramNormaliser.normaliseByValue();
 
     return ExecutionResult::Success;
 }
