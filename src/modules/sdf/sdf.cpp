efb21bab931257c24e2cf97dbf3b920aca8ec45c
Jared Swift <j.w.swift@outlook.com>
2024-02-04 13:43:53 +0000

    Drop procedure analysis.

--- src/modules/sdf/sdf.cpp
+++ src/modules/sdf/sdf.cpp
@@ -16,34 +16,6 @@
 
 SDFModule::SDFModule() : Module(ModuleTypes::SDF), analyser_(ProcedureNode::AnalysisContext)
 {
-    // Select: Site 'A'
-    selectA_ = analyser_.createRootNode<SelectProcedureNode>("A", std::vector<const SpeciesSite *>{},
-                                                             ProcedureNode::AnalysisContext, true);
-    auto &forEachA = selectA_->branch()->get();
-
-    // -- Select: Site 'B'
-    selectB_ = forEachA.create<SelectProcedureNode>("B");
-    selectB_->keywords().set("ExcludeSameSite", ConstNodeVector<SelectProcedureNode>{selectA_});
-    selectB_->keywords().set("ExcludeSameMolecule", ConstNodeVector<SelectProcedureNode>{selectA_});
-    auto &forEachB = selectB_->branch()->get();
-
-    // -- -- Calculate: 'v(B->A)'
-    auto calcVector = forEachB.create<CalculateVectorProcedureNode>({}, selectA_, selectB_, true);
-
-    // -- -- Collect3D: 'SDF'
-    collectVector_ =
-        forEachB.create<Collect3DProcedureNode>({}, calcVector, ProcedureNode::AnalysisContext, rangeX_, rangeY_, rangeZ_);
-
-    // Process3D: @dataName
-    processPosition_ = analyser_.createRootNode<Process3DProcedureNode>("SDF", collectVector_);
-    processPosition_->keywords().set("LabelValue", std::string("\\symbol{rho}(x,y,z)"));
-    processPosition_->keywords().set("LabelX", std::string("x, \\symbol{Angstrom}"));
-    processPosition_->keywords().set("LabelY", std::string("y, \\symbol{Angstrom}"));
-    processPosition_->keywords().set("LabelZ", std::string("z, \\symbol{Angstrom}"));
-    auto &sdfNormalisation = processPosition_->branch()->get();
-    sdfNormalisation.create<OperateSitePopulationNormaliseProcedureNode>({}, ConstNodeVector<SelectProcedureNode>({selectA_}));
-    sdfNormalisation.create<OperateGridNormaliseProcedureNode>({});
-
     /*
      * Keywords
      */
@@ -51,11 +23,9 @@ SDFModule::SDFModule() : Module(ModuleTypes::SDF), analyser_(ProcedureNode::Anal
     keywords_.addTarget<ConfigurationKeyword>("Configuration", "Set target configuration for the module", targetConfiguration_);
 
     keywords_.setOrganisation("Options", "Sites", "Specify the central (A) and surrounding sites (B).");
-    keywords_.add<SpeciesSiteVectorKeyword>("SiteA", "Set the site(s) 'A' which are to represent the origin of the SDF",
-                                            selectA_->speciesSites(), selectA_->axesRequired());
+    keywords_.add<SpeciesSiteVectorKeyword>("SiteA", "Set the site(s) 'A' which are to represent the origin of the SDF", a_);
     keywords_.add<SpeciesSiteVectorKeyword>(
-        "SiteB", "Set the site(s) 'B' for which the distribution around the origin sites 'A' should be calculated",
-        selectB_->speciesSites(), selectB_->axesRequired());
+        "SiteB", "Set the site(s) 'B' for which the distribution around the origin sites 'A' should be calculated", b_);
 
     keywords_.setOrganisation("Options", "Ranges", "Ranges over which to bin quantities from the calculation.");
     keywords_.add<Vec3DoubleKeyword>("RangeX", "Range along X axis", rangeX_, Vec3<double>(-1.0e6, -1.0e6, 0.05),
