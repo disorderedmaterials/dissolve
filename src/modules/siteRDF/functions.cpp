1963d003d8b107713562f9a25321b70aa7f9ca76
Jared Swift <j.w.swift@outlook.com>
2024-02-04 18:33:11 +0000

    Remove redundant includes, again, again.

--- src/modules/siteRDF/functions.cpp
+++ src/modules/siteRDF/functions.cpp
@@ -2,8 +2,6 @@
 // Copyright (c) 2024 Team Dissolve and contributors
 
 #include "modules/siteRDF/siteRDF.h"
-#include "procedure/nodes/process1D.h"
-#include "procedure/nodes/sum1D.h"
 
 // Return whether specified coordination number range is enabled
 bool SiteRDFModule::isRangeEnabled(int id) const { return rangeEnabled_[id]; }
