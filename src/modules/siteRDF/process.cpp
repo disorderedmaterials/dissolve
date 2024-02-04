70afcde23d9101d5d7d77a9b9de6b37641dca8bf
Jared Swift <j.w.swift@outlook.com>
2024-02-04 18:35:48 +0000

    Export.

--- src/modules/siteRDF/process.cpp
+++ src/modules/siteRDF/process.cpp
@@ -90,5 +90,17 @@ Module::ExecutionResult SiteRDFModule::process(ModuleContext &moduleContext)
             }
         }
 
+    // Save data?
+    if (exportFileAndFormat_.hasFilename())
+    {
+        if (moduleContext.processPool().isMaster())
+        {
+            if (exportFileAndFormat_.exportData(dataCN))
+                moduleContext.processPool().decideTrue();
+            else
+                moduleContext.processPool().decideFalse();
+        }
+    }
+
     return ExecutionResult::Success;
 }
