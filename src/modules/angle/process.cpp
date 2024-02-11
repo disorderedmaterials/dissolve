4469245a40992973c26e3790b93d2237490c3da3
Jared Swift <j.w.swift@outlook.com>
2024-02-11 19:45:02 +0000

    Cleanup

--- src/modules/angle/process.cpp
+++ src/modules/angle/process.cpp
@@ -9,6 +9,7 @@
 #include "math/histogram1D.h"
 #include "math/histogram2D.h"
 #include "math/histogram3D.h"
+#include "math/range.h"
 #include "module/context.h"
 #include "modules/angle/angle.h"
 
@@ -33,47 +34,48 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
     // Select site C
     SiteSelector c(targetConfiguration_, c_);
 
-    auto [rAB, status] = processingData.realiseIf<Histogram1D>("rAB", name(), GenericItem::InRestartFileFlag);
-    if (status == GenericItem::ItemStatus::Created)
+    auto [rAB, rABStatus] = processingData.realiseIf<Histogram1D>("rAB", name(), GenericItem::InRestartFileFlag);
+    if (rABStatus == GenericItem::ItemStatus::Created)
         rAB.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z);
-    auto [rBC, status] = processingData.realiseIf<Histogram1D>("rBC", name(), GenericItem::InRestartFileFlag);
-    if (status == GenericItem::ItemStatus::Created)
+    auto [rBC, rBCStatus] = processingData.realiseIf<Histogram1D>("rBC", name(), GenericItem::InRestartFileFlag);
+    if (rBCStatus == GenericItem::ItemStatus::Created)
         rBC.initialise(rangeBC_.x, rangeBC_.y, rangeBC_.z);
 
-    auto [aABC, status] = processingData.realiseIf<Histogram1D>("aABC", name(), GenericItem::InRestartFileFlag);
-    if (status == GenericItem::ItemStatus::Created)
+    auto [aABC, aABCStatus] = processingData.realiseIf<Histogram1D>("aABC", name(), GenericItem::InRestartFileFlag);
+    if (aABCStatus == GenericItem::ItemStatus::Created)
         aABC.initialise(angleRange_.x, angleRange_.y, angleRange_.z);
 
-    auto [daABc, status] = processingData.realiseIf<Histogram2D>("daABc", name(), GenericItem::InRestartFileFlag);
-    if (status == GenericItem::ItemStatus::Created)
+    auto [daABc, daABCStatus] = processingData.realiseIf<Histogram2D>("daABc", name(), GenericItem::InRestartFileFlag);
+    if (daABCStatus == GenericItem::ItemStatus::Created)
         daABc.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z, angleRange_.x, angleRange_.y, angleRange_.z);
 
-    auto [daaBC, status] = processingData.realiseIf<Histogram2D>("daaBC", name(), GenericItem::InRestartFileFlag);
-    if (status == GenericItem::ItemStatus::Created)
+    auto [daaBC, daaBCStatus] = processingData.realiseIf<Histogram2D>("daaBC", name(), GenericItem::InRestartFileFlag);
+    if (daaBCStatus == GenericItem::ItemStatus::Created)
         daaBC.initialise(rangeBC_.x, rangeBC_.y, rangeBC_.z, angleRange_.x, angleRange_.y, angleRange_.z);
 
-    auto [dda, status] = processingData.realiseIf<Histogram3D>("dda", name(), GenericItem::InRestartFileFlag);
-    if (status == GenericItem::ItemStatus::Created)
+    auto [dda, ddaStatus] = processingData.realiseIf<Histogram3D>("dda", name(), GenericItem::InRestartFileFlag);
+    if (ddaStatus == GenericItem::ItemStatus::Created)
         dda.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z, rangeBC_.x, rangeBC_.y, rangeBC_.z, angleRange_.x, angleRange_.y,
                        angleRange_.z);
 
-    for (const auto &siteA : a.sites())
+    for (const auto &[siteA, indexA] : a.sites())
     {
 
-        for (const auto &siteB : b.sites())
+        for (const auto &[siteB, indexB] : b.sites())
         {
 
             if (excludeSameMoleculeAB_ && siteB->molecule() == siteA->molecule())
                 continue;
 
-            if (!rangeAB_.contains(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin())))
+            if (!Range(rangeAB_.x, rangeAB_.y)
+                     .contains(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin())))
                 continue;
 
             rAB.zeroBins();
             rAB.bin(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin()));
             rAB.accumulate();
 
-            for (const auto &siteC : c.sites())
+            for (const auto &[siteC, indexC] : c.sites())
             {
 
                 if (excludeSameMoleculeBC_ && siteC->molecule() == siteB->molecule())
@@ -82,7 +84,8 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
                 if (excludeSameSiteAC_ && siteC == siteA)
                     continue;
 
-                if (!rangeBC_.contains(targetConfiguration_->box()->minimumDistance(siteC->origin(), siteB->origin())))
+                if (!Range(rangeBC_.x, rangeBC_.y)
+                         .contains(targetConfiguration_->box()->minimumDistance(siteC->origin(), siteB->origin())))
                     continue;
 
                 rBC.zeroBins();
@@ -97,7 +100,7 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
                 aABC.accumulate();
 
                 daABc.zeroBins();
-                daABC.bin(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin()), angle);
+                daABc.bin(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin()), angle);
                 daABc.accumulate();
 
                 daaBC.zeroBins();
@@ -112,28 +115,28 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
         }
     }
 
-    auto &normalisedAB = processingData.realise<Histogram1D>("RDF(AB)", name(), GenericItem::InRestartFileFlag);
+    auto &normalisedAB = processingData.realise<Data1D>("RDF(AB)", name(), GenericItem::InRestartFileFlag);
     normalisedAB = rAB.accumulatedData();
     DataNormaliser1D normaliserAB(normalisedAB);
     normaliserAB.normaliseBySitePopulation(a.sites().size());
     normaliserAB.normaliseByNumberDensity(b.sites().size(), targetConfiguration_);
-    normalisedAB.normaliseBySphericalShell();
+    normaliserAB.normaliseBySphericalShell();
 
-    auto &normalisedBC = processingData.realise<Histogram1D>("RDF(BC)", name(), GenericItem::InRestartFileFlag);
+    auto &normalisedBC = processingData.realise<Data1D>("RDF(BC)", name(), GenericItem::InRestartFileFlag);
     normalisedBC = rBC.accumulatedData();
     DataNormaliser1D normaliserBC(normalisedBC);
     normaliserBC.normaliseBySitePopulation(b.sites().size());
     normaliserBC.normaliseBySitePopulation(a.sites().size());
-    normalierBC.normaliseByNumberDensity(c.sites().size(), targetConfiguration_);
-    normalisedBC.normaliseBySphericalShell();
+    normaliserBC.normaliseByNumberDensity(c.sites().size(), targetConfiguration_);
+    normaliserBC.normaliseBySphericalShell();
 
-    auto &normalisedAngle = processingData.realise<Histogram1D>("Angle(ABC)", name(), GenericItem::InRestartFileFlag);
+    auto &normalisedAngle = processingData.realise<Data1D>("Angle(ABC)", name(), GenericItem::InRestartFileFlag);
     normalisedAngle = aABC.accumulatedData();
     DataNormaliser1D normaliserAngle(normalisedAngle);
     normaliserAngle.normaliseByExpression("value/sin(toRad(x))");
     normaliserAngle.normaliseByValue();
 
-    auto &normalisedDAngleAB = processingData.realise<Histogram2D>("DAngle((A-B)-C)", name(), GenericItem::InRestartFileFlag);
+    auto &normalisedDAngleAB = processingData.realise<Data2D>("DAngle((A-B)-C)", name(), GenericItem::InRestartFileFlag);
     normalisedDAngleAB = daABc.accumulatedData();
     DataNormaliser2D normaliserDAngleAB(normalisedDAngleAB);
     normaliserDAngleAB.normaliseByExpression(
@@ -143,7 +146,7 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
     normaliserDAngleAB.normaliseByNumberDensity(b.sites().size(), targetConfiguration_);
     normaliserDAngleAB.normaliseBySphericalShell();
 
-    auto &normalisedDAngleBC = processingData.realise<Histogram2D>("DAngle(A-(B-C))", name(), GenericItem::InRestartFileFlag);
+    auto &normalisedDAngleBC = processingData.realise<Data2D>("DAngle(A-(B-C))", name(), GenericItem::InRestartFileFlag);
     normalisedDAngleBC = daaBC.accumulatedData();
     DataNormaliser2D normaliserDAngleBC(normalisedDAngleBC);
     normaliserDAngleBC.normaliseByExpression(
