51a4de47f31210dae297e2c3a63bbbd7de984221
Jared Swift <j.w.swift@outlook.com>
2024-02-18 22:36:19 +0000

    WIP angle system test.

---
+++ tests/modules/angle.cpp
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
+#include "tests/testData.h"
+#include <gtest/gtest.h>
+#include <vector>
+
+namespace UnitTest
+{
+class AngleModuleTest : public ::testing::Test
+{
+    protected:
+    DissolveSystemTest systemTest;
+};
+
+TEST_F(AngleModuleTest, Water)
+{
+    ASSERT_NO_THROW(systemTest.setUp("dissolve/input/angle.txt"));
+    ASSERT_TRUE(systemTest.iterateRestart(95));
+
+    EXPECT_TRUE(systemTest.checkData1D("Angle(X-H...O)//Angle(ABC)",
+                                       {"dlpoly/water267-analysis/water-267-298K.dahist1_02_1_01_02.angle.norm",
+                                        Data1DImportFileFormat::Data1DImportFormat::XY},
+                                       6.0e-5));
+}
+
+} // namespace UnitTest
