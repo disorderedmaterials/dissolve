7440cb16fe82b6e32d0fc614151422238e913eec
Jared Swift <j.w.swift@outlook.com>
2024-02-25 22:24:40 +0000

    Tidy and export for Angle.

--- src/modules/angle/process.cpp
+++ src/modules/angle/process.cpp
@@ -34,46 +34,48 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
     // Select site C
     SiteSelector c(targetConfiguration_, c_);
 
+    // RDF(A-B)
     auto [rAB, rABStatus] = processingData.realiseIf<Histogram1D>("rAB", name(), GenericItem::InRestartFileFlag);
     if (rABStatus == GenericItem::ItemStatus::Created)
         rAB.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z);
+    
+    // RDF(B-C)
     auto [rBC, rBCStatus] = processingData.realiseIf<Histogram1D>("rBC", name(), GenericItem::InRestartFileFlag);
     if (rBCStatus == GenericItem::ItemStatus::Created)
         rBC.initialise(rangeBC_.x, rangeBC_.y, rangeBC_.z);
 
+    // Angle(A-B-C)
     auto [aABC, aABCStatus] = processingData.realiseIf<Histogram1D>("aABC", name(), GenericItem::InRestartFileFlag);
     if (aABCStatus == GenericItem::ItemStatus::Created)
         aABC.initialise(angleRange_.x, angleRange_.y, angleRange_.z);
 
-    auto [daABc, daABCStatus] = processingData.realiseIf<Histogram2D>("daABc", name(), GenericItem::InRestartFileFlag);
-    if (daABCStatus == GenericItem::ItemStatus::Created)
-        daABc.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z, angleRange_.x, angleRange_.y, angleRange_.z);
+    // DAngle((A-B)-C)
+    auto [dAngleAB, dAngleABStatus] = processingData.realiseIf<Histogram2D>("DAngleAB", name(), GenericItem::InRestartFileFlag);
+    if (dAngleABStatus == GenericItem::ItemStatus::Created)
+        dAngleAB.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z, angleRange_.x, angleRange_.y, angleRange_.z);
 
-    auto [daaBC, daaBCStatus] = processingData.realiseIf<Histogram2D>("daaBC", name(), GenericItem::InRestartFileFlag);
-    if (daaBCStatus == GenericItem::ItemStatus::Created)
-        daaBC.initialise(rangeBC_.x, rangeBC_.y, rangeBC_.z, angleRange_.x, angleRange_.y, angleRange_.z);
+    // DAngle(A-(B-C))
+    auto [dAngleBC, dAngleBCStatus] = processingData.realiseIf<Histogram2D>("DAngleBC", name(), GenericItem::InRestartFileFlag);
+    if (dAngleBCStatus == GenericItem::ItemStatus::Created)
+        dAngleBC.initialise(rangeBC_.x, rangeBC_.y, rangeBC_.z, angleRange_.x, angleRange_.y, angleRange_.z);
 
-    auto [dda, ddaStatus] = processingData.realiseIf<Histogram3D>("dda", name(), GenericItem::InRestartFileFlag);
-    if (ddaStatus == GenericItem::ItemStatus::Created)
-        dda.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z, rangeBC_.x, rangeBC_.y, rangeBC_.z, angleRange_.x, angleRange_.y,
+    // DDAngle(A-B-C)
+    auto [dAngleABC, dAngleABCStatus] = processingData.realiseIf<Histogram3D>("DAngleABC", name(), GenericItem::InRestartFileFlag);
+    if (dAngleABCStatus == GenericItem::ItemStatus::Created)
+        dAngleABC.initialise(rangeAB_.x, rangeAB_.y, rangeAB_.z, rangeBC_.x, rangeBC_.y, rangeBC_.z, angleRange_.x, angleRange_.y,
                        angleRange_.z);
 
     rAB.zeroBins();
     rBC.zeroBins();
     aABC.zeroBins();
-    daABc.zeroBins();
-    daaBC.zeroBins();
-    dda.zeroBins();
+    dAngleAB.zeroBins();
+    dAngleBC.zeroBins();
+    dAngleABC.zeroBins();
 
-    auto nACumulative = 0;
-    auto nBCumulative = 0;
-    auto nCCumulative = 0;
-    auto nAAvailable = 0;
-    auto nBAvailable = 0;
-    auto nCAvailable = 0;
+    auto nAAvailable = a.sites().size(), nACumulative = a.sites().size();
     auto nASelections = 1;
-    auto nBSelections = 0;
-    auto nCSelections = 0;
+    auto nBAvailable = 0, nBCumulative = 0, nBSelections = 0;
+    auto nCAvailable = 0, nCCumulative = 0, nCSelections = 0;
 
     for (const auto &[siteA, indexA] : a.sites())
     {
@@ -89,13 +91,6 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
             auto distAB = targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin());
 
             nBAvailable++;
-
-            if (!Range(rangeAB_.x, rangeAB_.y)
-                     .contains(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteA->origin())))
-                continue;
-
-            rAB.bin(distAB);
-
             nBCumulative++;
             nCSelections++;
 
@@ -109,78 +104,183 @@ Module::ExecutionResult AngleModule::process(ModuleContext &moduleContext)
                     continue;
 
                 nCAvailable++;
-
-                if (!Range(rangeBC_.x, rangeBC_.y)
-                         .contains(targetConfiguration_->box()->minimumDistance(siteC->origin(), siteB->origin())))
-                    continue;
-
                 nCCumulative++;
 
                 auto distBC = targetConfiguration_->box()->minimumDistance(siteC->origin(), siteB->origin());
-
-                rBC.bin(distBC);
-
                 auto angle = targetConfiguration_->box()->angleInDegrees(siteA->origin(), siteB->origin(), siteC->origin());
                 if (symmetric_ && angle > 90.0)
                     angle = 180.0 - angle;
-                aABC.bin(angle);
 
-                daABc.bin(distAB, angle);
+                if (dAngleABC.bin(distAB, distBC, angle))
+                {
+                    dAngleBC.bin(distBC, angle);
+                    dAngleAB.bin(distAB, angle);
+                    aABC.bin(angle);
+                    rAB.bin(distAB);
+                    rBC.bin(distBC);
+                }            
 
-                daaBC.bin(distBC, angle);
-
-                dda.bin(distAB, distBC, angle);
             }
         }
     }
 
+    // Accumulate histograms
     rAB.accumulate();
     rBC.accumulate();
     aABC.accumulate();
-    daABc.accumulate();
-    daaBC.accumulate();
-    dda.accumulate();
+    dAngleAB.accumulate();
+    dAngleBC.accumulate();
+    dAngleABC.accumulate();
 
+    // RDF(A-B)s
     auto &normalisedAB = processingData.realise<Data1D>("RDF(AB)", name(), GenericItem::InRestartFileFlag);
     normalisedAB = rAB.accumulatedData();
     DataNormaliser1D normaliserAB(normalisedAB);
+    // Normalise by A site population
     normaliserAB.normaliseDivide(double(nACumulative) / nASelections);
+    // Normalise by B site population density
     normaliserAB.normaliseDivide((double(nBCumulative) / nBSelections) / targetConfiguration_->box()->volume());
+    // Normalise by spherical shell
     normaliserAB.normaliseBySphericalShell();
 
+    // RDF(B-C)
     auto &normalisedBC = processingData.realise<Data1D>("RDF(BC)", name(), GenericItem::InRestartFileFlag);
     normalisedBC = rBC.accumulatedData();
     DataNormaliser1D normaliserBC(normalisedBC);
+    // Normalise by A site population
     normaliserBC.normaliseDivide(double(nACumulative) / nASelections);
+    // Normalise by B site population
     normaliserBC.normaliseDivide(double(nBCumulative) / nBSelections);
+    // Normalise by c site population density
     normaliserBC.normaliseDivide((double(nCAvailable) / nCSelections) / targetConfiguration_->box()->volume());
+    // Normalise by spherical shell
     normaliserBC.normaliseBySphericalShell();
 
+    // Angle(A-B-C)
     auto &normalisedAngle = processingData.realise<Data1D>("Angle(ABC)", name(), GenericItem::InRestartFileFlag);
     normalisedAngle = aABC.accumulatedData();
     DataNormaliser1D normaliserAngle(normalisedAngle);
+    // Normalise by value / sin(x)
     normaliserAngle.normalise([](const auto &x, const auto &xDelta, const auto &value) { return value / sin(x / DEGRAD); });
+    // Normalise to 1.0
     normaliserAngle.normaliseTo();
 
+    // DAngle((A-B)-C)
     auto &normalisedDAngleAB = processingData.realise<Data2D>("DAngle((A-B)-C)", name(), GenericItem::InRestartFileFlag);
-    normalisedDAngleAB = daABc.accumulatedData();
+    normalisedDAngleAB = dAngleAB.accumulatedData();
     DataNormaliser2D normaliserDAngleAB(normalisedDAngleAB);
+    // Normalise by value / sin(y) / sin(yDelta)
     normaliserDAngleAB.normalise([&](const auto &x, const auto &xDelta, const auto &y, const auto &yDelta, const auto &value)
                                  { return (symmetric_ ? value : value * 2.0) / sin(y / DEGRAD) / sin(yDelta / DEGRAD); });
+    // Normalise by A site population
     normaliserDAngleAB.normaliseDivide(double(nACumulative) / nASelections);
+    // Normalise by C site population
     normaliserDAngleAB.normaliseDivide(double(nCCumulative) / nCSelections);
+    // Normalise by B site population density
     normaliserDAngleAB.normaliseDivide((double(nBAvailable) / nBSelections) / targetConfiguration_->box()->volume());
+    // Normalise by spherical shell
     normaliserDAngleAB.normaliseBySphericalShell();
 
+    // DAngle(A-(B-C))
     auto &normalisedDAngleBC = processingData.realise<Data2D>("DAngle(A-(B-C))", name(), GenericItem::InRestartFileFlag);
-    normalisedDAngleBC = daaBC.accumulatedData();
+    normalisedDAngleBC = dAngleBC.accumulatedData();
     DataNormaliser2D normaliserDAngleBC(normalisedDAngleBC);
+    // Normalise by value / sin(y) / sin(yDelta)
     normaliserDAngleBC.normalise([&](const auto &x, const auto &xDelta, const auto &y, const auto &yDelta, const auto &value)
                                  { return (symmetric_ ? value : value * 2.0) / sin(y / DEGRAD) / sin(yDelta / DEGRAD); });
+    // Normalise by A site population
     normaliserDAngleBC.normaliseDivide(double(nACumulative) / nASelections);
+    // Normalise by B site population
     normaliserDAngleBC.normaliseDivide(double(nBCumulative) / nBSelections);
+    // Normalise by C site population density
     normaliserDAngleBC.normaliseDivide((double(nCAvailable) / nCSelections) / targetConfiguration_->box()->volume());
+    // Normalise by spherical shell
     normaliserDAngleBC.normaliseBySphericalShell();
+
+    // Save RDF(A-B) data?
+    if (exportFileAndFormatAB_.hasFilename())
+    {
+        if (moduleContext.processPool().isMaster())
+        {
+            if (exportFileAndFormatAB_.exportData(normalisedAB))
+                moduleContext.processPool().decideTrue();
+            else
+            {
+                moduleContext.processPool().decideFalse();
+                return ExecutionResult::Failed;
+            }
+        }
+        else if (!moduleContext.processPool().decision())
+            return ExecutionResult::Failed;
+    }
+
+    // Save RDF(B-C) data?
+    if (exportFileAndFormatBC_.hasFilename())
+    {
+        if (moduleContext.processPool().isMaster())
+        {
+            if (exportFileAndFormatBC_.exportData(normalisedBC))
+                moduleContext.processPool().decideTrue();
+            else
+            {
+                moduleContext.processPool().decideFalse();
+                return ExecutionResult::Failed;
+            }
+        }
+        else if (!moduleContext.processPool().decision())
+            return ExecutionResult::Failed;
+    }
+
+    // Save Angle(A-B-C) data?
+    if (exportFileAndFormatAngle_.hasFilename())
+    {
+        if (moduleContext.processPool().isMaster())
+        {
+            if (exportFileAndFormatAngle_.exportData(normalisedAngle))
+                moduleContext.processPool().decideTrue();
+            else
+            {
+                moduleContext.processPool().decideFalse();
+                return ExecutionResult::Failed;
+            }
+        }
+        else if (!moduleContext.processPool().decision())
+            return ExecutionResult::Failed;
+    }
+
+    // Save DAngle((A-B)-C) data?
+    if (exportFileAndFormatDAngleAB_.hasFilename())
+    {
+        if (moduleContext.processPool().isMaster())
+        {
+            if (exportFileAndFormatDAngleAB_.exportData(normalisedDAngleAB))
+                moduleContext.processPool().decideTrue();
+            else
+            {
+                moduleContext.processPool().decideFalse();
+                return ExecutionResult::Failed;
+            }
+        }
+        else if (!moduleContext.processPool().decision())
+            return ExecutionResult::Failed;
+    }
+
+    // Save DAngle(A-(B-C)) data?
+    if (exportFileAndFormatDAngleBC_.hasFilename())
+    {
+        if (moduleContext.processPool().isMaster())
+        {
+            if (exportFileAndFormatDAngleBC_.exportData(normalisedDAngleBC))
+                moduleContext.processPool().decideTrue();
+            else
+            {
+                moduleContext.processPool().decideFalse();
+                return ExecutionResult::Failed;
+            }
+        }
+        else if (!moduleContext.processPool().decision())
+            return ExecutionResult::Failed;
+    }
 
     return ExecutionResult::Success;
 }
