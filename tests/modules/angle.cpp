2828cbd4daba06225349290fa36b321d7892ad85
Jared Swift <j.w.swift@outlook.com>
2024-02-25 18:01:58 +0000

    Update system test to check RDF(B-C).

--- tests/modules/angle.cpp
+++ tests/modules/angle.cpp
@@ -18,6 +18,11 @@ TEST_F(AngleModuleTest, Water)
     ASSERT_NO_THROW(systemTest.setUp("dissolve/input/angle.txt"));
     ASSERT_TRUE(systemTest.iterateRestart(95));
 
+
+    EXPECT_TRUE(systemTest.checkData1D(
+        "Angle(X-H...O)//RDF(BC)",
+        {"dlpoly/water267-analysis/water-267-298K.aardf_21_23_inter_sum", Data1DImportFileFormat::Data1DImportFormat::XY},
+        3.0e-3));
     EXPECT_TRUE(systemTest.checkData1D("Angle(X-H...O)//Angle(ABC)",
                                        {"dlpoly/water267-analysis/water-267-298K.dahist1_02_1_01_02.angle.norm",
                                         Data1DImportFileFormat::Data1DImportFormat::XY},
