c220ff8006539d461f598e22031d7126f0c7df9d
Adam Washington <adam.washington@stfc.ac.uk>
2024-02-27 09:05:41 +0000

    perf: 1806 faster combinations (#1807)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>

---
+++ src/math/polynomial.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
+#include <utility>
+
+class Quadratic
+{
+    private:
+    double a_, b_, c_;
+
+    public:
+    Quadratic(double a = 0, double b = 0, double c = 0);
+    bool hasRoots() const;
+    std::pair<double, double> roots() const;
+    double at(double x) const;
+};
---
+++ src/templates/polynomial.cpp
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
+#include "templates/polynomial.h"
+#include <cmath>
+
+Quadratic::Quadratic(double a, double b, double c) : a_(a), b_(b), c_(c) {}
