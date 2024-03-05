1b60ce2b4c6b5e04b80208bcbaa38b1fc72f406d
Noella Spitz <101950441+rhinoella@users.noreply.github.com>
2024-03-05 15:22:13 +0000

    1620 Compare Module GUI Implementation (#1662)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>
    Co-authored-by: Adam Washington <adam.washington@stfc.ac.uk>
    Co-authored-by: Danbr4d <laydb3@nottingham.ac.uk>
    Co-authored-by: Jared Swift <j.w.swift@outlook.com>

--- src/modules/widgetProducer.cpp
+++ src/modules/widgetProducer.cpp
@@ -18,6 +18,8 @@
 #include "modules/benchmark/gui/benchmarkWidget.h"
 #include "modules/bragg/bragg.h"
 #include "modules/bragg/gui/braggWidget.h"
+#include "modules/compare/compare.h"
+#include "modules/compare/gui/compareWidget.h"
 #include "modules/dAngle/dAngle.h"
 #include "modules/dAngle/gui/dAngleWidget.h"
 #include "modules/energy/energy.h"
@@ -64,6 +66,7 @@ ModuleWidgetProducer::ModuleWidgetProducer()
     registerProducer<AxisAngleModule, AxisAngleModuleWidget>();
     registerProducer<BenchmarkModule, BenchmarkModuleWidget>();
     registerProducer<BraggModule, BraggModuleWidget>();
+    registerProducer<CompareModule, CompareModuleWidget>();
     registerProducer<DAngleModule, DAngleModuleWidget>();
     registerProducer<EnergyModule, EnergyModuleWidget>();
     registerProducer<EPSRModule, EPSRModuleWidget>();
---
+++ tests/modules/compare.cpp
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
+#include "modules/compare/compare.h"
+#include "tests/testData.h"
+#include <gtest/gtest.h>
+
+namespace UnitTest
+{
+using RangeErrorPair = std::pair<std::vector<Range>, std::vector<double>>;
+
+// Water Molecule
+class CompareModuleTest : public ::testing::Test
+{
+    protected:
+    DissolveSystemTest systemTest;
+
+    void SetUp() override
+    {
+        ASSERT_NO_THROW(systemTest.setUp("dissolve/input/epsr-benzene-3n.txt"));
+        ASSERT_TRUE(systemTest.dissolve().iterate(1));
+    }
+};
+
+TEST_F(CompareModuleTest, IntVsIntErrors)
+{
+    auto *compareModule = systemTest.getModule<CompareModule>("Compare01");
+    ASSERT_TRUE(compareModule);
+
+    const auto &ranges = compareModule->data1dSourcesErrors().at(&compareModule->data1dSources()[0]).first;
+    const auto &errors = compareModule->data1dSourcesErrors().at(&compareModule->data1dSources()[0]).second;
+
+    EXPECT_NEAR(ranges[0].minimum(), 0, 1.0e-2);
+    EXPECT_NEAR(ranges[0].maximum(), 50, 1.0e-4);
+    EXPECT_NEAR(ranges[1].minimum(), 0, 1.0e-4);
+    EXPECT_NEAR(ranges[1].maximum(), 10, 1.0e-4);
+
+    EXPECT_NEAR(errors[0], 3.0942e-1, 1.0e-4);
+    EXPECT_NEAR(errors[1], 9.2657e-1, 1.0e-4);
+}
+
+TEST_F(CompareModuleTest, IntVsExtErrors)
+{
+    auto *compareModule = systemTest.getModule<CompareModule>("Compare01");
+    ASSERT_TRUE(compareModule);
+
+    const auto &ranges = compareModule->data1dSourcesErrors().at(&compareModule->data1dSources()[1]).first;
+    const auto &errors = compareModule->data1dSourcesErrors().at(&compareModule->data1dSources()[1]).second;
+
+    EXPECT_NEAR(ranges[0].minimum(), 0, 1.0e-2);
+    EXPECT_NEAR(ranges[0].maximum(), 50, 1.0e-4);
+    EXPECT_NEAR(ranges[1].minimum(), 0, 1.0e-4);
+    EXPECT_NEAR(ranges[1].maximum(), 10, 1.0e-4);
+
+    EXPECT_NEAR(errors[0], 3.0942e-1, 1.0e-4);
+    EXPECT_NEAR(errors[1], 9.2657e-1, 1.0e-4);
+}
+
+TEST_F(CompareModuleTest, ExtVsExtErrors)
+{
+    auto *compareModule = systemTest.getModule<CompareModule>("Compare01");
+    ASSERT_TRUE(compareModule);
+
+    const auto &ranges = compareModule->data1dSourcesErrors().at(&compareModule->data1dSources()[2]).first;
+    const auto &errors = compareModule->data1dSourcesErrors().at(&compareModule->data1dSources()[2]).second;
+
+    EXPECT_NEAR(ranges[0].minimum(), 0, 1.0e-2);
+    EXPECT_NEAR(ranges[0].maximum(), 50, 1.0e-4);
+    EXPECT_NEAR(ranges[1].minimum(), 0, 1.0e-4);
+    EXPECT_NEAR(ranges[1].maximum(), 10, 1.0e-4);
+
+    EXPECT_NEAR(errors[0], 7.0069e-3, 1.0e-4);
+    EXPECT_NEAR(errors[1], 2.0418e-2, 1.0e-4);
+}
+
+TEST_F(CompareModuleTest, IntVsIntDelta)
+{
+    EXPECT_TRUE(systemTest.checkData1D(
+        "Compare01//Pair1_Delta", {"dissolve/compare/intvsint.dat", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}));
+}
+
+TEST_F(CompareModuleTest, IntVsExtDelta)
+{
+    EXPECT_TRUE(systemTest.checkData1D(
+        "Compare01//Pair2_Delta", {"dissolve/compare/intvsext.dat", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}));
+}
+
+TEST_F(CompareModuleTest, ExtVsExtDelta)
+{
+    EXPECT_TRUE(systemTest.checkData1D(
+        "Compare01//Pair3_Delta", {"dissolve/compare/extvsext.dat", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}));
+}
+
+} // namespace UnitTest
