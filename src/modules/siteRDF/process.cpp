f3bd189356125775fef8e91f2d58730afab2f651
Danbr4d <laydb3@nottingham.ac.uk>
2024-03-15 16:40:49 +0000

    initial loop setup

--- src/modules/siteRDF/process.cpp
+++ src/modules/siteRDF/process.cpp
@@ -66,12 +66,11 @@ Module::ExecutionResult SiteRDFModule::process(ModuleContext &moduleContext)
     // CN
     auto &dataCN = processingData.realise<Data1D>("HistogramNorm", name(), GenericItem::InRestartFileFlag);
     dataCN = histAB.accumulatedData();
-    auto &runningCN = processingData.realise<Data1D>("RunningCNNorm", name(), GenericItem::InRestartFileFlag);
-    runningCN = histAB.accumulatedData();
 
     // Normalise
     DataOperator1D normaliserCN(dataCN);
     // Normalise by A site population
+    normaliserCN.divide(double(a.sites().size()));
     DataNormaliser1D normaliserRunningCN(runningCN);
     normaliserRunningCN.normaliseDivide(double(a.sites().size()));
 
@@ -100,8 +99,15 @@ Module::ExecutionResult SiteRDFModule::process(ModuleContext &moduleContext)
                 }
             }
         }
-    //Loop over all distanceRange and calculate running CN
-    // for (double x; distanceRange_[2]; x += distanceRange_[3])
+    auto &sumRunNHist = processingData.realise<Histogram1D>("RunningCN", name(), GenericItem::InRestartFileFlag);
+    auto &sumRunN = processingData.realise<SampledDouble>(fmt::format("RunningCNCount//{}", distanceRange_), name(),
+                                                          GenericItem::InRestartFileFlag);
+    // Loop over all distanceRange and calculate running CN
+    for (double x; distanceRange_[2]; x += distanceRange_[3])
+    {
+        sumRunN += Integrator::sum(dataCN, x, x + distanceRange_[3]);
+        sumRunNHist.bin(sumRunN);
+    }
 
     // Save RDF data?
     if (!DataExporter<Data1D, Data1DExportFileFormat>::exportData(dataRDF, exportFileAndFormat_, moduleContext.processPool()))
