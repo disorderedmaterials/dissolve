947ca80ccc920c1d3025fcf5b57ca844c2b77a06
Jared Swift <j.w.swift@outlook.com>
2024-03-17 13:07:05 +0000

    SelectionProcessor setup.

---
+++ src/analyser/selectionProcessor.cpp
@@ -0,0 +1,2 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
---
+++ src/analyser/selectionProcessor.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
+#pragma once
+
+class SelectionProcessor
+{
+    /*
+    TODO:
+        - Implement a method that takes as input a vector of "candidate selections sites", and a vector of "rules" and a
+    "processing function" and applies the "processing function" to the "selected sites" according to the "rules"
+        - This should also keep track of various statistics about the selections made
+
+    */
+};
