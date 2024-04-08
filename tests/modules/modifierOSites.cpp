d7fc3a7ef9c7a079437af915d16b287fa4e82d52
Dan Bradley <laydb3@nottingham.ac.uk>
2024-04-08 10:45:06 +0100

    updated tests

--- tests/modules/modifierOSites.cpp
+++ tests/modules/modifierOSites.cpp
@@ -29,25 +29,15 @@ TEST_F(ModifierOSitesModuleTest, TotalOSitesWBondLengths)
     ASSERT_NO_THROW_VERBOSE(systemTest.setUp("dissolve/input/modifierOSites-test2.txt"));
     ASSERT_TRUE(systemTest.dissolve().iterate(1));
 
-    const auto &mFOdata = systemTest.dissolve().processingModuleData().search<const Data1D>("M//MFOBondLength")->get();
-    const auto &mNBOdata = systemTest.dissolve().processingModuleData().search<const Data1D>("M//MNBOBondLength")->get();
-    const auto &mBOdata = systemTest.dissolve().processingModuleData().search<const Data1D>("M//MBOBondLength")->get();
-
-    // 1A M-O
-    ASSERT_EQ(mFOdata.value(20), 0.5);
-    // 2A M-O
-    ASSERT_EQ(mFOdata.value(40), 0.5);
-
-    // 1.5A M-O
-    ASSERT_EQ(mNBOdata.value(30), 0.125);
-    ASSERT_EQ(mNBOdata.value(40), 0.5);
-    // sqrt(5) M-O
-    ASSERT_EQ(mNBOdata.value(44), 0.25);
-    // 3 M-O
-    ASSERT_EQ(mNBOdata.value(60), 0.125);
-
-    ASSERT_NEAR(mBOdata.value(20), 0.33333333333, 1e-8);
-    ASSERT_NEAR(mBOdata.value(40), 0.66666666666, 1e-8);
+    EXPECT_TRUE(systemTest.checkData1D(
+        "M//MFOBondLength", {"dissolve/input/FOBondLengthTest.dat", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2},
+        1.0e-8));
+    EXPECT_TRUE(systemTest.checkData1D(
+        "M//MNBOBondLength", {"dissolve/input/NBOBondLengthTest.dat", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2},
+        1.0e-8));
+    EXPECT_TRUE(systemTest.checkData1D(
+        "M//MBOBondLength", {"dissolve/input/BOBondLengthTest.dat", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2},
+        1.0e-8));
 }
 
 } // namespace UnitTest
