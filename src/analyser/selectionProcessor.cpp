baf72ada9d6d46b07615761cc1b61b121ef48da4
Jared Swift <j.w.swift@outlook.com>
2024-04-21 12:18:06 +0100

    feat: basic logic.

--- src/analyser/selectionProcessor.cpp
+++ src/analyser/selectionProcessor.cpp
@@ -1,2 +1,93 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2024 Team Dissolve and contributors
+
+#include "analyser/selectionProcessor.h"
+#include "classes/site.h"
+
+int SelectionProcessor::nASelections() const { return nASelections_; }
+int SelectionProcessor::nAAvailable() const { return nAAvailable_; }
+int SelectionProcessor::nACumulative() const { return nACumulative_; }
+int SelectionProcessor::nBSelections() const { return nBSelections_; }
+int SelectionProcessor::nBAvailable() const { return nBAvailable_; }
+int SelectionProcessor::nBCumulative() const { return nBCumulative_; }
+int SelectionProcessor::nCSelections() const { return nCSelections_; }
+int SelectionProcessor::nCAvailable() const { return nCAvailable_; }
+int SelectionProcessor::nCCumulative() const { return nCCumulative_; }
+
+// First focus on getting the actual processing working, then we worry about multithreading afterwards.
+bool SelectionProcessor::process(
+    const Analyser::SiteVector &sitesA, std::optional<Analyser::SiteVector &> sitesB,
+    std::optional<Analyser::SiteVector &> sitesC, const Flags<SelectionRuleFlags> &flags,
+    std::function<bool(const Site *, std::optional<Site *>, std::optional<const Site *>)> processFunction)
+{
+    reset();
+    nASelections_ = 1;
+
+    // Loop over A sites
+    for (const auto &[siteA, indexA] : sitesA)
+    {
+        ++nAAvailable_;
+        ++nACumulative_;
+
+        // Check if B sites are available
+        if (sitesB.has_value())
+        {
+            ++nBSelections_;
+
+            for (const auto &[siteB, indexB] : sitesB.value())
+            {
+
+                if (flags.isSet(IndexALTIndexB) && indexA >= indexB)
+                    continue;
+                if (flags.isSet(IndexAGTIndexB) && indexA <= indexB)
+                    continue;
+                if (flags.isSet(MoleculeAEQMoleculeB) && siteA->molecule() != siteB->molecule())
+                    continue;
+                if (flags.isSet(MoleculeANEQMoleculeB) && siteA->molecule() == siteB->molecule())
+                    continue;
+
+                ++nBAvailable_;
+                ++nBCumulative_;
+
+                // Check if C sites are available
+                if (sitesC.has_value())
+                {
+                    ++nCSelections_;
+                    for (const auto &[siteC, indexC] : sitesC.value())
+                    {
+                        if (flags.isSet(IndexBLTIndexC) && indexB >= indexC)
+                            continue;
+                        if (flags.isSet(IndexBGTIndexC) && indexB <= indexC)
+                            continue;
+                        if (flags.isSet(MoleculeBEQMoleculeC) && siteB->molecule() != siteC->molecule())
+                            continue;
+                        if (flags.isSet(MoleculeBNEQMoleculeC) && siteB->molecule() == siteC->molecule())
+                            continue;
+
+                        ++nCAvailable_;
+                        ++nCCumulative_;
+
+                        processFunction(siteA, siteB, siteC);
+                    }
+                }
+                else
+                    processFunction(siteA, siteB, std::nullopt);
+            }
+        }
+        else
+            processFunction(siteA, std::nullopt, std::nullopt);
+    }
+}
+
+void SelectionProcessor::reset()
+{
+    nASelections_ = 0;
+    nAAvailable_ = 0;
+    nACumulative_ = 0;
+    nBSelections_ = 0;
+    nBAvailable_ = 0;
+    nBCumulative_ = 0;
+    nCSelections_ = 0;
+    nCAvailable_ = 0;
+    nCCumulative_ = 0;
+}
--- src/analyser/selectionProcessor.h
+++ src/analyser/selectionProcessor.h
@@ -3,6 +3,54 @@
 
 #pragma once
 
+#include "analyser/typeDefs.h"
+#include "templates/flags.h"
+#include <functional>
+#include <optional>
+
 class SelectionProcessor
 {
+
+    public:
+    // Selection rule flags
+    enum SelectionRuleFlags
+    {
+        IndexALTIndexB,        /* Index A less than Index B */
+        IndexAGTIndexB,        /* Index A greater than Index B */
+        IndexBLTIndexC,        /* Index B less than Index C */
+        IndexBGTIndexC,        /* Index B greater than Index C */
+        MoleculeAEQMoleculeB,  /* Molecule A equal to Molecule B */
+        MoleculeBEQMoleculeC,  /* Molecule B equal to Molecule C */
+        MoleculeANEQMoleculeB, /* Molecule A not equal to Molecule B */
+        MoleculeBNEQMoleculeC, /* Molecule B not equal to Molecule C */
+    };
+
+    public:
+    SelectionProcessor() = default;
+    ~SelectionProcessor() = default;
+
+    private:
+    int nASelections_{0}, nAAvailable_{0}, nACumulative_{0};
+    int nBSelections_{0}, nBAvailable_{0}, nBCumulative_{0};
+    int nCSelections_{0}, nCAvailable_{0}, nCCumulative_{0};
+
+    public:
+    int nASelections() const;
+    int nAAvailable() const;
+    int nACumulative() const;
+    int nBSelections() const;
+    int nBAvailable() const;
+    int nBCumulative() const;
+    int nCSelections() const;
+    int nCAvailable() const;
+    int nCCumulative() const;
+
+    public:
+    bool
+    process(const Analyser::SiteVector &sitesA, std::optional<Analyser::SiteVector &> sitesB = std::nullopt,
+            std::optional<Analyser::SiteVector &> sitesC = std::nullopt, const Flags<SelectionRuleFlags> &flags = {},
+            std::function<bool(const Site *, std::optional<const Site *>, std::optional<const Site *>)> processFunction = {});
+
+    private:
+    void reset();
 };
