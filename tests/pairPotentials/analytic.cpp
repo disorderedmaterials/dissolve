e8f131a8bf43f8f146a9eeb8d2b6dadc918d6929
Danbr4d <laydb3@nottingham.ac.uk>
2024-04-30 16:09:24 +0100

    corrected test valyes

--- tests/pairPotentials/analytic.cpp
+++ tests/pairPotentials/analytic.cpp
@@ -89,7 +89,7 @@ TEST_F(PairPotentialsTest, LennardJonesForm)
 TEST_F(PairPotentialsTest, Buckingham)
 {
     // Values put in for TeO2 from https://pubs.rsc.org/en/content/articlelanding/2014/cp/c4cp01273a
-    testEnergy(Functions1D::Form::Buckingham, "A=1595.266748  B=2.8912848  C=1", testRDelta_ * 5);
-    testForce(Functions1D::Form::Buckingham, "A=82970.688434  B=6.211565  C=31.361954", testRDelta_ * 5);
+    testEnergy(Functions1D::Form::Buckingham, "A=153919.5503  B=2.8912848  C=96.48514925", testRDelta_ * 5);
+    testForce(Functions1D::Form::Buckingham, "A=8005439.257  B=6.211565  C=3025.962812", testRDelta_ * 5);
 }
 } // namespace UnitTest
