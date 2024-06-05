35176b97dfde29138ac52f10a380ce78d8cf795b
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2024-06-05 09:12:25 +0100

    Add missing copyright header.

--- src/gui/models/externalPotentialModel.cpp
+++ src/gui/models/externalPotentialModel.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
 #include "gui/models/externalPotentialModel.h"
 #include "kernels/potentials/directional.h"
 
