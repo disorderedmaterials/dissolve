20a1d628deff27f5bf244622dc66898c3f05390d
Danbr4d <laydb3@nottingham.ac.uk>
2024-06-27 16:49:55 +0100

    initial tr setup

--- src/modules/registry.cpp
+++ src/modules/registry.cpp
@@ -36,6 +36,7 @@
 #include "modules/sq/sq.h"
 #include "modules/temperatureSchedule/temperatureSchedule.h"
 #include "modules/test/test.h"
+#include "modules/tr/tr.h"
 #include "modules/voxelDensity/voxelDensity.h"
 #include "modules/xRaySQ/xRaySQ.h"
 
@@ -91,6 +92,7 @@ ModuleRegistry::ModuleRegistry()
     registerProducer<TemperatureScheduleModule>(ModuleTypes::TemperatureSchedule,
                                                 "Adjust the temperature of a configuration during a simulation", "Evolution");
     registerProducer<TestModule>(ModuleTypes::Test, "Development Module");
+    registerProducer<TRModule>(ModuleTypes::TR, "Calculate the partial and total t(r)", "Correlation Functions");
     registerProducer<VoxelDensityModule>(
         ModuleTypes::VoxelDensity,
         "Describe distribution of atomic number, mass, and scattering length density across unit cell voxels", "Analysis");
