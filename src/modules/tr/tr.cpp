a7c0070f36f1d5d293e21d55e3b6e2c675966ba2
Danbr4d <laydb3@nottingham.ac.uk>
2024-07-01 14:43:55 +0100

    add in tr functionality

--- src/modules/tr/tr.cpp
+++ src/modules/tr/tr.cpp
@@ -10,51 +10,14 @@
 #include "keywords/module.h"
 #include "keywords/optionalDouble.h"
 #include "keywords/optionalInt.h"
+#include "modules/neutronSQ/neutronSQ.h"
 
 TRModule::TRModule() : Module(ModuleTypes::TR)
 {
-    keywords_.addTarget<ConfigurationVectorKeyword>("Configurations", "Set target configuration(s) for the module",
-                                                    targetConfigurations_);
-
-    keywords_.setOrganisation("Options", "Range");
-    keywords_.add<DoubleKeyword>("BinWidth", "Bin width (spacing in r) to use", binWidth_, 1.0e-3);
-    keywords_.add<OptionalDoubleKeyword>("Range", "Maximum r to calculate g(r) out to", requestedRange_, 0.0, std::nullopt, 1.0,
-                                         "Use Half Cell Range");
-
-    keywords_.setOrganisation("Options", "Averaging & Smoothing");
-    keywords_.add<OptionalIntegerKeyword>("Averaging", "Number of historical partial sets to combine into final partials",
-                                          averagingLength_, 1, std::nullopt, 1, "Off");
-    keywords_.add<EnumOptionsKeyword<Averaging::AveragingScheme>>(
-        "AveragingScheme", "Weighting scheme to use when averaging partials", averagingScheme_, Averaging::averagingSchemes());
-    keywords_.add<Function1DKeyword>("IntraBroadening", "Type of broadening to apply to intramolecular g(r)", intraBroadening_,
-                                     FunctionProperties::Normalisation);
-    keywords_.add<OptionalIntegerKeyword>("Smoothing", "Specifies the degree of smoothing to apply to calculated g(r)",
-                                          nSmooths_, 0, 100, 1, "Off");
-
-    keywords_.setOrganisation("Export");
-    keywords_.add<BoolKeyword>("Save", "Whether to save partials and total functions to disk", save_);
-    keywords_.add<BoolKeyword>("SaveOriginal", "Whether to save original (unbroadened) partials and total functions to disk",
-                               saveOriginal_);
-
-    keywords_.setOrganisation("Advanced");
-    keywords_.add<BoolKeyword>(
-        "InternalTest",
-        "Perform internal check of calculated partials against a set calculated by a simple unoptimised double-loop",
-        internalTest_);
-    keywords_.add<EnumOptionsKeyword<TRModule::PartialsMethod>>(
-        "Method", "Calculation method for partial radial distribution functions", partialsMethod_, TRModule::partialsMethods());
-
+    keywords_.addTarget<ModuleKeyword<const NeutronSQModule>>(
+        "SourceNeutronSQs", "Source NeutronSQ to calculate total T(r) from", sourceNeutronSQ_, ModuleTypes::NeutronSQ);
     // Deprecated
     static bool deprecatedBool_{false};
     keywords_.addDeprecated<BoolKeyword>(
         "UseHalfCellRange", "Whether to use the maximal RDF range possible that avoids periodic images", deprecatedBool_);
-}
-
-// Return enum option info for NormalisationType
-EnumOptions<TRModule::PartialsMethod> TRModule::partialsMethods()
-{
-    return EnumOptions<TRModule::PartialsMethod>("PartialsMethod", {{TRModule::AutoMethod, "Auto"},
-                                                                    {TRModule::CellsMethod, "Cells"},
-                                                                    {TRModule::SimpleMethod, "Simple"},
-                                                                    {TRModule::TestMethod, "Test"}});
 }
--- src/modules/tr/tr.h
+++ src/modules/tr/tr.h
@@ -12,6 +12,7 @@
 class Dissolve;
 class ModuleGroup;
 class PartialSet;
+class NeutronSQModule;
 
 // RDF Module
 class TRModule : public Module
@@ -24,74 +25,15 @@ class TRModule : public Module
      * Definition
      */
     public:
-    // Partial Calculation Method enum
-    enum PartialsMethod
-    {
-        AutoMethod,
-        CellsMethod,
-        SimpleMethod,
-        TestMethod,
-        nPartialsMethods
-    };
-    // Return enum option info for PartialsMethod
-    EnumOptions<TRModule::PartialsMethod> partialsMethods();
-
     private:
-    // Target configurations
-    std::vector<Configuration *> targetConfigurations_;
-    // Number of historical partial sets to combine into final partials
-    std::optional<int> averagingLength_{5};
-    // Weighting scheme to use when averaging partials
-    Averaging::AveragingScheme averagingScheme_{Averaging::LinearAveraging};
-    // Bin width (spacing in r) to use
-    double binWidth_{0.025};
-    // Perform internal check of calculated partials against a set calculated by a simple unoptimised double-loop
-    bool internalTest_{false};
-    // Type of broadening to apply to intramolecular g(r)
-    Function1DWrapper intraBroadening_{Functions1D::Form::Gaussian, {0.18}};
-    // Degree of smoothing to apply
-    std::optional<int> nSmooths_;
-    // Calculation method for partials
-    TRModule::PartialsMethod partialsMethod_{TRModule::AutoMethod};
-    // Maximum r to calculate g(r) out to, unless UseHalfCellRange is true
-    std::optional<double> requestedRange_;
-    // Whether to save partials and total functions to disk
-    bool save_{false};
-    // Whether to save original (unbroadened) partials and total functions to disk
-    bool saveOriginal_{false};
+    // Source module for calculation
+    const NeutronSQModule *sourceNeutronSQ_{nullptr};
+    PartialSet unweightedtr;
+    PartialSet weightedtr;
 
     /*
      * Functions
      */
-    private:
-    // Calculate partial g(r) in serial with simple double-loop
-    bool calculateGRTestSerial(Configuration *cfg, PartialSet &partialSet);
-    // Calculate partial g(r) with optimised double-loop
-    bool calculateGRSimple(const ProcessPool &procPool, Configuration *cfg, PartialSet &partialSet, const double rdfRange);
-    // Calculate partial g(r) utilising Cell neighbour lists
-    bool calculateGRCells(const ProcessPool &procPool, Configuration *cfg, PartialSet &partialSet, const double binWidth);
-
-    public:
-    // Calculate and return effective density based on target Configurations
-    std::optional<double> effectiveDensity() const;
-    // Calculate and return used species populations based on target Configurations
-    std::vector<std::pair<const Species *, double>> speciesPopulations() const;
-    // (Re)calculate partial g(r) for the specified Configuration
-    bool calculateGR(GenericList &processingData, const ProcessPool &procPool, Configuration *cfg,
-                     TRModule::PartialsMethod method, const double rdfRange, const double rdfBinWidth, bool &alreadyUpToDate);
-    // Calculate smoothed/broadened partial g(r) from supplied partials
-    static bool calculateUnweightedGR(const ProcessPool &procPool, Configuration *cfg, const PartialSet &originalgr,
-                                      PartialSet &weightedgr, const Function1DWrapper intraBroadening, int smoothing);
-    // Sum unweighted g(r) over the supplied Module's target Configurations
-    static bool sumUnweightedGR(GenericList &processingData, const ProcessPool &procPool, std::string_view targetPrefix,
-                                std::string_view parentPrefix, const std::vector<Configuration *> &parentCfgs,
-                                PartialSet &summedUnweightedGR);
-    // Test supplied PartialSets against each other
-    static bool testReferencePartials(PartialSet &setA, PartialSet &setB, double testThreshold);
-    // Test calculated partial against supplied reference data
-    static bool testReferencePartial(const PartialSet &partials, double testThreshold, const Data1D &testData,
-                                     std::string_view typeIorTotal, std::string_view typeJ = "", std::string_view target = "");
-
     /*
      * Processing
      */
