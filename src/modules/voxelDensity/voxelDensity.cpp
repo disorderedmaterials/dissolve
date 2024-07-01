dc5b20b742d4f0a7a7ecad9dabe20a6de3b1ed6f
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-07-01 15:14:34 +0100

    added enum options to keywords

--- src/modules/voxelDensity/voxelDensity.cpp
+++ src/modules/voxelDensity/voxelDensity.cpp
@@ -15,19 +15,23 @@ VoxelDensityModule::VoxelDensityModule() : Module(ModuleTypes::VoxelDensity)
 
     keywords_.setOrganisation("Options", "Voxel Region");
     keywords_.add<Vec3DoubleKeyword>("UnitCellVoxelRegion", "3D bounds for the region within the voxelised unit cell", unitCellVoxelRegion_,
-                                    targetConfiguration_->box()->axisLengths(), std::nullopt, Vec3Labels::MinMaxDeltaLabels);
-
+                                    Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxDeltaLabels);
     keywords_.setOrganisation("Options", "Species");
     keywords_.add<SpeciesVectorKeyword>("SpeciesRange", "Constrain species for analysis", restrictToSpecies_);
+    keywords_.add<EnumOptionsKeyword<VoxelDensityModule::TargetPropertyType>>("TargetProperty", 
+                                    "Target property for analysis", targetProperty_, VoxelDensityModule::targetPropertyTypes());
+}
 
-    keywords_.setOrganisation("Options", "Atomic Number");
-    keywords_.add<BoolKeyword>("AtomicNumberSelected", "Atomic number distribution by voxel", atomicNumberSelected_);
-
-    keywords_.setOrganisation("Options", "Mass");
-    keywords_.add<BoolKeyword>("MassSelected", "Mass distribution by voxel", massSelected_);
-
-    keywords_.setOrganisation("Options", "Scattering Length Density");
-    keywords_.add<BoolKeyword>("ScatteringLengthDensitySelected", "Scattering length density (SLD) distribution by voxel", scatteringLengthDensitySelected_);
+// Return enum option info for TargetPropertyType
+EnumOptions<VoxelDensityModule::TargetPropertyType> VoxelDensityModule::targetPropertyTypes()
+{
+    return EnumOptions<VoxelDensityModule::TargetPropertyType>(
+        "TargetPropertyType", {
+            {TargetPropertyType::Mass, "Mass"},
+            {TargetPropertyType::AtomicNumber, "AtomicNumber"},
+            {TargetPropertyType::ScatteringLengthDensity, "ScatteringLengthDensity"}
+        }
+    );
 }
 
 Module::ExecutionResult VoxelDensityModule::process(ModuleContext &moduleContext) 
