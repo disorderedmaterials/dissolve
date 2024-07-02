87556a973f49fe82cfe63c4fc520414a0ccfffd2
Danbr4d <laydb3@nottingham.ac.uk>
2024-07-02 11:24:00 +0100

    save data added

--- src/modules/tr/tr.cpp
+++ src/modules/tr/tr.cpp
@@ -4,9 +4,12 @@
 #include "modules/tr/tr.h"
 #include "keywords/module.h"
 #include "modules/neutronSQ/neutronSQ.h"
+#include "keywords/bool.h"
 
 TRModule::TRModule() : Module(ModuleTypes::TR)
 {
     keywords_.addTarget<ModuleKeyword<const NeutronSQModule>>(
         "SourceNeutronSQs", "Source NeutronSQ to calculate total T(r) from", sourceNeutronSQ_, ModuleTypes::NeutronSQ);
+    keywords_.setOrganisation("Export");
+    keywords_.add<BoolKeyword>("SaveTR", "Save weighted g(r) and G(r)", saveTR_);
 }
