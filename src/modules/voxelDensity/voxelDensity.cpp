b60f38ed0552430d66bf060d33a1d4b236deffe1
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-07-05 10:02:30 +0100

    added process

--- src/modules/voxelDensity/voxelDensity.cpp
+++ src/modules/voxelDensity/voxelDensity.cpp
@@ -13,11 +13,19 @@ VoxelDensityModule::VoxelDensityModule() : Module(ModuleTypes::VoxelDensity)
     keywords_.addTarget<ConfigurationKeyword>("Configuration", "Set target configuration for the module", targetConfiguration_)
         ->setEditSignals({KeywordBase::ClearModuleData, KeywordBase::RecreateRenderables});
 
-    keywords_.setOrganisation("Options", "Voxel Region");
-    keywords_.add<Vec3DoubleKeyword>("UnitCellVoxelRegion", "3D bounds for the region within the voxelised unit cell", unitCellVoxelRegion_,
-                                    Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxDeltaLabels);
     keywords_.setOrganisation("Options", "Species");
-    keywords_.add<SpeciesVectorKeyword>("SpeciesRange", "Constrain species for analysis", restrictToSpecies_);
+    keywords_.add<SpeciesVectorKeyword>("RestrictToSpecies", "Restrict the calculation to the specified Species",
+                                        restrictToSpecies_);
+
+    keywords_.setOrganisation("Options", "Ranges", "Ranges over which to bin quantities from the calculation.");
+    keywords_.add<Vec3DoubleKeyword>("RangeX", "Range (min, max, binwidth) of X distance binning", xAxisRange_,
+                                     Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxBinwidthlabels);
+    keywords_.add<Vec3DoubleKeyword>("RangeY", "Range (min, max, binwidth) of Y distance binning", yAxisRange_,
+                                     Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxBinwidthlabels);
+    keywords_.add<Vec3DoubleKeyword>("RangeZ", "Range (min, max, binwidth) of Z distance binning", zAxisRange_,
+                                     Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxBinwidthlabels);
+
+    keywords_.setOrganisation("Options", "TargetProperty", "Target property for analysis");
     keywords_.add<EnumOptionsKeyword<VoxelDensityModule::TargetPropertyType>>("TargetProperty", 
                                     "Target property for analysis", targetProperty_, VoxelDensityModule::targetPropertyTypes());
 }
