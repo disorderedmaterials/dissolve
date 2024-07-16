7b7f8e44068dce7efbfa7e5876d438309573350b
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-07-16 17:36:29 +0100

    remove unused module keywords

--- src/modules/voxelDensity/voxelDensity.cpp
+++ src/modules/voxelDensity/voxelDensity.cpp
@@ -17,15 +17,7 @@ VoxelDensityModule::VoxelDensityModule() : Module(ModuleTypes::VoxelDensity)
     keywords_.setOrganisation("Options", "Species");
     keywords_.add<SpeciesVectorKeyword>("RestrictToSpecies", "Restrict the calculation to the specified Species",
                                         restrictToSpecies_);
-    /*
-    keywords_.setOrganisation("Options", "Ranges", "Ranges over which to bin quantities from the calculation.");
-    keywords_.add<Vec3DoubleKeyword>("RangeX", "Range (min, max, binwidth) of X distance binning", xAxisRange_,
-                                     Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxBinwidthlabels);
-    keywords_.add<Vec3DoubleKeyword>("RangeY", "Range (min, max, binwidth) of Y distance binning", yAxisRange_,
-                                     Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxBinwidthlabels);
-    keywords_.add<Vec3DoubleKeyword>("RangeZ", "Range (min, max, binwidth) of Z distance binning", zAxisRange_,
-                                     Vec3<double>(0.0, 0.0, 1.0e-5), std::nullopt, Vec3Labels::MinMaxBinwidthlabels);
-    */
+
     keywords_.setOrganisation("Options", "Density", "Number of points over which analysis is performed");
     keywords_.add<IntegerKeyword>("Density", "Number of points over which analysis is performed", numPoints_, 100);
     
