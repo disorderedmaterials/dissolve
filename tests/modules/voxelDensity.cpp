6421d7b84cb135cb7a377dc358a5a857f1223154
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-08-02 15:43:21 +0100

    refactor unit tests

--- tests/modules/voxelDensity.cpp
+++ tests/modules/voxelDensity.cpp
@@ -9,23 +9,23 @@
 #include "keywords/vec3Double.h"
 #include "module/types.h"
 #include "tests/testData.h"
+#include <cmath>
 #include <gtest/gtest.h>
 #include <vector>
-#include <cmath>
+
 
 namespace UnitTest
 {
-#define BOX_LENGTH 8
-#define NUM_ATOMS static_cast<int>(std::pow(BOX_LENGTH, 3))
-#define MASS_HELIUM 4.002602
-#define Z_HELIUM 2
-#define SCATTERING_LENGTH_DENSITY_HELIUM 3.26
 
 // Helium Array
 class VoxelDensityModuleTest : public ::testing::Test
 {
     protected:
     DissolveSystemTest systemTest;
+
+    const double massHelium{4.002602};
+    const int zHelium{2};
+    const double scatteringLengthDensityHelium{3.26};
 };
 
 TEST_F(VoxelDensityModuleTest, ConfigurationTest)
@@ -45,6 +45,7 @@ TEST_F(VoxelDensityModuleTest, Mass)
     for (auto n = 0; n < 4; ++n)
     {
         const auto nAxisVoxels = DissolveMath::power(2, n);
+        auto nVoxels = DissolveMath::power(nAxisVoxels, 3);
         Vec3<double> defaultBinRange(0.0, 10.0, 0.05);
 
         ASSERT_NO_THROW_VERBOSE(systemTest.setUp("dissolve/input/voxelDensity-helium.txt"));
@@ -60,9 +61,9 @@ TEST_F(VoxelDensityModuleTest, Mass)
         // Check data
         const auto &data = systemTest.dissolve().processingModuleData().search<const Data1D>("VoxelDensity//Data1D")->get();
         auto maxBin = const_cast<Data1D *>(&data)->maxValueAt();
-        auto binRange = module->keywords().get<Vec3<double>, Vec3DoubleKeyword>("BinRange").value_or(defaultBinRange);
-        ASSERT_NEAR(maxBin.first * binRange.z, MASS_HELIUM, 10e2);
-        ASSERT_EQ(maxBin.second, std::pow(nAxisVoxels, 3));
+        auto binRange = module->keywords().get<Vec3<double>, Vec3DoubleKeyword>("BinRange");
+        ASSERT_NEAR(maxBin.first * (*binRange).z, VoxelDensityModuleTest::massHelium, 10e2);
+        ASSERT_EQ(maxBin.second, nVoxels);
     }
 }
 
