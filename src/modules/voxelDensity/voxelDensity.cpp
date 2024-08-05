1f84d96597945875f5d58d1fae3dfc6764e141e0
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-08-05 15:31:07 +0100

    add option to fit gaussian to data

--- src/modules/voxelDensity/voxelDensity.cpp
+++ src/modules/voxelDensity/voxelDensity.cpp
@@ -4,6 +4,7 @@
 #include "modules/voxelDensity/voxelDensity.h"
 #include "keywords/bool.h"
 #include "keywords/configuration.h"
+#include "keywords/double.h"
 #include "keywords/fileAndFormat.h"
 #include "keywords/integer.h"
 #include "keywords/speciesVector.h"
@@ -26,6 +27,7 @@ VoxelDensityModule::VoxelDensityModule() : Module(ModuleTypes::VoxelDensity)
     keywords_.add<IntegerKeyword>("NAxisVoxels", "Number of points per axis over which analysis is performed", nAxisVoxels_, 1);
     keywords_.add<EnumOptionsKeyword<VoxelDensityModule::TargetPropertyType>>(
         "TargetProperty", "Target property for analysis", targetProperty_, VoxelDensityModule::targetPropertyTypes());
+    keywords_.add<BoolKeyword>("FitGaussian", "Apply a 1D Gaussian function to the data", fitGaussian_);
 
     keywords_.setOrganisation("Export");
     keywords_.add<FileAndFormatKeyword>("Export", "File format and file name under which to save calculated Histogram1D data",
