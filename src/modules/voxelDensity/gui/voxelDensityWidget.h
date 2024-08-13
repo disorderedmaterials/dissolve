2070ae18941b7e499d856e4d9dbf8f033c77e55e
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-08-13 14:23:41 +0100

    module gui and benchmark

---
+++ src/modules/voxelDensity/gui/voxelDensityWidget.h
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2024 Team Dissolve and contributors
+
+#pragma once
+
+#include "modules/voxelDensityWidget/gui/ui_voxelDensityWidget.h"
+#include "modules/voxelDensity/voxelDensity.h"
+#include "modules/widget.h"
+
+// Forward Declarations
+class VoxelDensityModule;
+class DataViewer;
+
+// Module Widget
+class VoxelDensityModuleWidget : public ModuleWidget
+{
+    // All Qt declarations derived from QObject must include this macro
+    Q_OBJECT
+
+    private:
+    // Associated Module
+    VoxelDensityModule *module_;
+
+    public:
+    VoxelDensityModuleWidget(QWidget *parent, VoxelDensityModule *module, Dissolve &dissolve)
+
+    /*
+     * UI
+     */
+    private:
+    // Main form declaration
+    Ui::VoxelDensityModuleWidget ui_;
+    // DataViewers contained within this widget
+    DataViewer *voxelDensityGraph_;
+    // Get the horizontal axis label for the module 1D data
+    std::optional<std::string> getData1DAxisLabel();
+
+    public:
+    // Update controls within widget
+    void updateControls(const Flags<ModuleWidget::UpdateFlags> &updateFlags = {}) override;
+};
