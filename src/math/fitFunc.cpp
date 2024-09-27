95b4d9b68c64dba1f71c45382e0a84e5c07d5c72
RobBuchananCompPhys <robertbb269@hotmail.com>
2024-09-27 10:52:03 +0100

    clang format

--- src/math/fitFunc.cpp
+++ src/math/fitFunc.cpp
@@ -7,189 +7,173 @@
 
 namespace DissolveFit
 {
-    /*
-     * Regression1D
-    */
+/*
+ * Regression1D
+ */
 
+/*
+ * Peaks
+ */
+Peaks::Peaks(const std::vector<double> &values, const std::vector<double> &domain) : values_(values), domain_(domain)
+{
+    maxValue_ = *std::max_element(values_.begin(), values_.end());
+    resetConstraints();
+}
 
-    /*
-     * Peaks
-    */
-    Peaks::Peaks(const std::vector<double> &values, const std::vector<double> &domain) 
-        : values_(values), domain_(domain)
+Peaks::Peaks(const Data1D &source) : values_(source.values()), domain_(source.xAxis())
+{
+    maxValue_ = *std::max_element(values_.begin(), values_.end());
+    resetConstraints();
+}
+
+// Set vertical threshold for peaks
+void Peaks::setThreshold(double threshold) { threshold_ = threshold; }
+
+// Set horizontal isolation radius for peaks
+void Peaks::setIsolation(double radius) { isolation_ = radius; }
+
+// Reset the threshold and isolation constraints
+void Peaks::resetConstraints() { threshold_ = -10e9, isolation_ = 0; }
+
+// Check if neighbouring points correspond to a local maximum
+bool Peaks::isLocalMaximum(double *points) { return (points[1] - points[0] > 0) && (points[2] - points[1] <= 0); }
+
+// Check if neighbouring points correspond to a local minimum
+bool Peaks::isLocalMinimum(double *points)
+{
+    double invertedPoints[3] = {-1.0 * points[0], -1.0 * points[1], -1.0 * points[2]};
+    return isLocalMaximum(invertedPoints);
+}
+
+// Check if neighbouring points correspond to an inflection point
+bool Peaks::isInflectionPoint(double *points)
+{
+    return (((points[1] - points[0] > 0) || ((points[1] - points[0] < 0))) && (points[2] - points[1] == 0)) ||
+           (((points[2] - points[1] > 0) || ((points[2] - points[1] < 0))) && (points[1] - points[0] == 0));
+}
+
+// Sort peaks in place by peak value, from highest to lowest
+void Peaks::sortPeaks(std::vector<Peaks::Peak1D> &peaks)
+{
+    std::sort(peaks.begin(), peaks.end(), [](const auto &a, const auto &b) { return a.peak > b.peak; });
+}
+
+// Sort peaks in place by index, from first to last
+template <typename T> void Peaks::sortIndices(std::vector<T> &items)
+{
+    std::sort(items.begin(), items.end(), [](const auto &a, const auto &b) { return a.index < b.index; });
+}
+
+// Sort prominences in place by prominence value, from highest to lowest
+void Peaks::sortProminences(std::vector<Peaks::Prominence1D> &proms)
+{
+    std::sort(proms.begin(), proms.end(), [](const auto &a, const auto &b) { return a.prominence > b.prominence; });
+}
+
+// Get top n peaks
+std::vector<Peaks::Peak1D> Peaks::top(int n, std::vector<Peaks::Peak1D> peaks)
+{
+    sortPeaks(peaks);
+
+    if (abs(n) == peaks.size())
+        return peaks;
+
+    std::vector<Peaks::Peak1D> isolatedPeaks;
+    isolatedPeaks.reserve(peaks.size());
+
+    for (const auto &peak : peaks)
     {
-        maxValue_ = *std::max_element(values_.begin(), values_.end());
-        resetConstraints();
+        bool withinRadius = std::any_of(isolatedPeaks.begin(), isolatedPeaks.end(),
+                                        [this, &peak](const auto &p)
+                                        {
+                                            // return compare(p, peak);
+                                            return (p.index != peak.index) && ((p.valueAt > peak.valueAt - isolation_) &&
+                                                                               (p.valueAt < peak.valueAt + isolation_));
+                                        });
+
+        if (!withinRadius)
+            isolatedPeaks.push_back(peak);
+
+        if (isolatedPeaks.size() == abs(n))
+            break;
     }
+    return isolatedPeaks;
+}
 
-    Peaks::Peaks(const Data1D &source) : values_(source.values()), domain_(source.xAxis())
+// Find the peaks (local maxima) of data
+std::vector<Peaks::Peak1D> Peaks::find(bool heightOrder)
+{
+    std::vector<Peaks::Peak1D> peaks;
+    peaks.reserve(values_.size());
+
+    for (int i = 1; i < values_.size() - 1; i++)
     {
-        maxValue_ = *std::max_element(values_.begin(), values_.end());
-        resetConstraints();
-    }
+        // Check if the gradients either side show this is a local maximum
+        double neighbours[3] = {values_[i - 1], values_[i], values_[i + 1]};
 
-    // Set vertical threshold for peaks
-    void Peaks::setThreshold(double threshold)
-    {
-        threshold_ = threshold;
-    }
-
-    // Set horizontal isolation radius for peaks
-    void Peaks::setIsolation(double radius)
-    {
-        isolation_ = radius;
-    }
-
-    // Reset the threshold and isolation constraints
-    void Peaks::resetConstraints()
-    {
-        threshold_=-10e9, isolation_=0;
-    }
-
-    // Check if neighbouring points correspond to a local maximum
-    bool Peaks::isLocalMaximum(double *points)
-    {
-        return (points[1]-points[0] > 0) && (points[2]-points[1] <= 0);
-    }
-
-    // Check if neighbouring points correspond to a local minimum
-    bool Peaks::isLocalMinimum(double *points)
-    {
-        double invertedPoints[3] = { -1.0*points[0], -1.0*points[1], -1.0*points[2] };
-        return isLocalMaximum(invertedPoints);
-    }
-
-    // Check if neighbouring points correspond to an inflection point
-    bool Peaks::isInflectionPoint(double *points)
-    {
-        return (
-            ((points[1]-points[0] > 0) || ((points[1]-points[0] < 0)) ) && (points[2]-points[1] == 0)
-        ) ||
-        (
-            ((points[2]-points[1] > 0) || ((points[2]-points[1] < 0)) ) && (points[1]-points[0] == 0)
-        );
-    }
-
-    // Sort peaks in place by peak value, from highest to lowest
-    void Peaks::sortPeaks(std::vector<Peaks::Peak1D> &peaks)
-    {
-        std::sort(peaks.begin(), peaks.end(), [](const auto &a, const auto &b) { return a.peak > b.peak; });
-    }
-
-    // Sort peaks in place by index, from first to last
-    template <typename T> void Peaks::sortIndices(std::vector<T> &items)
-    {
-        std::sort(items.begin(), items.end(), [](const auto &a, const auto &b) { return a.index < b.index; });
-    }
-
-    // Sort prominences in place by prominence value, from highest to lowest
-    void Peaks::sortProminences(std::vector<Peaks::Prominence1D> &proms)
-    {
-        std::sort(proms.begin(), proms.end(), [](const auto &a, const auto &b) { return a.prominence > b.prominence; });
-    }
-
-    // Get top n peaks
-    std::vector<Peaks::Peak1D> Peaks::top(int n, std::vector<Peaks::Peak1D> peaks)
-    {
-        sortPeaks(peaks);
-
-        if (abs(n)==peaks.size())
-            return peaks;
-
-        std::vector<Peaks::Peak1D> isolatedPeaks;
-        isolatedPeaks.reserve(peaks.size());
-        
-        for (const auto &peak : peaks)
+        if (values_[i] > threshold_ && isLocalMaximum(neighbours))
         {
-            bool withinRadius = std::any_of(
-                isolatedPeaks.begin(), isolatedPeaks.end(), [this, &peak](const auto &p)
-                {
-                    //return compare(p, peak);
-                    return (p.index != peak.index) && ((p.valueAt > peak.valueAt - isolation_) && (p.valueAt < peak.valueAt + isolation_));
-                }
-            );
-
-            if (!withinRadius)
-                isolatedPeaks.push_back(peak);
-            
-            if (isolatedPeaks.size() == abs(n))
-                break;
+            peaks.emplace_back(values_[i], domain_[i], i);
         }
+    }
+
+    if (isolation_ > 0)
+    {
+        auto maxX = *std::max_element(domain_.begin(), domain_.end());
+        auto isolatedPeaks = top(std::round(maxX / isolation_), peaks);
+        if (!heightOrder)
+            sortIndices<Peak1D>(isolatedPeaks);
         return isolatedPeaks;
     }
+    return peaks;
+}
 
-    // Find the peaks (local maxima) of data
-    std::vector<Peaks::Peak1D> Peaks::find(bool heightOrder)
+// Calculate prominences of peaks
+std::vector<Peaks::Prominence1D> Peaks::prominences(bool heightOrder)
+{
+    auto peaks = find(heightOrder);
+    return prominences(peaks, heightOrder);
+}
+
+std::vector<Peaks::Prominence1D> Peaks::prominences(const std::vector<Peaks::Peak1D> &peaks, bool heightOrder)
+{
+    std::vector<Peaks::Prominence1D> proms;
+    proms.reserve(peaks.size());
+
+    for (const auto &peak : peaks)
     {
-        std::vector<Peaks::Peak1D> peaks;
-        peaks.reserve(values_.size());
-
-        for (int i = 1; i < values_.size()-1; i++)
+        int iLeft = peak.index - 1, iRight = peak.index + 1;
+        double heightRef = 0;
+        while (heightRef == 0)
         {
-            // Check if the gradients either side show this is a local maximum
-            double neighbours[3] = {values_[i-1], values_[i], values_[i+1]};
+            double pointsLeft[3] = {values_[iLeft + 1], values_[iLeft], values_[iLeft - 1]};
+            double pointsRight[3] = {values_[iRight - 1], values_[iRight], values_[iRight + 1]};
 
-            if (values_[i] > threshold_ && isLocalMaximum(neighbours))
+            if ((iLeft > 0) && (isLocalMinimum(pointsLeft) || isInflectionPoint(pointsLeft)))
             {
-                peaks.emplace_back(values_[i], domain_[i], i);
+                heightRef += iLeft;
+                break;
             }
-        }
-
-        if (isolation_ > 0)
-        {
-            auto maxX = *std::max_element(domain_.begin(), domain_.end());
-            auto isolatedPeaks = top(std::round(maxX/isolation_), peaks);
-            if (!heightOrder)
-                sortIndices<Peak1D>(isolatedPeaks);
-            return isolatedPeaks;
-        }
-        return peaks;
-    } 
-
-    // Calculate prominences of peaks
-    std::vector<Peaks::Prominence1D> Peaks::prominences(bool heightOrder)
-    {
-        auto peaks = find(heightOrder);
-        return prominences(peaks, heightOrder);
-    }
-
-    std::vector<Peaks::Prominence1D> Peaks::prominences(const std::vector<Peaks::Peak1D> &peaks, bool heightOrder)
-    {
-        std::vector<Peaks::Prominence1D> proms;
-        proms.reserve(peaks.size());
-
-        for (const auto &peak : peaks)
-        {
-            int iLeft = peak.index-1, iRight = peak.index+1;
-            double heightRef = 0;
-            while (heightRef == 0)
+            else if ((iRight < values_.size()) && (isLocalMinimum(pointsRight) || isInflectionPoint(pointsRight)))
             {
-                double pointsLeft[3] = { values_[iLeft+1], values_[iLeft], values_[iLeft-1] };
-                double pointsRight[3] = { values_[iRight-1], values_[iRight], values_[iRight+1] };
-
-                if ((iLeft > 0) && (isLocalMinimum(pointsLeft) || isInflectionPoint(pointsLeft)))
-                {
-                    heightRef += iLeft;
-                    break;
-                }
-                else if ((iRight < values_.size()) && (isLocalMinimum(pointsRight) || isInflectionPoint(pointsRight)))
-                {
-                    heightRef += iRight;
-                    break;
-                }
-                else if ((iLeft <= 0) && (iRight >= values_.size()))
-                {
-                    Messenger::print("Could not calculate reference height for prominence of peak at index {} (no local minima or inflection point found in either direction)\n", peak.index);
-                    break;
-                }
-                else
-                    iLeft--, iRight++;
+                heightRef += iRight;
+                break;
             }
-            auto prominence = abs(values_[heightRef]-peak.peak);
-            proms.emplace_back(&peak, prominence);
+            else if ((iLeft <= 0) && (iRight >= values_.size()))
+            {
+                Messenger::print("Could not calculate reference height for prominence of peak at index {} (no local minima or "
+                                 "inflection point found in either direction)\n",
+                                 peak.index);
+                break;
+            }
+            else
+                iLeft--, iRight++;
         }
-        if (!heightOrder)
-            sortIndices<Prominence1D>(proms);
-        return proms;
+        auto prominence = abs(values_[heightRef] - peak.peak);
+        proms.emplace_back(&peak, prominence);
     }
+    if (!heightOrder)
+        sortIndices<Prominence1D>(proms);
+    return proms;
+}
 } // namespace DissolveFit
