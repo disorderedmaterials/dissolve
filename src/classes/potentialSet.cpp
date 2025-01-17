1c1ee7d094cf801db70fc4f21326fa3137c1c505
Danbr4d <laydb3@nottingham.ac.uk>
2025-01-17 16:12:14 +0000

    potentialSet-update

--- src/classes/potentialSet.cpp
+++ src/classes/potentialSet.cpp
@@ -60,13 +60,6 @@ PotentialSet &PotentialSet::operator*=(const double factor)
     return (*this);
 }
 
-PotentialSet &PotentialSet::operator*=(const double factor)
-{
-    for (auto &[key, potential] : potentials_)
-        potential.ep *= factor;
-    return (*this);
-}
-
 /*
  * Serialisation
  */
@@ -76,25 +69,6 @@ bool PotentialSet::deserialise(LineParser &parser, const CoreData &coreData)
 {
     if (parser.readNextLine(LineParser::Defaults, fingerprint_) != LineParser::Success)
         return false;
-
-    if (parser.getArgsDelim(LineParser::Defaults) != LineParser::Success)
-        return false;
-    auto size = parser.argli(0);
-    for (auto n = 0; n < size; ++n)
-    {
-        if (parser.getArgsDelim(LineParser::Defaults) != LineParser::Success)
-            return false;
-        PotentialData value;
-        auto key = parser.args(0);
-        value.count = parser.argi(1);
-        value.at1 = coreData.findAtomType(parser.args(2));
-        value.at2 = coreData.findAtomType(parser.args(3));
-
-        if (!value.potential.deserialise(parser))
-            return false;
-
-        potentials_[key] = value;
-    }
 
     if (parser.getArgsDelim(LineParser::Defaults) != LineParser::Success)
         return false;
