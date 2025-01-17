826a789a1e5a1c45cef1edd69650a8de5f075411
Danbr4d <laydb3@nottingham.ac.uk>
2025-01-17 14:37:12 +0000

    gr testing

--- src/modules/tr/tr.h
+++ src/modules/tr/tr.h
@@ -27,7 +27,7 @@ class TRModule : public Module
     // Step size in Q for S(Q) calculation
     double qDelta_{0.05};
     // Maximum Q for calculated S(Q)
-    double qMax_{30.0};
+    double qMax_{50.0};
     // Minimum Q for calculated S(Q)
     double qMin_{0.01};
     // Window function to use when Fourier-transforming reference S(Q) to g(r))
