1db6fd398e6e4cb8b79363c8b4d993225872671d
Adam Washington <adam.washington@stfc.ac.uk>
2025-02-26 16:41:08 +0000

    Start adding ParameterLink

---
+++ src/nodes/parameterLink.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#include "nodes/parameters/parameterLink.h"
+
+ParameterLink::ParameterLink(ParameterBase &source, ParameterBase &sink) : source_(source), sink_(sink) {}
+
+std::optional<ParameterLink> ParameterLink::link(ParameterBase &source, ParameterBase &sink)
+{
+    if (source.type() != sink.type())
+        return {};
+    return ParameterLink(source, sink);
+}
---
+++ src/nodes/parameterLink.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#pragma once
+
+#include "nodes/parameter.h"
+
+// A link between two parameters
+class ParameterLink
+{
+
+    private:
+    // The constructor is private because it can only be constructed by the factory method
+    ParameterLink(ParameterBase &source, ParameterBase &sink);
+    ParameterBase &source_;
+    ParameterBase &sink_;
+
+    public:
+    static std::optional<ParameterLink> link(ParameterBase &source, ParameterBase &sink);
+};
