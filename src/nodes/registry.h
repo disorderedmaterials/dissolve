d9b8219b458e14bffd3f243a75f166540f13f70d
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-02-26 13:41:09 +0000

    reformat

--- src/nodes/registry.h
+++ src/nodes/registry.h
@@ -1,15 +1,13 @@
+#include "atomShake.h"
+#include "node.h"
 #include <functional>
 #include <map>
 #include <memory>
 #include <string>
-#include "atomShake.h"
-#include "node.h"
 
-using NodeProducer = std::function<std::unique_ptr<Node> (void)>;
+using NodeProducer = std::function<std::unique_ptr<Node>(void)>;
 
 const std::map<std::string, NodeProducer> registry{
-	{
-        "AtomShake", []() -> std::unique_ptr<Node> { return std::make_unique<AtomShakeNode>(); }
-    },
+	{"AtomShake", []() -> std::unique_ptr<Node> { return std::make_unique<AtomShakeNode>(); }},
     // etc...
 }
