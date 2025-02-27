127c6d7df0b9492e11b6ac782a31a942aafff306
Adam Washington <adam.washington@stfc.ac.uk>
2025-02-27 15:11:23 +0000

    More documentation

--- src/nodes/parameterLink.h
+++ src/nodes/parameterLink.h
@@ -10,13 +10,27 @@
 class ParameterLink
 {
 
+    // This class uses references instead of pointers for two reasons:
+    // 1) Neither end of the link should EVER be null
+    // 2) The link itself is immutable.  You can create links and
+    // destroy links, but *never mutate links*.
+    //
+    // The immutability is important because the factory ensures that
+    // all links are valid at creation time.  By keeping the links
+    // immutable, that means that all links are valid at all times.
+    // This enables us to skip a great deal of error checking when
+    // processing a chain of links.
+
     private:
-    // The constructor is private because it can only be constructed by the factory method
+    // The constructor is private because it can only be constructed
+    // by the factory method
     ParameterLink(ParameterBase &source, ParameterBase &sink);
     ParameterBase &source_;
     ParameterBase &sink_;
 
     public:
+    // A factory method to create a link between two parameters.
+    // Returns the null option if the link cannot be created.
     static std::optional<ParameterLink> link(ParameterBase &source, ParameterBase &sink);
     bool updateSource();
 };
