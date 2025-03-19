fff6c94d22b322f51d190e22fb5ce024144add77
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-03-19 14:35:24 +0000

    atom shake initial commit

--- src/nodes/atomShake.h
+++ src/nodes/atomShake.h
@@ -5,6 +5,7 @@
 
 #include "classes/configuration.h"
 #include "nodes/node.h"
+#include "nodes/number.h"
 
 // AtomShake Node
 class AtomShakeNode : public Node
@@ -24,22 +25,22 @@ class AtomShakeNode : public Node
     // Target configurations
     Configuration *targetConfiguration_{nullptr};
     // Interatomic cutoff distance to use for energy calculation
-    std::optional<double> cutoffDistance_;
+    std::optional<Number> cutoffDistance_;
     // Number of shakes to attempt per atom
-    int nShakesPerAtom_{1};
+    Number nShakesPerAtom_{1};
     // Step size in Angstroms to use in Monte Carlo moves
-    double stepSize_{0.05};
+    Number stepSize_{0.05};
     // Maximum allowed value for step size, in Angstroms
-    double stepSizeMax_{1.0};
+    Number stepSizeMax_{1.0};
     // Minimum allowed value for step size, in Angstroms
-    double stepSizeMin_{0.001};
+    Number stepSizeMin_{0.001};
     // Target acceptance rate for Monte Carlo moves
-    double targetAcceptanceRate_{0.33};
+    Number targetAcceptanceRate_{0.33};
 
     /*
      * Processing
      */
     private:
     // Run main processing
-    Module::ExecutionResult process(ModuleContext &moduleContext);
+    ProcessResult process();
 };
