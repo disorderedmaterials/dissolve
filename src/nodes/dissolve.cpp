b8c2bb6ac913f016cbbbf8a9d024005f4e6255c8
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-04-02 14:26:44 +0100

    fixup 1

--- src/nodes/dissolve.cpp
+++ src/nodes/dissolve.cpp
@@ -1,13 +1,7 @@
 #include "nodes/dissolve.h"
 
 // Return dissolve
-Dissolve& DissolveGraph::dissolve() const
-{ 
-	return dissolve_;
-}
+Dissolve& DissolveGraph::dissolve() const { return dissolve_; }
 
 // Return world pool
-ProcessPool& DissolveGraph::processPool() const
-{ 
-	return dissolve_.worldPool();
-}
+ProcessPool& DissolveGraph::processPool() const { return dissolve_.worldPool(); }
