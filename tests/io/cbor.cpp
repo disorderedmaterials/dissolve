6426ec376a07ad427cff7d45fea6aaeb95616edc
Adam Washington <adam.washington@stfc.ac.uk>
2025-04-02 17:17:01 +0100

    CBORTest fixture

--- tests/io/cbor.cpp
+++ tests/io/cbor.cpp
@@ -7,70 +7,72 @@
 
 namespace UnitTest
 {
-void compare_toml(std::string location, SerialisedValue toml, SerialisedValue toml2)
+class CBORTest : public ::testing::Test
 {
-    if (toml.is_table())
+    protected:
+    void compare_toml(std::string location, SerialisedValue toml, SerialisedValue toml2)
     {
-        ASSERT_TRUE(toml2.is_table()) << location;
-        for (auto &[k, v] : toml.as_table())
+        if (toml.is_table())
         {
-            ASSERT_TRUE(toml2.contains(k)) << location << "." << k << std::endl << "Expected:" << std::endl << toml[k];
-            compare_toml(std::format("{}.{}", location, k), v, toml2.at(k));
+            ASSERT_TRUE(toml2.is_table()) << location;
+            for (auto &[k, v] : toml.as_table())
+            {
+                ASSERT_TRUE(toml2.contains(k)) << location << "." << k << std::endl << "Expected:" << std::endl << toml[k];
+                compare_toml(std::format("{}.{}", location, k), v, toml2.at(k));
+            }
+        }
+        else if (toml.is_array())
+        {
+            auto arr = toml.as_array();
+            auto arr2 = toml2.as_array();
+            ASSERT_EQ(arr.size(), arr2.size()) << location << std::endl << "Expected" << std::endl << toml;
+            for (int i = 0; i < arr.size(); ++i)
+                compare_toml(std::format("{}[{}]", location, i), arr[i], arr2[i]);
+        }
+        else
+        {
+            EXPECT_EQ(toml, toml2) << location;
         }
     }
-    else if (toml.is_array())
+
+    void basic_test(SerialisedValue node)
     {
-        auto arr = toml.as_array();
-        auto arr2 = toml2.as_array();
-        ASSERT_EQ(arr.size(), arr2.size()) << location << std::endl << "Expected" << std::endl << toml;
-        for (int i = 0; i < arr.size(); ++i)
-            compare_toml(std::format("{}[{}]", location, i), arr[i], arr2[i]);
+        auto cbor = toCBOR(node);
+        std::ranges::subrange sub{cbor.begin(), cbor.end()};
+        auto [copy, remainder] = fromCBOR(sub);
+
+        EXPECT_EQ(remainder.begin(), remainder.end());
+        compare_toml("", node, copy);
     }
-    else
-    {
-        EXPECT_EQ(toml, toml2) << location;
-    }
-}
+};
 
-void basic_test(SerialisedValue node)
-{
-    auto cbor = toCBOR(node);
-    std::ranges::subrange sub{cbor.begin(), cbor.end()};
-    auto [copy, remainder] = fromCBOR(sub);
-
-    EXPECT_EQ(remainder.begin(), remainder.end());
-    UnitTest::compare_toml("", node, copy);
-}
-
-} // namespace UnitTest
-
-TEST(CBORTest, BasicTypes)
+TEST_F(CBORTest, BasicTypes)
 {
     // Check Positive Int
-    UnitTest::basic_test(37);
+    basic_test(37);
     // Check Negative Int
-    UnitTest::basic_test(-917);
+    basic_test(-917);
     // Check Boolean True
-    UnitTest::basic_test(true);
+    basic_test(true);
     // Check Boolean False
-    UnitTest::basic_test(false);
+    basic_test(false);
     // Check Float
-    UnitTest::basic_test(3.14159);
+    basic_test(3.14159);
     // Check String
-    UnitTest::basic_test("test string");
+    basic_test("test string");
 }
 
-TEST(CBORTest, BasicArray)
+TEST_F(CBORTest, BasicArray)
 {
     // Check Array
     toml::array array{12, 4.56, "in an array"};
     SerialisedValue value = array;
     ASSERT_EQ(value.type(), toml::value_t::array);
     ASSERT_EQ(value.as_array().size(), 3);
-    UnitTest::basic_test(value);
+    basic_test(value);
 }
 
-TEST(CBORTest, BasicTable)
+TEST_F(CBORTest, BasicTable)
 {
     // Check Array
     SerialisedValue value, inner;
@@ -82,5 +84,7 @@ TEST(CBORTest, BasicTable)
     value["third"] = "in a map";
     value["last"] = inner;
 
-    UnitTest::basic_test(value);
+    basic_test(value);
 }
+
+} // namespace UnitTest
