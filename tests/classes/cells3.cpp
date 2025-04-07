c93384d2661ec1b7e003303b001994cc730974a1
Tristan Youngs <trisyoungs@googlemail.com>
2025-04-07 14:16:55 +0100

    Add triclinic test to cells3, formatting.

--- tests/classes/cells3.cpp
+++ tests/classes/cells3.cpp
@@ -23,7 +23,8 @@ class CellsEnergyTest : public ::testing::Test
         // Add atom type
         auto arType = coreData_.addAtomType(Elements::Ar);
         arType->setName("Ar");
-        arType->interactionPotential().setFormAndParameters(ShortRangeFunctions::Form::LennardJones, "epsilon=0.77404 sigma=3.445996");
+        arType->interactionPotential().setFormAndParameters(ShortRangeFunctions::Form::LennardJones,
+                                                            "epsilon=0.77404 sigma=3.445996");
 
         // Set up pseudo-species
         argon_ = coreData_.addSpecies();
@@ -58,17 +59,18 @@ class CellsEnergyTest : public ::testing::Test
         auto *box = cfg->box();
         auto *pp = dissolve_.pairPotential("Ar", "Ar");
         auto energy = 0.0;
-        dissolve::for_each_pair(ParallelPolicies::seq, cfg->molecules().begin(), cfg->molecules().end(), [&](int i, const auto &molI, int j, const auto &molJ) {
+        dissolve::for_each_pair(ParallelPolicies::seq, cfg->molecules().begin(), cfg->molecules().end(),
+                                [&](int i, const auto &molI, int j, const auto &molJ)
+                                {
+                                    if (i == j)
+                                        return;
+                                    auto ii = molI->atom(0);
+                                    auto jj = molJ->atom(0);
 
-            if (i == j)
-                return;
-             auto ii = molI->atom(0);
-             auto jj = molJ->atom(0);
-
-             auto r = box->minimumDistance(ii->r(), jj->r());
-            if (r <= cutoff)
-                energy += pp->energy(r);
-        });
+                                    auto r = box->minimumDistance(ii->r(), jj->r());
+                                    if (r <= cutoff)
+                                        energy += pp->energy(r);
+                                });
         return energy;
     }
     // Calculate analytic energy directly (without using Cells)
@@ -77,8 +79,9 @@ class CellsEnergyTest : public ::testing::Test
         auto *box = cfg->box();
         auto *pp = dissolve_.pairPotential("Ar", "Ar");
         auto energy = 0.0;
-        dissolve::for_each_pair(ParallelPolicies::seq, cfg->molecules().begin(), cfg->molecules().end(), [&](int i, const auto &molI, int j, const auto &molJ) {
-
+        dissolve::for_each_pair(ParallelPolicies::seq, cfg->molecules().begin(), cfg->molecules().end(),
+                                [&](int i, const auto &molI, int j, const auto &molJ)
+                                {
                                     if (i == j)
                                         return;
                                     auto ii = molI->atom(0);
@@ -107,7 +110,8 @@ class CellsEnergyTest : public ::testing::Test
 
         // Calculate total Cell-based energy
         EXPECT_NEAR(analyticEnergyNoCells(cfg, rCut), tabulatedEnergyNoCells(cfg, rCut), 1.0e-2);
-        EXPECT_NEAR(tabulatedEnergyNoCells(cfg, rCut), kernel->totalPairPotentialEnergy(false, ProcessPool::PoolStrategy).total(), 1.0e-6);
+        EXPECT_NEAR(tabulatedEnergyNoCells(cfg, rCut),
+                    kernel->totalPairPotentialEnergy(false, ProcessPool::PoolStrategy).total(), 1.0e-6);
         EXPECT_NEAR(refEnergy - lrc, kernel->totalPairPotentialEnergy(false, ProcessPool::PoolStrategy).total(), 1.5e-2);
     }
 };
@@ -117,14 +121,16 @@ TEST_F(CellsEnergyTest, Cubic)
     auto *cfg = createConfiguration({100, 100, 100}, {90, 90, 90}, 6755);
 
     // Load the test coordinates
-    CoordinateImportFileFormat importer("dlpoly/argon/cubic/big_argon.CONFIG", CoordinateImportFileFormat::CoordinateImportFormat::DLPOLY);
+    CoordinateImportFileFormat importer("dlpoly/argon/cubic/big_argon.CONFIG",
+                                        CoordinateImportFileFormat::CoordinateImportFormat::DLPOLY);
     ASSERT_TRUE(importer.importData(cfg));
 
     // Test range of cell sizes for various cutoffs - should make no difference to total energy
     std::vector<std::tuple<double, double, double, double>> states = {
-            {9.0, 5.0, -8.077004e3, -0.678949e3}, {9.0, 7.0, -8.077004e3, -0.678949e3}, {9.0, 9.0, -8.077004e3, -0.678949e3},  {9.0, 12.0, -8.077004e3, -0.678949e3}};
-    //        {12.0, 5.0, -9.748103e3, -0.335651e3}, {12.0, 7.0, -9.748103e3, -0.335651e3}, {12.0, 9.0, -9.748103e3, -0.335651e3}, {12.0, 12.0, -9.748103e3, -0.335651e3},
-    //        {15.0, 5.0, -9.747836e3, -0.171877e3}, {15.0, 7.0, -9.747836e3, -0.171877e3}, {15.0, 9.0, -9.747836e3, -0.171877e3}, {15.0, 15.0, -9.747836e3, -0.171877e3}};
+        {9.0, 5.0, -8.077004e3, -0.678949e3},  {9.0, 7.0, -8.077004e3, -0.678949e3},   {9.0, 9.0, -8.077004e3, -0.678949e3},
+        {9.0, 12.0, -8.077004e3, -0.678949e3}, {12.0, 5.0, -8.075795e3, -0.286679e3},  {12.0, 7.0, -8.075795e3, -0.286679e3},
+        {12.0, 9.0, -8.075795e3, -0.286679e3}, {12.0, 12.0, -8.075795e3, -0.286679e3}, {15.0, 5.0, -8.075748e3, -0.146800e3},
+        {15.0, 7.0, -8.075748e3, -0.146800e3}, {15.0, 9.0, -8.075748e3, -0.146800e3},  {15.0, 15.0, -8.075748e3, -0.146800e3}};
 
     for (auto &state : states)
         testEnergy(cfg, state);
@@ -135,14 +141,38 @@ TEST_F(CellsEnergyTest, Monoclinic)
     auto *cfg = createConfiguration({100, 100, 100}, {90, 90, 120}, 6802);
 
     // Load the test coordinates
-    CoordinateImportFileFormat importer("dlpoly/argon/monoclinic/big_argon.CONFIG", CoordinateImportFileFormat::CoordinateImportFormat::DLPOLY);
+    CoordinateImportFileFormat importer("dlpoly/argon/monoclinic/big_argon.CONFIG",
+                                        CoordinateImportFileFormat::CoordinateImportFormat::DLPOLY);
     ASSERT_TRUE(importer.importData(cfg));
 
     // Test range of cell sizes for various cutoffs - should make no difference to total energy
-    std::vector<std::tuple<double, double, double, double>> states = { {9.0, 5.0, -9.747556e3, -0.794931e3} };
-//        {9.0, 5.0, -9.747556e3, -0.794931e3}, {9.0, 7.0, -9.747556e3, -0.794931e3}, {9.0, 9.0, -9.747556e3, -0.794931e3},  {9.0, 12.0, -9.747556e3, -0.794931e3},
-//        {12.0, 5.0, -9.748103e3, -0.335651e3}, {12.0, 7.0, -9.748103e3, -0.335651e3}, {12.0, 9.0, -9.748103e3, -0.335651e3}, {12.0, 12.0, -9.748103e3, -0.335651e3},
-//        {15.0, 5.0, -9.747836e3, -0.171877e3}, {15.0, 7.0, -9.747836e3, -0.171877e3}, {15.0, 9.0, -9.747836e3, -0.171877e3}, {15.0, 15.0, -9.747836e3, -0.171877e3}};
+    std::vector<std::tuple<double, double, double, double>> states = {
+        {9.0, 5.0, -9.747556e3, -0.794931e3},  {9.0, 7.0, -9.747556e3, -0.794931e3},   {9.0, 9.0, -9.747556e3, -0.794931e3},
+        {9.0, 12.0, -9.747556e3, -0.794931e3}, {12.0, 5.0, -9.748103e3, -0.335651e3},  {12.0, 7.0, -9.748103e3, -0.335651e3},
+        {12.0, 9.0, -9.748103e3, -0.335651e3}, {12.0, 12.0, -9.748103e3, -0.335651e3}, {15.0, 5.0, -9.747836e3, -0.171877e3},
+        {15.0, 7.0, -9.747836e3, -0.171877e3}, {15.0, 9.0, -9.747836e3, -0.171877e3},  {15.0, 15.0, -9.747836e3, -0.171877e3}};
+
+    for (auto &state : states)
+        testEnergy(cfg, state);
+}
+
+TEST_F(CellsEnergyTest, TriclinicBox)
+{
+    ASSERT_TRUE(false);
+    // TODO Need to set the correct cell angles here.
+    auto *cfg = createConfiguration({100, 100, 100}, {90, 90, 120}, 6528);
+
+    // Load the test coordinates
+    CoordinateImportFileFormat importer("dlpoly/argon/monoclinic/big_argon.CONFIG",
+                                        CoordinateImportFileFormat::CoordinateImportFormat::DLPOLY);
+    ASSERT_TRUE(importer.importData(cfg));
+
+    // Test range of cell sizes for various cutoffs - should make no difference to total energy
+    std::vector<std::tuple<double, double, double, double>> states = {
+        {9.0, 5.0, -9.471742e3, -0.779171e3},  {9.0, 7.0, -9.471742e3, -0.779171e3},   {9.0, 9.0, -9.471742e3, -0.779171e3},
+        {9.0, 12.0, -9.471742e3, -0.779171e3}, {12.0, 5.0, -9.471045e3, -0.328997e3},  {12.0, 7.0, -9.471045e3, -0.328997e3},
+        {12.0, 9.0, -9.471045e3, -0.328997e3}, {12.0, 12.0, -9.471045e3, -0.328997e3}, {15.0, 5.0, -9.470832e3, -0.168470e3},
+        {15.0, 7.0, -9.470832e3, -0.168470e3}, {15.0, 9.0, -9.470832e3, -0.168470e3},  {15.0, 15.0, -9.470832e3, -0.168470e3}};
 
     for (auto &state : states)
         testEnergy(cfg, state);
