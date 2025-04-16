b84222b01b642a62c24477a0e5fbf6e15c0716f5
RobBuchanan <106311829+RobBuchananCompPhys@users.noreply.github.com>
2025-04-16 01:34:15 -0700

    feat: Configuration node (#2138)

---
+++ src/nodes/configuration.h
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#pragma once
+
+#include "classes/configuration.h"
+#include "nodes/node.h"
+#include <memory>
+
+class ConfigurationNode : public Node
+{
+    public:
+    ConfigurationNode(Graph *parentGraph);
+    ~ConfigurationNode() override = default;
+
+    /*
+     * Definition
+     */
+    public:
+    std::string_view type() const override;
+    std::string_view summary() const override;
+
+    /*
+     * Data
+     */
+    private:
+    // Configuration object
+    std::unique_ptr<Configuration> configuration_{std::make_unique<Configuration>()};
+    // Local raw pointer to configuration
+    Configuration *rawConfiguration_{configuration_.get()};
+
+    /*
+     * Processing
+     */
+    private:
+    // Run main processing
+    NodeConstants::ProcessResult process() override;
+};
