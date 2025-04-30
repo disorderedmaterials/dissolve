be1059cd5a9068cc3fa9b821eefabf4007963725
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-04-30 14:47:57 +0100

    atomic species node

---
+++ src/nodes/atomicSpecies.cpp
@@ -0,0 +1,21 @@
+#include "atomicSpecies.h"
+#include "math/vector3.h"
+
+AtomicSpeciesNode::AtomicSpeciesNode(Graph *parentGraph) : Node(parentGraph)
+{
+    addOption<std::string_view>("Element symbol", "Atomic species element symbol", Z_);
+    addOption<std::string_view>("Name", "Atomic species name", name_);
+    addOutput<Species *>("Species", "Atomic species", rawSpecies_);
+}
+
+std::string_view AtomicSpeciesNode::type() const { return "Atomic Species"; }
+
+std::string_view AtomicSpeciesNode::summary() const { return "Produce an atomic species"; }
+
+NodeConstants::ProcessResult AtomicSpeciesNode::process()
+{
+    rawSpecies_->addAtom(Elements::element(Z_), Vector3());
+    rawSpecies_->setName(name_);
+
+    return NodeConstants::ProcessResult::Success;
+}
---
+++ src/nodes/atomicSpecies.h
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#pragma once
+
+#include "classes/moleculeSet.h"
+#include "classes/species.h"
+#include "nodes/node.h"
+#include <string>
+
+// Derivative1D Node
+class AtomicSpeciesNode : public Node
+{
+    public:
+    AtomicSpeciesNode(Graph *parentGraph);
+    ~AtomicSpeciesNode() override = default;
+
+    /*
+     * Definition
+     */
+    public:
+    std::string_view type() const override;
+    std::string_view summary() const override;
+
+    /*
+     * Data
+     */
+    private:
+    // Species object
+    std::unique_ptr<Species> species_{std::make_unique<Species>()};
+    // Local raw pointer to species
+    Species *rawSpecies_{species_.get()};
+    // Element symbol
+    std::string_view Z_{"Unknown"};
+    // Species name
+    std::string_view name_;
+
+    /*
+     * Processing
+     */
+    private:
+    // Run main processing
+    NodeConstants::ProcessResult process() override;
+};
