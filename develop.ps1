75fb2a57534852c1bbc70c29d9fc0700ddf67b1a
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-05-01 08:27:59 +0100

    don

--- develop.ps1
+++ develop.ps1
@@ -25,7 +25,8 @@
 
 param (
     [string]$qtVersion,
-    [string]$antlrVersion = "4.13.1", 
+    [string]$antlrVersion = "4.13.1",
+    [string]$forcePythonVersion = "3.11",
     [switch]$release = $false
 )
 
@@ -73,18 +74,26 @@ try {
 }
 
 # Find python, install if not found
-try {
-    & "python" --version
-    Write-Output "Found system Python..." @info_colors
-    $pythonVersion = $(python -c "import sys; v = sys.version_info; print(v.major == 3, v.minor == 12)")
-    $versionParts = $pythonVersion -split " "
-    if (-not ($versionParts[0] -eq "True" -and $versionParts[1] -eq "True")) {
-        Write-Output "System Python is version $(python --version) and it is recommended to be == 3.12 - installing with Chocolatey..." @info_colors
+if (-not [string]::IsNullOrEmpty($forcePythonVersion))
+{
+    Write-Output "Installing requested Python version $forcePythonVersion..."
+    choco install -y python --version=$forcePythonVersion --force
+}
+else
+{
+    try {
+        & "python" --version
+        Write-Output "Found system Python..." @info_colors
+        $pythonVersion = $(python -c "import sys; v = sys.version_info; print(v.major == 3, v.minor == 12)")
+        $versionParts = $pythonVersion -split " "
+        if (-not ($versionParts[0] -eq "True" -and $versionParts[1] -eq "True")) {
+            Write-Output "System Python is version $(python --version) and it is recommended to be == 3.12 - installing with Chocolatey..." @info_colors
+            choco install -y python --version=3.12.0
+        }
+    } catch {
+        Write-Output "Could not find system Python - installing with Chocolatey..." @info_colors
         choco install -y python --version=3.12.0
     }
-} catch {
-    Write-Output "Could not find system Python - installing with Chocolatey..." @info_colors
-    choco install -y python --version=3.12.0
 }
 
 refreshenv
