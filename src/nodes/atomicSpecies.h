61acc38cdaa6f98df9ed762e2b1805464b8081c5
RobBuchanan <106311829+RobBuchananCompPhys@users.noreply.github.com>
2025-05-02 13:25:34 +0100

    feat: Atomic species node (#2146)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>
    Co-authored-by: Adam Washington <adam.washington@stfc.ac.uk>
    Co-authored-by: Tristan Youngs <trisyoungs@googlemail.com>

---
+++ src/nodes/atomicSpecies.h
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#pragma once
+
+#include "classes/moleculeSet.h"
+#include "classes/species.h"
+#include "math/vector3.h"
+#include "nodes/node.h"
+#include <string>
+
+// AtomicSpecies Node
+class AtomicSpeciesNode : public Node
+{
+    public:
+    AtomicSpeciesNode(Graph *parentGraph) : Node(parentGraph) {}
+    AtomicSpeciesNode(Graph *parentGraph, std::string_view name, Elements::Element Z = Elements::Element::Ar,
+                      Vector3 r = Vector3());
+    ~AtomicSpeciesNode() override = default;
+
+    /*
+     * Definition
+     */
+    public:
+    std::string_view type() const override;
+    std::string_view summary() const override;
+
+    /*
+     * Data
+     */
+    private:
+    // Species object
+    Species species_;
+    // Local raw pointer to species
+    const Species *rawSpecies_{&species_};
+
+    /*
+     * Processing
+     */
+    private:
+    // Run main processing
+    NodeConstants::ProcessResult process() override;
+};
