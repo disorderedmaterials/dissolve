9fa58390fb68204a1dca9c879ac5fad2042bfdc0
Adam Washington <adam.washington@stfc.ac.uk>
2025-05-14 12:14:33 +0100

    Fix ordering of includes

--- src/gui/models/nodeGraph/parameterModel.cpp
+++ src/gui/models/nodeGraph/parameterModel.cpp
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2025 Team Dissolve and contributors
 
-#include "nodes/number.h"
 #include "parameterModel.h"
+#include "nodes/number.h"
 #include <qvariant.h>
 
 enum Roles
