f5a7bbdda7ec3c79ad6c542d8e1dc6adc4aaba42
Alexander Boswell <ab3839@bath.ac.uk>
2025-05-15 15:44:16 +0100

    Update includes

--- src/gui/speciesEditor.cpp
+++ src/gui/speciesEditor.cpp
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2025 Team Dissolve and contributors
 
+#include "gui/speciesEditor.h"
 #include "classes/empiricalFormula.h"
 #include "classes/species.h"
 #include "data/ff/library.h"
 #include "gui/speciesCellDialog.h"
-#include "gui/speciesEditor.h"
 #include "gui/widgets/elementSelector.h"
 #include "main/dissolve.h"
 #include "modules/geomOpt/geomOpt.h"
