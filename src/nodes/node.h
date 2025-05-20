f5c337ed77fecfc1d6e87277f4be2d2ee96d6b6e
RobBuchanan <robertbb269@hotmail.com>
2025-05-20 15:20:24 +0100

    return type

--- src/nodes/node.h
+++ src/nodes/node.h
@@ -81,7 +81,7 @@ class Node : public Serialisable<>
             echo();
     }
     // Node error format
-    template <typename... Args> bool error(std::format_string<Args...> format, Args... args)
+    template <typename... Args> NodeConstants::ProcessResult error(std::format_string<Args...> format, Args... args)
     {
         messages_.emplace_back(std::make_pair(MessageStatus::Error, std::format(format, std::forward<Args>(args)...)));
 
