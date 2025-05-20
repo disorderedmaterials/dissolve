b896131ff77dc5c20fec04652f55dfada3af9aa4
RobBuchanan <robertbb269@hotmail.com>
2025-05-20 10:49:09 +0100

    averaging has enum options

--- src/math/averaging.h
+++ src/math/averaging.h
@@ -21,6 +21,8 @@ enum AveragingScheme
 // Return enum options for AveragingScheme
 EnumOptions<Averaging::AveragingScheme> averagingSchemes();
 
+EnumOptions<Averaging::AveragingScheme> getEnumOptions(Averaging::AveragingScheme);
+
 // Establish the number of stored datasets, shift indices down, and lose oldest dataset if necessary
 int pruneOldData(GenericList &processingData, std::string_view name, std::string_view prefix, int nSetsInAverage);
 
