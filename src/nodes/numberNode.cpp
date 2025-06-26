a30b1d91802a152c450478c8274ece207ff1dfef
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2025-06-26 10:23:45 +0100

    Add simple NumberNode for testing.

---
+++ src/nodes/numberNode.cpp
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#include "nodes/numberNode.h"
+
+NumberNode::NumberNode(Graph *parentGraph) : Node(parentGraph)
+{
+    addOption<Number>("A", "A number", a_);
+    addOutput<Number>("A", "A number", a_);
+}
+
+/*
+ * Definition (Virtuals)
+ */
+
+// Return type of the node
+std::string_view NumberNode::type() const { return "Number"; }
+
+// Return short summary of the node's purpose
+std::string_view NumberNode::summary() const { return "A number"; }
+
+// Perform processing
+NodeConstants::ProcessResult NumberNode::process() { return NodeConstants::ProcessResult::Success; }
