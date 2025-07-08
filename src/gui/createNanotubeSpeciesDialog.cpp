8b34cbdcda1ff03dbdd4c7c1e64ac20ea960dffe
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2025-07-08 14:30:00 +0100

    Tidy up.

--- src/gui/createNanotubeSpeciesDialog.cpp
+++ src/gui/createNanotubeSpeciesDialog.cpp
@@ -16,6 +16,8 @@ CreateNanotubeSpeciesDialog::CreateNanotubeSpeciesDialog(QWidget *parent, Dissol
     ui_.ElementAButton->setText(QString::fromStdString(std::string(Elements::symbol(zA_))));
     ui_.ElementBButton->setText(QString::fromStdString(std::string(Elements::symbol(zB_))));
 
+    ui_.MSpin->setMaximum(ui_.NSpin->value());
+
     // Set display configuration
     ui_.StructureViewer->setSpecies(&species_);
 
@@ -33,69 +35,84 @@ void CreateNanotubeSpeciesDialog::regenerate()
 {
     species_.clear();
 
-    const auto r = ui_.BondLengthSpin->value();
+    /*
+     * Procedure based on "Determination of the chiral indices (n,m) of carbon nanotubes by electron diffraction",
+     * Lu-Chan Qin, Phys. Chem. Chem. Phys. 2007, *9*, 31-48. https://doi.org/10.1039/B614121H
+     */
 
-    const auto a0 = 2.46;
-    const auto a1 = Vec3<double>(a0, 0.0, 0.0);
-    const auto a2 = Vec3<double>(a0*sin(M_PI/6.0), a0*cos(M_PI/6.0), 0.0);
+    const auto root3 = sqrt(3.0);
+    const auto oneSixthPi = M_PI / 6.0; // == 30 degrees
+
+    // Get n,m values for tube
     const auto n = ui_.NSpin->value();
-    a2.print();
     const auto m = ui_.MSpin->value();
 
-    auto A = a0 * sqrt(n*n + m*m + n*m);
-    auto alpha = atan((sqrt(3.0) * m) / (2 * n + m));
-    const auto radius = A / (2.0 * M_PI);
-    std::cout << std::format("Helicity (alpha) = {} rad, {} degrees\n", alpha, alpha*DEGRAD);
+    // Determine a0, the principal unit length of the structure, from the specified bond distance. See Figure 1a, noting that
+    // a1 == a2 == a0.
+    const auto r = ui_.BondLengthSpin->value();
+    const auto a0 = r * root3;
 
-    // TEST
+    // Set principal vectors for reference
+    const auto va1 = Vec3<double>(a0, 0.0, 0.0);
+    const auto va2 = Vec3<double>(a0 * sin(oneSixthPi), a0 * cos(oneSixthPi), 0.0);
+
+    // Calculate repeat dimensions of the (n,m) sheet
+    // -- A is magnitude of vecA (equation 1) and represents our X dimension
+    auto A = a0 * sqrt(n * n + m * m + n * m);
+    // -- c is the magnitude of the vector perpendicular to vecA, along the tube axis, and represents our Y dimension
     auto M = std::gcd(2 * n + m, n + 2 * m);
-//    auto nc = -((n + 2*m) / M);
-//    auto mc = ((2*n + m) / M);
-//    auto vecAPerp = a1 * nc + a2 * mc;
-//    auto Aperp = a0 * sqrt(nc*nc + mc*mc + nc*mc);
-//
     auto c = sqrt(3.0) * A / M;
 
-    std::cout << std::format("Rectangle dims are A = {} c = {}\n", A, c);
-    auto d = std::gcd(n,m);
-    auto acc = a0 / sqrt(3.0);
-    auto T = 3.0 * acc * sqrt(n*n + n*m + m*m);
-    T /= (n-m)%(3*d) == 0 ? 3 * d : d;
-    std::cout << std::format("T = {}, acc = {}, d = {}\n", T, acc, d);
+    // Determine angle alpha, describing the tilt of the sheet away from va1 (equation 3)
+    auto alpha = atan((sqrt(3.0) * m) / (2 * n + m));
 
-    species_.createBox({A, T, 3.0}, {90,90,90});
+    // The radius of the tube is then the circumference (== A) divided by 2PI
+    const auto radius = A / (2.0 * M_PI);
+    std::cout << std::format("Helicity (alpha) = {} rad, {} degrees\n", alpha, alpha * DEGRAD);
 
-    // Generate the full coordinates
-    auto dy = a2.y/cos(alpha);
-    auto H = round(T / dy);
-    std::cout << std::format("Repeats H = {}, T/a0CosAlpha = {}, dy = {}\n", H, T / dy, dy);
-    for (auto j = 0; j <= n + m -1; ++j)
+    // TEST
+
+    //    std::cout << std::format("Rectangle dims are A = {} c = {}\n", A, c);
+    //    auto d = std::gcd(n,m);
+    //    auto acc = a0 / sqrt(3.0);
+    //    auto T = 3.0 * acc * sqrt(n*n + n*m + m*m);
+    //    T /= (n-m)%(3*d) == 0 ? 3 * d : d;
+    //    std::cout << std::format("T = {}, acc = {}, d = {}\n", T, acc, d);
+
+    species_.createBox({A, c, 3.0}, {90, 90, 90});
+
+    /*
+     * Generate the full coordinates of the sheet. This process is based on equations 7 - 9, but does not follow it precisely.
+     * Specifically, the innermost loop to translate the primary helix pair is originally stated to run over j (here 'i') from
+     * 0 - (m-1) inclusive, but this does not generate the correct number of copies for most cases. Instead, the number of
+     * required helix copies 'H' is calculated from c and the rotated y component of va2.
+     */
+    auto dy = va2.y / cos(alpha);
+    auto H = round(c / dy);
+    std::cout << std::format("Repeats H = {}, T/a0CosAlpha = {}, dy = {}\n", H, c / dy, dy);
+    for (auto j = 0; j <= n + m - 1; ++j)
     {
-        // Primary helix atoms
+        // Primary helix atoms (equation 7)
         auto x1j = j * a0 * cos(alpha);
         auto y1j = -j * a0 * sin(alpha);
 
-        // Secondary helix atoms
-        auto x2j = x1j - (a0 / sqrt(3.0)) * cos(M_PI/6.0 - alpha);
-        auto y2j = y1j - (a0 / sqrt(3.0)) * sin(M_PI/6.0-alpha);
+        // Secondary helix atoms (equation 8)
+        auto x2j = x1j - (a0 / sqrt(3.0)) * cos(oneSixthPi - alpha);
+        auto y2j = y1j - (a0 / sqrt(3.0)) * sin(oneSixthPi - alpha);
 
+        // Create H copies of the helix pairs (equation 9)
         for (auto i = 0; i < H; ++i)
         {
-            auto delta = Vec3<double>(-i * a0 * sin(M_PI/6.0 - alpha), i * a0 * cos(M_PI/6.0 - alpha), 0.0);
+            auto delta = Vec3<double>(-i * a0 * sin(oneSixthPi - alpha), i * a0 * cos(oneSixthPi - alpha), 0.0);
             auto r1 = delta + Vec3<double>(x1j, y1j, 0.0);
             auto r2 = delta + Vec3<double>(x2j, y2j, 0.0);
 
-            // Map negative y values back into their positive periodic image
-//            if (r1.y < 0.0)
-//                r1.y += T * d;
-//            if (r2.y < 0.0)
-//                r2.y += T * d;
+            species_.addAtom(Elements::C, species_.box()->fold(r1));
+            species_.addAtom(Elements::N, species_.box()->fold(r2));
 
-                species_.addAtom(Elements::C,     species_.box()->fold(r1));
-                species_.addAtom(Elements::N,     species_.box()->fold(r2));
-
-//            species_.addAtom(Elements::C, {radius*sin(2.0 * M_PI * r1.x / A) , r1.y, radius* cos(2.0 * M_PI * r1.x / A)});
-//            species_.addAtom(Elements::N, {radius*sin(2.0 * M_PI * r2.x / A) , r2.y, radius * cos(2.0 * M_PI * r2.x / A)});
+            //            species_.addAtom(Elements::C, {radius*sin(2.0 * M_PI * r1.x / A) , r1.y, radius* cos(2.0 * M_PI * r1.x
+            //            / A)}); species_.addAtom(Elements::N, {radius*sin(2.0 * M_PI * r2.x / A) , r2.y, radius * cos(2.0 *
+            //            M_PI * r2.x / A)});
         }
     }
 
@@ -179,17 +196,17 @@ void CreateNanotubeSpeciesDialog::on_TypeCombo_currentIndexChanged(int index)
 {
     Locker refreshLock(widgetsUpdating_);
 
-//    // Limit tube axial length depending on mode requested
-//    if (index == 0)
-//    {
-//        ui_.AxialRingLengthSpin->setSingleStep(1);
-//        ui_.AxialRingLengthSpin->setMinimum(1);
-//    }
-//    else
-//    {
-//        ui_.AxialRingLengthSpin->setSingleStep(2);
-//        ui_.AxialRingLengthSpin->setMinimum(2);
-//    }
+    //    // Limit tube axial length depending on mode requested
+    //    if (index == 0)
+    //    {
+    //        ui_.AxialRingLengthSpin->setSingleStep(1);
+    //        ui_.AxialRingLengthSpin->setMinimum(1);
+    //    }
+    //    else
+    //    {
+    //        ui_.AxialRingLengthSpin->setSingleStep(2);
+    //        ui_.AxialRingLengthSpin->setMinimum(2);
+    //    }
 
     refreshLock.unlock();
 
