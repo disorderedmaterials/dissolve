c570ad3084cb3a9837d68423354c58ecf9646da3
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2025-07-22 09:22:27 +0100

    Redo serialisation from AtomicMC.

--- src/nodes/atomicMC/atomicMC.cpp
+++ src/nodes/atomicMC/atomicMC.cpp
@@ -5,14 +5,21 @@
 
 AtomicMCNode::AtomicMCNode(Graph *parentGraph) : Node(parentGraph)
 {
+    // Inputs
     addInput<Configuration *>("Configuration", "Set target configuration for the module", targetConfiguration_)
         ->setFlags({ParameterBase::Required, ParameterBase::ClearData});
 
+    // Options
     addOption<Number>("ShakesPerAtom", "Number of shakes to attempt per atom", nShakesPerAtom_);
     addOption<Number>("TargetAcceptanceRate", "Target acceptance rate for Monte Carlo moves", targetAcceptanceRate_);
     addOption<Number>("StepSizeMax", "Maximum allowed value for step size, in Angstroms", stepSizeMax_);
     addOption<Number>("StepSizeMin", "Minimum allowed value for step size, in Angstroms", stepSizeMin_);
+
+    // Outputs
     addOutput<Configuration *>("Configuration", "Output configuration", targetConfiguration_);
+
+    // Serialisables
+    addSerialisable("stepSize", stepSize_);
 }
 
 std::string_view AtomicMCNode::type() const { return "AtomicMC"; }
