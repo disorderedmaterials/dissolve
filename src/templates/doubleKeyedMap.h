77fdd65742b80d67f42b211901c5a12738aede9e
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2025-07-28 14:41:29 +0100

    Just string keying.

--- src/templates/doubleKeyedMap.h
+++ src/templates/doubleKeyedMap.h
@@ -14,29 +14,38 @@
  * This is intended to replace the use of "static" Array2D instances containing e.g. partials between different atom types,
  * removing the critical dependence of an immutably ordered vector of AtomTypes.
  */
-template <typename KeyClass, typename ValueClass> class DoubleKeyedMap
+using DoubleKeyedMapKey = std::pair<std::string_view, std::string_view>;
+template <typename ValueClass> class DoubleKeyedMap
 {
     public:
-    using KeyGetter = std::function<std::string_view(const KeyClass key)>;
-    DoubleKeyedMap(KeyGetter getter, bool mirrored = false) : keyGetter_(std::move(getter)), mirroredAreEquivalent_(mirrored) {}
+    DoubleKeyedMap(bool mirrored = false) : mirroredAreEquivalent_(mirrored) {}
 
     private:
+    const std::string separator_{"//"};
     // Map of data
     std::map<std::string, ValueClass> data_;
-    // Getter for keys
-    KeyGetter keyGetter_;
     // Whether the mirrored key pairs A-B and B-A are equivalent
     bool mirroredAreEquivalent_;
 
     private:
     // Return the full key for the specified pair
-    const std::string &key(KeyClass A, KeyClass B) { return std::format("{}-{}", keyGetter_(A), keyGetter_(B)); }
+    const std::string key(std::string_view A, std::string_view B) { return std::format("{}{}{}", A, separator_, B); }
     // Return iterator to the specified key if it exists, obeying mirrored equivalents
-    std::map<std::string, ValueClass>::const_iterator find(KeyClass A, KeyClass B)
+    std::map<std::string, ValueClass>::iterator find(std::string_view A, std::string_view B)
     {
         if (mirroredAreEquivalent_)
         {
-            auto it = data_.find(kwy(A, B));
+            auto it = data_.find(key(A, B));
+            return it != data_.end() ? it : data_.find(key(B, A));
+        }
+        else
+            return data_.find(key(A, B));
+    }
+    std::map<std::string, ValueClass>::const_iterator find(std::string_view A, std::string_view B) const
+    {
+        if (mirroredAreEquivalent_)
+        {
+            auto it = data_.find(key(A, B));
             return it != data_.end() ? it : data_.find(key(B, A));
         }
         else
@@ -45,38 +54,58 @@ template <typename KeyClass, typename ValueClass> class DoubleKeyedMap
 
     public:
     // Clear data
-    void clear() { data_.clear(); }
+    void clear(bool mirrored = false)
+    {
+        data_.clear();
+        mirroredAreEquivalent_ = mirrored;
+    }
     // Set / overwrite key
-    void set(KeyClass A, KeyClass B, ValueClass value)
+    void set(std::string_view A, std::string_view B, ValueClass value)
     {
         auto it = find(A, B);
         if (it != data_.end())
-            it->value = value;
+            it->second = value;
         else
             data_[key(A, B)] = value;
     }
     // Remove the specified key
-    void erase(KeyClass A, KeyClass B)
+    void erase(std::string_view A, std::string_view B)
     {
         auto it = find(A, B);
         if (it != data_.end())
             data_.erase(it);
     }
     // Return whether the specified key exists
-    bool contains(KeyClass A, KeyClass B) const { return find(A, B) != data_.end(); }
+    bool contains(std::string_view A, std::string_view B) const { return find(A, B) != data_.end(); }
     // Get keyed value
-    const std::optional<ValueClass> get(KeyClass A, KeyClass B) const
+    ValueClass &get(const std::pair<std::string_view, std::string_view> &pair) { return get(pair.first, pair.second); }
+    ValueClass &get(std::string_view A, std::string_view B)
     {
         auto it = find(A, B);
         if (it == data_.end())
-            return {};
+        {
+            data_[key(A, B)] = ValueClass();
+            return data_[key(A, B)];
+        }
+        else
+            return it->second;
+    };
+    const ValueClass &get(const std::pair<std::string_view, std::string_view> &pair) const
+    {
+        return get(pair.first, pair.second);
+    }
+    const ValueClass &get(std::string_view A, std::string_view B) const
+    {
+        auto it = find(A, B);
+        if (it == data_.end())
+            throw(std::runtime_error(std::format("Key {} is not in DoubleKeyedMap.\n", key(A, B))));
         else
             return it->second;
     }
     // Iterators
-    std::map<std::string, ValueClass>::const_iterator begin() { return data_.begin(); }
+    std::map<std::string, ValueClass>::iterator begin() { return data_.begin(); }
     std::map<std::string, ValueClass>::const_iterator begin() const { return data_.begin(); }
-    std::map<std::string, ValueClass>::const_iterator end() { return data_.end(); }
+    std::map<std::string, ValueClass>::iterator end() { return data_.end(); }
     std::map<std::string, ValueClass>::const_iterator end() const { return data_.end(); }
     // Return the underlying map
     std::map<std::string, ValueClass> &map() { return data_; }
