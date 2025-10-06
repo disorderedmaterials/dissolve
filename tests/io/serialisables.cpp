d9a41a179f6223934fc51ecbf3cdff0fb567850b
Tristan Youngs <trisyoungs@googlemail.com>
2025-10-06 14:38:58 +0100

    Fix Histogram1D deserialisation.

--- tests/io/serialisables.cpp
+++ tests/io/serialisables.cpp
@@ -69,6 +69,33 @@ TEST(SerialisableTest, Data1DErrors)
     }
 }
 
+TEST(SerialisableTest, Histogram1D)
+{
+    Histogram1D a, b;
+    a.initialise(1.0, 5.0, 1.0);
+    a.bin(1.5);
+    a.bin(0.2);
+    a.bin(1.5);
+    a.bin(4.0);
+    a.accumulate();
+
+    for (auto n = 0; n < 2; ++n)
+    {
+        EXPECT_NO_THROW(roundTrip(a, b));
+        ASSERT_EQ(a.nBins(), b.nBins());
+        ASSERT_EQ(a.minimum(), b.minimum());
+        ASSERT_EQ(a.maximum(), b.maximum());
+        ASSERT_EQ(a.binWidth(), b.binWidth());
+        for (auto &&[aValue, bValue] : zip(a.accumulatedData().values(), b.accumulatedData().values()))
+            EXPECT_DOUBLE_EQ(aValue, bValue);
+
+        // Add an extra point to test clearing of the old data
+        a.zeroBins();
+        a.bin(2.0);
+        a.accumulate();
+    }
+}
+
 TEST(SerialisableTest, SampledDouble)
 {
     SampledDouble a, b;
@@ -107,33 +134,6 @@ TEST(SerialisableTest, SampledData1D)
 
         // Add extra data to test clearing of the old data
         a += {5.0, 5.0, 5.0};
-    }
-}
-
-TEST(SerialisableTest, Histogram1D)
-{
-    Histogram1D a, b;
-    a.initialise(1.0, 5.0, 1.0);
-    a.bin(1.5);
-    a.bin(0.2);
-    a.bin(1.5);
-    a.bin(4.0);
-    a.accumulate();
-
-    for (auto n = 0; n < 2; ++n)
-    {
-        EXPECT_NO_THROW(roundTrip(a, b));
-        ASSERT_EQ(a.nBins(), b.nBins());
-        ASSERT_EQ(a.minimum(), b.minimum());
-        ASSERT_EQ(a.maximum(), b.maximum());
-        ASSERT_EQ(a.binWidth(), b.binWidth());
-        for (auto &&[aValue, bValue] : zip(a.accumulatedData().values(), b.accumulatedData().values()))
-            EXPECT_DOUBLE_EQ(aValue, bValue);
-
-        // Add an extra point to test clearing of the old data
-        a.zeroBins();
-        a.bin(2.0);
-        a.accumulate();
     }
 }
 
