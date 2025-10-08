542ddf0ae793bdeb9334000b342a71b13d4a40b4
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2025-10-08 15:19:45 +0100

    Start adding DoubleKeyedMap test.

---
+++ tests/classes/doubleKeyedMap.cpp
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025 Team Dissolve and contributors
+
+#include "templates/doubleKeyedMap.h"
+#include <gtest/gtest.h>
+
+namespace UnitTest
+{
+
+TEST(DoubleKeyedMap, BasicMirrored)
+{
+    DoubleKeyedMap<int> map;
+
+    // Add and retrieve an entry
+    DoubleKeyedMapKey key("A", "B");
+    map[key] = 10;
+    EXPECT_TRUE(map.contains(key));
+    EXPECT_TRUE(map.contains("A", "B"));
+    EXPECT_EQ(map[key], 10);
+
+    // Triangular mapping...
+    EXPECT_TRUE(map.contains("B", "A"));
+
+    // Remove key
+    ASSERT_NO_THROW(map.erase("A", "B"));
+    EXPECT_FALSE(map.contains("A", "B"));
+
+    // Remove non-existent key
+    ASSERT_ANY_THROW(map.erase("A", "B"));
+}
+
+TEST(DoubleKeyedMap, BasicFull)
+{
+    DoubleKeyedMap<int> map(false);
+
+    // Add and retrieve an entry
+    DoubleKeyedMapKey key("A", "B");
+    map[key] = 10;
+    EXPECT_TRUE(map.contains(key));
+    EXPECT_TRUE(map.contains("A", "B"));
+    EXPECT_EQ(map[key], 10);
+
+    // Full mapping...
+    EXPECT_FALSE(map.contains("B", "A"));
+}
+
+} // namespace UnitTest
