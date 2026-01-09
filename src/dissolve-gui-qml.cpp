29e01d534ea1835512cd285f938a299df8926ded
Adam Washington <adam.washington@stfc.ac.uk>
2026-01-09 09:35:55 +0000

    chore:Fix nix bundle version and fix copyright dates (#2294)

--- src/dissolve-gui-qml.cpp
+++ src/dissolve-gui-qml.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2025 Team Dissolve and contributors
+// Copyright (c) 2026 Team Dissolve and contributors
 
 #include "base/messenger.h"
 #include "base/processPool.h"
@@ -51,7 +51,7 @@ int main(int args, char **argv)
     QLocale::setDefault(QLocale::C);
 
     // Print GPL license information
-    Messenger::print("Dissolve-GUI-QML {} version {}, Copyright (C) 2025 Team Dissolve and contributors.\n", Version::appType(),
+    Messenger::print("Dissolve-GUI-QML {} version {}, Copyright (C) 2026 Team Dissolve and contributors.\n", Version::appType(),
                      Version::info());
     Messenger::print("Source repository: {}.\n", Version::repoUrl());
     Messenger::print("Dissolve comes with ABSOLUTELY NO WARRANTY.\n");
--- src/math/cuboid.cpp
+++ src/math/cuboid.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (c) 2025 Team Dissolve and contributors
+// Copyright (c) 2026 Team Dissolve and contributors
 
 #include "math/cuboid.h"
 #include "math/mathFunc.h"
