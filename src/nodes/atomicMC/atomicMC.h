6acd0d0e9a0f9706c59876f70a5a148672b9a47a
RobBuchanan <robertbb269@hotmail.com>
2026-01-15 13:37:51 +0000

    default temp to 300K and stepsize

--- src/nodes/atomicMC/atomicMC.h
+++ src/nodes/atomicMC/atomicMC.h
@@ -27,17 +27,17 @@ class AtomicMCNode : public Node
     // Target configurations
     Configuration *targetConfiguration_{nullptr};
     // Temperature (K)
-    Number temperature_{0};
+    Number temperature_{300};
     // Interatomic cutoff distance to use for energy calculation
     std::optional<double> cutoffDistance_;
     // Number of shakes to attempt per atom
     Number nShakesPerAtom_{1};
     // Step size in Angstroms to use in Monte Carlo moves
-    Number stepSize_{0.05};
+    Number stepSize_{0.0001};
     // Maximum allowed value for step size, in Angstroms
     Number stepSizeMax_{1.0};
     // Minimum allowed value for step size, in Angstroms
-    Number stepSizeMin_{0.001};
+    Number stepSizeMin_{0.0001};
     // Target acceptance rate for Monte Carlo moves
     Number targetAcceptanceRate_{0.33};
 
