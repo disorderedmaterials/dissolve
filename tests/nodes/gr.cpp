2f75e502a87c6d606280e6438071e4d7f6799adb
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-01-19 16:29:50 +0000

    Water correlations test complete.

--- tests/nodes/gr.cpp
+++ tests/nodes/gr.cpp
@@ -41,4 +41,46 @@ TEST(GRNodeTest, Methods)
     ASSERT_TRUE(DissolveSystemTest::checkPartialSet(rawGRBaseline, rawGRCells, 1.0e-8));
 }
 
+TEST(GRNodeTest, WaterCorrelationsVsEPSR)
+{
+    GraphTestData data;
+    createWater1000Graph(&data.graphRoot, CoordinateImportFileFormat("epsr25/water1000-neutron/waterbox.ato",
+                                                                     CoordinateImportFileFormat::CoordinateImportFormat::EPSR));
+
+    // Set GR options
+    auto grNode = data.graphRoot.findNode("GR");
+    ASSERT_TRUE(grNode);
+    ASSERT_TRUE(grNode->setOption("IntraBroadening", Function1DWrapper()));
+    ASSERT_TRUE(grNode->setOption<Number>("BinWidth", 0.03));
+
+    // Run the graph
+    ASSERT_EQ(grNode->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(grNode->versionIndex(), 0);
+
+    // Get the raw GR
+    auto rawGR = grNode->getOutputValue<PartialSet *>("RawGR");
+
+    // Partial g(r) (unbound terms)
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->unboundPartials().get(DoubleKeyedMapKey("OW", "OW")), "OW-OW Unbound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.g01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 6.0e-2));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->unboundPartials().get(DoubleKeyedMapKey("HW", "OW")), "HW-OW Unbound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.g01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 2.0e-2));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->unboundPartials().get(DoubleKeyedMapKey("HW", "HW")), "HW-HW Unbound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.g01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 2.0e-2));
+
+    // Partial g(r) (intramolecular terms)
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->boundPartials().get(DoubleKeyedMapKey("OW", "OW")), "OW-OW Bound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 1.0e-5,
+        Error::ErrorType::RMSEError));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->boundPartials().get(DoubleKeyedMapKey("HW", "OW")), "HW-OW Bound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 0.1));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->boundPartials().get(DoubleKeyedMapKey("HW", "HW")), "HW-HW Bound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 1.5e-2));
+}
 } // namespace UnitTest
