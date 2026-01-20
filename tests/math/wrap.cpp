5aad6d029c3eecdc74dcca11538a8f102ae26252
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-01-20 14:22:16 +0000

    Missing source file.

---
+++ tests/math/wrap.cpp
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#include "math/vector3.h"
+#include "tests/testData.h"
+#include <gtest/gtest.h>
+
+namespace UnitTest
+{
+TEST(WrapIntegers, WrapIntegers)
+{
+    EXPECT_EQ(DissolveMath::wrap(2, 0, 4), 2);
+    EXPECT_EQ(DissolveMath::wrap(0, 0, 4), 0);
+    EXPECT_EQ(DissolveMath::wrap(-5, 0, 4), 0);
+    EXPECT_EQ(DissolveMath::wrap(-15, 0, 4), 0);
+    EXPECT_EQ(DissolveMath::wrap(20, 0, 4), 0);
+    EXPECT_EQ(DissolveMath::wrap(4, 0, 4), 4);
+    EXPECT_EQ(DissolveMath::wrap(8, 0, 4), 3);
+    EXPECT_EQ(DissolveMath::wrap(9, 0, 4), 4);
+
+    EXPECT_EQ(DissolveMath::wrap(2, -5, 5), 2);
+    EXPECT_EQ(DissolveMath::wrap(0, -5, 5), 0);
+    EXPECT_EQ(DissolveMath::wrap(-5, -5, 5), -5);
+    EXPECT_EQ(DissolveMath::wrap(-16, -5, 5), -5);
+    EXPECT_EQ(DissolveMath::wrap(5, -5, 5), 5);
+    EXPECT_EQ(DissolveMath::wrap(16, -5, 5), 5);
+    EXPECT_EQ(DissolveMath::wrap(6, -5, 5), -5);
+}
+
+} // namespace UnitTest
