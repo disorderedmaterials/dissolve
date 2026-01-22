861322b2de9decc3e8992062bb73443386bfea1d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-01-22 14:19:25 +0000

    FullWeights doesn't exist anymore.

--- src/modules/epsr/process.cpp
+++ src/modules/epsr/process.cpp
@@ -22,6 +22,7 @@
 #include "modules/gr/gr.h"
 #include "modules/neutronSQ/neutronSQ.h"
 #include "modules/sq/sq.h"
+#include "modules/xRaySQ/xRaySQ.h"
 #include "templates/algorithms.h"
 #include "templates/array3D.h"
 #include <functional>
@@ -451,7 +452,7 @@ Module::ExecutionResult EPSRModule::process(Dissolve &dissolve)
         }
         else if (module->type() == ModuleTypes::XRaySQ)
         {
-            auto &weights = moduleData.retrieve<XRayWeights>("FullWeights", module->name());
+            const auto weights = dynamic_cast<XRaySQModule *>(module)->weights();
 
             // For X-ray data we always add the reference data normalised to AverageOfSquares in order to give consistency
             // in terms of magnitude with any neutron data. If the calculated data have not been normalised, or were
