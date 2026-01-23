d16500f37743df2d761c081abad84c186a63e6c9
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-01-23 09:49:20 +0000

    Basic NeutronSQ tests - exchangeables required.

--- tests/graphData.h
+++ tests/graphData.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include "classes/isotopologueSet.h"
 #include "main/dissolve.h"
 #include "nodes/atomicSpecies.h"
 #include "nodes/configuration.h"
@@ -87,11 +88,16 @@ inline void createWater1000Graph(Graph *root, CoordinateImportFileFormat initial
 
     auto d2o = root->createNode("NeutronSQ", "D2O");
     ASSERT_TRUE(d2o);
+    auto isoD = waterNode->species().findIsotopologue("D2O");
+    ASSERT_TRUE(isoD);
+    ASSERT_TRUE(d2o->setOption("Isotopologues", IsotopologueSet({{isoD, 1.0}})));
     ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "D2O", "UnweightedGR"}));
     ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "D2O", "UnweightedSQ"}));
 
     auto hdo = root->createNode("NeutronSQ", "HDO");
     ASSERT_TRUE(hdo);
+    ASSERT_TRUE(
+        hdo->setOption("Isotopologues", IsotopologueSet({{isoD, 1.0}, {waterNode->species().naturalIsotopologue(), 1.0}})));
     ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "HDO", "UnweightedGR"}));
     ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "HDO", "UnweightedSQ"}));
 
@@ -144,6 +150,33 @@ inline void createWaterMethanolGraph(Graph *root)
     auto sqNode = root->createNode("SQ");
     ASSERT_TRUE(sqNode);
     ASSERT_TRUE(root->addEdge({"GR", "UnweightedGR", "SQ", "UnweightedGR"}));
+
+    // Create NeutronSQ nodes
+    auto h2o = waterNode->species().findIsotopologue("Natural");
+    ASSERT_TRUE(h2o);
+    auto d2o = waterNode->species().findIsotopologue("D2O");
+    ASSERT_TRUE(d2o);
+    auto methylD_OH = waterNode->species().findIsotopologue("MethylD-OH");
+    ASSERT_TRUE(methylD_OH);
+    auto OD_methylH = waterNode->species().findIsotopologue("OD-MethylH");
+    ASSERT_TRUE(OD_methylH);
+    auto methanolH = waterNode->species().findIsotopologue("Natural");
+    ASSERT_TRUE(methanolH);
+    auto methanolD = waterNode->species().findIsotopologue("Deuteriated");
+    ASSERT_TRUE(methanolD);
+    std::vector<std::tuple<std::string, std::vector<std::pair<const Isotopologue *, double>>>> samples = {
+        {"HHH", {{h2o, 1.0}, {methanolH, 1.0}}},  {"H5H", {{h2o, 1.0}, {methanolH, 0.5}, {OD_methylH, 0.5}}},
+        {"DHH", {{d2o, 1.0}, {methanolH, 1.0}}},  {"HDH", {{h2o, 1.0}, {OD_methylH, 1.0}}},
+        {"HHD", {{h2o, 1.0}, {methylD_OH, 1.0}}}, {"DDH", {{d2o, 1.0}, {OD_methylH, 1.0}}},
+        {"HDD", {{h2o, 1.0}, {methanolD, 1.0}}},  {"DDD", {{d2o, 1.0}, {methanolD, 1.0}}}};
+    for (const auto &[name, isotopologues] : samples)
+    {
+        auto neutronSQNode = root->createNode("NeutronSQ", name);
+        ASSERT_TRUE(neutronSQNode);
+        ASSERT_TRUE(neutronSQNode->setOption("Isotopologues", IsotopologueSet(isotopologues)));
+        ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", name, "UnweightedGR"}));
+        ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", name, "UnweightedSQ"}));
+    }
 }
 
 // Create a benzene graph in the supplied root node
@@ -179,20 +212,25 @@ inline void createBenzeneGraph(Graph *root)
     ASSERT_TRUE(root->addEdge({"GR", "UnweightedGR", "SQ", "UnweightedGR"}));
 
     // Add in NeutronSQ
-    auto H = root->createNode("NeutronSQ", "H");
-    ASSERT_TRUE(H);
-    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "H", "UnweightedGR"}));
-    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "H", "UnweightedSQ"}));
+    auto C6H6 = root->createNode("NeutronSQ", "C6H6");
+    ASSERT_TRUE(C6H6);
+    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "C6H6", "UnweightedGR"}));
+    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "C6H6", "UnweightedSQ"}));
 
-    auto D = root->createNode("NeutronSQ", "D");
-    ASSERT_TRUE(D);
-    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "D", "UnweightedGR"}));
-    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "D", "UnweightedSQ"}));
+    auto C6D6 = root->createNode("NeutronSQ", "C6D6");
+    ASSERT_TRUE(C6D6);
+    auto isoD = benzeneNode->species().findIsotopologue("C6D6");
+    ASSERT_TRUE(isoD);
+    ASSERT_TRUE(C6D6->setOption("Isotopologues", IsotopologueSet({{isoD, 1.0}})));
+    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "C6D6", "UnweightedGR"}));
+    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "C6D6", "UnweightedSQ"}));
 
-    auto HD = root->createNode("NeutronSQ", "HD");
+    auto HD = root->createNode("NeutronSQ", "5050");
     ASSERT_TRUE(HD);
-    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "HD", "UnweightedGR"}));
-    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "HD", "UnweightedSQ"}));
+    ASSERT_TRUE(
+        HD->setOption("Isotopologues", IsotopologueSet({{benzeneNode->species().naturalIsotopologue(), 1.0}, {isoD, 1.0}})));
+    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedGR", "5050", "UnweightedGR"}));
+    ASSERT_TRUE(root->addEdge({"SQ", "UnweightedSQ", "5050", "UnweightedSQ"}));
 
     // Add in XRaySQ?
     // auto X = root->createNode("XRaySQ", "X");
