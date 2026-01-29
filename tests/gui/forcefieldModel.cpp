474eaff756b44ef2d17ebfcc54f4d70dee42dbc2
Adam Washington <adam.washington@stfc.ac.uk>
2026-01-29 11:26:07 +0000

    feat:Add forcefield node (#2306)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>

---
+++ tests/gui/forcefieldModel.cpp
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#include "data/ff/library.h"
+#include "gui/models/nodeGraph/graphModel.h"
+#include "gui/models/simpleForcefieldModel.h"
+#include "nodes/forcefield.h"
+#include <gtest/gtest.h>
+
+namespace UnitTest
+{
+TEST(ForcefieldModel, Basic)
+{
+    // Stage 1: Simulate GUI interactions to create species
+    Graph graph(nullptr);
+    GraphModel graphModel;
+    graphModel.setGraph(&graph);
+
+    ASSERT_EQ(graph.nodes().size(), 2);
+
+    SimpleForcefieldModel model;
+    model.assignModel(&graphModel);
+    model.setName("Kulmala2010");
+    model.create(0, 0);
+
+    ASSERT_EQ(graph.nodes().size(), 3);
+    auto node = dynamic_cast<ForcefieldNode *>(graph.node("Kulmala2010"));
+    ASSERT_TRUE(node);
+    EXPECT_EQ(node->forcefield().get(), ForcefieldLibrary::forcefield("Kulmala2010").get());
+}
+} // namespace UnitTest
