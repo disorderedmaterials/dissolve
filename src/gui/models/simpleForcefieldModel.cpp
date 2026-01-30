828d5c856988f18a6decdc84556d642e5d030ad1
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-01-30 09:29:34 +0000

    Use findNode().

--- src/gui/models/simpleForcefieldModel.cpp
+++ src/gui/models/simpleForcefieldModel.cpp
@@ -34,7 +34,7 @@ void SimpleForcefieldModel::create(int x, int y)
 {
     auto name = ff_->name();
     graphModel_->emplace_back(x, y, "Forcefield", QString::fromStdString(std::string(name)));
-    auto ff = dynamic_cast<ForcefieldNode *>(graphModel_->graph()->node(name));
+    auto ff = dynamic_cast<ForcefieldNode *>(graphModel_->graph()->findNode(name));
     if (ff)
         ff->forcefield() = ff_;
 }
