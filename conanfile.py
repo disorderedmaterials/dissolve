07d4673e79317c823e6dd5b3622fea7c0537ac95
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-02-10 10:17:47 +0000

    Request hwloc build in shared.

--- conanfile.py
+++ conanfile.py
@@ -14,6 +14,7 @@ class DissolveRecipe(ConanFile):
         "msvc_dev": False,
     }
     def configure(self):
+        self.options["hwloc"].shared = True
         self.options["puxixml"].header_only = False
         self.options["antlr4-cppruntime"].shared = True
 
