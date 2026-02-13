7fba76ce75107784069d6e45ad3fc5fc28b592b4
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-02-13 11:39:12 +0000

    Start forming unit test.

--- tests/ff/energy-water-SPCFw.cpp
+++ tests/ff/energy-water-SPCFw.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2026 Team Dissolve and contributors
 
-#include "math/windowFunction.h"
 #include "nodes/gr/gr.h"
 #include "tests/graphData.h"
 #include "tests/testData.h"
@@ -12,42 +11,18 @@ namespace UnitTest
 TEST(WaterSPCFwEnergyTest, Water)
 {
     GraphTestData data;
-    createWater1000Graph(&data.graphRoot, CoordinateImportFileFormat("epsr25/water1000-neutron/waterbox.ato",
-                                                                     CoordinateImportFileFormat::CoordinateImportFormat::EPSR));
+    createWaterGraph(&data.graphRoot, 1000,
+                     CoordinateImportFileFormat("dlpoly/water3000_energyForce/CONFIG",
+                                                CoordinateImportFileFormat::CoordinateImportFormat::DLPOLY));
 
-    // Set GR options
-    auto grNode = data.graphRoot.findNode("GR");
-    ASSERT_TRUE(grNode);
-    ASSERT_TRUE(grNode->setOption("IntraBroadening", Function1DWrapper()));
-    ASSERT_TRUE(grNode->setOption<Number>("BinWidth", 0.03));
+    // Add energy node
+    auto energyNode = data.graphRoot.createNode("Energy");
+    ASSERT_TRUE(energyNode);
+    ASSERT_TRUE(data.graphRoot.addEdge({"Import", "Configuration", "Energy", "Configuration"}));
 
-    // Run the graph from each NeutronSQ node
-    auto H2O = data.graphRoot.findNode("H2O");
-    ASSERT_TRUE(H2O);
-    ASSERT_EQ(H2O->run(), NodeConstants::ProcessResult::Success);
-    ASSERT_EQ(grNode->versionIndex(), 0);
-    ASSERT_EQ(H2O->versionIndex(), 0);
-    auto D2O = data.graphRoot.findNode("D2O");
-    ASSERT_TRUE(D2O);
-    ASSERT_EQ(D2O->run(), NodeConstants::ProcessResult::Success);
-    ASSERT_EQ(grNode->versionIndex(), 0);
-    ASSERT_EQ(D2O->versionIndex(), 0);
-    auto HDO = data.graphRoot.findNode("HDO");
-    ASSERT_TRUE(HDO);
-    ASSERT_EQ(HDO->run(), NodeConstants::ProcessResult::Success);
-    ASSERT_EQ(grNode->versionIndex(), 0);
-    ASSERT_EQ(HDO->versionIndex(), 0);
-
-    // Check total F(Q)
-    EXPECT_TRUE(DissolveSystemTest::checkData1D(
-        D2O->getOutputValue<PartialSet *>("WeightedSQ")->total(), "Total F(Q)",
-        {"epsr25/water1000-neutron/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 3.0e-4));
-    EXPECT_TRUE(DissolveSystemTest::checkData1D(
-        H2O->getOutputValue<PartialSet *>("WeightedSQ")->total(), "Total F(Q)",
-        {"epsr25/water1000-neutron/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 6.0e-4));
-    EXPECT_TRUE(DissolveSystemTest::checkData1D(
-        HDO->getOutputValue<PartialSet *>("WeightedSQ")->total(), "Total F(Q)",
-        {"epsr25/water1000-neutron/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 2.0e-5));
+    // Run the graph from the Energy node
+    ASSERT_EQ(energyNode->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(energyNode->versionIndex(), 0);
 }
 
 } // namespace UnitTest
