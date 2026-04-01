c736d83873719a9e6ceb3b854fe5b3ce8687a77e
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-04-01 16:43:18 +0100

    Use static function.

--- tests/nodes/flow.cpp
+++ tests/nodes/flow.cpp
@@ -6,6 +6,7 @@
 #include "nodes/graph.h"
 #include "nodes/number.h"
 #include "nodes/numberNode.h"
+#include "tests/graphData.h"
 #include <gtest/gtest.h>
 
 namespace UnitTest
@@ -230,7 +231,7 @@ TEST_F(GraphFlowTest, RemoveEdges)
     x2a->set(Number{20});
     EXPECT_EQ(z_->run(), NodeConstants::ProcessResult::Success);
     EXPECT_EQ(z_->findOutput("Result")->get<Number>().asInteger(), 30);
-    exportMermaidGraph(graph_);
+    TestGraph::exportMermaidGraph(graph_);
 }
 
 } // namespace UnitTest
