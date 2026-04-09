09c69fc319b4cd76fe55723395a918fe8d273d0d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-04-09 14:52:40 +0100

    Rework water test to use a graph.

--- tests/workflows/phantomAtoms.cpp
+++ tests/workflows/phantomAtoms.cpp
@@ -2,133 +2,169 @@
 // Copyright (c) 2026 Team Dissolve and contributors
 
 #include "data/atomicMasses.h"
-#include "generator/add.h"
-#include "generator/box.h"
+#include "tests/graphData.h"
 #include "tests/testData.h"
 #include <gtest/gtest.h>
-#include <vector>
 
 namespace UnitTest
 {
-class PhantomAtomsTest : public ::testing::Test
+TEST(PhantomAtomsTest, Basic)
 {
-    protected:
-    DissolveSystemTest systemTest;
-};
+    // Construct the test graph
+    TestGraph testGraph;
 
-TEST_F(PhantomAtomsTest, Basic)
-{
-    CoreData coreData;
-    Dissolve dissolve(coreData);
-
-    // Set up species
-    auto arType = coreData.addAtomType(Elements::Ar);
-    auto artType = coreData.addAtomType(Elements::Phantom);
-    auto *artAr = coreData.copySpecies(&tetrahedralArgonSpecies());
-    artAr->atom(0).setAtomType(arType);
-    for (auto n = 1; n < 5; ++n)
-        artAr->atom(n).setAtomType(artType);
-
-    // Create a configuration
-    auto *cfg = coreData.addConfiguration();
-    auto &procedure = cfg->generator();
-    auto boxLength = 20.0;
-    auto box = procedure.createRootNode<BoxGeneratorNode>("Box", Vector3NodeValue(boxLength, boxLength, boxLength),
-                                                          Vector3NodeValue(90, 90, 90));
-
-    // Add in some artificial argon atoms
     const auto nMolecules = 100;
-    procedure.createRootNode<AddGeneratorNode>("ArtAr", artAr, nMolecules);
+    auto lastNode =
+        testGraph.createConfiguration("Box", {{createTetrahedralArgon, nMolecules}}, {20.0, 20.0, 20.0}, {90.0, 90.0, 90.0});
 
-    // Set up the prior configuration
-    dissolve.updatePairPotentials();
-    cfg->generate({dissolve});
+    // Run the graph to set up the configuration
+    ASSERT_TRUE(lastNode);
+    ASSERT_EQ(lastNode->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(lastNode->versionIndex(), 0);
+
+    // Grab the species
+    auto speciesNode = dynamic_cast<SpeciesNode *>(testGraph.findNode("TetrahedralArgon"));
+    ASSERT_TRUE(speciesNode);
+    auto &species = speciesNode->species();
 
     // Basic species checks
-    EXPECT_EQ(artAr->nAtoms(), 5);
-    EXPECT_EQ(artAr->nAtoms(SpeciesAtom::Presence::Phantom), 4);
-    EXPECT_DOUBLE_EQ(artAr->mass(), AtomicMass::mass(Elements::Ar));
+    EXPECT_EQ(species.nAtoms(), 5);
+    EXPECT_EQ(species.nAtoms(SpeciesAtom::Presence::Phantom), 4);
+    EXPECT_DOUBLE_EQ(species.mass(), AtomicMass::mass(Elements::Ar));
+
+    // Get the configuration
+    auto *cfg = lastNode->getOutputValue<Configuration *>("Configuration");
+    ASSERT_TRUE(cfg);
 
     // Basic configuration checks
     EXPECT_EQ(cfg->nMolecules(), nMolecules);
-    EXPECT_EQ(cfg->nAtoms(), nMolecules * artAr->nAtoms());
-    EXPECT_EQ(cfg->nAtoms(SpeciesAtom::Presence::Phantom), nMolecules * artAr->nAtoms(SpeciesAtom::Presence::Phantom));
+    EXPECT_EQ(cfg->nAtoms(), nMolecules * species.nAtoms());
+    EXPECT_EQ(cfg->nAtoms(SpeciesAtom::Presence::Phantom), nMolecules * species.nAtoms(SpeciesAtom::Presence::Phantom));
 
     // Check density - should correspond to number density of physical atoms only
-    EXPECT_NEAR(*cfg->atomicDensity(), (nMolecules * artAr->nAtoms(SpeciesAtom::Presence::Physical)) / cfg->box()->volume(),
+    EXPECT_NEAR(*cfg->atomicDensity(), (nMolecules * species.nAtoms(SpeciesAtom::Presence::Physical)) / cfg->box()->volume(),
                 1.0e-5);
 }
 
-TEST_F(PhantomAtomsTest, Water)
+TEST(PhantomAtomsTest, Water)
 {
-    ASSERT_NO_THROW_VERBOSE(systemTest.setUp("dissolve/input/water-with-phantom-atoms.txt"));
-    ASSERT_TRUE(systemTest.dissolve().iterate(1));
+    // Set up the test graph
+    TestGraph testGraph;
+    auto lastNode = testGraph.createConfiguration("Box", {{createWaterPhantom, 1000}}, 0.1);
+    lastNode = testGraph.appendImportCoordinates(
+        lastNode,
+        CoordinateImportFileFormat("xyz/water1000-phantom.xyz", CoordinateImportFileFormat::CoordinateImportFormat::XYZ));
+
+    // Add correlation function nodes
+    auto &&[grNode, sqNode] = testGraph.appendGRSQ(lastNode, false, true);
+    ASSERT_TRUE(grNode);
+    ASSERT_TRUE(grNode->setOption<Number>("BinWidth", 0.03));
+    ASSERT_TRUE(sqNode);
+
+    // Add in NeutronSQ
+    auto H2O = testGraph.appendNeutronSQ(sqNode, "H2O");
+    auto D2O = testGraph.appendNeutronSQ(sqNode, "D2O", {{"Water", "D2O", 1.0}});
+    auto HDO =
+        testGraph.appendNeutronSQ(sqNode, "5050", {{"Water", "Natural", 1.0}, {"Water", "D2O", 1.0}}, Exchangeables({"HW"}));
+
+    // Add XRaySQ
+    auto H2Ox = testGraph.appendXRaySQ(
+        sqNode, "H2Ox",
+        Data1DImportFileFormat("epsr25/water1000-neutron-xray/PCCPfofq.txt", Data1DImportFileFormat::Data1DImportFormat::XY));
+    ASSERT_TRUE(H2Ox);
+    ASSERT_TRUE(H2Ox->setOption<StructureFactors::NormalisationType>(
+        "NormaliseTo", StructureFactors::NormalisationType::AverageOfSquaresNormalisation));
+    ASSERT_TRUE(H2Ox->setOption<StructureFactors::NormalisationType>(
+        "ReferenceNormalisedTo", StructureFactors::NormalisationType::AverageOfSquaresNormalisation));
+    ASSERT_TRUE(H2Ox->setOption<std::optional<double>>("ReferenceFTQMin", 0.5));
+
+    // Run the graph from each NeutronSQ and XRaySQ node
+    ASSERT_TRUE(H2O);
+    ASSERT_EQ(H2O->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(grNode->versionIndex(), 0);
+    ASSERT_EQ(H2O->versionIndex(), 0);
+    ASSERT_TRUE(D2O);
+    ASSERT_EQ(D2O->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(grNode->versionIndex(), 0);
+    ASSERT_EQ(D2O->versionIndex(), 0);
+    ASSERT_TRUE(HDO);
+    ASSERT_EQ(HDO->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(grNode->versionIndex(), 0);
+    ASSERT_EQ(HDO->versionIndex(), 0);
+    ASSERT_EQ(H2Ox->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(grNode->versionIndex(), 0);
+    ASSERT_EQ(H2Ox->versionIndex(), 0);
+
+    // Get the raw GR
+    auto rawGR = grNode->getOutputValue<PartialSet *>("RawGR");
 
     // Partial g(r) (unbound terms)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "GR01//Bulk//OriginalGR//OW-OW//Unbound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->unboundPartials().get(DoubleKeyedMapKey("OW", "OW")), "OW-OW Unbound Partial",
         {"epsr25/water1000-neutron/water.EPSR.g01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 6.0e-2));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "GR01//Bulk//OriginalGR//OW-HW//Unbound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->unboundPartials().get(DoubleKeyedMapKey("HW", "OW")), "HW-OW Unbound Partial",
         {"epsr25/water1000-neutron/water.EPSR.g01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 2.0e-2));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "GR01//Bulk//OriginalGR//HW-HW//Unbound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->unboundPartials().get(DoubleKeyedMapKey("HW", "HW")), "HW-HW Unbound Partial",
         {"epsr25/water1000-neutron/water.EPSR.g01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 2.0e-2));
 
     // Partial g(r) (intramolecular terms)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "GR01//Bulk//OriginalGR//OW-HW//Bound",
-        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 0.1));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "GR01//Bulk//OriginalGR//HW-HW//Bound",
-        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 1.5e-2));
-
-    // Partial g(r) (intramolecular terms)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "GR01//Bulk//OriginalGR//OW-OW//Bound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->boundPartials().get(DoubleKeyedMapKey("OW", "OW")), "OW-OW Bound Partial",
         {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 1.0e-5,
         Error::ErrorType::RMSEError));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->boundPartials().get(DoubleKeyedMapKey("HW", "OW")), "HW-OW Bound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 0.1));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        rawGR->boundPartials().get(DoubleKeyedMapKey("HW", "HW")), "HW-HW Bound Partial",
+        {"epsr25/water1000-neutron/water.EPSR.y01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 1.5e-2));
+
+    // Get the SQ
+    auto unweightedSQ = sqNode->getOutputValue<PartialSet *>("UnweightedSQ");
+    ASSERT_TRUE(unweightedSQ);
 
     // Partial S(Q) (unbound terms)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "SQ01//UnweightedSQ//OW-OW//Unbound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        unweightedSQ->unboundPartials().get(DoubleKeyedMapKey("OW", "OW")), "OW-OW Unbound Partial",
         {"epsr25/water1000-neutron/water.EPSR.f01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 1.0e-3));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "SQ01//UnweightedSQ//OW-HW//Unbound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        unweightedSQ->unboundPartials().get(DoubleKeyedMapKey("OW", "HW")), "OW-HW Unbound Partial",
         {"epsr25/water1000-neutron/water.EPSR.f01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 3.0e-3));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "SQ01//UnweightedSQ//HW-HW//Unbound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        unweightedSQ->unboundPartials().get(DoubleKeyedMapKey("HW", "HW")), "HW-HW Unbound Partial",
         {"epsr25/water1000-neutron/water.EPSR.f01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 3.0e-3));
 
     // Partial S(Q) (intramolecular terms)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "SQ01//UnweightedSQ//OW-HW//Bound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        unweightedSQ->boundPartials().get(DoubleKeyedMapKey("OW", "HW")), "OW-HW Bound Partial",
         {"epsr25/water1000-neutron/water.EPSR.s01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 1.0e-4));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "SQ01//UnweightedSQ//HW-HW//Bound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        unweightedSQ->boundPartials().get(DoubleKeyedMapKey("HW", "HW")), "HW-HW Bound Partial",
         {"epsr25/water1000-neutron/water.EPSR.s01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 4.0e-5));
 
     // Partial S(Q) (intramolecular terms)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "SQ01//UnweightedSQ//OW-OW//Bound",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        unweightedSQ->boundPartials().get(DoubleKeyedMapKey("OW", "OW")), "OW-OW Bound Partial",
         {"epsr25/water1000-neutron/water.EPSR.s01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 1.0e-5,
         Error::ErrorType::RMSEError));
 
     // Total F(Q)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "D2O//WeightedSQ//Total",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        D2O->getOutputValue<PartialSet *>("WeightedSQ")->total(), "D2O F(Q)",
         {"epsr25/water1000-neutron/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 3.0e-4));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "H2O//WeightedSQ//Total",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        H2O->getOutputValue<PartialSet *>("WeightedSQ")->total(), "H2O F(Q)",
         {"epsr25/water1000-neutron/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 6.0e-4));
-    EXPECT_TRUE(systemTest.checkData1D(
-        "HDO//WeightedSQ//Total",
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        HDO->getOutputValue<PartialSet *>("WeightedSQ")->total(), "HDO F(Q)",
         {"epsr25/water1000-neutron/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 2.0e-5));
 
-    // Total x-ray F(Q)
-    EXPECT_TRUE(systemTest.checkData1D(
-        "H2Oxray//WeightedSQ//Total",
-        {"epsr25/water1000-neutron-xray/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 8}, 2.0e-4));
+    // Total F(Q)
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        H2Ox->getOutputValue<PartialSet *>("WeightedSQ")->total(), "H2Ox F(Q)",
+        {"epsr25/water1000-neutron-xray/water.EPSR.u01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 8}, 1.5e-4));
 }
 
 } // namespace UnitTest
