f79b13f8f571cf09e9a4214d2341b75ba7e2ffd6
RobBuchanan <robertbb269@hotmail.com>
2026-04-14 20:15:01 +0100

    intra angle node

---
+++ src/nodes/intraAngle.cpp
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#include "nodes/intraAngle.h"
+#include "analyser/dataExporter.h"
+#include "analyser/dataOperator1D.h"
+#include "analyser/dataOperator2D.h"
+#include "analyser/siteSelector.h"
+#include "math/mathFunc.h"
+
+IntraAngleNode::IntraAngleNode(Graph *parentGraph) : Node(parentGraph)
+{
+    // Inputs
+    addInput<Configuration *>("Configuration", "Set target configuration for the module", targetConfiguration_);
+
+    // Options
+    addOption("SiteA", "Specify site(s) which represent 'A' in the interaction A-B-C", a_);
+    addOption("SiteB", "Specify site(s) which represent 'B' in the interaction A-B-C", b_);
+    addOption("SiteC", "Specify site(s) which represent 'C' in the interaction A-B-C", c_);
+    addOption("RangeAB", "Range (min, max, binwidth) of A-B distance binning", rangeAB_);
+    addOption("RangeBC", "Range (min, max, binwidth) of B-C distance binning", rangeBC_);
+    addOption<Vector3>("AngleRange", "Range (min, max, binwidth) of angle binning", angleRange_);
+    addOption<bool>("Symmetric", "Whether the calculated angle should be mapped to 0 - 90 (i.e. is symmetric about 90)",
+                    symmetric_);
+
+    // Outputs
+    addOutput<Configuration *>("Configuration", "Output configuration", targetConfiguration_);
+}
+
+std::string_view IntraAngleNode::type() const { return "IntraAngle"; }
+
+std::string_view IntraAngleNode::summary() const { return "Calculate angle distributions within molecules"; }
+
+// Run main processing
+NodeConstants::ProcessResult IntraAngleNode::process()
+{
+    // Select site A
+    SiteSelector a(targetConfiguration_, a_);
+
+    // Select site B
+    SiteSelector b(targetConfiguration_, b_);
+
+    // Select site C
+    SiteSelector c(targetConfiguration_, c_);
+
+    // Intramolecular angle histogram
+    if (!intraAngleHist_)
+        intraAngleHist_.emplace(angleRange_);
+
+    intraAngleHist_->zeroBins();
+
+    for (const auto &[siteA, indexA] : a.sites())
+    {
+        for (const auto &[siteB, indexB] : b.sites())
+        {
+
+            if (siteB->molecule() != siteA->molecule())
+                continue;
+
+            if (siteB == siteA)
+                continue;
+
+            if (!rangeAB_.contains(targetConfiguration_->box()->minimumDistance(siteA->origin(), siteB->origin())))
+                continue;
+
+            for (const auto &[siteC, indexC] : c.sites())
+            {
+
+                if (siteC->molecule() != siteA->molecule())
+                    continue;
+
+                if (siteC == siteA)
+                    continue;
+
+                if (siteC == siteB)
+                    continue;
+
+                if (!rangeBC_.contains(targetConfiguration_->box()->minimumDistance(siteB->origin(), siteC->origin())))
+                    continue;
+
+                auto angle = targetConfiguration_->box()->angleInDegrees(siteA->origin(), siteB->origin(), siteC->origin());
+                if (symmetric_ && angle > 90.0)
+                    angle = 180.0 - angle;
+                intraAngleHist_->bin(angle);
+            }
+        }
+    }
+
+    // Accumulate histogram
+    intraAngleHist_->accumulate();
+
+    // Angle(ABC)
+    angleABC_ = intraAngleHist_->accumulatedData();
+
+    // Normalise
+    DataOperator1D normaliser(angleABC_);
+    // Normalise by sin(x)
+    normaliser.operate([](const auto &x, const auto &xDelta, const auto &value)
+                       { return value / sin(DissolveMath::toRadians(x)); });
+    // Normalise by value
+    normaliser.normaliseSumTo();
+
+    // Save Angle(A-B-C) data?
+    /*
+    if (!DataExporter::exportData(angleABC_, exportFileAndFormat_))
+        return NodeConstants::ProcessResult::Failed;
+    */
+    return NodeConstants::ProcessResult::Success;
+}
+
+// Temporary accessors to data for testing
+const Data1D &IntraAngleNode::intraAngleHist() const { return angleABC_; }
---
+++ src/nodes/intraAngle.h
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#pragma once
+
+#include "classes/speciesSites.h"
+#include "io/export/data1D.h"
+#include "io/export/data2D.h"
+#include "math/histogram1D.h"
+#include "math/histogram2D.h"
+#include "math/histogram3D.h"
+#include "math/rangedVector3.h"
+#include "nodes/node.h"
+
+// IntraAngle Node
+class IntraAngleNode : public Node
+{
+    public:
+    IntraAngleNode(Graph *parentGraph);
+    ~IntraAngleNode() override = default;
+
+    public:
+    std::string_view type() const override;
+    std::string_view summary() const override;
+
+    /*
+     * Definition
+     */
+    private:
+    // Target configuration
+    Configuration *targetConfiguration_{nullptr};
+    // Target SpeciesSite definitions
+    std::vector<const SpeciesSite *> a_, b_, c_;
+    // Range of A-B distance axis
+    Range rangeAB_{0.0, 10.0};
+    // Range of B-C distance axis
+    Range rangeBC_{0.0, 10.0};
+    // Range (min, max, binwidth) of angle axis
+    Vector3 angleRange_{0.0, 180.0, 1.0};
+    // Angle(ABC)
+    std::optional<Histogram1D> intraAngleHist_;
+    Data1D angleABC_;
+    // Whether the angular range should be considered symmetric about 90
+    bool symmetric_{false};
+    // Export target
+    Data1DExportFileFormat exportFileAndFormat_;
+
+    /*
+     * Processing
+     */
+    private:
+    // Run main processing
+    NodeConstants::ProcessResult process() override;
+
+    /*
+     * Getters
+     */
+    public:
+    // Intramolecular angle histogram
+    const Data1D &intraAngleHist() const;
+};
