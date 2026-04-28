e1eeb7d8fb3d4dec16adfef767d41a27624578e8
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-04-28 09:47:15 +0100

    Add Structure classes.

---
+++ src/classes/structure.cpp
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#include "classes/structure.h"
+#include "classes/species.h"
+
+Structure::Structure() { box_ = std::make_unique<SingleImageBox>(); }
+
+// Clear Data
+void Structure::clear()
+{
+    bonds_.clear();
+    atoms_.clear();
+}
+
+/*
+ * Atoms
+ */
+
+// Renumber atoms so they are sequential in the list
+void Structure::renumberAtoms()
+{
+    auto count = 0;
+    for (auto &i : atoms_)
+        i->setIndex(count++);
+}
+
+// Add a new atom to the Species, returning its index
+StructureAtom *Structure::addAtom(Elements::Element Z, Vector3 r, double q)
+{
+    auto &i = atoms_.emplace_back(std::make_unique<StructureAtom>());
+    i->set(Z, r, q);
+
+    i->setIndex(atoms_.size() - 1);
+
+    return i.get();
+}
+
+// Remove the specified atom from the species
+void Structure::removeAtom(const StructureAtom *atom)
+{
+    auto bonds = atom->bonds();
+    for (auto bond : bonds)
+        removeBond(bond);
+
+    // Now remove the atom
+    auto atomIt = std::find_if(atoms_.begin(), atoms_.end(), [&](const auto &i) { return i.get() == atom; });
+    atoms_.erase(atomIt);
+
+    renumberAtoms();
+}
+
+// Remove set of atom indices
+void Structure::removeAtoms(const std::vector<const StructureAtom *> &atomsToRemove)
+{
+    for (auto atom : atomsToRemove)
+    {
+        auto bonds = atom->bonds();
+        for (auto bond : bonds)
+            removeBond(bond);
+    }
+
+    // Now remove the atoms
+    auto atomIt = std::remove_if(
+        atoms_.begin(), atoms_.end(), [&](const auto &i)
+        { return std::ranges::find_if(atomsToRemove, [&i](const auto j) { return i.get() == j; }) != atomsToRemove.end(); });
+    atoms_.erase(atomIt, atoms_.end());
+    renumberAtoms();
+}
+
+// Return the number of atoms in the species (or only those with the specified presence)
+int Structure::nAtoms(Atom::Presence withPresence) const
+{
+    return withPresence == Atom::Presence::Any ? atoms_.size()
+                                               : std::count_if(atoms_.begin(), atoms_.end(), [withPresence](const auto &i)
+                                                               { return i->isPresence(withPresence); });
+}
+
+// Return atoms
+const std::vector<std::unique_ptr<StructureAtom>> &Structure::atoms() const { return atoms_; }
+std::vector<std::unique_ptr<StructureAtom>> &Structure::atoms() { return atoms_; }
+
+/*
+ * Connectivity
+ */
+
+// Add new bond definition
+StructureBond *Structure::addBond(StructureAtom *i, StructureAtom *j)
+{
+    // Check for existence of bond already
+    auto bond = getBond(i, j);
+    if (bond)
+    {
+        Messenger::warn("Refused to add a new bond between atoms {} and {} since it already exists.\n", i->index(), j->index());
+        return bond;
+    }
+
+    // OK to add new Bond
+    return bonds_.emplace_back(std::make_unique<StructureBond>(i, j)).get();
+}
+StructureBond *Structure::addBond(int i, int j) { return addBond(atoms_[i].get(), atoms_[j].get()); }
+
+// Remove bond
+void Structure::removeBond(StructureAtom *i, StructureAtom *j)
+{
+    // Find the bond
+    auto it = std::remove_if(bonds_.begin(), bonds_.end(), [i, j](const auto &bond) { return bond->isBetween(i, j); });
+    if (it == bonds_.end())
+        return;
+
+    auto bond = it->get();
+    bond->i()->removeBond(bond);
+    bond->j()->removeBond(bond);
+
+    // Erase the bond
+    bonds_.erase(it);
+}
+void Structure::removeBond(StructureBond *bondToRemove)
+{
+    auto it = std::ranges::find_if(bonds_, [bondToRemove](const auto &bond) { return bond.get() == bondToRemove; });
+    if (it == bonds_.end())
+        return;
+
+    auto bond = it->get();
+    bond->i()->removeBond(bond);
+    bond->j()->removeBond(bond);
+
+    bonds_.erase(it);
+}
+
+// Return vector of bonds
+std::vector<std::unique_ptr<StructureBond>> &Structure::bonds() { return bonds_; }
+const std::vector<std::unique_ptr<StructureBond>> &Structure::bonds() const { return bonds_; }
+
+// Return whether bond between specified atoms exists
+bool Structure::hasBond(const StructureAtom *i, const StructureAtom *j) const
+{
+    return std::any_of(bonds_.cbegin(), bonds_.cend(), [i, j](const auto &bond) { return bond->isBetween(i, j); });
+}
+
+// Return the bond between the specified atoms
+StructureBond *Structure::getBond(StructureAtom *i, StructureAtom *j)
+{
+    auto it = std::find_if(bonds_.begin(), bonds_.end(), [i, j](auto &bond) { return bond->isBetween(i, j); });
+    if (it == bonds_.end())
+        return {};
+
+    return it->get();
+}
+StructureBond *Structure::getBond(const StructureAtom *i, const StructureAtom *j) const
+{
+    auto it = std::find_if(bonds_.cbegin(), bonds_.cend(), [i, j](const auto &bond) { return bond->isBetween(i, j); });
+    if (it == bonds_.end())
+        return {};
+
+    return it->get();
+}
+
+/*
+ * Operations
+ */
+
+// Recursively add atoms along any path from the specified one, ignoring the bond(s) provided
+void Structure::getIndicesRecursive(std::vector<int> &indices, int index, StructureBond *exclude,
+                                    StructureBond *excludeToo) const
+{
+    // Loop over Bonds on specified Atom
+    indices.emplace_back(index);
+    const auto &i = atoms_[index];
+    for (const auto *bond : i->bonds())
+    {
+        // Is this either of the excluded bonds?
+        if (exclude == bond or excludeToo == bond)
+            continue;
+
+        // Get the partner atom in the bond and select it (if it is not selected already)
+        auto *j = bond->partner(i.get());
+        if (std::find(indices.begin(), indices.end(), j->index()) == indices.end())
+            getIndicesRecursive(indices, j->index(), exclude, excludeToo);
+    }
+}
+
+// Return the fragment containing the specified atom, optionally ignoring paths along the bond(s) provided
+std::vector<int> Structure::fragment(int startIndex, StructureBond *exclude, StructureBond *excludeToo) const
+{
+    std::vector<int> indices;
+    getIndicesRecursive(indices, startIndex, exclude, excludeToo);
+    return indices;
+}
+
+/*
+ * Serialisation
+ */
+
+// Express as a serialisable value
+void Structure::serialise(std::string tag, SerialisedValue &target) const
+{
+    auto &result = target[tag];
+    Serialisable::fromVector<>(atoms_, "atoms", result);
+    Serialisable::fromVector<>(bonds_, "bonds", result);
+}
+
+// Read values from a serialisable value
+void Structure::deserialise(const SerialisedValue &node)
+{
+    Serialisable::toVector(node, "atoms", [this](const SerialisedValue &atom) { atoms_.emplace_back()->deserialise(atom); });
+
+    Serialisable::toVector(node, "bonds",
+                           [this](const SerialisedValue &bond)
+                           {
+                               auto &i = atoms_.at(toml::find<int>(bond, "i"));
+                               auto &j = atoms_.at(toml::find<int>(bond, "j"));
+                               bonds_.emplace_back(std::make_unique<StructureBond>(i.get(), j.get()));
+                           });
+}
---
+++ src/classes/structure.h
@@ -0,0 +1,172 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#pragma once
+
+#include "base/serialiser.h"
+#include "classes/atom.h"
+#include "classes/box.h"
+#include <vector>
+
+// Forward Declarations
+class StructureBond;
+
+// StructureAtom
+class StructureAtom : public Atom
+{
+    private:
+    // Bonds to this atom
+    std::vector<StructureBond *> bonds_;
+
+    public:
+    // Return bonds to this atom
+    const std::vector<StructureBond *> &bonds() const { return bonds_; }
+    // Add a bond to this atom
+    void addBond(StructureBond *bond) { bonds_.push_back(bond); }
+    // Remove bond from atom
+    void removeBond(StructureBond *bond) { bonds_.erase(std::remove(bonds_.begin(), bonds_.end(), bond)); }
+};
+
+// StructureBond
+class StructureBond : public Serialisable<>
+{
+    public:
+    StructureBond(StructureAtom *i, StructureAtom *j) : i_(i), j_(j)
+    {
+        i_->addBond(this);
+        j_->addBond(this);
+    }
+
+    private:
+    StructureAtom *i_{nullptr}, *j_{nullptr};
+
+    public:
+    // Return the involved atoms
+    StructureAtom *i() const { return i_; }
+    StructureAtom *j() const { return j_; }
+    // Return the 'other' atom
+    StructureAtom *partner(const StructureAtom *atom) const { return (atom == i_ ? j_ : i_); }
+    // Return whether the bond's atoms match those provided
+    bool isBetween(const StructureAtom *i, const StructureAtom *j) const
+    {
+        return (i == i_ && j == j_) || (i == j_ && j == i_);
+    }
+
+    /*
+     * Serialisation
+     */
+    public:
+    // Express as a serialisable value
+    void serialise(std::string tag, SerialisedValue &target) const override
+    {
+        target[tag] = {{"i", i_->index()}, {"j", j_->index()}};
+    }
+};
+
+// Structure
+class Structure : public Serialisable<>
+{
+    public:
+    Structure();
+    ~Structure() = default;
+    // Clear Data
+    void clear();
+
+    /*
+     * Atom
+     */
+    private:
+    // Atoms in the structure
+    std::vector<std::unique_ptr<StructureAtom>> atoms_;
+
+    private:
+    // Renumber atoms so they are sequential in the list
+    void renumberAtoms();
+
+    public:
+    // Add a new atom
+    StructureAtom *addAtom(Elements::Element Z, Vector3 r, double q = 0.0);
+    // Remove the specified atom
+    void removeAtom(const StructureAtom *atom);
+    // Remove a number of atoms
+    void removeAtoms(const std::vector<const StructureAtom *> &atoms);
+    // Return the number of atoms
+    int nAtoms(Atom::Presence withPresence = Atom::Presence::Any) const;
+    // Return atoms
+    const std::vector<std::unique_ptr<StructureAtom>> &atoms() const;
+    std::vector<std::unique_ptr<StructureAtom>> &atoms();
+
+    /*
+     * Connectivity
+     */
+    private:
+    // Connectivity within the structure
+    std::vector<std::unique_ptr<StructureBond>> bonds_;
+
+    public:
+    // Add new bond between specified atoms
+    StructureBond *addBond(int i, int j);
+    StructureBond *addBond(StructureAtom *i, StructureAtom *j);
+    // Remove bond
+    void removeBond(StructureAtom *i, StructureAtom *j);
+    void removeBond(StructureBond *bond);
+    // Return vector of defined bonds
+    std::vector<std::unique_ptr<StructureBond>> &bonds();
+    const std::vector<std::unique_ptr<StructureBond>> &bonds() const;
+    // Return whether bond between atoms exists
+    bool hasBond(const StructureAtom *i, const StructureAtom *j) const;
+    // Return the bond between the specified Atoms
+    StructureBond *getBond(StructureAtom *i, StructureAtom *j);
+    StructureBond *getBond(const StructureAtom *i, const StructureAtom *j) const;
+
+    /*
+     * Operations
+     */
+    private:
+    // Recursively add atoms along any path from the specified one, ignoring the bond(s) provided
+    void getIndicesRecursive(std::vector<int> &indices, int index, StructureBond *exclude,
+                             StructureBond *excludeToo = nullptr) const;
+
+    public:
+    // Return the fragment (vector of indices) containing the specified atom, optionally ignoring paths along the bond(s)
+    // provided
+    std::vector<int> fragment(int startIndex, StructureBond *exclude = nullptr, StructureBond *excludeToo = nullptr) const;
+    // Apply random noise to atoms
+    void randomiseCoordinates(double maxDisplacement);
+
+    /*
+     * Intramolecular Data
+     */
+
+    public:
+    // Add missing bonds
+    void addMissingBonds(double tolerance = 1.1, bool preventMetallic = false);
+    // Remove bonds crossing periodic boundaries
+    void removePeriodicBonds();
+    // Clear and regenerate bonds based on tolerance
+    void recalculateIntermolecularTerms(double tolerance = 1.1);
+
+    /*
+     * Box Definition (if any)
+     */
+    private:
+    // Periodic Box
+    std::unique_ptr<Box> box_;
+
+    public:
+    // Return periodic box
+    const Box *box() const;
+    // Remove Box definition and revert to single image
+    void removeBox();
+    // Create Box definition with specified lengths and angles
+    void createBox(const Vector3 lengths, const Vector3 angles, bool nonPeriodic = false);
+
+    /*
+     * Serialisation
+     */
+    public:
+    // Express as a serialisable value
+    void serialise(std::string tag, SerialisedValue &target) const override;
+    // Read values from a serialisable value
+    void deserialise(const SerialisedValue &node);
+};
