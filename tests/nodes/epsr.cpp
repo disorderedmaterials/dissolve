09c37b83342cd2c34428a124ae4880c7a80f2b3a
RobBuchanan <robertbb269@hotmail.com>
2026-04-30 16:24:22 +0100

    start benzene

--- tests/nodes/epsr.cpp
+++ tests/nodes/epsr.cpp
@@ -258,4 +258,118 @@ TEST(EPSRNodeTest, Water3NX)
         {"epsr25/water1000-neutron-xray/water.EPSR.q01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 1.3e-2));
 }
 
+TEST(EPSRNodeTest, Benzene)
+{
+    //  Set up the test graph
+    TestGraph testGraph;
+    EXPECT_TRUE(testGraph.createConfiguration("Liquid", {{createBenzene, 200}}, 0.876,
+                                              Units::DensityUnits::GramsPerCentimetreCubedUnits));
+    EXPECT_TRUE(testGraph.appendImportCoordinates(CoordinateImportFileFormat(
+        "epsr25/benzene200-neutron/boxbenz.ato", CoordinateImportFileFormat::CoordinateImportFormat::EPSR)));
+    auto importCoords = testGraph.head<ImportConfigurationCoordinatesNode>();
+
+    // Adjust pair potential properties
+    PairPotential::setRange(12.0);
+
+    // Add correlation function nodes
+    auto &&[grNode, sqNode] = testGraph.appendGRSQ(false, true);
+    ASSERT_TRUE(grNode);
+    ASSERT_TRUE(grNode->setOption<Number>("BinWidth", 0.03));
+    ASSERT_TRUE(grNode->setOption("Averaging", std::optional<Number>(5)));
+    ASSERT_TRUE(grNode->setOption<Function1DWrapper>("IntraBroadening", {Functions1D::Form::None}));
+
+    ASSERT_TRUE(sqNode);
+    ASSERT_TRUE(sqNode->setOption("QMin", Number(0.01)));
+    ASSERT_TRUE(sqNode->setOption<Function1DWrapper>("QBroadening", {Functions1D::Form::OmegaDependentGaussian, {0.02}}));
+    ASSERT_TRUE(sqNode->setOption<WindowFunction::Form>("WindowFunction", WindowFunction::Form::Lorch0));
+
+    // Add in NeutronSQ
+    auto C6H6 = testGraph.appendNeutronSQ(
+        sqNode, "C6H6", {{"Benzene", "Natural", 1.0}}, {},
+        {"epsr25/benzene200-neutron/C6H6.mint01", Data1DImportFileFormat::Data1DImportFormat::GudrunMint});
+    ASSERT_TRUE(C6H6);
+    auto C6D6 = testGraph.appendNeutronSQ(
+        sqNode, "C6D6", {{"Benzene", "C6D6", 1.0}}, {},
+        {"epsr25/benzene200-neutron/C6D6.mint01", Data1DImportFileFormat::Data1DImportFormat::GudrunMint});
+    ASSERT_TRUE(C6D6);
+    auto FiftyFifty = testGraph.appendNeutronSQ(
+        sqNode, "5050", {{"Benzene", "Natural", 0.5}, {"Benzene", "C6D6", 0.5}}, {},
+        {"epsr25/benzene200-neutron/5050.mint01", Data1DImportFileFormat::Data1DImportFormat::GudrunMint});
+    ASSERT_TRUE(FiftyFifty);
+
+    for (const auto &neutronSQ : {C6H6, C6D6, FiftyFifty})
+        ASSERT_TRUE(neutronSQ->setOption("ReferenceFTQMin", std::optional<Number>(0.3)));
+
+    // Add EPSR
+    auto epsrNode = static_cast<EPSRNode *>(testGraph.appendNode("EPSR", "EPSR01"));
+    ASSERT_TRUE(epsrNode);
+    ASSERT_TRUE(testGraph.addEdge({std::string(importCoords->name()), "Configuration", "EPSR01", "Configuration"}));
+    ASSERT_TRUE(epsrNode->setOption("EReq", Number(3.0)));
+    ASSERT_TRUE(epsrNode->setOption("Feedback", Number(0.9)));
+    ASSERT_TRUE(epsrNode->setOption<std::string>("InpAFile", "epsr25/benzene200-neutron/benzene.EPSR.inpa"));
+    ASSERT_TRUE(epsrNode->setOption("QMin", Number(0.05)));
+    ASSERT_TRUE(epsrNode->setOption("Smoothing", std::optional<Number>(0)));
+    ASSERT_TRUE(epsrNode->setOption("NPItSs", std::optional<Number>(0)));
+
+    // Run graph
+    ASSERT_EQ(C6H6->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(C6D6->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(FiftyFifty->run(), NodeConstants::ProcessResult::Success);
+    ASSERT_EQ(epsrNode->run(), NodeConstants::ProcessResult::Success);
+
+    // Test total neutron-weighted F(r)
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->targetProcessData(C6H6).simulatedFR, "EPSR01//SimulatedFR//C6H6",
+        {"epsr25/benzene200-neutron/benzene.EPSR.x01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 1.5e-3));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->targetProcessData(C6D6).simulatedFR, "EPSR01//SimulatedFR//C6D6",
+        {"epsr25/benzene200-neutron/benzene.EPSR.x01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 1.7e-2));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->targetProcessData(FiftyFifty).simulatedFR, "EPSR01//SimulatedFR//5050",
+        {"epsr25/benzene200-neutron/benzene.EPSR.x01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 1.1e-2));
+
+    // Test partial S(Q) derived from experiment via matrix inversion
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->estimatedSQ("CA", "CA"), "EPSR01//EstimatedSQ//CA-CA",
+        {"epsr25/benzene200-neutron/benzene.EPSR.q01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 2.0e-2));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->estimatedSQ("CA", "HA"), "EPSR01//EstimatedSQ//CA-HA",
+        {"epsr25/benzene200-neutron/benzene.EPSR.q01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 2.0e-2));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->estimatedSQ("HA", "HA"), "EPSR01//EstimatedSQ//HA-HA",
+        {"epsr25/benzene200-neutron/benzene.EPSR.q01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 2.0e-2));
+
+    // DeltaFQ Fits
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->targetProcessData(C6H6).deltaFQFit, "EPSR01//DeltaFQFit//C6H6",
+        {"epsr25/benzene200-neutron/FQ.delfit", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 2.0e-4));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->targetProcessData(C6D6).deltaFQFit, "EPSR01//DeltaFQFit//C6D6",
+        {"epsr25/benzene200-neutron/FQ.delfit", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 5.0e-4));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->targetProcessData(FiftyFifty).deltaFQFit, "EPSR01//DeltaFQFit//5050",
+        {"epsr25/benzene200-neutron/FQ.delfit", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 5.0e-4));
+
+    // Generated potentials
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->additionalPotential("CA", "CA"), "Dissolve//Potential_CA-CA_Additional",
+        {"epsr25/benzene200-neutron/benzene.EPSR.p01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 2}, 8.0e-3));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->additionalPotential("CA", "HA"), "Dissolve//Potential_CA-HA_Additional",
+        {"epsr25/benzene200-neutron/benzene.EPSR.p01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 4}, 2.0e-2));
+    EXPECT_TRUE(DissolveSystemTest::checkData1D(
+        epsrNode->additionalPotential("HA", "HA"), "Dissolve//Potential_HA-HA_Additional",
+        {"epsr25/benzene200-neutron/benzene.EPSR.p01", Data1DImportFileFormat::Data1DImportFormat::XY, 1, 6}, 4.0e-3));
+
+    // Absolute magnitudes of EP
+    auto cfg = epsrNode->getInputValue<Configuration *>("Configuration");
+    auto &coefficients = epsrNode->potentialCoefficients(cfg->atomTypePopulations().size());
+    ASSERT_FALSE(coefficients.empty());
+    std::vector<std::tuple<int, int, double>> expectedEPMagnitudes = {{0, 0, 0.4023}, {0, 1, 0.1966}, {1, 1, 0.2491}};
+    for (auto &&[i, j, epMag] : expectedEPMagnitudes)
+    {
+        testAbsoluteEPEnergy(coefficients[{i, j}], epMag, 1.0e-4);
+    }
+}
+
 } // namespace UnitTest
