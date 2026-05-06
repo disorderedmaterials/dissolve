d3a05eb2e5cf2faa467b00092c5a160692c27eea
Adam Washington <adam.washington@stfc.ac.uk>
2026-05-06 14:11:46 +0100

    Remove widget producer

--- src/modules/widgetProducer.cpp
+++ src/modules/widgetProducer.cpp
@@ -2,101 +2,10 @@
 // Copyright (c) 2026 Team Dissolve and contributors
 
 #include "modules/widgetProducer.h"
-#include "modules/accumulate/accumulate.h"
-// #include "modules/accumulate/gui/accumulateWidget.h"
-#include "modules/atomShake/atomShake.h"
-// #include "modules/atomShake/gui/atomShakeWidget.h"
-#include "modules/avgMol/avgMol.h"
-// #include "modules/avgMol/gui/avgMolWidget.h"
-#include "modules/axisAngle/axisAngle.h"
-// #include "modules/axisAngle/gui/axisAngleWidget.h"
-// #include "modules/benchmark/benchmark.h"
-// #include "modules/benchmark/gui/benchmarkWidget.h"
-#include "modules/bragg/bragg.h"
-// #include "modules/bragg/gui/braggWidget.h"
-#include "modules/clustering/clustering.h"
-// #include "modules/clustering/gui/clusteringWidget.h"
-#include "modules/compare/compare.h"
-// #include "modules/compare/gui/compareWidget.h"
-#include "modules/dAngle/dAngle.h"
-// #include "modules/dAngle/gui/dAngleWidget.h"
-#include "modules/energy/energy.h"
-// #include "modules/energy/gui/energyWidget.h"
-#include "modules/epsr/epsr.h"
-// #include "modules/epsr/gui/epsrWidget.h"
-#include "modules/epsrManager/epsrManager.h"
-// #include "modules/epsrManager/gui/epsrManagerWidget.h"
-// #include "modules/geomOpt/geomOpt.h"
-// #include "modules/geomOpt/gui/geomOptWidget.h"
-#include "modules/gr/gr.h"
-// #include "modules/gr/gui/grWidget.h"
-// #include "modules/histogramCN/gui/histogramCNWidget.h"
-#include "modules/histogramCN/histogramCN.h"
-// #include "modules/intraAngle/gui/intraAngleWidget.h"
-#include "modules/intraAngle/intraAngle.h"
-// #include "modules/intraDistance/gui/intraDistanceWidget.h"
-#include "modules/intraDistance/intraDistance.h"
-// #include "modules/modifierOSites/gui/modifierOSitesWidget.h"
-#include "modules/modifierOSites/modifierOSites.h"
-// #include "modules/moleculeTorsion/gui/moleculeTorsionWidget.h"
-#include "modules/moleculeTorsion/moleculeTorsion.h"
-// #include "modules/neutronSQ/gui/neutronSQWidget.h"
-#include "modules/neutronSQ/neutronSQ.h"
-// #include "modules/orientedSDF/gui/orientedSDFWidget.h"
-#include "modules/orientedSDF/orientedSDF.h"
-// #include "modules/qSpecies/gui/qSpeciesWidget.h"
-#include "modules/qSpecies/qSpecies.h"
-// #include "modules/sdf/gui/sdfWidget.h"
-#include "modules/sdf/sdf.h"
-// #include "modules/siteRDF/gui/siteRDFWidget.h"
-#include "modules/siteRDF/siteRDF.h"
-// #include "modules/skeleton/gui/skeletonWidget.h"
-#include "modules/skeleton/skeleton.h"
-// #include "modules/sq/gui/sqWidget.h"
-#include "modules/sq/sq.h"
-// #include "modules/test/gui/testWidget.h"
-#include "modules/test/test.h"
-// #include "modules/tr/gui/trWidget.h"
-#include "modules/tr/tr.h"
-// #include "modules/voxelDensity/gui/voxelDensityWidget.h"
-#include "modules/voxelDensity/voxelDensity.h"
-// #include "modules/xRaySQ/gui/xRaySQWidget.h"
-#include "modules/xRaySQ/xRaySQ.h"
+#include "module/module.h"
 #include <ios>
 
-ModuleWidgetProducer::ModuleWidgetProducer()
-{
-    // registerProducer<AccumulateModule, AccumulateModuleWidget>();
-    // registerProducer<AtomShakeModule, AtomShakeModuleWidget>();
-    // registerProducer<AvgMolModule, AvgMolModuleWidget>();
-    // registerProducer<AxisAngleModule, AxisAngleModuleWidget>();
-    // registerProducer<BenchmarkModule, BenchmarkModuleWidget>();
-    // registerProducer<BraggModule, BraggModuleWidget>();
-    // registerProducer<CompareModule, CompareModuleWidget>();
-    // registerProducer<ClusteringModule, ClusteringModuleWidget>();
-    // registerProducer<DAngleModule, DAngleModuleWidget>();
-    // registerProducer<EnergyModule, EnergyModuleWidget>();
-    // registerProducer<EPSRModule, EPSRModuleWidget>();
-    // registerProducer<EPSRManagerModule, EPSRManagerModuleWidget>();
-    // registerProducer<GeometryOptimisationModule, GeometryOptimisationModuleWidget>();
-    // registerProducer<GRModule, GRModuleWidget>();
-    // registerProducer<HistogramCNModule, HistogramCNModuleWidget>();
-    // registerProducer<IntraAngleModule, IntraAngleModuleWidget>();
-    // registerProducer<IntraDistanceModule, IntraDistanceModuleWidget>();
-    // registerProducer<ModifierOSitesModule, ModifierOSitesModuleWidget>();
-    // registerProducer<MoleculeTorsionModule, MoleculeTorsionModuleWidget>();
-    // registerProducer<NeutronSQModule, NeutronSQModuleWidget>();
-    // registerProducer<OrientedSDFModule, OrientedSDFModuleWidget>();
-    // registerProducer<QSpeciesModule, QSpeciesModuleWidget>();
-    // registerProducer<SDFModule, SDFModuleWidget>();
-    // registerProducer<SkeletonModule, SkeletonModuleWidget>();
-    // registerProducer<SiteRDFModule, SiteRDFModuleWidget>();
-    // registerProducer<SQModule, SQModuleWidget>();
-    // registerProducer<TestModule, TestModuleWidget>();
-    // registerProducer<TRModule, TRModuleWidget>();
-    // registerProducer<VoxelDensityModule, VoxelDensityModuleWidget>();
-    // registerProducer<XRaySQModule, XRaySQModuleWidget>();
-}
+ModuleWidgetProducer::ModuleWidgetProducer() {}
 
 /*
  * Producers
