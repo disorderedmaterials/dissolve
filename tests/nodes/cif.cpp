1cef16007e3f93b55b77609e401b4dc68102585b
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-05-15 15:02:41 +0100

    Remove SpeciesImportFileFormat.

--- tests/nodes/cif.cpp
+++ tests/nodes/cif.cpp
@@ -2,7 +2,6 @@
 // Copyright (c) 2026 Team Dissolve and contributors
 
 #include "classes/empiricalFormula.h"
-#include "io/import/species.h"
 #include "nodes/cif/importCIFStructure.h"
 #include "tests/graphData.h"
 #include "tests/testData.h"
