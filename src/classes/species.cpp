a1a08426a572ea1f34731a0b9ebeae179769c129
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-05-21 11:28:26 +0100

    Must set connectivity before creating (fragment) sites.

--- src/classes/species.cpp
+++ src/classes/species.cpp
@@ -294,8 +294,9 @@ void Species::deserialise(const SerialisedValue &node)
     Serialisable::toMap(node, "isotopologues", [this](const std::string &name, const SerialisedValue &iso)
                         { isotopologues_.emplace_back(std::make_unique<Isotopologue>(this, name))->deserialise(iso); });
 
+    // We must finalise the geometry before we attempt to add sites as Fragment sites need the bond connectivity
+    finaliseGeometry();
+
     Serialisable::toMap(node, "sites", [this](const std::string &name, const SerialisedValue &site)
                         { sites_.emplace_back(std::make_unique<SpeciesSite>(this, name))->deserialise(site); });
-
-    finaliseGeometry();
 }
