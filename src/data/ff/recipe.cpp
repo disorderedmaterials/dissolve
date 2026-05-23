0246c8e5df8e666ffddb9f6a6019e3383a2cd4cf
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-05-23 13:54:33 +0100

    Update recipe.

--- src/data/ff/recipe.cpp
+++ src/data/ff/recipe.cpp
@@ -18,13 +18,108 @@ bool ForcefieldRecipe::apply(Species &species) const
         species.clearCommonTerms();
 
     // Assign atom types to the species
-    // if (!ff->assignAtomTypes(this, Forcefield::TypeAll, false).empty())
-    // return false;
+    Messenger::print("Assigning atom types to species '{}' from forcefield '{}'...\n", species.name(), ff->name());
+    const auto shortRangeForm = ff->shortRangeForm();
+    auto nAtomTypesFailed = 0;
+    std::map<SpeciesAtom *, std::reference_wrapper<const ForcefieldAtomType>> ffaMap;
+    for (auto &i : species.atoms())
+    {
+        auto optFFAtomType = ff->determineAtomType(i);
+        if (!optFFAtomType)
+        {
+            Messenger::error("No matching forcefield type for atom {} ({}).\n", i.index(), Elements::symbol(i.Z()));
+            ++nAtomTypesFailed;
+            continue;
+        }
+
+        // Check if an AtomType of the same name already exists - if it does, just use that one
+        auto &ffa = optFFAtomType->get();
+        auto at = i.parent()->findAtomType(ffa.name());
+        if (!at)
+        {
+            at = i.parent()->addAtomType(i.Z(), ffa.name());
+            Messenger::print("Adding atom type '{}' for atom {} ({}).\n", at->name(), i.index(), Elements::symbol(i.Z()));
+        }
+        else
+            Messenger::print("Re-using atom type '{}' for atom {} ({}).\n", at->name(), i.index(), Elements::symbol(i.Z()));
+
+        // Set type in the SpeciesAtom
+        i.setAtomType(at);
+        ffaMap.emplace(&i, ffa);
+
+        // Copy parameters from the assigned atom type - we take only the required number for the specified shortRangeType.
+        // This is to avoid copying e.g. generator data (stored after the short range parameters) and causing issues elsewhere
+        std::vector<double> params;
+        params.insert(params.begin(), ffa.parameters().begin(),
+                      ffa.parameters().begin() + ShortRangeFunctions::forms().minArgs(shortRangeForm).value_or(0));
+        at->interactionPotential().setFormAndParameters(shortRangeForm, params);
+    }
+
+    if (nAtomTypesFailed > 0)
+        return false;
 
     // Assign intramolecular terms
-    // FROM SPECIES
-    // if (!ff->assignIntramolecular(this))
-    // return false;
+    Messenger::print("Assigning intramolecular terms to species '{}' from forcefield '{}'...\n", species.name(), ff->name());
+
+    // Assign bond terms
+    for (auto &bond : species.bonds())
+    {
+        if (!ff->assignBondTermParameters(bond, {ffaMap.at(bond.i()), ffaMap.at(bond.j())}))
+            return false;
+    }
+
+    // Generate angle parameters
+    for (auto &angle : species.angles())
+    {
+        if (!ff->assignAngleTermParameters(angle, {ffaMap.at(angle.i()), ffaMap.at(angle.j()), ffaMap.at(angle.k())}))
+            return false;
+    }
+
+    // Generate torsion parameters
+    for (auto &torsion : species.torsions())
+    {
+        if (!ff->assignTorsionTermParameters(
+                torsion, {ffaMap.at(torsion.i()), ffaMap.at(torsion.j()), ffaMap.at(torsion.k()), ffaMap.at(torsion.l())}))
+            return false;
+    }
+
+    // Generate improper terms
+    // Loop over potential improper sites in the Species and see if any match terms in the forcefield
+    for (auto &i : species.atoms())
+    {
+        // If we don't have exactly three bonds to the central atom 'i', move on
+        if (i.bonds().size() != 3)
+            continue;
+
+        auto ffi = ffaMap.at(&i);
+
+        // Get SpeciesAtom 'j'
+        auto *j = i.bonds().front()->partner(&i);
+        auto ffj = ffaMap.at(j);
+
+        for (auto indexK = 1; indexK < 2; ++indexK)
+        {
+            // Get SpeciesAtom 'k'
+            auto *k = i.bonds()[indexK]->partner(&i);
+            auto ffk = ffaMap.at(k);
+
+            for (auto indexL = indexK + 1; indexL < 3; ++indexL)
+            {
+                // Get SpeciesAtom 'l'
+                auto *l = i.bonds()[indexL]->partner(&i);
+                auto ffl = ffaMap.at(l);
+
+                // See if this improper term exists in the forcefield
+                auto optImproperTerm = ff->getImproperTerm(ffi, ffj, ffk, ffl);
+                if (optImproperTerm)
+                {
+                    auto &improper = species.addImproper(&i, j, k, l);
+
+                    improper.setInteractionFormAndParameters(optImproperTerm->form(), optImproperTerm->parameters());
+                }
+            }
+        }
+    }
 
     // FROM AddForcefieldDialogModel
     // Detach any MasterTerm references, and delete the MasterTerms
@@ -35,16 +130,9 @@ bool ForcefieldRecipe::apply(Species &species) const
     // Assign intramolecular terms
     // if (intramolecularRadio_ != Radio::None)
     // {
-    //     auto flags = 0;
-    //     if (ignoreCurrentTypes_)
-    //         flags += Forcefield::DetermineTypesFlag;
-    //     if (!noImproperTerms_)
-    //         flags += Forcefield::GenerateImpropersFlag;
-    //     if (intramolecularRadio_ == Radio::Selected)
-    //         flags += Forcefield::SelectionOnlyFlag;
     //
     //     // Try to assign terms across the species
-    //     if (!ff->assignIntramolecular(modifiedSpecies_.get(), flags))
+    //     if (!ff->assignIntramolecular(modifiedSpecies_.get()))
     //         return;
     //
     //     // Reduce to master terms?
