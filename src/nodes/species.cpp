e62ff9c2737997fb2cd0aadc4624b83a7b24ef46
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-05-27 14:59:39 +0100

    Typo.

--- src/nodes/species.cpp
+++ src/nodes/species.cpp
@@ -9,7 +9,7 @@
 SpeciesNode::SpeciesNode(Graph *parentGraph) : Node(parentGraph)
 {
     addInput("Structure", "Structure of the species", structure_);
-    addInput("Recipe", "Forcefield recipe to apply tothe species", recipe_);
+    addInput("Recipe", "Forcefield recipe to apply to the species", recipe_);
 
     addPointerOutput<const Species>("Species", "Created species", species_);
 }
