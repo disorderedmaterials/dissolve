08d6a05ee3fe039d7e71a808ff9fd1a792d0b1e7
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-05-27 12:47:36 +0100

    Formatting.

--- src/data/ff/improperTerm.cpp
+++ src/data/ff/improperTerm.cpp
@@ -6,8 +6,8 @@
 #include "data/ff/ff.h"
 
 ForcefieldImproperTerm::ForcefieldImproperTerm(std::string_view typeI, std::string_view typeJ, std::string_view typeK,
-                                             std::string_view typeL, TorsionFunctions::Form form,
-                                             const std::vector<double> &parameters)
+                                               std::string_view typeL, TorsionFunctions::Form form,
+                                               const std::vector<double> &parameters)
     : typeI_(typeI), typeJ_(typeJ), typeK_(typeK), typeL_(typeL), form_(form), parameters_(parameters)
 {
 }
