11654de18d6842735e4b9be76e9a865c34e54fc8
Adam Washington <adam.washington@stfc.ac.uk>
2026-05-28 12:00:41 +0000

    fix: Directly return atomType index from atomTypeIndex (#2457)
    Co-authored-by: Tristan Youngs <tristan.youngs@stfc.ac.uk>

--- src/classes/species.cpp
+++ src/classes/species.cpp
@@ -4,7 +4,6 @@
 #include "classes/species.h"
 #include "classes/atomType.h"
 #include "data/ff/ff.h"
-#include "data/ff/library.h"
 #include "data/isotopes.h"
 
 Species::Species(std::string name) : name_(name), naturalIsotopologue_(this, "Natural"), attachedAtomListsGenerated_(false)
@@ -201,8 +200,13 @@ void Species::deserialise(const SerialisedValue &node)
 {
     setName(toml::find<std::string>(node, "name"));
 
-    Serialisable::toMap(node, "atomTypes", [this](const std::string &name, const auto &data)
-                        { atomTypes_.emplace_back(std::make_shared<AtomType>(name))->deserialise(data); });
+    Serialisable::toMap(node, "atomTypes",
+                        [this](const std::string &name, const auto &data)
+                        {
+                            auto &at = atomTypes_.emplace_back(std::make_shared<AtomType>(name));
+                            at->deserialise(data);
+                            at->setIndex(atomTypes_.size() - 1);
+                        });
 
     Serialisable::toVector(node, "atoms", [this](const SerialisedValue &atom) { atoms_.emplace_back(this).deserialise(atom); });
 
