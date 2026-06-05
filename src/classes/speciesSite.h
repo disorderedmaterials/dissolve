d6d3516861c27de88903218b8895c610d038f552
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-06-05 14:48:29 +0100

    Remove old and broken function from SpeciesSite.

--- src/classes/speciesSite.h
+++ src/classes/speciesSite.h
@@ -95,9 +95,6 @@ class SpeciesSite : public Serialisable<>
     std::vector<const SpeciesAtom *> staticYAxisAtoms_;
 
     public:
-    // Set static atoms
-    bool setStaticIndices(const std::vector<int> &originIndices, const std::vector<int> &xAxisIndices,
-                          const std::vector<int> &yAxisIndices);
     // Add origin atom
     bool addStaticOriginAtom(const SpeciesAtom *originAtom);
     // Add origin atom from index
