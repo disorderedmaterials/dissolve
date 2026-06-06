1106c35a7c4008595e3fed4c7326ee266e38dd21
Tristan Youngs <trisyoungs@googlemail.com>
2026-06-06 20:14:10 +0100

    Add QSpeciesNode.

--- src/nodes/registry.cpp
+++ src/nodes/registry.cpp
@@ -38,6 +38,7 @@
 #include "nodes/multiply.h"
 #include "nodes/neutronSQ/neutronSQ.h"
 #include "nodes/numberNode.h"
+#include "nodes/qSpecies.h"
 #include "nodes/setCell.h"
 #include "nodes/setCoordinates.h"
 #include "nodes/siteRDF.h"
@@ -101,6 +102,7 @@ void NodeRegistry::instantiateNodeProducers()
                   {"Multiply", makeDerivedNode<MultiplyNode>()},
                   {"NeutronSQ", makeDerivedNode<NeutronSQNode>()},
                   {"Number", makeDerivedNode<NumberNode>()},
+                  {"QSpecies", makeDerivedNode<QSpeciesNode>()},
                   {"SetCell", makeDerivedNode<SetCellNode>()},
                   {"SetCoordinates", makeDerivedNode<SetCoordinatesNode>()},
                   {"SiteRDF", makeDerivedNode<SiteRDFNode>()},
