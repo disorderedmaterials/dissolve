aaed3e3fca177aecbdd930ee45f385f5f7d68e50
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-06-08 13:19:29 +0100

    refactor: HistogramCN node (#2473)

---
+++ src/nodes/histogramCN.h
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2026 Team Dissolve and contributors
+
+#pragma once
+
+#include "classes/speciesSites.h"
+#include "math/integerHistogram1D.h"
+#include "math/range.h"
+#include "nodes/node.h"
+
+// Histogrammed Coordination Numbers
+class HistogramCNNode : public Node
+{
+    public:
+    HistogramCNNode(Graph *parentGraph);
+    ~HistogramCNNode() override = default;
+
+    public:
+    std::string_view type() const override;
+    std::string_view summary() const override;
+
+    /*
+     * Definition
+     */
+    private:
+    // Target configuration
+    Configuration *configuration_{nullptr};
+    // Target SpeciesSite definitions
+    SpeciesSites a_, b_;
+    // Distance range for calculation
+    Range distanceRange_{0.0, 5.0};
+
+    /*
+     * Data
+     */
+    private:
+    // Histogrammed coordination numbers
+    std::optional<IntegerHistogram1D> histogram_;
+    Data1D cn_;
+
+    public:
+    // Clear any local data
+    void clearData() override;
+    // Temporary accessors to data for testing
+    const Data1D &cn() const;
+
+    /*
+     * Processing
+     */
+    private:
+    // Run main processing
+    NodeConstants::ProcessResult process() override;
+};
