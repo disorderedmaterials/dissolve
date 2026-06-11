62e005cf2b84862579d174421acf97a8466b79fd
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-06-11 11:01:21 +0100

    Remove all old modules, disable the GR benchmarking for now.

--- benchmark/modules/gr.cpp
+++ benchmark/modules/gr.cpp
@@ -1,49 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2026 Team Dissolve and contributors
 
-#include "modules/gr/gr.h"
+#include "nodes/gr/gr.h"
 #include "benchmark/benchmark.h"
 #include "common/problems.h"
 
 namespace Benchmarks
 {
-template <SpeciesType speciesType, SpeciesPopulation population, GRModule::PartialsMethod method>
+template <SpeciesType speciesType, SpeciesPopulation population, GRNode::PartialsMethod method>
 static void BM_CalculateGR(benchmark::State &state)
 {
     Problem<speciesType, population> problemDef;
 
-    // Setup the GR module
-    auto grModule = std::make_unique<GRModule>();
-    grModule->keywords().set("Configurations", std::vector<Configuration *>{problemDef.configuration()});
+    // Setup the GR node
+    GRNode grNode(nullptr);
+    grNode.setOption<Configuration *>("Configurations", problemDef.configuration());
 
     double rdfRange = problemDef.configuration()->box()->inscribedSphereRadius();
     bool upToDate = false;
-    for (auto _ : state)
-    {
-        grModule->calculateGR(problemDef.dissolve().processingModuleData(), problemDef.configuration(), method, rdfRange, 0.05,
-                              upToDate);
-        problemDef.dissolve().processingModuleData().clearAll();
-    }
+    // for (auto _ : state)
+    // {
+    // grNode.calculateGR(problemDef.dissolve().processingModuleData(), problemDef.configuration(), method, rdfRange, 0.05,
+    // upToDate);
+    // problemDef.dissolve().processingModuleData().clearAll();
+    // }
 }
-
-BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Small, GRModule::PartialsMethod::SimpleMethod)
-    ->Iterations(5)
-    ->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Small, GRModule::PartialsMethod::CellsMethod)
-    ->Iterations(5)
-    ->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Medium, GRModule::PartialsMethod::SimpleMethod)
-    ->Iterations(5)
-    ->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Medium, GRModule::PartialsMethod::CellsMethod)
-    ->Iterations(5)
-    ->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Large, GRModule::PartialsMethod::SimpleMethod)
-    ->Iterations(5)
-    ->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Large, GRModule::PartialsMethod::CellsMethod)
-    ->Iterations(5)
-    ->Unit(benchmark::kMillisecond);
+//
+// BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Small, GRNode::PartialsMethod::SimpleMethod)
+//     ->Iterations(5)
+//     ->Unit(benchmark::kMillisecond);
+// BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Small, GRNode::PartialsMethod::CellsMethod)
+//     ->Iterations(5)
+//     ->Unit(benchmark::kMillisecond);
+// BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Medium, GRNode::PartialsMethod::SimpleMethod)
+//     ->Iterations(5)
+//     ->Unit(benchmark::kMillisecond);
+// BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Medium, GRNode::PartialsMethod::CellsMethod)
+//     ->Iterations(5)
+//     ->Unit(benchmark::kMillisecond);
+// BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Large, GRNode::PartialsMethod::SimpleMethod)
+//     ->Iterations(5)
+//     ->Unit(benchmark::kMillisecond);
+// BENCHMARK_TEMPLATE(BM_CalculateGR, SpeciesType::Atomic, SpeciesPopulation::Large, GRNode::PartialsMethod::CellsMethod)
+//     ->Iterations(5)
+//     ->Unit(benchmark::kMillisecond);
 } // namespace Benchmarks
 
 BENCHMARK_MAIN();
