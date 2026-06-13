3841f9260c68adab8a460a71edfd41552f321bc2
Tristan Youngs <trisyoungs@googlemail.com>
2026-06-13 10:37:51 +0100

    Add Graph-based problem class.

--- benchmark/common/problems.h
+++ benchmark/common/problems.h
@@ -8,6 +8,12 @@
 #include "classes/coreData.h"
 #include "expression/variable.h"
 #include "main/dissolve.h"
+#include "nodes/configuration.h"
+#include "nodes/dissolve.h"
+#include "nodes/importDLPOLYStructure.h"
+#include "nodes/insert.h"
+#include "nodes/setCoordinates.h"
+#include "nodes/species.h"
 #include <string>
 
 namespace Benchmarks
@@ -27,14 +33,24 @@ enum class SpeciesPopulation
     Large
 };
 
+enum class SystemType
+{
+    ArgonCubic,
+    ArgonMonoclinic,
+    ArgonTriclinic,
+    Water1000,
+    Hexane200
+};
+
 template <SpeciesType speciesType, SpeciesPopulation population> class Problem
 {
     public:
     Problem() : dissolve_(coreData_)
     {
         // Species Types
-        static std::map<SpeciesType, std::string> speciesTypes = {
-            {SpeciesType::Atomic, "argon"}, {SpeciesType::SmallMolecule, "water"}, {SpeciesType::MediumMolecule, "hexane"}};
+        static std::map<SpeciesType, std::string> speciesTypes = {{SpeciesType::Atomic, "Ar"},
+                                                                  {SpeciesType::SmallMolecule, "species/water.toml"},
+                                                                  {SpeciesType::MediumMolecule, "species/hexane.toml"}};
 
         // Population Sizes
         static std::map<SpeciesPopulation, int> populationSizes = {{SpeciesPopulation::Single, 1},
@@ -71,5 +87,70 @@ template <SpeciesType speciesType, SpeciesPopulation population> class Problem
     CoreData &coreData() { return coreData_; }
     Dissolve &dissolve() { return dissolve_; }
     Configuration *configuration() { return coreData_.configurations().front().get(); }
+};
+
+template <SystemType systemType> class GraphProblem
+{
+    public:
+    GraphProblem()
+    {
+        // System Types
+        static std::map<SystemType, std::tuple<std::string, int, std::string>> systemTypes = {
+            {SystemType::ArgonCubic, {"Ar|epsilon=0.774040 sigma=3.445996", 6755, "dlpoly/argon/cubic/big_argon.CONFIG"}},
+            {SystemType::ArgonMonoclinic,
+             {"Ar|epsilon=0.774040 sigma=3.445996", 6802, "dlpoly/argon/monoclinic/big_argon.CONFIG"}},
+            {SystemType::ArgonTriclinic,
+             {"Ar|epsilon=0.774040 sigma=3.445996", 6528, "dlpoly/argon/triclinic/big_argon.CONFIG"}},
+            {SystemType::Water1000, {"species/water.toml", 1000, "dlpoly/water1000/full.CONFIG"}},
+            {SystemType::Hexane200, {"species/hexane.toml", 200, "dlpoly/hexane200/full.REVCON"}}};
+
+        auto &[speciesString, speciePopulation, referenceCoordinates] = systemTypes[systemType];
+
+        Messenger::setQuiet(true);
+
+        // Create the species node
+        auto *speciesNode = dynamic_cast<SpeciesNode *>(graph_.createNode("Species"));
+        species_ = &speciesNode->species();
+
+        if (speciesString.ends_with(".toml"))
+            species_->load(speciesString);
+        else
+        {
+            if (speciesString.find('|') == std::string::npos)
+                species_->createAtomic(Elements::element(speciesString), {ShortRangeFunctions::Form::Undefined});
+            else
+                species_->createAtomic(Elements::element(DissolveSys::beforeChar(speciesString, '|')),
+                                       {ShortRangeFunctions::Form::LennardJones, DissolveSys::afterChar(speciesString, '|')});
+        }
+
+        // Add an Insert node for the Species
+        auto *insertNode = dynamic_cast<InsertNode *>(graph_.createNode("Insert"));
+        insertNode->setInput<Number>("Population", speciePopulation);
+        insertNode->setInput<Number>("Density", 0.1);
+        graph_.addEdge({"Species", "Species", "Insert", "Species"});
+
+        // Create a configuration node
+        auto *configurationNode = dynamic_cast<ConfigurationNode *>(graph_.createNode("Configuration"));
+        configuration_ = &configurationNode->configuration();
+        graph_.addEdge({"Configuration", "Configuration", "Insert", "Configuration"});
+
+        // Add SetCoordinatesand ImportDLPOLYStructure
+        auto *setCoordinatesnNode = dynamic_cast<SetCoordinatesNode *>(graph_.createNode("SetCoordinates"));
+        auto *importStructureNode = dynamic_cast<ImportDLPOLYStructureNode *>(graph_.createNode("ImportDLPOLYStructure"));
+        importStructureNode->setOption<std::string>("FilePath", referenceCoordinates);
+        graph_.addEdge({"ImportDLPOLYStructure", "Structure", "SetCoordinates", "Structure"});
+        graph_.addEdge({"Configuration", "Configuration", "SetCoordinates", "Configuration"});
+
+        // Adjust pair potential properties
+        PairPotential::setShortRangeTruncationScheme(PairPotential::ShortRangeTruncationScheme::NoShortRangeTruncation);
+        PairPotential::setRange(std::min(configuration_->box()->inscribedSphereRadius(), 15.0));
+    }
+
+    private:
+    DissolveGraph graph_;
+    Configuration *configuration_{nullptr};
+    Species *species_{nullptr};
+
+    public:
 };
 } // namespace Benchmarks
