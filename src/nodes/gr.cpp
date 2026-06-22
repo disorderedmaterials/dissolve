f03a200465a8c70c08ca573ca70255754cab9fde
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-06-22 16:07:44 +0100

    Appease the Windows people.

--- src/nodes/gr.cpp
+++ src/nodes/gr.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2026 Team Dissolve and contributors
 
+#define _USE_MATH_DEFINES
 #include "nodes/gr.h"
 #include "classes/atomType.h"
 #include "classes/box.h"
