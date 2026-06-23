098ff87369359541ae3fc904adf250c2505f3122
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-06-23 08:51:38 +0100

    Another one.

--- src/nodes/neutronSQ.cpp
+++ src/nodes/neutronSQ.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2026 Team Dissolve and contributors
 
+#define _USE_MATH_DEFINES
 #include "nodes/neutronSQ.h"
 #include "classes/configuration.h"
 #include "classes/neutronWeights.h"
