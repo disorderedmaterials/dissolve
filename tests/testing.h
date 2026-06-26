e0033dc50c339300bcde1e6d16f61a2394d27861
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-06-26 10:56:55 +0100

    Bring checks in from testData.h.

--- tests/testing.h
+++ tests/testing.h
@@ -4,6 +4,12 @@
 #pragma once
 
 #include "base/units.h"
+#include "classes/partialSet.h"
+#include "kernels/common.h"
+#include "math/data1D.h"
+#include "math/data2D.h"
+#include "math/data3D.h"
+#include "math/error.h"
 #include "nodes/dissolve.h"
 #include "nodes/insert.h"
 #include <gtest/gtest.h>
@@ -99,6 +105,55 @@ class TestGraph : public DissolveGraph
      * Data Test Functions
      */
     public:
+    // Test simple double
+    [[nodiscard]] bool checkDouble(std::string_view quantity, double A, double B, double threshold);
+    // Test sampled double
+    [[nodiscard]] bool checkSampledDouble(std::string_view quantity, SampledDouble A, double B, double threshold);
+    // Test Data1D
+    [[nodiscard]] bool checkData1D(const Data1D &dataA, std::string_view nameA, const Data1D &dataB, std::string_view nameB,
+                                   double tolerance = 5.0e-3, Error::ErrorType errorType = Error::ErrorType::EuclideanError);
+    [[nodiscard]] bool checkData1D(const Data1D &dataA, std::string_view nameA, std::string filePath, int xColumn, int yColumn,
+                                   double tolerance = 5.0e-3, Error::ErrorType errorType = Error::ErrorType::EuclideanError);
+    // Test Data2D
+    [[nodiscard]] bool checkData2D(const Data2D &dataA, std::string_view nameA, const Data2D &dataB, std::string_view nameB,
+                                   double tolerance = 5.0e-3, Error::ErrorType errorType = Error::ErrorType::EuclideanError);
+    // Test Data3D
+    [[nodiscard]] bool checkData3D(const Data3D &dataA, std::string_view nameA, const Data3D &dataB, std::string_view nameB,
+                                   double tolerance = 5.0e-3, Error::ErrorType errorType = Error::ErrorType::EuclideanError);
+    // Test Vec3 data
+    void checkVec3(const Vector3 &A, const Vector3 &B, double tolerance = 1.0e-6);
+    // Test Vec3 vector data
+    void checkVec3Vector(const std::vector<Vector3> &A, const std::vector<Vector3> &B, double tolerance = 1.0e-6);
+    // Test species atom type
+    void checkSpeciesAtomType(Species *sp, const std::map<int, std::string> &namesById);
+    // Test interaction parameters
+    template <class Intra>
+    void checkIntramolecularTerms(const std::string &termInfo, const InteractionPotential<Intra> &expectedParams,
+                                  const InteractionPotential<Intra> &actualParams, double tolerance = 1.0e-6);
+    // Test species bond term
+    void checkSpeciesIntramolecular(Species *sp, std::vector<int> atoms,
+                                    const InteractionPotential<BondFunctions> &expectedParams, double tolerance = 1.0e-6);
+    // Test species angle term
+    void checkSpeciesIntramolecular(Species *sp, std::vector<int> atoms,
+                                    const InteractionPotential<AngleFunctions> &expectedParams, double tolerance = 1.0e-6);
+    // Test species torsion / improper term
+    void checkSpeciesIntramolecular(Species *sp, std::vector<int> atoms,
+                                    const InteractionPotential<TorsionFunctions> &expectedParams, double tolerance = 1.0e-6);
+    // Test consistency between the two supplied double-keyed Data1D maps
+    static bool checkDoubleKeyedMap(std::string_view mapContents, const DoubleKeyedMap<Data1D> &mapA,
+                                    const DoubleKeyedMap<Data1D> &mapB, double testThreshold);
+    // Test consistency, and error, between supplied partial sets
+    bool checkPartialSet(const PartialSet &setA, const PartialSet &setB, double testThreshold);
+    // Check consistency between production, molecular, and test energies, returning production values
+    Kernel::EnergyResult checkEnergyConsistency(const std::unique_ptr<EnergyKernel> &kernel, double testThreshold = 1.0e-6);
+    // Check consistency between production and test forces
+    void checkForceConsistency(const std::unique_ptr<ForceKernel> &kernel, std::vector<Vector3> &ppForces,
+                               std::vector<Vector3> &geomForces, Flags<Kernel::CalculationFlags> flags = {},
+                               double ppMaxDeviation = 1.0e-2, double geomMaxDeviation = 1.0e-6);
+    // Check consistency of supplied forces
+    void checkReferenceForceConsistency(const std::vector<Vector3> &ppForces, const std::vector<Vector3> &geomForces,
+                                        const std::vector<Vector3> &referenceForces, double maxDeviation = 1.0e-3);
+
     /*
      * Utility Functions
      */
