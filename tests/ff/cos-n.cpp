33e450b46d41bf200999d1c5bf03ba4efd4b85d9
Adam Washington <adam.washington@stfc.ac.uk>
2026-06-29 20:35:53 +0100

    Remove extra includes

--- tests/ff/cos-n.cpp
+++ tests/ff/cos-n.cpp
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2026 Team Dissolve and contributors
 
-#include "data/ff/library.h"
 #include "tests/graphData.h"
-#include "tests/tempFile.h"
 #include "tests/testData.h"
 #include <gtest/gtest.h>
 
