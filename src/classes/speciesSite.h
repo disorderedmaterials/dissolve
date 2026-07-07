68a8b9adf6240307d9308e77a3a083ae8d9789b2
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-07-07 09:09:35 +0100

    Missing override.

--- src/classes/speciesSite.h
+++ src/classes/speciesSite.h
@@ -187,7 +187,7 @@ class SpeciesSite : public Serialisable
     // Express as a serialisable value
     void serialise(std::string tag, SerialisedValue &target) const override;
     // Read values from a serialisable value
-    void deserialise(const SerialisedValue &node);
+    void deserialise(const SerialisedValue &node) override;
 };
 
 template <> struct Context<const SpeciesSite *>
