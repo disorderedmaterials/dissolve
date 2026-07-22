5e663d0bdc9e2dd225befcb293474083a20329a3
RobBuchanan <robertbb269@hotmail.com>
2026-07-22 13:55:20 +0100

    refactor and add toml round-trip test case

--- tests/nodes/loop.cpp
+++ tests/nodes/loop.cpp
@@ -7,6 +7,7 @@
 #include "nodes/numberNode.h"
 #include "nodes/outputs.h"
 #include "nodes/registry.h"
+#include "tests/testGraphFixture.h"
 #include <gtest/gtest.h>
 
 namespace UnitTest
@@ -37,39 +38,52 @@ class IterableGraphTest : public ::testing::Test
 
         // Create nodes
         i_ = dynamic_cast<NumberNode *>(root_.createNode("Number", "i"));
-        loop_ = dynamic_cast<IterableGraph *>(root_.createNode("Iterator", "Iterator"));
-        x_ = dynamic_cast<AddNode *>(loop_->createNode("Add", "x"));
+        loopGraph_ = dynamic_cast<IterableGraph *>(root_.createNode("Iterator", "Iterator"));
+        x_ = dynamic_cast<AddNode *>(loopGraph_->createNode("Add", "x"));
         y_ = dynamic_cast<AddNode *>(root_.createNode("Add", "y"));
 
         ASSERT_TRUE(i_);
         ASSERT_TRUE(x_);
         ASSERT_TRUE(y_);
-        ASSERT_TRUE(loop_);
+        ASSERT_TRUE(loopGraph_);
 
         ASSERT_EQ(i_->name(), "i");
         ASSERT_EQ(x_->name(), "x");
         ASSERT_EQ(y_->name(), "y");
-        ASSERT_EQ(loop_->name(), "Iterator");
+        ASSERT_EQ(loopGraph_->name(), "Iterator");
 
         // Create edge connections
         // - Number 'i' is a dynamic input to the IterableGraph - we'll call the input "I"
         EXPECT_TRUE(root_.addEdge({"i", "X", "Iterator", "I"}));
         // - Add 'x' takes the IterableGraph input "I" as its parameter "X"
-        EXPECT_TRUE(loop_->addEdge({"Inputs", "I", "x", "X"}));
+        EXPECT_TRUE(loopGraph_->addEdge({"Inputs", "I", "x", "X"}));
         // - Result from Add 'x' goes to graph output (which we will call "C") as well as loopback to "I"
-        EXPECT_TRUE(loop_->addEdge({"x", "Result", "Outputs", "C"}));
-        EXPECT_TRUE(loop_->addEdge({"x", "Result", "LoopBacks", "I"}));
+        EXPECT_TRUE(loopGraph_->addEdge({"x", "Result", "Outputs", "C"}));
+        EXPECT_TRUE(loopGraph_->addEdge({"x", "Result", "LoopBacks", "I"}));
         // - The output "C" of the loop graph then goes to input "X" of Add 'y'
         EXPECT_TRUE(root_.addEdge({"Iterator", "C", "y", "X"}));
     }
 
     protected:
     // We need a CoreData and Dissolve definition to properly instantiate DissolveGraph at present.
-    DissolveGraph root_;
+    TestGraph root_;
     NumberNode *i_{nullptr};
     AddNode *x_{nullptr}, *y_{nullptr};
-    IterableGraph *loop_{nullptr};
+    IterableGraph *loopGraph_{nullptr};
 };
+
+TEST_F(IterableGraphTest, RoundTrip)
+{
+    createGraph();
+
+    // Serialised graph TOML
+    SerialisedValue graphTOML_;
+    ASSERT_NO_THROW(root_.serialise("graph", graphTOML_));
+
+    // Deserialise from the stored TOML
+    auto deserialisedGraph = std::make_unique<DissolveGraph>();
+    ASSERT_NO_THROW(deserialisedGraph->deserialise(graphTOML_["graph"]));
+}
 
 TEST_F(IterableGraphTest, BasicNonLoopingSeries)
 {
@@ -184,7 +198,7 @@ TEST_F(IterableGraphTest, NoRun)
     EXPECT_TRUE(iA);
     iA->set<Number>(1);
 
-    auto nLoops = loop_->findOption("N");
+    auto nLoops = loopGraph_->findOption("N");
     EXPECT_TRUE(nLoops);
 
     nLoops->set<Number>(0);
@@ -194,13 +208,13 @@ TEST_F(IterableGraphTest, NoRun)
 
     // Check node versioning
     EXPECT_EQ(i_->versionIndex(), 0);
-    EXPECT_EQ(loop_->proxyInputs().versionIndex(), NodeConstants::InvalidVersion);
+    EXPECT_EQ(loopGraph_->proxyInputs().versionIndex(), NodeConstants::InvalidVersion);
     EXPECT_EQ(x_->versionIndex(), NodeConstants::InvalidVersion);
-    EXPECT_EQ(loop_->proxyOutputs().versionIndex(), NodeConstants::InvalidVersion);
+    EXPECT_EQ(loopGraph_->proxyOutputs().versionIndex(), NodeConstants::InvalidVersion);
     EXPECT_EQ(y_->versionIndex(), 0);
 
     // Loopbacks node only runs on iteration i > 0
-    EXPECT_EQ(loop_->loopBacks()->versionIndex(), NodeConstants::InvalidVersion);
+    EXPECT_EQ(loopGraph_->loopBacks()->versionIndex(), NodeConstants::InvalidVersion);
 }
 
 TEST_F(IterableGraphTest, NoFeedback)
@@ -219,7 +233,7 @@ TEST_F(IterableGraphTest, NoFeedback)
     EXPECT_TRUE(iA);
     iA->set<Number>(1);
 
-    auto nLoops = loop_->findOption("N");
+    auto nLoops = loopGraph_->findOption("N");
     EXPECT_TRUE(nLoops);
 
     // Zero iterations: We expect 1 + (xB = 1) = 1 + 1 = 2
@@ -230,13 +244,13 @@ TEST_F(IterableGraphTest, NoFeedback)
 
     // Check node versioning
     EXPECT_EQ(i_->versionIndex(), 0);
-    EXPECT_EQ(loop_->proxyInputs().versionIndex(), 0);
+    EXPECT_EQ(loopGraph_->proxyInputs().versionIndex(), 0);
     EXPECT_EQ(x_->versionIndex(), 0);
-    EXPECT_EQ(loop_->proxyOutputs().versionIndex(), 0);
+    EXPECT_EQ(loopGraph_->proxyOutputs().versionIndex(), 0);
     EXPECT_EQ(y_->versionIndex(), 0);
 
     // Loopbacks node only runs on iteration i > 0
-    EXPECT_EQ(loop_->loopBacks()->versionIndex(), NodeConstants::InvalidVersion);
+    EXPECT_EQ(loopGraph_->loopBacks()->versionIndex(), NodeConstants::InvalidVersion);
 }
 
 TEST_F(IterableGraphTest, SingleFeedback)
@@ -255,7 +269,7 @@ TEST_F(IterableGraphTest, SingleFeedback)
     EXPECT_TRUE(iA);
     iA->set<Number>(1);
 
-    auto nLoops = loop_->findOption("N");
+    auto nLoops = loopGraph_->findOption("N");
     EXPECT_TRUE(nLoops);
 
     // One iteration: We expect (LB = 2) + (xB = 1) = 2 + 1 = 3
@@ -266,13 +280,13 @@ TEST_F(IterableGraphTest, SingleFeedback)
 
     // Check node versioning
     EXPECT_EQ(i_->versionIndex(), 0);
-    EXPECT_EQ(loop_->proxyInputs().versionIndex(), 1);
+    EXPECT_EQ(loopGraph_->proxyInputs().versionIndex(), 1);
     EXPECT_EQ(x_->versionIndex(), 1);
-    EXPECT_EQ(loop_->proxyOutputs().versionIndex(), 1);
+    EXPECT_EQ(loopGraph_->proxyOutputs().versionIndex(), 1);
     EXPECT_EQ(y_->versionIndex(), 0);
 
     // Loopbacks node only runs on iteration i > 0
-    EXPECT_EQ(loop_->loopBacks()->versionIndex(), 0);
+    EXPECT_EQ(loopGraph_->loopBacks()->versionIndex(), 0);
 }
 
 TEST_F(IterableGraphTest, ExtendedFeedback)
@@ -291,7 +305,7 @@ TEST_F(IterableGraphTest, ExtendedFeedback)
     EXPECT_TRUE(iA);
     iA->set<Number>(1);
 
-    auto nLoops = loop_->findOption("N");
+    auto nLoops = loopGraph_->findOption("N");
     EXPECT_TRUE(nLoops);
 
     /*
@@ -318,27 +332,27 @@ TEST_F(IterableGraphTest, ExtendedFeedback)
 
     // Check node versioning
     EXPECT_EQ(i_->versionIndex(), 0);
-    EXPECT_EQ(loop_->proxyInputs().versionIndex(), 9);
+    EXPECT_EQ(loopGraph_->proxyInputs().versionIndex(), 9);
     EXPECT_EQ(x_->versionIndex(), 9);
-    EXPECT_EQ(loop_->proxyOutputs().versionIndex(), 9);
+    EXPECT_EQ(loopGraph_->proxyOutputs().versionIndex(), 9);
     EXPECT_EQ(y_->versionIndex(), 0);
 
     // Loopbacks node only runs on iteration i > 1
-    EXPECT_EQ(loop_->loopBacks()->versionIndex(), 8);
+    EXPECT_EQ(loopGraph_->loopBacks()->versionIndex(), 8);
 }
 
 TEST_F(IterableGraphTest, ReleaseLoopBack)
 {
     createGraph();
 
-    const auto nEdges = loop_->edges().size();
+    const auto nEdges = loopGraph_->edges().size();
 
-    auto flagged = loop_->proxyInputs().findOutput("I");
+    auto flagged = loopGraph_->proxyInputs().findOutput("I");
 
-    loop_->removeEdge({"x", "Result", "LoopBacks", "I"});
+    loopGraph_->removeEdge({"x", "Result", "LoopBacks", "I"});
 
-    ASSERT_EQ(loop_->loopEdges().size(), 0);
-    ASSERT_EQ(loop_->edges().size(), nEdges);
+    ASSERT_EQ(loopGraph_->loopEdges().size(), 0);
+    ASSERT_EQ(loopGraph_->edges().size(), nEdges);
 }
 
 TEST_F(IterableGraphTest, UpstreamChange)
@@ -357,7 +371,7 @@ TEST_F(IterableGraphTest, UpstreamChange)
     EXPECT_TRUE(iA);
     iA->set<Number>(1);
 
-    auto nLoops = loop_->findOption("N");
+    auto nLoops = loopGraph_->findOption("N");
     EXPECT_TRUE(nLoops);
 
     /*
@@ -373,14 +387,14 @@ TEST_F(IterableGraphTest, UpstreamChange)
 
     // Check node versioning
     EXPECT_EQ(i_->versionIndex(), 0);
-    EXPECT_EQ(loop_->proxyInputs().versionIndex(), 99);
+    EXPECT_EQ(loopGraph_->proxyInputs().versionIndex(), 99);
     EXPECT_EQ(x_->versionIndex(), 99);
-    EXPECT_EQ(loop_->proxyOutputs().versionIndex(), 99);
+    EXPECT_EQ(loopGraph_->proxyOutputs().versionIndex(), 99);
     EXPECT_EQ(y_->versionIndex(), 0);
 
     // Loopbacks node only runs on iteration 0 < i <= nLoops
     // (in 100 runs, loop backs up version 99 times, starting from -1)
-    EXPECT_EQ(loop_->loopBacks()->versionIndex(), 98);
+    EXPECT_EQ(loopGraph_->loopBacks()->versionIndex(), 98);
 
     /*
      * Alter upstream number node and run for another 100 iterations
@@ -395,14 +409,14 @@ TEST_F(IterableGraphTest, UpstreamChange)
 
     // Check node versioning
     EXPECT_EQ(i_->versionIndex(), 1);
-    EXPECT_EQ(loop_->proxyInputs().versionIndex(), 199);
+    EXPECT_EQ(loopGraph_->proxyInputs().versionIndex(), 199);
     EXPECT_EQ(x_->versionIndex(), 199);
-    EXPECT_EQ(loop_->proxyOutputs().versionIndex(), 199);
+    EXPECT_EQ(loopGraph_->proxyOutputs().versionIndex(), 199);
     EXPECT_EQ(y_->versionIndex(), 1);
 
     // Loopbacks node only runs on iteration 0 < i <= nLoops
     // (after another 100 runs, loop backs up version a further 99 times, starting from 98)
-    EXPECT_EQ(loop_->loopBacks()->versionIndex(), 197);
+    EXPECT_EQ(loopGraph_->loopBacks()->versionIndex(), 197);
 }
 
 } // namespace UnitTest
