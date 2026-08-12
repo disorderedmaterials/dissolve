0a6ab10fb858b6feca6bc1f2bd6457ac05cf271d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2026-08-12 13:15:44 +0100

    Copy Box as part of creation.

--- src/classes/species_transform.cpp
+++ src/classes/species_transform.cpp
@@ -314,6 +314,9 @@ void Species::create(const Structure &structure)
     for (auto &bond : structure.bonds())
         bonds_.emplace_back(this, &atoms_[bond->i()->index()], &atoms_[bond->j()->index()]);
 
+    // Copy box
+    box_ = structure.box();
+
     // Copy instances
     instances_ = structure.instances();
 
