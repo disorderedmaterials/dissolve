0a7bbf975eedc4457d2d2587696890386abc296c
Tristan Youngs <trisyoungs@googlemail.com>
2026-08-26 10:43:11 +0100

    No overrides.

--- src/nodes/edge.h
+++ src/nodes/edge.h
@@ -121,7 +121,7 @@ class LoopEdge : public Edge
      */
     public:
     // Express as a serialisable value
-    void serialise(std::string tag, SerialisedValue &target) const override;
+    void serialise(std::string tag, SerialisedValue &target) const;
     // Read values from a serialisable value
-    void deserialise(const SerialisedValue &node) override;
+    void deserialise(const SerialisedValue &node);
 };
--- src/nodes/iterableGraph.h
+++ src/nodes/iterableGraph.h
@@ -88,7 +88,7 @@ class IterableGraph : public Graph
      */
     public:
     // Express as a serialisable value
-    void serialise(std::string tag, SerialisedValue &target) const override;
+    void serialise(std::string tag, SerialisedValue &target) const;
     // Read values from a serialisable value
-    void deserialise(const SerialisedValue &node) override;
+    void deserialise(const SerialisedValue &node);
 };
